e2255976a3e7ef377b2bb4a5ba91ff16acec37f1
Alex Zamorzaev <zamorzaev@chromium.org>
2019-10-09 18:18:16 +0000

    skylab_local_state: Seal results dir
    This is based on
    https://cs.chromium.org/chromium/infra/go/src/infra/cmd/skylab_swarming_worker/internal/swmbot/harness/resultsdir/resultsdir.go?l=55&rcl=167c14750935921832fb5a3897363494f32808c5
    
    Bug: 1012553
    Change-Id: I39147ec0df5b578ab7b97dafd95692f92b8f0347
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/1848596
    Commit-Queue: Alex Zamorzaev <zamorzaev@chromium.org>
    Reviewed-by: Prathmesh Prabhu <pprabhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#26370}

--- go/src/infra/cros/cmd/skylab_local_state/internal/cmd/save.go
+++ go/src/infra/cros/cmd/skylab_local_state/internal/cmd/save.go
@@ -6,7 +6,11 @@ package cmd
 
 import (
 	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 	"strings"
+	"time"
 
 	"github.com/maruel/subcommands"
 	"go.chromium.org/luci/common/errors"
@@ -50,7 +54,7 @@ func (c *saveRun) Run(a subcommands.Application, args []string, env subcommands.
 
 	err := c.innerRun(a, args, env)
 	if err != nil {
-		fmt.Fprintf(a.GetErr(), err.Error())
+		fmt.Fprintln(a.GetErr(), err.Error())
 		return 1
 	}
 	return 0
@@ -87,6 +91,12 @@ func (c *saveRun) innerRun(a subcommands.Application, args []string, env subcomm
 	updateDutStateFromHostInfo(s, i)
 
 	writeDutState(request.Config.AutotestDir, request.DutId, s)
+
+	if request.GetSealResultsDir() {
+		if err := sealResultsDir(request.ResultsDir); err != nil {
+			return err
+		}
+	}
 
 	return nil
 }
@@ -178,5 +188,27 @@ func writeDutState(autotestDir string, dutID string, s *lab_platform.DutState) e
 		return errors.Annotate(err, "write DUT state").Err()
 	}
 
+	return nil
+}
+
+const gsOffloaderMarker = ".ready_for_offload"
+
+// sealResultsDir drops a special timestamp file in the results directory
+// notifying gs_offloader to offload the directory. The results directory
+// should not be touched once sealed. This should not be called on an
+// already sealed results directory.
+func sealResultsDir(dir string) error {
+	ts := []byte(fmt.Sprintf("%d", time.Now().Unix()))
+	tsFile := filepath.Join(dir, gsOffloaderMarker)
+	_, err := os.Stat(tsFile)
+	if err == nil {
+		return fmt.Errorf("seal results dir %s: already sealed", dir)
+	}
+	if !os.IsNotExist(err) {
+		return fmt.Errorf("seal results dir %s: encountered corrupted file %s", dir, tsFile)
+	}
+	if err := ioutil.WriteFile(tsFile, ts, 0666); err != nil {
+		return errors.Annotate(err, "seal results dir %s", dir).Err()
+	}
 	return nil
 }
