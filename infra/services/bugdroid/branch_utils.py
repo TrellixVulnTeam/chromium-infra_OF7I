8082c1ff7d98e86f7f30c4fd38bc2387af27e005
Ben Mason <benmason@chromium.org>
2019-10-22 19:09:01 +0000

    Retrieve branch info from Chromium Dash.
    TBR=govind
    
    Bug: 1016660
    Change-Id: I81631310141e86425972efb80d656322d705de6a
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/1874150
    Commit-Queue: Ben Mason <benmason@chromium.org>
    Reviewed-by: Ben Mason <benmason@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#26655}

--- infra/services/bugdroid/branch_utils.py
+++ infra/services/bugdroid/branch_utils.py
@@ -7,26 +7,10 @@ import urllib2
 
 
 def get_mstone(branch, raise_exception=True):
-  # TODO(crbug.com/1002078): Remove this hard-coded hack once chromiumdash adds
-  # an endpoint to provide the same branch/milestone information. That interface
-  # is TBD, but should be something like:
-  #
-  #   milestones_json = urllib2.urlopen(
-  #     'https://chromiumdash.appspot.com/branches/mstone?limit=5')
-  #   recent_milestones = json.load(milestones_json)
-  #
-  # Note, the result should probably also be memoized, since it should almost
-  # always be constant throughout a given run of bugdroid (and in the rare case
-  # where bugdroid runs at exactly the time when a new milestone branch is being
-  # created, there's not likely to be any legitimate bugs for that brand new
-  # branch), so no need to repeatedly fetch the data.
-  recent_milestones = {
-      '3729': 74,
-      '3770': 75,
-      '3809': 76,
-      '3865': 77,
-      '3904': 78,
-  }
+  milestones_json = urllib2.urlopen(
+      'https://chromiumdash.appspot.com/fetch_milestones?num=5')
+  recent_milestones = {m['chromium_branch']: m['milestone']
+                       for m in json.load(milestones_json)}
   if not recent_milestones and raise_exception:
     raise Exception('Failed to fetch milestone data')
   return recent_milestones.get(str(branch), None)
