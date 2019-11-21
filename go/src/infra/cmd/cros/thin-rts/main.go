e307cef1eba54ff39a3737f38170be239e5a9f8f
Allen Li <ayatane@chromium.org>
2019-11-21 00:48:02 +0000

    thin-rts: Create package skeleton
    Bug: 1026860
    Change-Id: I930ae4f4c855917c99507197e19cc81d51c009a7
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/1926894
    Reviewed-by: Prathmesh Prabhu <pprabhu@chromium.org>
    Commit-Queue: Allen Li <ayatane@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#27205}

---
+++ go/src/infra/cmd/cros/thin-rts/main.go
@@ -0,0 +1,10 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Command thin-rts is a thin/fake implementation of the RTS API.
+// This is not finalized yet (don't depend on backward compatibility).
+package main
+
+func main() {
+}
