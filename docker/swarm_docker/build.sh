cf32a592dabaf93a2110c8745e1808b8d73a662c
Stephen Roe <steveroe@google.com>
2019-11-26 19:23:29 +0000

    Check in Dockerfile packages for linux-arm64 swarming bots.
    The build.sh file checks its host operating system. If it is an arm64 system, arm64-specific packages are added to the docker image.  The arm64 packages are required to run Cr-Fuchsia qemu-kvm tests in arm64 swarming bots.  If if the host is not an arm64 system, no packages are added to the docker image.
    
    Bug: 1028320
    Change-Id: Ibef6253741a2bf02d6c32e12a6fcb4c20563127a
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/1933034
    Commit-Queue: Stephen Roe <steveroe@google.com>
    Reviewed-by: Elliott Friedman <friedman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#27354}

--- docker/swarm_docker/build.sh
+++ docker/swarm_docker/build.sh
@@ -3,6 +3,16 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+arch=$(uname -m)
+if [[ $arch == *"aarch64"* ]]; then
+  # Add packages required to run qemu-kvm in linux-arm64 docker image.
+  arch_packages="libatomic1 libvirt-bin llvm virtinst"
+  arch_suffix="_arm64"
+else
+  arch_packages=""
+  arch_suffix=""
+fi
+
 date=$(/bin/date +"%Y-%m-%d_%H-%M")
 par_dir="$(dirname "${0}")"
 
@@ -10,8 +20,9 @@ echo "Building for swarm_docker..."
 /usr/bin/docker build \
   --no-cache=true \
   --pull \
-  -t "swarm_docker:${date}" \
-  -t swarm_docker:latest \
+  --build-arg "ARCH_PACKAGES=${arch_packages}" \
+  -t "swarm_docker${arch_suffix}:${date}" \
+  -t "swarm_docker${arch_suffix}:latest" \
   "${par_dir}"
 
 exit 0
