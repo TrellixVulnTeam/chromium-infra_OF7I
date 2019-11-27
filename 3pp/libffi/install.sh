7d3494f1ccacd62f2f5a606505a7bce6ad7fc3a1
Robert Iannucci <iannucci@chromium.org>
2019-11-27 10:09:35 +0000

    [3pp/libffi] Update build process.
    TBR=tandrii@chromium.org
    
    Bug: 1026340
    Change-Id: I718f4643ef44dfafe478413aea7b97888fa009ed
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/1939552
    Reviewed-by: Robbie Iannucci <iannucci@chromium.org>
    Commit-Queue: Robbie Iannucci <iannucci@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#27411}

--- 3pp/libffi/install.sh
+++ 3pp/libffi/install.sh
@@ -8,10 +8,6 @@ set -x
 set -o pipefail
 
 PREFIX=$1
-DEPS_PREFIX=$2
-
-# generate configure, using libtool from DEPS_PREFIX
-PATH=$DEPS_PREFIX/bin:$PATH ./autogen.sh
 
 if [[ $_3PP_PLATFORM == mac* ]]; then
   python generate-darwin-source-and-headers.py --only-osx
@@ -21,10 +17,10 @@ fi
   --disable-docs \
   --host "$CROSS_TRIPLE" \
   --prefix "$PREFIX"
-make install -j $(nproc)
+make install -j "$(nproc)"
 
 # Some programs (like python) expect to be able to `#include <ffi.h>`, so
 # create those symlinks.
-mkdir $PREFIX/include
-(cd $PREFIX/include && ln -s ../lib/libffi*/include/*.h ./)
-(cd $PREFIX/lib && ln -s ../lib64/* ./)
+mkdir "$PREFIX"/include
+(cd "$PREFIX"/include && ln -s ../lib/libffi*/include/*.h ./)
+(cd "$PREFIX"/lib && ln -s ../lib64/* ./)
