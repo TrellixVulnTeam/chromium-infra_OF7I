877744a4d4863e9fe65ba92eb65360ae4f3eff3b
Junji Watanabe <jwata@google.com>
2019-12-02 14:43:54 +0000

    [luci-py] refactor luci-py recipe
    Needs to merge crrev.com/c/1943728 before this CL.
    
    - add python3 bot tests to Windows and Mac with ok_ret = 'any'
    - specify different ok_ret to client steps
      - Mac python2 client tests pass currently
    - simplified test.py args
    - removed test_seq.py
    
    Linux: https://ci.chromium.org/swarming/task/48dfc1a7ae2d9410?server=chromium-swarm.appspot.com
    Mac: https://ci.chromium.org/swarming/task/48dfc296bab27610?server=chromium-swarm.appspot.com
    Windows: https://ci.chromium.org/swarming/task/48dfc1d6c3302f10?server=chromium-swarm.appspot.com
    
    Bug: 1017545
    Change-Id: Ib8a8ec4c994a44e71051fc933ff1a315dbdfb24e
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/1944106
    Commit-Queue: Junji Watanabe <jwata@google.com>
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Auto-Submit: Junji Watanabe <jwata@google.com>
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#27495}

--- recipes/recipes/luci_py.py
+++ recipes/recipes/luci_py.py
@@ -27,87 +27,73 @@ def RunSteps(api):
 
   luci_dir = api.path['checkout'].join('luci')
   with api.context(cwd=luci_dir):
+    appeng_dir = luci_dir.join('appengine')
 
     if api.platform.is_linux:
-      _step_run_tests(api, 'auth_service',
-                      luci_dir.join('appengine', 'auth_service'))
+      with api.step.nest('auth_service'):
+        _step_run_py_tests(api, appeng_dir.join('auth_service'))
 
-      _step_run_tests(api, 'config_service',
-                      luci_dir.join('appengine', 'config_service'))
+      with api.step.nest('config_service'):
+        _step_run_py_tests(api, appeng_dir.join('config_service'))
 
-      _step_run_tests(api, 'components',
-                      luci_dir.join('appengine', 'components'),
-                      run_test_seq=True,
-                      run_python3=True)
+      with api.step.nest('components'):
+        components_dir = appeng_dir.join('components')
+        _step_run_py_tests(api, components_dir)
+        _step_run_py_tests(api, components_dir, python3=True)
 
-      _step_run_tests(api, 'isolate',
-                      luci_dir.join('appengine', 'isolate'))
+      with api.step.nest('isolate'):
+        _step_run_py_tests(api, appeng_dir.join('isolate'))
 
-    # TODO(crbug.com/1017545): enable python3 on windows and mac
-    # client
-    ok_ret = (0,)
-    if not api.platform.is_linux:
-      ok_ret = 'any'
-    _step_run_tests(api, 'client',
-                    luci_dir.join('client'),
-                    run_test_seq=True,
-                    run_python3=True,
-                    ok_ret=ok_ret)
+    # TODO(crbug.com/1017545): enable on Windows and Mac.
+    # clients tests run in python2/3, but it ignores failures on Windows
+    # in python2, and ignores failures on Windows and Mac in python3.
+    with api.step.nest('client'):
+      ok_ret = 'any' if api.platform.is_win else (0,)
+      _step_run_py_tests(api, luci_dir.join('client'), ok_ret=ok_ret)
+      ok_ret = (0,) if api.platform.is_linux else 'any'
+      _step_run_py_tests(api, luci_dir.join('client'), ok_ret=ok_ret,
+                         python3=True)
 
-    # TODO(crbug.com/1019105): remove this timeout.
-    if api.platform.is_mac:
-      timeout = 120
-    else:
-      timeout = None
-
-    # TODO(crbug.com/1017545): enable python3 on windows and mac
-    # swarming bot
-    _step_run_tests(api, 'swarming bot',
-                    luci_dir.join('appengine', 'swarming', 'swarming_bot'),
-                    run_test_seq=True,
-                    run_python3=api.platform.is_linux,
-                    timeout=timeout)
+    with api.step.nest('swarming bot'):
+      bot_dir = appeng_dir.join('swarming', 'swarming_bot')
+      # TODO(crbug.com/1019105): remove this timeout.
+      if api.platform.is_mac:
+        timeout = 120
+      else:
+        timeout = None
+      _step_run_py_tests(api, bot_dir, timeout=timeout)
+      # TODO(crbug.com/1017545): enable python3 on Windows and Mac.
+      # swarming bot tests run in python3, but it ignores failures on Windows
+      # and Mac.
+      ok_ret = (0,) if api.platform.is_linux else 'any'
+      _step_run_py_tests(api, bot_dir, timeout=timeout, ok_ret=ok_ret,
+                         python3=True)
 
     # swarming server
     if api.platform.is_linux:
-      _step_run_tests(api, 'swarming',
-                      luci_dir.join('appengine', 'swarming'),
-                      run_test_seq=True)
+      with api.step.nest('swarming'):
+        _step_run_py_tests(api, appeng_dir.join('swarming'))
 
     # swarming ui
     if api.platform.is_linux:
       _step_swarming_ui_tests(api)
 
 
-def _step_run_tests(
-    api, name, cwd, run_test_seq=False, run_python3=False, timeout=None,
-    ok_ret=(0,)):
+def _step_run_py_tests(api, cwd, python3=False, timeout=None, ok_ret=(0,)):
   luci_dir = api.context.cwd
-  with api.step.nest(name):
-    with api.context(cwd=cwd):
-      cfg = api.context.cwd.join('unittest.cfg')
-      testpy_args = ['-v', '--conf', cfg, '-A', '!no_run']
+  with api.context(cwd=cwd):
+    cfg = api.context.cwd.join('unittest.cfg')
+    testpy_args = ['-v', '--conf', cfg]
 
-      if run_python3:
-        # python3
-        venv3 = luci_dir.join('.vpython3')
-        api.python('run tests python3',
-                   'test.py', args=testpy_args, venv=venv3,
-                   timeout=timeout, ok_ret=ok_ret)
-        if run_test_seq:
-          api.python('run tests seq python3',
-                     'test_seq.py', args=['-v'], venv=venv3,
-                     timeout=timeout, ok_ret=ok_ret)
-
-      # python2
+    if python3:
+      venv = luci_dir.join('.vpython3')
+      py = 'python3'
+    else:
       venv = luci_dir.join('.vpython')
-      api.python('run tests python2',
-                 'test.py', args=testpy_args, venv=venv,
-                 timeout=timeout, ok_ret=ok_ret)
-      if run_test_seq:
-        api.python('run tests seq python2',
-                   'test_seq.py', args=['-v'], venv=venv,
-                   timeout=timeout, ok_ret=ok_ret)
+      py = 'python2'
+
+    api.python('run tests %s' % py, 'test.py', args=testpy_args, venv=venv,
+               timeout=timeout, ok_ret=ok_ret)
 
 
 def _step_swarming_ui_tests(api):
