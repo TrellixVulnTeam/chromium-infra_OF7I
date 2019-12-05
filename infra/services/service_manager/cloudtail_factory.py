eda66c528b4dfeb1236aad20b1467a61642d5ea5
Andrii Shyshkalov <tandrii@google.com>
2019-12-05 21:59:50 +0000

    service_manager: send cloudtail local logs to file instead of devnull.
    R=vadimsh
    
    Bug: 1031233
    Change-Id: Id2c49498b9c041f4a6d970eb216cc847c0a9719a
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/1953759
    Reviewed-by: Vadim Shtayura <vadimsh@chromium.org>
    Commit-Queue: Andrii Shyshkalov <tandrii@google.com>
    Cr-Commit-Position: refs/heads/master@{#27648}

--- infra/services/service_manager/cloudtail_factory.py
+++ infra/services/service_manager/cloudtail_factory.py
@@ -3,7 +3,12 @@
 # found in the LICENSE file.
 
 import os
+import logging
 import subprocess
+import tempfile
+import threading
+
+import infra_libs.logs.logs
 
 
 class DummyCloudtailFactory(object):
@@ -17,6 +22,9 @@ class CloudtailFactory(object):
   def __init__(self, path, ts_mon_credentials):
     self._path = path
     self._ts_mon_credentials = ts_mon_credentials
+    self._lock = threading.Lock()
+    self._counter = 0
+    self._log_dir = None
 
   def start(self, log_name, stdin_fh, **popen_kwargs):
     """Starts cloudtail in a subprocess.  Thread-safe.
@@ -25,7 +33,14 @@ class CloudtailFactory(object):
       log_name: --log-id argument passed to cloudtail.
       stdin_fh: File object or descriptor to be connected to cloudtail's stdin.
       popen_kwargs: Any additional keyword arguments to pass to Popen.
+
+    Raises:
+      OSError
     """
+    log_out, log_err = self._open_log_files()
+    logging.info(
+        'cloudtail for %s is itself logged into %s and %s',
+        log_name, log_out, log_err)
 
     args = [
         self._path, 'pipe',
@@ -36,10 +51,36 @@ class CloudtailFactory(object):
     if self._ts_mon_credentials:
       args.extend(['--ts-mon-credentials', self._ts_mon_credentials])
 
-    with open(os.devnull, 'w') as null_fh:
+    with open(log_out, 'w') as fout, open(log_err, 'w') as ferr:
       subprocess.Popen(
           args,
           stdin=stdin_fh,
-          stdout=null_fh,
-          stderr=null_fh,
+          stdout=fout,
+          stderr=ferr,
           **popen_kwargs)
+
+  def _open_log_files(self):
+    with self._lock:
+      if self._log_dir is None:
+        self._log_dir = _choose_log_dir()
+      self._counter += 1
+      counter = self._counter
+    base = 'sm.%d.cloudtail.%d.std' % (os.getpid(), counter)
+    out, err = [os.path.join(self._log_dir, base + e) for e in ['out', 'err']]
+    return out, err
+
+
+def _choose_log_dir():
+  # On Chrome puppet managed machines, one of the log directories should exist.
+  candidates = infra_libs.logs.logs.DEFAULT_LOG_DIRECTORIES
+  for d in candidates:
+    try:
+      with tempfile.TemporaryFile(dir=d):
+        pass
+      return d
+    except OSError:
+      continue
+  d = tempfile.gettempdir()
+  logging.warn(
+      'default infra log %s dirs not writable, using %s instead', candidates, d)
+  return d
--- infra/services/service_manager/test/cloudtail_factory_test.py
+++ infra/services/service_manager/test/cloudtail_factory_test.py
@@ -2,8 +2,11 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import logging
 import os
-import StringIO
+import shutil
+import sys
+import tempfile
 import unittest
 
 import mock
@@ -13,10 +16,18 @@ from infra.services.service_manager import cloudtail_factory
 
 class CloudtailFactoryTest(unittest.TestCase):
   def setUp(self):
+    self.tmpdir = tempfile.mkdtemp()
+    mock.patch(
+        'infra_libs.logs.logs.DEFAULT_LOG_DIRECTORIES', [self.tmpdir]).start()
     self.mock_popen = mock.patch('subprocess.Popen', autospec=True).start()
 
   def tearDown(self):
     mock.patch.stopall()
+    try:
+      shutil.rmtree(self.tmpdir)
+    except OSError:  # pragma: no cover
+      # Don't raise, as this may prevent from seeing actual test failure.
+      logging.exception('failed to rmtree(%r)', self.tmpdir)
 
   def test_start(self):
     fh = mock.Mock()
@@ -31,8 +42,25 @@ class CloudtailFactoryTest(unittest.TestCase):
 
     kwargs = self.mock_popen.call_args[1]
     self.assertEqual(fh, kwargs['stdin'])
-    self.assertIn('stdout', kwargs)
-    self.assertIn('stderr', kwargs)
+    self.assertTrue(kwargs['stdout'].closed)
+    self.assertTrue(kwargs['stderr'].closed)
+
+  def test_start_avoids_log_collisions(self):
+    f = cloudtail_factory.CloudtailFactory('/foo', None)
+    f.start('invoke1', mock.Mock())
+    f.start('invoke2', mock.Mock())
+    self.assertEqual(2, self.mock_popen.call_count)
+    self.assertEqual(2, f._counter)
+
+    kwargs1 = self.mock_popen.call_args_list[0][1]
+    kwargs2 = self.mock_popen.call_args_list[1][1]
+    logging.debug('%s\n\n%s\n\n%s', self.mock_popen.call_args_list,
+        self.mock_popen.call_args_list[0], kwargs1)
+    base = os.path.join(self.tmpdir, 'sm.%d.cloudtail.' % os.getpid())
+    self.assertEqual(kwargs1['stdout'].name, base + '1.stdout')
+    self.assertEqual(kwargs1['stderr'].name, base + '1.stderr')
+    self.assertEqual(kwargs2['stdout'].name, base + '2.stdout')
+    self.assertEqual(kwargs2['stderr'].name, base + '2.stderr')
 
   def test_start_with_credentials(self):
     f = cloudtail_factory.CloudtailFactory('/foo', '/bar')
@@ -51,6 +79,30 @@ class CloudtailFactoryTest(unittest.TestCase):
     self.assertEqual(1, self.mock_popen.call_count)
     kwargs = self.mock_popen.call_args[1]
     self.assertEqual('bar', kwargs['cwd'])
+
+  def test_choose_log_dir_default_works(self):
+    candidates = [os.path.join(self.tmpdir, 'not-exists')]
+
+    if not sys.platform.startswith('win'):  # pragma: no branch cover
+      # It's non-trivial to create a read-only (in Unix sense) dir on Windows.
+      # https://superuser.com/a/1247851
+      # So, just don't do this windows.
+      read_only = os.path.join(self.tmpdir, 'ro')
+      os.mkdir(read_only, 0444)
+      candidates.append(read_only)
+
+    writable = os.path.join(self.tmpdir, 'w')
+    os.mkdir(writable, 0777)
+    candidates.append(writable)
+
+    with mock.patch('infra_libs.logs.logs.DEFAULT_LOG_DIRECTORIES', candidates):
+      self.assertEqual(cloudtail_factory._choose_log_dir(), writable)
+
+  def test_choose_log_dir_failsafe_to_temp(self):
+    candidates = [os.path.join(self.tmpdir, 'not-exists')]
+    with mock.patch('infra_libs.logs.logs.DEFAULT_LOG_DIRECTORIES', candidates):
+      with mock.patch('tempfile.gettempdir', autospec=True, return_value='/t'):
+        self.assertEqual(cloudtail_factory._choose_log_dir(), '/t')
 
 
 class DummyCloudtailFactoryTest(unittest.TestCase):
