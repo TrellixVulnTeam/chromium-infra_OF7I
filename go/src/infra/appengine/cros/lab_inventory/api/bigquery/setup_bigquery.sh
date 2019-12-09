2ea8d22b03e2f1e79f15f913a56229420e92d1ef
Xixuan Wu <xixuan@google.com>
2019-12-09 06:52:49 +0000

    lab-inventory: setup hwid label bigquery schema.
    BUG=chromium:1028717
    TEST=./api/bigquery/setup_bigquery.sh cros-lab-inventory
    https://pantheon.corp.google.com/bigquery?project=cros-lab-inventory&organizationId=433637338589&p=cros-lab-inventory&d=inventory&t=lab&page=table
    
    Change-Id: I6e7bf0541006d4bdba158483b26cc90a4384f2e1
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/1955217
    Commit-Queue: Xixuan Wu <xixuan@chromium.org>
    Reviewed-by: Congbin Guo <guocb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#27699}

--- go/src/infra/appengine/cros/lab_inventory/api/bigquery/setup_bigquery.sh
+++ go/src/infra/appengine/cros/lab_inventory/api/bigquery/setup_bigquery.sh
@@ -32,7 +32,7 @@ APPID=$1
 
 echo "- Make sure the BigQuery API is enabled for the project:"
 # It is enabled by default for new projects, but it wasn't for older projects.
-gcloud services enable --project ${APPID} bigquery-json.googleapis.com
+gcloud services enable --project "${APPID}" bigquery-json.googleapis.com
 
 
 # TODO(xixuan): The stock role "roles/bigquery.dataEditor" grants too much rights.
@@ -44,8 +44,8 @@ gcloud services enable --project ${APPID} bigquery-json.googleapis.com
 # https://cloud.google.com/iam/docs/granting-roles-to-service-accounts
 # https://cloud.google.com/bigquery/docs/access-control
 echo "- Grant access to the AppEngine app to the role account:"
-gcloud projects add-iam-policy-binding ${APPID} \
-    --member serviceAccount:${APPID}@appspot.gserviceaccount.com \
+gcloud projects add-iam-policy-binding "${APPID}" \
+    --member serviceAccount:"${APPID}"@appspot.gserviceaccount.com \
     --role roles/bigquery.dataEditor
 
 
@@ -56,7 +56,7 @@ echo "    credentials and will ask to select a default app; just press enter to"
 echo "    not select a default."
 
 if ! (bq --location=US mk --dataset \
-  --description 'Lab inventory statistics' ${APPID}:inventory); then
+  --description 'Lab inventory statistics' "${APPID}":inventory); then
   echo ""
   echo "Dataset creation failed. Assuming the dataset already exists. At worst"
   echo "the following command will fail."
@@ -68,12 +68,25 @@ echo "  Warning: On first 'bqschemaupdater' invocation, it'll request default"
 echo "    credentials which is stored independently than 'bq'."
 if ! (bqschemaupdater -force \
     -message apibq.LabInventory  \
-    -table ${APPID}.inventory.lab); then
+    -table "${APPID}".inventory.lab); then
   echo ""
   echo ""
   echo "Oh no! You may need to restart from scratch. You can do so with:"
   echo ""
   echo "  bq rm ${APPID}:inventory.lab"
+  echo ""
+  echo "and run this script again."
+  exit 1
+fi
+
+if ! (bqschemaupdater -force \
+    -message apibq.HWIDInventory  \
+    -table "${APPID}".inventory.hwid_server); then
+  echo ""
+  echo ""
+  echo "Oh no! You may need to restart from scratch. You can do so with:"
+  echo ""
+  echo "  bq rm ${APPID}:inventory.hwid_server"
   echo ""
   echo "and run this script again."
   exit 1
