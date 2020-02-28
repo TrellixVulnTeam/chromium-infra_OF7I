abb2e2ed8cd5145a522bace87c24682fedb04291
Gregory Nisbet <gregorynisbet@google.com>
2020-02-28 18:04:36 +0000

    [inventory v2] add ToLabInventory function for DeviceOpResult records
    BUG=chromium:1028717
    TEST=go test convert_test.go
    
    Change-Id: I573a15b15f2f4bbda0638aa004daee2855f9965b
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/2077585
    Reviewed-by: Congbin Guo <guocb@chromium.org>
    Commit-Queue: Gregory Nisbet <gregorynisbet@google.com>
    Auto-Submit: Gregory Nisbet <gregorynisbet@google.com>
    Cr-Commit-Position: refs/heads/master@{#29370}

---
+++ go/src/infra/appengine/cros/lab_inventory/app/converter/converter.go
@@ -0,0 +1,45 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package converter
+
+import (
+	"fmt"
+
+	"github.com/golang/protobuf/ptypes"
+	"go.chromium.org/chromiumos/infra/proto/go/lab"
+
+	apibq "infra/appengine/cros/lab_inventory/api/bigquery"
+	ds "infra/libs/cros/lab_inventory/datastore"
+)
+
+// ToBQLabInventory converts a DeviceOpResult to a LabInventory item with the same content.
+func ToBQLabInventory(d *ds.DeviceOpResult) (*apibq.LabInventory, error) {
+	dev := &lab.ChromeOSDevice{}
+	id := ""
+	hostname := ""
+	if d == nil {
+		return nil, fmt.Errorf("deviceOpResult cannot be nil")
+	}
+	if d.Err != nil {
+		return nil, fmt.Errorf("failed device response: %s", d.Err)
+	}
+	if d.Entity != nil {
+		id = string(d.Entity.ID)
+		hostname = d.Entity.Hostname
+		if d.Entity.LabConfig == nil {
+			return nil, fmt.Errorf("labConfig cannot be nil")
+		}
+		if err := d.Entity.GetCrosDeviceProto(dev); err != nil {
+			return nil, fmt.Errorf("get device proto: %s", err)
+		}
+	}
+	utime, _ := ptypes.TimestampProto(d.Timestamp)
+	return &apibq.LabInventory{
+		Id:          id,
+		Hostname:    hostname,
+		Device:      dev,
+		UpdatedTime: utime,
+	}, nil
+}
---
+++ go/src/infra/appengine/cros/lab_inventory/app/converter/converter_test.go
@@ -0,0 +1,109 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package converter
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/golang/protobuf/proto"
+	"github.com/golang/protobuf/ptypes"
+	"github.com/golang/protobuf/ptypes/timestamp"
+	"github.com/google/go-cmp/cmp"
+
+	"go.chromium.org/chromiumos/infra/proto/go/lab"
+
+	apibq "infra/appengine/cros/lab_inventory/api/bigquery"
+	"infra/libs/cros/lab_inventory/datastore"
+)
+
+var testToBQLabInventoryData = []struct {
+	name   string
+	in     *datastore.DeviceOpResult
+	out    *apibq.LabInventory
+	isGood bool
+}{
+	{
+		"empty",
+		nil,
+		nil,
+		false,
+	},
+	{
+		"just timestamp",
+		&datastore.DeviceOpResult{
+			Timestamp: time.Unix(42, 67),
+		},
+		&apibq.LabInventory{
+			UpdatedTime: timestampOrPanic(time.Unix(42, 67)),
+			Device:      &lab.ChromeOSDevice{},
+		},
+		true,
+	},
+	{
+		"full device",
+		&datastore.DeviceOpResult{
+			Timestamp: time.Unix(42, 67),
+			Data:      nil,
+			Err:       nil,
+			Entity: &datastore.DeviceEntity{
+				ID:       datastore.DeviceEntityID("476528fa-29f9-4bf8-8f39-92dc6c291ca1"),
+				Hostname: "ac60fddf-0e51-45a0-a8ad-4c291270e649",
+				LabConfig: marshalOrPanic(&lab.ChromeOSDevice{
+					Id:           &lab.ChromeOSDeviceID{Value: "476528fa-29f9-4bf8-8f39-92dc6c291ca1"},
+					SerialNumber: "",
+				}),
+				DutState: nil,
+				Updated:  time.Unix(42, 67),
+				Parent:   nil,
+			},
+		},
+		&apibq.LabInventory{
+			UpdatedTime: timestampOrPanic(time.Unix(42, 67)),
+			Device: &lab.ChromeOSDevice{
+				Id:           &lab.ChromeOSDeviceID{Value: "476528fa-29f9-4bf8-8f39-92dc6c291ca1"},
+				SerialNumber: "",
+			},
+			Id:       "476528fa-29f9-4bf8-8f39-92dc6c291ca1",
+			Hostname: "ac60fddf-0e51-45a0-a8ad-4c291270e649",
+		},
+		true,
+	},
+}
+
+func TestToBQLabInventory(t *testing.T) {
+	t.Parallel()
+	for _, tt := range testToBQLabInventoryData {
+		t.Run(tt.name, func(t *testing.T) {
+			out, err := ToBQLabInventory(tt.in)
+			isGood := err == nil
+			if isGood != tt.isGood {
+				t.Errorf("error mismatch: expected (%v) got (%v) err was (%#v)", tt.isGood, isGood, err)
+			}
+			diff := cmp.Diff(tt.out, out)
+			if diff != "" {
+				msg := fmt.Sprintf("unexpected diff (%s)", diff)
+				t.Errorf("%s", msg)
+			}
+		})
+	}
+}
+
+func timestampOrPanic(t time.Time) *timestamp.Timestamp {
+	out, err := ptypes.TimestampProto(t)
+	if err != nil {
+		panic(err)
+	}
+	return out
+}
+
+func marshalOrPanic(item proto.Message) []byte {
+	out, err := proto.Marshal(item)
+	if err == nil {
+		return out
+	}
+	panic(err)
+}
