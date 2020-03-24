25c313ba99295b750dea8ccbe5ab9d23dca9f5fa
Xixuan Wu <xixuan@google.com>
2020-03-24 21:09:57 +0000

    UFS: [Not backward compatible] involve machineID & rackID to LSE object.
    Also renamse Prototype to LSEPrototype.
    
    BUG=chromium:1060674
    TEST=generate.sh
    
    Change-Id: Iac361a558576aa6c5d3e846bdafbcb6018806a2d
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/2118616
    Commit-Queue: Xixuan Wu <xixuan@chromium.org>
    Reviewed-by: Eshwar Narayan <eshwarn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#29944}

--- go/src/infra/libs/fleet/protos/go/lse_prototype.pb.go
+++ go/src/infra/libs/fleet/protos/go/lse_prototype.pb.go
@@ -20,78 +20,78 @@ var _ = math.Inf
 // proto package needs to be updated.
 const _ = proto.ProtoPackageIsVersion3 // please upgrade the proto package
 
-type RackPrototypeID struct {
+type RackLSEPrototypeID struct {
 	Value                string   `protobuf:"bytes,1,opt,name=value,proto3" json:"value,omitempty"`
 	XXX_NoUnkeyedLiteral struct{} `json:"-"`
 	XXX_unrecognized     []byte   `json:"-"`
 	XXX_sizecache        int32    `json:"-"`
 }
 
-func (m *RackPrototypeID) Reset()         { *m = RackPrototypeID{} }
-func (m *RackPrototypeID) String() string { return proto.CompactTextString(m) }
-func (*RackPrototypeID) ProtoMessage()    {}
-func (*RackPrototypeID) Descriptor() ([]byte, []int) {
+func (m *RackLSEPrototypeID) Reset()         { *m = RackLSEPrototypeID{} }
+func (m *RackLSEPrototypeID) String() string { return proto.CompactTextString(m) }
+func (*RackLSEPrototypeID) ProtoMessage()    {}
+func (*RackLSEPrototypeID) Descriptor() ([]byte, []int) {
 	return fileDescriptor_4844cac9ae81b3e2, []int{0}
 }
 
-func (m *RackPrototypeID) XXX_Unmarshal(b []byte) error {
-	return xxx_messageInfo_RackPrototypeID.Unmarshal(m, b)
+func (m *RackLSEPrototypeID) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_RackLSEPrototypeID.Unmarshal(m, b)
 }
-func (m *RackPrototypeID) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
-	return xxx_messageInfo_RackPrototypeID.Marshal(b, m, deterministic)
+func (m *RackLSEPrototypeID) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_RackLSEPrototypeID.Marshal(b, m, deterministic)
 }
-func (m *RackPrototypeID) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_RackPrototypeID.Merge(m, src)
+func (m *RackLSEPrototypeID) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_RackLSEPrototypeID.Merge(m, src)
 }
-func (m *RackPrototypeID) XXX_Size() int {
-	return xxx_messageInfo_RackPrototypeID.Size(m)
+func (m *RackLSEPrototypeID) XXX_Size() int {
+	return xxx_messageInfo_RackLSEPrototypeID.Size(m)
 }
-func (m *RackPrototypeID) XXX_DiscardUnknown() {
-	xxx_messageInfo_RackPrototypeID.DiscardUnknown(m)
+func (m *RackLSEPrototypeID) XXX_DiscardUnknown() {
+	xxx_messageInfo_RackLSEPrototypeID.DiscardUnknown(m)
 }
 
-var xxx_messageInfo_RackPrototypeID proto.InternalMessageInfo
+var xxx_messageInfo_RackLSEPrototypeID proto.InternalMessageInfo
 
-func (m *RackPrototypeID) GetValue() string {
+func (m *RackLSEPrototypeID) GetValue() string {
 	if m != nil {
 		return m.Value
 	}
 	return ""
 }
 
-type MachinePrototypeID struct {
+type MachineLSEPrototypeID struct {
 	Value                string   `protobuf:"bytes,1,opt,name=value,proto3" json:"value,omitempty"`
 	XXX_NoUnkeyedLiteral struct{} `json:"-"`
 	XXX_unrecognized     []byte   `json:"-"`
 	XXX_sizecache        int32    `json:"-"`
 }
 
-func (m *MachinePrototypeID) Reset()         { *m = MachinePrototypeID{} }
-func (m *MachinePrototypeID) String() string { return proto.CompactTextString(m) }
-func (*MachinePrototypeID) ProtoMessage()    {}
-func (*MachinePrototypeID) Descriptor() ([]byte, []int) {
+func (m *MachineLSEPrototypeID) Reset()         { *m = MachineLSEPrototypeID{} }
+func (m *MachineLSEPrototypeID) String() string { return proto.CompactTextString(m) }
+func (*MachineLSEPrototypeID) ProtoMessage()    {}
+func (*MachineLSEPrototypeID) Descriptor() ([]byte, []int) {
 	return fileDescriptor_4844cac9ae81b3e2, []int{1}
 }
 
-func (m *MachinePrototypeID) XXX_Unmarshal(b []byte) error {
-	return xxx_messageInfo_MachinePrototypeID.Unmarshal(m, b)
+func (m *MachineLSEPrototypeID) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_MachineLSEPrototypeID.Unmarshal(m, b)
 }
-func (m *MachinePrototypeID) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
-	return xxx_messageInfo_MachinePrototypeID.Marshal(b, m, deterministic)
+func (m *MachineLSEPrototypeID) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_MachineLSEPrototypeID.Marshal(b, m, deterministic)
 }
-func (m *MachinePrototypeID) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_MachinePrototypeID.Merge(m, src)
+func (m *MachineLSEPrototypeID) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_MachineLSEPrototypeID.Merge(m, src)
 }
-func (m *MachinePrototypeID) XXX_Size() int {
-	return xxx_messageInfo_MachinePrototypeID.Size(m)
+func (m *MachineLSEPrototypeID) XXX_Size() int {
+	return xxx_messageInfo_MachineLSEPrototypeID.Size(m)
 }
-func (m *MachinePrototypeID) XXX_DiscardUnknown() {
-	xxx_messageInfo_MachinePrototypeID.DiscardUnknown(m)
+func (m *MachineLSEPrototypeID) XXX_DiscardUnknown() {
+	xxx_messageInfo_MachineLSEPrototypeID.DiscardUnknown(m)
 }
 
-var xxx_messageInfo_MachinePrototypeID proto.InternalMessageInfo
+var xxx_messageInfo_MachineLSEPrototypeID proto.InternalMessageInfo
 
-func (m *MachinePrototypeID) GetValue() string {
+func (m *MachineLSEPrototypeID) GetValue() string {
 	if m != nil {
 		return m.Value
 	}
@@ -99,19 +99,19 @@ func (m *MachinePrototypeID) GetValue() string {
 }
 
 func init() {
-	proto.RegisterType((*RackPrototypeID)(nil), "fleet.RackPrototypeID")
-	proto.RegisterType((*MachinePrototypeID)(nil), "fleet.MachinePrototypeID")
+	proto.RegisterType((*RackLSEPrototypeID)(nil), "fleet.RackLSEPrototypeID")
+	proto.RegisterType((*MachineLSEPrototypeID)(nil), "fleet.MachineLSEPrototypeID")
 }
 
 func init() { proto.RegisterFile("lse_prototype.proto", fileDescriptor_4844cac9ae81b3e2) }
 
 var fileDescriptor_4844cac9ae81b3e2 = []byte{
-	// 106 bytes of a gzipped FileDescriptorProto
+	// 109 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x12, 0xce, 0x29, 0x4e, 0x8d,
 	0x2f, 0x28, 0xca, 0x2f, 0xc9, 0x2f, 0xa9, 0x2c, 0x48, 0xd5, 0x03, 0xb3, 0x84, 0x58, 0xd3, 0x72,
-	0x52, 0x53, 0x4b, 0x94, 0xd4, 0xb9, 0xf8, 0x83, 0x12, 0x93, 0xb3, 0x03, 0x60, 0xb2, 0x9e, 0x2e,
-	0x42, 0x22, 0x5c, 0xac, 0x65, 0x89, 0x39, 0xa5, 0xa9, 0x12, 0x8c, 0x0a, 0x8c, 0x1a, 0x9c, 0x41,
-	0x10, 0x8e, 0x92, 0x16, 0x97, 0x90, 0x6f, 0x62, 0x72, 0x46, 0x66, 0x5e, 0x2a, 0x41, 0xb5, 0x4e,
-	0x9c, 0x51, 0xec, 0x7a, 0xd6, 0x60, 0xf3, 0x93, 0xd8, 0xc0, 0xb6, 0x19, 0x03, 0x02, 0x00, 0x00,
-	0xff, 0xff, 0x52, 0x21, 0x12, 0xf0, 0x84, 0x00, 0x00, 0x00,
+	0x52, 0x53, 0x4b, 0x94, 0xb4, 0xb8, 0x84, 0x82, 0x12, 0x93, 0xb3, 0x7d, 0x82, 0x5d, 0x03, 0x60,
+	0x0a, 0x3c, 0x5d, 0x84, 0x44, 0xb8, 0x58, 0xcb, 0x12, 0x73, 0x4a, 0x53, 0x25, 0x18, 0x15, 0x18,
+	0x35, 0x38, 0x83, 0x20, 0x1c, 0x25, 0x5d, 0x2e, 0x51, 0xdf, 0xc4, 0xe4, 0x8c, 0xcc, 0xbc, 0x54,
+	0x62, 0x94, 0x3b, 0x71, 0x46, 0xb1, 0xeb, 0x59, 0x83, 0x6d, 0x49, 0x62, 0x03, 0xdb, 0x69, 0x0c,
+	0x08, 0x00, 0x00, 0xff, 0xff, 0xc0, 0xe7, 0xa3, 0xd5, 0x8a, 0x00, 0x00, 0x00,
 }
