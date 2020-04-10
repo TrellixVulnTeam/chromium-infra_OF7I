5f551aba8086a78d908883e94fbdf01358b1bcad
Gregory Nisbet <gregorynisbet@google.com>
2020-04-10 21:02:55 +0000

    [skylab-swarming-worker] Remove unnecessary logging from hostinfo.go
    BUG=chromium:1069874
    TEST=None
    
    Change-Id: I2ab5f303d3e74d25ae60d8b1857de7c31b721a66
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/2145768
    Reviewed-by: Xixuan Wu <xixuan@chromium.org>
    Commit-Queue: Gregory Nisbet <gregorynisbet@google.com>
    Cr-Commit-Position: refs/heads/master@{#30464}

--- go/src/infra/cmd/skylab_swarming_worker/internal/autotest/hostinfo/hostinfo.go
+++ go/src/infra/cmd/skylab_swarming_worker/internal/autotest/hostinfo/hostinfo.go
@@ -8,7 +8,6 @@ package hostinfo
 
 import (
 	"encoding/json"
-	"log"
 )
 
 // HostInfo stores the host information.  Hostinfo files are used to
@@ -44,45 +43,36 @@ func Unmarshal(blob []byte) (*HostInfo, error) {
 
 // Marshal serializes the HostInfo struct into a slice of bytes.
 func Marshal(hi *HostInfo) ([]byte, error) {
-	log.Printf("hostinfo::Marshal: before default values applied (%#v)", hi)
 	ensureDefaultFieldValues(hi)
-	log.Printf("hostinfo::Marshal: after default values applied (%#v)", hi)
 	vhi := versionedHostInfo{
 		HostInfo:          hi,
 		SerializerVersion: currentSerializerVersion,
 	}
 	out, err := json.Marshal(vhi)
-	log.Printf("hostinfo::Marshal: marshalled hostinfo (%s) err (%s)", string(out), err)
 	return out, err
 }
 
 // MarshalIndent serializes the HostInfo struct into a slice of bytes.
 // The prefix and indent options of json.MarshalIndent are not exposed to the user because You Don't Need Them.
 func MarshalIndent(hi *HostInfo) ([]byte, error) {
-	log.Printf("hostinfo::MarshalIndent: before default values applied (%#v)", hi)
 	ensureDefaultFieldValues(hi)
-	log.Printf("hostinfo::MarshalIndent: after default values applied (%#v)", hi)
 	vhi := versionedHostInfo{
 		HostInfo:          hi,
 		SerializerVersion: currentSerializerVersion,
 	}
 	out, err := json.MarshalIndent(vhi, "", "\t")
-	log.Printf("hostinfo::MarshalIndent: marshalled hostinfo (%s) err (%s)", string(out), err)
 	return out, err
 }
 
 // ensureDefaultFieldValues -- ensure that maps and slices in HostInfo are non-nil
 func ensureDefaultFieldValues(hi *HostInfo) {
 	if hi.Labels == nil {
-		log.Printf("ensureDefaultFieldValues: empty Labels")
 		hi.Labels = []string{}
 	}
 	if hi.Attributes == nil {
-		log.Printf("ensureDefaultFieldValues: empty Attributes")
 		hi.Attributes = make(map[string]string)
 	}
 	if hi.StableVersions == nil {
-		log.Printf("ensureDefaultFieldValues: empty StableVersions")
 		hi.StableVersions = make(map[string]string)
 	}
 }
