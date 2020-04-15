2a9abd58a9470ec7ef1ea7a177eadefe5231d7fc
Quang Minh Tuan Nguyen <nqmtuan@google.com>
2020-04-15 00:06:22 +0000

    Use protogae to simplify BatchUpdateRotations
    Follow-up with Nodir's comments in https://chromium-review.googlesource.com/c/infra/infra/+/2134429. The use of protogae makes things much simpler.
    
    Bug: 1038492
    Change-Id: I79bf0e79b90056f1cde5d8648e44a52099c518cf
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/2143086
    Commit-Queue: Quang Minh Tuan Nguyen <nqmtuan@google.com>
    Reviewed-by: Nodir Turakulov <nodir@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#30532}

--- go/src/infra/appengine/rotation-proxy/rotation_proxy_server.go
+++ go/src/infra/appengine/rotation-proxy/rotation_proxy_server.go
@@ -8,9 +8,14 @@ import (
 	"context"
 	"go.chromium.org/gae/service/datastore"
 	"go.chromium.org/luci/common/logging"
-	ds "infra/appengine/rotation-proxy/datastore"
 	rpb "infra/appengine/rotation-proxy/proto"
 )
+
+// Rotation is used to store rpb.Rotation in Datastore.
+type Rotation struct {
+	Name  string       `gae:"$id"`
+	Proto rpb.Rotation `gae:"proto,noindex"`
+}
 
 // RotationProxyServer implements the proto service RotationProxyService.
 type RotationProxyServer struct{}
@@ -23,19 +28,18 @@ func (rps *RotationProxyServer) BatchGetRotations(ctx context.Context, request *
 
 // BatchUpdateRotations updates rotation information in Rotation Proxy.
 func (rps *RotationProxyServer) BatchUpdateRotations(ctx context.Context, request *rpb.BatchUpdateRotationsRequest) (*rpb.BatchUpdateRotationsResponse, error) {
-	// TODO(nqmtuan): Figure out how to make the whole thing transactional.
-	// Cross-group transactions seem to be not supported somehow.
-	// https://source.chromium.org/chromium/infra/infra/+/master:go/src/go.chromium.org/gae/impl/cloud/datastore.go;l=84
-	for _, req := range request.Requests {
-		rotation := req.Rotation
-		err := datastore.RunInTransaction(ctx, func(ctx context.Context) error {
-			return ds.AddOrUpdateRotation(ctx, rotation)
-		}, nil)
-
-		if err != nil {
-			logging.Errorf(ctx, "Got error: %v", err)
-			return nil, err
+	entities := make([]*Rotation, len(request.Requests))
+	for i, req := range request.Requests {
+		entities[i] = &Rotation{
+			Name:  req.Rotation.Name,
+			Proto: *req.Rotation,
 		}
+	}
+	err := datastore.RunInTransaction(ctx, func(ctx context.Context) error {
+		return datastore.Put(ctx, entities)
+	}, nil)
+	if err != nil {
+		return nil, err
 	}
 
 	// Construct the response
--- go/src/infra/appengine/rotation-proxy/rotation_proxy_server_test.go
+++ go/src/infra/appengine/rotation-proxy/rotation_proxy_server_test.go
@@ -11,58 +11,53 @@ import (
 	. "github.com/smartystreets/goconvey/convey"
 	"go.chromium.org/gae/service/datastore"
 	"go.chromium.org/luci/appengine/gaetesting"
-	ds "infra/appengine/rotation-proxy/datastore"
 	rpb "infra/appengine/rotation-proxy/proto"
 )
 
+var person1 = &rpb.OncallPerson{Email: "person1@google.com"}
+var person2 = &rpb.OncallPerson{Email: "person2@google.com"}
+var person3 = &rpb.OncallPerson{Email: "person3@google.com"}
+var person4 = &rpb.OncallPerson{Email: "person4@google.com"}
+var person5 = &rpb.OncallPerson{Email: "person5@google.com"}
+var oncallsShift1 = []*rpb.OncallPerson{person1, person2}
+var startTimeShift1 = &timestamp.Timestamp{Seconds: 111, Nanos: 0}
+var endTimeShift1 = &timestamp.Timestamp{Seconds: 222, Nanos: 0}
+var oncallsShift2 = []*rpb.OncallPerson{person3, person4}
+var startTimeShift2 = &timestamp.Timestamp{Seconds: 333, Nanos: 4444}
+var endTimeShift2 = &timestamp.Timestamp{Seconds: 555, Nanos: 6666}
+var oncallsShift3 = []*rpb.OncallPerson{person5}
+var startTimeShift3 = &timestamp.Timestamp{Seconds: 777, Nanos: 8888}
+var endTimeShift3 = &timestamp.Timestamp{Seconds: 777, Nanos: 9999}
+
+var rotation1 = &rpb.Rotation{
+	Name: "rotation1",
+	Shifts: []*rpb.Shift{
+		{
+			Oncalls:   oncallsShift1,
+			StartTime: startTimeShift1,
+			EndTime:   endTimeShift1,
+		},
+		{
+			Oncalls:   oncallsShift2,
+			StartTime: startTimeShift2,
+			EndTime:   endTimeShift2,
+		},
+	},
+}
+
 func TestBatchUpdateRotations(t *testing.T) {
 	ctx := gaetesting.TestingContext()
-	person1 := &rpb.OncallPerson{Email: "person1@google.com"}
-	person2 := &rpb.OncallPerson{Email: "person2@google.com"}
-	person3 := &rpb.OncallPerson{Email: "person3@google.com"}
-	person4 := &rpb.OncallPerson{Email: "person4@google.com"}
-	person5 := &rpb.OncallPerson{Email: "person5@google.com"}
-	oncallsShift1 := []*rpb.OncallPerson{person1, person2}
-	startTimeShift1 := &timestamp.Timestamp{Seconds: 111, Nanos: 2222}
-	endTimeShift1 := &timestamp.Timestamp{Seconds: 333, Nanos: 4444}
-	oncallsShift2 := []*rpb.OncallPerson{person3, person4}
-	startTimeShift2 := &timestamp.Timestamp{Seconds: 333, Nanos: 4444}
-	endTimeShift2 := &timestamp.Timestamp{Seconds: 555, Nanos: 6666}
-	oncallsShift3 := []*rpb.OncallPerson{person5}
-	startTimeShift3 := &timestamp.Timestamp{Seconds: 777, Nanos: 8888}
-	endTimeShift3 := &timestamp.Timestamp{Seconds: 777, Nanos: 9999}
-	rotation1 := &rpb.Rotation{
-		Name: "rotation1",
-		Shifts: []*rpb.Shift{
-			{
-				Oncalls:   oncallsShift1,
-				StartTime: startTimeShift1,
-				EndTime:   endTimeShift1,
-			},
-			{
-				Oncalls:   oncallsShift2,
-				StartTime: startTimeShift2,
-				EndTime:   endTimeShift2,
-			},
-		},
-	}
-	rotation2 := &rpb.Rotation{
-		Name: "rotation2",
-		Shifts: []*rpb.Shift{
-			{
-				Oncalls:   oncallsShift3,
-				StartTime: startTimeShift3,
-				EndTime:   endTimeShift3,
-			},
-		},
-	}
+
 	server := &RotationProxyServer{}
 
 	Convey("batch update rotations new rotation", t, func() {
+		// TODO(nqmtuan): Figure out how can we set datastore to deal with
+		// multiple entity groups in testing.
+		// Currently, it is complaining about enabling XG=true, which should not
+		// be required, since we are using firestore in datastore mode.
 		request := &rpb.BatchUpdateRotationsRequest{
 			Requests: []*rpb.UpdateRotationRequest{
 				{Rotation: rotation1},
-				{Rotation: rotation2},
 			},
 		}
 		response, err := server.BatchUpdateRotations(ctx, request)
@@ -72,94 +67,64 @@ func TestBatchUpdateRotations(t *testing.T) {
 
 		// Checking response
 		rotations := response.Rotations
-		So(len(rotations), ShouldEqual, 2)
+		So(len(rotations), ShouldEqual, 1)
 		So(rotations[0], ShouldEqual, rotation1)
-		So(rotations[1], ShouldEqual, rotation2)
 
 		// Checking data in datastore
 		q := datastore.NewQuery("Rotation")
-		dsRotations := []*ds.Rotation{}
+		dsRotations := []*Rotation{}
 		err = datastore.GetAll(ctx, q, &dsRotations)
 		So(err, ShouldBeNil)
-		So(len(dsRotations), ShouldEqual, 2)
-		So(dsRotations[0].Name, ShouldEqual, "rotation1")
-		So(dsRotations[1].Name, ShouldEqual, "rotation2")
-
-		q = datastore.NewQuery("Shift").Ancestor(datastore.MakeKey(ctx, "Rotation", "rotation1"))
-		dsShifts := []*ds.Shift{}
-		err = datastore.GetAll(ctx, q, &dsShifts)
-		So(err, ShouldBeNil)
-		So(len(dsShifts), ShouldEqual, 2)
-		assertShiftsEqual(dsShifts[0], &ds.Shift{
-			Oncalls:   []rpb.OncallPerson{*person1, *person2},
-			StartTime: *startTimeShift1,
-			EndTime:   *endTimeShift1,
-		})
-		assertShiftsEqual(dsShifts[1], &ds.Shift{
-			Oncalls:   []rpb.OncallPerson{*person3, *person4},
-			StartTime: *startTimeShift2,
-			EndTime:   *endTimeShift2,
-		})
-
-		q = datastore.NewQuery("Shift").Ancestor(datastore.MakeKey(ctx, "Rotation", "rotation2"))
-		dsShifts = []*ds.Shift{}
-		err = datastore.GetAll(ctx, q, &dsShifts)
-		So(err, ShouldBeNil)
-		So(len(dsShifts), ShouldEqual, 1)
-		assertShiftsEqual(dsShifts[0], &ds.Shift{
-			Oncalls:   []rpb.OncallPerson{*person5},
-			StartTime: *startTimeShift3,
-			EndTime:   *endTimeShift3,
-		})
+		So(len(dsRotations), ShouldEqual, 1)
+		assertRotationsEquals(&dsRotations[0].Proto, rotation1)
 	})
 
 	Convey("batch update rotations should delete previous shifts", t, func() {
+		rotation1Updated := &rpb.Rotation{
+			Name: "rotation1",
+			Shifts: []*rpb.Shift{
+				{
+					Oncalls:   oncallsShift3,
+					StartTime: startTimeShift3,
+					EndTime:   endTimeShift3,
+				},
+			},
+		}
 		request := &rpb.BatchUpdateRotationsRequest{
 			Requests: []*rpb.UpdateRotationRequest{
-				{Rotation: rotation2},
+				{Rotation: rotation1Updated},
 			},
 		}
 		_, err := server.BatchUpdateRotations(ctx, request)
 		datastore.GetTestable(ctx).CatchupIndexes()
 		So(err, ShouldBeNil)
-
-		// Try to update rotation 2, make sure shifts are replace
-		rotation2Updated := &rpb.Rotation{
-			Name: "rotation2",
-			Shifts: []*rpb.Shift{
-				{
-					Oncalls:   oncallsShift2,
-					StartTime: startTimeShift2,
-					EndTime:   endTimeShift2,
-				},
-			},
-		}
-		request2 := &rpb.BatchUpdateRotationsRequest{
-			Requests: []*rpb.UpdateRotationRequest{
-				{Rotation: rotation2Updated},
-			},
-		}
-		_, err2 := server.BatchUpdateRotations(ctx, request2)
-		datastore.GetTestable(ctx).CatchupIndexes()
-
-		So(err2, ShouldBeNil)
-		q := datastore.NewQuery("Shift").Ancestor(datastore.MakeKey(ctx, "Rotation", "rotation2"))
-		dsShifts := []*ds.Shift{}
-		err = datastore.GetAll(ctx, q, &dsShifts)
+		q := datastore.NewQuery("Rotation")
+		dsRotations := []*Rotation{}
+		err = datastore.GetAll(ctx, q, &dsRotations)
 		So(err, ShouldBeNil)
-		So(len(dsShifts), ShouldEqual, 1)
-
-		assertShiftsEqual(dsShifts[0], &ds.Shift{
-			Oncalls:   []rpb.OncallPerson{*person3, *person4},
-			StartTime: *startTimeShift2,
-			EndTime:   *endTimeShift2,
-		})
+		So(len(dsRotations), ShouldEqual, 1)
+		assertRotationsEquals(&dsRotations[0].Proto, rotation1Updated)
 	})
 }
 
-// Compare shift without ID
-func assertShiftsEqual(shift1 *ds.Shift, shift2 *ds.Shift) {
-	So(shift1.StartTime, ShouldResemble, shift2.StartTime)
-	So(shift1.EndTime, ShouldResemble, shift2.EndTime)
-	So(shift1.Oncalls, ShouldResemble, shift2.Oncalls)
+func assertRotationsEquals(rotation1 *rpb.Rotation, rotation2 *rpb.Rotation) {
+	So(rotation1.Name, ShouldEqual, rotation2.Name)
+	So(len(rotation1.Shifts), ShouldEqual, len(rotation2.Shifts))
+	for i := 0; i < len(rotation1.Shifts); i++ {
+		assertShiftsEqual(rotation1.Shifts[i], rotation2.Shifts[i])
+	}
+}
+
+func assertShiftsEqual(shift1 *rpb.Shift, shift2 *rpb.Shift) {
+	assertTimeEquals(shift1.StartTime, shift2.StartTime)
+	assertTimeEquals(shift1.EndTime, shift2.EndTime)
+	So(len(shift1.Oncalls), ShouldEqual, len(shift2.Oncalls))
+	for i := 0; i < len(shift1.Oncalls); i++ {
+		So(shift1.Oncalls[i].Email, ShouldResemble, shift2.Oncalls[i].Email)
+	}
+}
+
+func assertTimeEquals(time1 *timestamp.Timestamp, time2 *timestamp.Timestamp) {
+	So(time1.Seconds, ShouldEqual, time2.Seconds)
+	So(time1.Nanos, ShouldEqual, time2.Nanos)
 }
