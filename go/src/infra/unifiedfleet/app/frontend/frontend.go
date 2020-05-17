e5e11dc802c863c80221220875627fb14198e4a2
Xixuan Wu <xixuan@google.com>
2020-05-17 10:54:20 +0000

    UFS: add auth for UFS rpcs
    BUG=chromium:1060683
    TEST=make test; make dev; Call RPC locally.
    
    Change-Id: Ied7401ee237146363fea7e020a0685aec9bca180
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/2205168
    Reviewed-by: Eshwar Narayan <eshwarn@chromium.org>
    Commit-Queue: Xixuan Wu <xixuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#31622}

--- go/src/infra/unifiedfleet/app/frontend/frontend.go
+++ go/src/infra/unifiedfleet/app/frontend/frontend.go
@@ -10,6 +10,10 @@ import (
 	"github.com/golang/protobuf/proto"
 	"go.chromium.org/luci/common/logging"
 	"go.chromium.org/luci/grpc/prpc"
+	"go.chromium.org/luci/server/auth"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
 	api "infra/unifiedfleet/api/v1/rpc"
 )
 
@@ -24,5 +28,16 @@ func InstallServices(apiServer *prpc.Server) {
 // checkAccess verifies that the request is from an authorized user.
 func checkAccess(ctx context.Context, rpcName string, _ proto.Message) (context.Context, error) {
 	logging.Debugf(ctx, "Check access for %s", rpcName)
+	// TODO(xixuan): use the two below groups first before setting up the ufs auth group in config file
+	group := []string{"mdb/chrome-fleet-software-team", "mdb/chrome-labs"}
+	allow, err := auth.IsMember(ctx, group...)
+	if err != nil {
+		logging.Warningf(ctx, "Check group '%s' membership failed: %s", group, err.Error())
+		return ctx, status.Errorf(codes.Internal, "can't check access group membership: %s", err)
+	}
+	if !allow {
+		return ctx, status.Errorf(codes.PermissionDenied, "%s is not a member of %s", auth.CurrentIdentity(ctx), group)
+	}
+	logging.Infof(ctx, "%s is a member of %s", auth.CurrentIdentity(ctx), group)
 	return ctx, nil
 }
