65b086ca0ec689cc91786cd5e6105cffd20bf938
Anh Le <anhdle@google.com>
2020-06-08 20:17:49 +0000

    [tls] Implement TLS ExecDutCommand
    BUG=chromium:1049437
    
    Change-Id: I6a75e6c053961fd918f42bfc9e6d2913ca79d289
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/2233089
    Commit-Queue: Anh Le <anhdle@chromium.org>
    Auto-Submit: Anh Le <anhdle@chromium.org>
    Reviewed-by: Allen Li <ayatane@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#32179}

--- go/src/infra/cros/cmd/common-tls/server.go
+++ go/src/infra/cros/cmd/common-tls/server.go
@@ -5,12 +5,17 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"fmt"
 	"net"
+	"strings"
 
 	"go.chromium.org/chromiumos/config/go/api/test/tls"
 	"golang.org/x/crypto/ssh"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type server struct {
@@ -37,20 +42,109 @@ func (s *server) Serve(l net.Listener) error {
 	return server.Serve(l)
 }
 
+func (s *server) ExecDutCommand(req *tls.ExecDutCommandRequest, stream tls.Common_ExecDutCommandServer) error {
+	ctx := stream.Context()
+
+	resp := &tls.ExecDutCommandResponse{
+		ExitInfo: &tls.ExecDutCommandResponse_ExitInfo{
+			Started: false,
+			Status:  255,
+		},
+	}
+
+	addr, err := s.getSSHAddr(ctx, req.GetName())
+
+	if err != nil {
+		resp.ExitInfo.ErrorMessage = err.Error()
+		stream.Send(resp)
+		return status.Errorf(codes.FailedPrecondition, err.Error())
+	}
+
+	var c *ssh.Client
+	clientOk := false
+	defer func() {
+		if c == nil {
+			return
+		}
+		if clientOk {
+			s.clientPool.Put(addr, c)
+		} else {
+			go c.Close()
+		}
+	}()
+
+	var session *ssh.Session
+
+	// Retry once, in case we get a bad SSH client out of the pool.
+	for i := 0; i < 2; i++ {
+		c, err = s.clientPool.Get(addr)
+		if err != nil {
+			resp.ExitInfo.ErrorMessage = err.Error()
+			stream.Send(resp)
+			return status.Errorf(codes.FailedPrecondition, fmt.Sprintf("ExecDutCommand %s %#v: %s", req.GetName(), req.GetCommand(), err))
+		}
+		session, err = c.NewSession()
+		if err != nil {
+			// This client is probably bad, so close and stop using it.
+			go c.Close()
+			continue
+		}
+		break
+	}
+
+	if err != nil {
+		resp.ExitInfo.ErrorMessage = err.Error()
+		stream.Send(resp)
+		return status.Errorf(codes.FailedPrecondition, fmt.Sprintf("ExecDutCommand %s %#v: %s", req.GetName(), req.GetCommand(), err))
+	}
+	defer session.Close()
+
+	var stdout bytes.Buffer
+	var stderr bytes.Buffer
+	session.Stdout = &stdout
+	session.Stderr = &stderr
+
+	args := req.GetArgs()
+	if len(args) == 0 {
+		err = session.Run(req.GetCommand())
+	} else {
+		err = session.Run(req.GetCommand() + " " + strings.Join(args, " "))
+	}
+
+	resp.ExitInfo.Started = true
+
+	switch err := err.(type) {
+	case *ssh.ExitError:
+		clientOk = true
+		resp.ExitInfo.Status = int32(err.Waitmsg.ExitStatus())
+		if err.Waitmsg.Signal() != "" {
+			resp.ExitInfo.Signaled = true
+		}
+		resp.ExitInfo.ErrorMessage = err.Error()
+	case *ssh.ExitMissingError:
+		clientOk = true
+		resp.ExitInfo.ErrorMessage = err.Error()
+	default:
+		resp.ExitInfo.ErrorMessage = err.Error()
+		fmt.Fprintf(&stderr, "tls: unknown SSH session error: %s\n", err)
+	}
+
+	resp.Stdout = stdout.Bytes()
+	resp.Stderr = stderr.Bytes()
+	resp.ExitInfo.Status = 0
+	_ = stream.Send(resp)
+	return nil
+}
+
 // getSSHAddr returns the SSH address to use for the DUT, through the wiring service.
-func (s *server) getSSHAddr(ctx context.Context, dut string) (string, error) {
+func (s *server) getSSHAddr(ctx context.Context, name string) (string, error) {
 	c := tls.NewWiringClient(s.wiringConn)
 	resp, err := c.OpenDutPort(ctx, &tls.OpenDutPortRequest{
-		// TODO(ayatane): Temporarily commenting due to proto change.
-		// Dut:     dut,
-		// DutPort: 22,
+		Name: name,
+		Port: 22,
 	})
 	if err != nil {
 		return "", err
 	}
-	// TODO(ayatane): Temporarily commenting due to proto change.
-	// if s := resp.GetStatus(); s != tls.OpenDutPortResponse_STATUS_OK {
-	// 	return "", fmt.Errorf("get SSH addr %s: %s %s", dut, s, resp.GetReason())
-	// }
-	return resp.GetAddress(), nil
+	return fmt.Sprintf("%s:%d", resp.GetAddress(), resp.GetPort()), nil
 }
