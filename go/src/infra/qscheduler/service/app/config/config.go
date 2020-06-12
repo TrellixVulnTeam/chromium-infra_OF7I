7b93de6b05da32c9daaec987c8865c3672470fe7
Vadim Shtayura <vadimsh@chromium.org>
2020-06-12 06:07:54 +0000

    [qscheduler] Remove dependency on server/cfgclient.
    It is all dead code. It was used on GAE, but not on GKE.
    
    R=tandrii@chromium.org, pprabhu@chromium.org, zamorzaev@chromium.org
    BUG=1090916
    
    Change-Id: I045c15e6b6263139e41674c6e49777884f421f4f
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/2242773
    Reviewed-by: Andrii Shyshkalov <tandrii@google.com>
    Commit-Queue: Vadim Shtayura <vadimsh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#32337}

--- go/src/infra/qscheduler/service/app/config/config.go
+++ go/src/infra/qscheduler/service/app/config/config.go
@@ -15,18 +15,8 @@
 package config
 
 import (
-	"net/http"
-
-	"github.com/golang/protobuf/proto"
-	"go.chromium.org/luci/common/logging"
-	"go.chromium.org/luci/config/server/cfgclient"
-	"go.chromium.org/luci/config/server/cfgclient/textproto"
-	"go.chromium.org/luci/config/validation"
-	"go.chromium.org/luci/server/router"
-	"golang.org/x/net/context"
+	"context"
 )
-
-const configFile = "config.cfg"
 
 // unique key used to store and retrieve context.
 var contextKey = "qscheduler-swarming luci-config key"
@@ -35,7 +25,6 @@ var contextKey = "qscheduler-swarming luci-config key"
 type Provider func() *Config
 
 // Get returns the config in c if it exists, or nil.
-// See also Use and MiddlewareForGAE.
 func Get(c context.Context) *Config {
 	if p, _ := c.Value(&contextKey).(Provider); p != nil {
 		return p()
@@ -43,46 +32,7 @@ func Get(c context.Context) *Config {
 	return nil
 }
 
-// MiddlewareForGAE loads the service config and installs it into the context.
-//
-// Works only on GAE currently, since 'cfgclient' library is not yet supported
-// on GKE.
-func MiddlewareForGAE(c *router.Context, next router.Handler) {
-	var cfg Config
-	err := cfgclient.Get(
-		c.Context,
-		cfgclient.AsService,
-		cfgclient.CurrentServiceConfigSet(c.Context),
-		configFile,
-		textproto.Message(&cfg),
-		nil,
-	)
-	if err != nil {
-		logging.WithError(err).Errorf(c.Context, "could not load application config")
-		http.Error(c.Writer, "Internal server error", http.StatusInternalServerError)
-		return
-	}
-
-	c.Context = Use(c.Context, func() *Config { return &cfg })
-	next(c)
-}
-
 // Use installs a config provider into c.
 func Use(c context.Context, p Provider) context.Context {
 	return context.WithValue(c, &contextKey, p)
-}
-
-// SetupValidation adds validation rules for configuration data pushed via luci-config.
-func SetupValidation() {
-	rules := &validation.Rules
-	rules.Add("services/${appid}", configFile, validateConfig)
-}
-
-func validateConfig(c *validation.Context, configSet, path string, content []byte) error {
-	cfg := &Config{}
-	if err := proto.UnmarshalText(string(content), cfg); err != nil {
-		c.Errorf("not a valid Config proto message: %s", err)
-		return nil
-	}
-	return nil
 }
