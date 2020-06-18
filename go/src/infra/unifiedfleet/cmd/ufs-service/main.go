4b53210fae1b600e7cd8bb1c0ef79eb028c7db71
Eshwar Narayan <eshwarn@google.com>
2020-06-18 17:46:25 +0000

    UFS: client version check
    Bug: 1059027
    Test: run shivas client with lower version against a local UFS server
    
    ./shivas machine ls -n 2
    Using UnifiedFleet service 127.0.0.1:8800
    
    machine: rpc error: code = FailedPrecondition desc = Unsupported client version. Please update your client version to v2.X.X or above.
    Change-Id: I252897401fb0d74b3c9c1fdfac39e2e11d798193
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/2242735
    Commit-Queue: Eshwar Narayan <eshwarn@chromium.org>
    Reviewed-by: Anushruth  <anushruth@chromium.org>
    Reviewed-by: Xixuan Wu <xixuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#32520}

--- go/src/infra/unifiedfleet/cmd/ufs-service/main.go
+++ go/src/infra/unifiedfleet/cmd/ufs-service/main.go
@@ -5,15 +5,29 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"fmt"
+	"strconv"
 
 	"go.chromium.org/luci/server"
 	"go.chromium.org/luci/server/gaeemulation"
 	"go.chromium.org/luci/server/module"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/status"
 
 	"infra/unifiedfleet/app/config"
 	"infra/unifiedfleet/app/frontend"
 )
+
+// SupportedClientMajorVersionNumber indicates the minimum client version
+// supported by this server
+//
+// any client with major version number lower than this number will get an
+// error to update their client to this major version or above.
+const SupportedClientMajorVersionNumber = 1
 
 func main() {
 	modules := []module.Module{
@@ -32,7 +46,34 @@ func main() {
 		srv.RunInBackground("ufs.config", cfgLoader.ReloadLoop)
 
 		srv.Context = config.Use(srv.Context, cfgLoader.Config())
+		srv.RegisterUnaryServerInterceptor(versionInterceptor)
 		frontend.InstallServices(srv.PRPC)
 		return nil
 	})
+}
+
+// versionInterceptor interceptor to handle client version check per RPC call
+func versionInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+	md, ok := metadata.FromIncomingContext(ctx)
+	if !ok {
+		return nil, status.Errorf(codes.InvalidArgument, "Retrieving metadata failed.")
+	}
+	version, ok := md["clientversion"]
+	if !ok || len(version) == 0 {
+		return nil, status.Errorf(codes.InvalidArgument, "Client major version not supplied.")
+	}
+	major, err := strconv.ParseInt(version[0], 10, 32)
+	if err != nil {
+		return nil, status.Errorf(codes.InvalidArgument, "Client major version format error.")
+	}
+	if major < SupportedClientMajorVersionNumber {
+		return nil, status.Errorf(codes.FailedPrecondition,
+			fmt.Sprintf("Unsupported client version. Please update your client "+
+				"version to v%d.X.X or above.", SupportedClientMajorVersionNumber))
+	}
+
+	// Calls the handler
+	h, err := handler(ctx, req)
+
+	return h, err
 }
