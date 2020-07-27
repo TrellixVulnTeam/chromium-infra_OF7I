33ed2ba60aa7938bc106af289a86eafca42d2732
Benjamin Gordon <bmgordon@chromium.org>
2020-07-27 23:28:00 +0000

    [tricium] Update shellcheck to 0.7.1-r22
    Binary downloaded from the Chrome OS SDK version 2020.07.26.091855.
    
    Bug: 1045555
    Test: make testrun; PATH=$PWD/bin/shellcheck:$PATH make test
    Change-Id: I844085dd6a2a565119d2cb3b22261661e4127671
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/2320551
    Reviewed-by: Quinten Yearsley <qyearsley@chromium.org>
    Commit-Queue: Benjamin Gordon <bmgordon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#33636}

--- go/src/infra/tricium/functions/shellcheck/fetch_shellcheck.sh
+++ go/src/infra/tricium/functions/shellcheck/fetch_shellcheck.sh
@@ -2,8 +2,8 @@
 
 # When updating the version, you must update the SHA512 sum as well, e.g.:
 # shasum -a 512 "${ARCHIVE}" > "${ARCHIVE}.sum"
-VERSION=0.7.1-r21
-SDK_VERSION=2020.07.11.232516
+VERSION=0.7.1-r22
+SDK_VERSION=2020.07.26.091855
 ARCHIVE="shellcheck-${VERSION}.tbz2"
 URL="https://storage.googleapis.com/chromeos-prebuilt/host/amd64/amd64-host/chroot-${SDK_VERSION}/packages/dev-util/${ARCHIVE}"
 SUMFILE="${ARCHIVE}.sum"
