c824466a0e658b9c4cb4ca79620ac64b7b140134
Anushruth <anushruth@chromium.org>
2020-07-29 21:54:15 +0000

    [UFS] Add GetMachineFromAssets func to create machine from asset data
    Bug: 1064071
    Test: Tested on local dumper instance
    
    GetMachineFromAssets will be used to create machine proto objects from
    asset location and data.
    
    Change-Id: Iad182b015b5ffb4a07ecc7ec6cfd3a913f38f2c3
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/2315230
    Commit-Queue: Anushruth  <anushruth@chromium.org>
    Reviewed-by: Xixuan Wu <xixuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#33713}

--- go/src/infra/unifiedfleet/app/dumper/iv2.go
+++ go/src/infra/unifiedfleet/app/dumper/iv2.go
@@ -2,16 +2,22 @@ package dumper
 
 import (
 	"context"
+	"regexp"
+	"strings"
 
 	"cloud.google.com/go/bigquery"
 	"cloud.google.com/go/datastore"
+	"github.com/golang/protobuf/ptypes"
 	"go.chromium.org/luci/common/logging"
 	"google.golang.org/api/iterator"
 
 	iv2ds "infra/libs/cros/lab_inventory/datastore"
 	iv2pr "infra/libs/fleet/protos"
 	iv2pr2 "infra/libs/fleet/protos/go"
+	ufspb "infra/unifiedfleet/api/v1/proto"
 )
+
+var macAddress = regexp.MustCompile(`^([0-9A-Fa-f]{2}[:]){5}([0-9A-Fa-f]{2})$`)
 
 // GetAllAssets retrieves all the asset data from inventory-V2
 func GetAllAssets(ctx context.Context, client *datastore.Client) ([]*iv2pr.ChopsAsset, error) {
@@ -88,4 +94,81 @@ func GetAssetToHostnameMap(ctx context.Context, client *bigquery.Client) (map[st
 	}
 	logging.Debugf(ctx, "Found hostnames for %v devices", len(assetsToHostname))
 	return assetsToHostname, nil
+}
+
+// GetMachineFromAssets returns Machine proto constructed from ChopsAsset
+// and AssetInfo
+//
+// Machine object is constructed by directly assigning most of the data. Lab in
+// Location is converted from string to enum using a switch case and
+// hostname is used for BarcodeName. If hostname is empty, Lab name is used for
+// BarcodeName
+func GetMachineFromAssets(asset *iv2pr.ChopsAsset, assetInfo *iv2pr2.AssetInfo, hostname string) *ufspb.Machine {
+	device := &ufspb.ChromeOSMachine{
+		ReferenceBoard: assetInfo.GetReferenceBoard(),
+		BuildTarget:    assetInfo.GetBuildTarget(),
+		Model:          assetInfo.GetModel(),
+		GoogleCodeName: assetInfo.GetGoogleCodeName(),
+		MacAddress:     assetInfo.GetEthernetMacAddress(),
+		Sku:            assetInfo.GetSku(),
+		Phase:          assetInfo.GetPhase(),
+		CostCenter:     assetInfo.GetCostCenter(),
+	}
+	if strings.Contains(hostname, "labstation") ||
+		strings.Contains(device.GoogleCodeName, "Labstation") {
+		device.DeviceType = ufspb.ChromeOSDeviceType_DEVICE_LABSTATION
+	} else if strings.Contains(device.GoogleCodeName, "Servo") ||
+		macAddress.MatchString(asset.GetId()) {
+		device.DeviceType = ufspb.ChromeOSDeviceType_DEVICE_SERVO
+	} else {
+		// TODO(anushruth): Default cannot be chromebook, But currently
+		// we don't have data to determine this.
+		device.DeviceType = ufspb.ChromeOSDeviceType_DEVICE_CHROMEBOOK
+	}
+	machine := &ufspb.Machine{
+		Name:         asset.GetId(),
+		SerialNumber: assetInfo.GetSerialNumber(),
+		Location: &ufspb.Location{
+			Aisle:       asset.GetLocation().GetAisle(),
+			Row:         asset.GetLocation().GetRow(),
+			Rack:        asset.GetLocation().GetRack(),
+			Shelf:       asset.GetLocation().GetShelf(),
+			Position:    asset.GetLocation().GetPosition(),
+			BarcodeName: hostname,
+		},
+		Device: &ufspb.Machine_ChromeosMachine{
+			ChromeosMachine: device,
+		},
+		UpdateTime: ptypes.TimestampNow(),
+	}
+	if hostname == "" {
+		machine.Location.BarcodeName = asset.GetLocation().GetLab()
+	}
+	// Mapping: chromeos1 => Santiam; chromeos2 => Atlantis;
+	// chromeos4 => Destiny; chromeos6 => Prometheus;
+	// chromeos{3,5,7,9,15} => Linda Vista
+	switch asset.GetLocation().GetLab() {
+	case "chromeos1":
+		machine.Location.Lab = ufspb.Lab_LAB_CHROMEOS_SANTIAM
+	case "chromeos2":
+		machine.Location.Lab = ufspb.Lab_LAB_CHROMEOS_ATLANTIS
+	case "chromeos3":
+		machine.Location.Lab = ufspb.Lab_LAB_CHROMEOS_LINDAVISTA
+	case "chromeos4":
+		machine.Location.Lab = ufspb.Lab_LAB_CHROMEOS_DESTINY
+	case "chromeos5":
+		machine.Location.Lab = ufspb.Lab_LAB_CHROMEOS_LINDAVISTA
+	case "chromeos6":
+		machine.Location.Lab = ufspb.Lab_LAB_CHROMEOS_PROMETHEUS
+	case "chromeos7":
+		machine.Location.Lab = ufspb.Lab_LAB_CHROMEOS_LINDAVISTA
+	case "chromeos9":
+		machine.Location.Lab = ufspb.Lab_LAB_CHROMEOS_LINDAVISTA
+	case "chromeos15":
+		machine.Location.Lab = ufspb.Lab_LAB_CHROMEOS_LINDAVISTA
+	default:
+		machine.Location.Lab = ufspb.Lab_LAB_UNSPECIFIED
+	}
+
+	return machine
 }
