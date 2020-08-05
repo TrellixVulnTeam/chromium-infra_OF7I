a12c2a3f1fd0cc0a2858df466578cc2e5fbb038f
Prathmesh Prabhu <pprabhu@chromium.org>
2020-08-05 13:40:22 +0000

    cros_test_platform: Store testTaskSet in a map
    This is preparation for storing other data related to a test in the
    RequestTaskSet
    
    Bug: 1112514
    Change-Id: I8b43480e80ff48902e57bbf77368c2d9335a3abe
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/2335025
    Commit-Queue: Prathmesh Prabhu <pprabhu@chromium.org>
    Reviewed-by: Jared Loucks <jaredloucks@google.com>
    Reviewed-by: Sean Abraham <seanabraham@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#33916}

--- go/src/infra/cmd/cros_test_platform/internal/execution/request_task_set.go
+++ go/src/infra/cmd/cros_test_platform/internal/execution/request_task_set.go
@@ -6,6 +6,7 @@ package execution
 
 import (
 	"context"
+	"fmt"
 	"infra/cmd/cros_test_platform/internal/execution/skylab"
 	"time"
 
@@ -19,7 +20,11 @@ import (
 // RequestTaskSet encapsulates the running state of the set of tasks for one
 // cros_test_platform request.
 type RequestTaskSet struct {
-	testTaskSets     []*testTaskSet
+	// Unique names for tests preserving the order of incoming arguments.
+	// This is used to preserve the order in the response.
+	testKeys []string
+
+	testTaskSets     map[string]*testTaskSet
 	globalMaxRetries int32
 	retries          int32
 }
@@ -33,18 +38,27 @@ type TaskSetConfig struct {
 
 // NewRequestTaskSet creates a new RequestTaskSet.
 func NewRequestTaskSet(tests []*steps.EnumerationResponse_AutotestInvocation, params *test_platform.Request_Params, workerConfig *config.Config_SkylabWorker, tc *TaskSetConfig) (*RequestTaskSet, error) {
-	testTaskSets := make([]*testTaskSet, len(tests))
+	testKeys := make([]string, len(tests))
+	testTaskSets := make(map[string]*testTaskSet)
 	for i, test := range tests {
+		k := testKey(i, test)
+		testKeys[i] = k
 		t, err := newTestTaskSet(test, params, workerConfig, tc)
 		if err != nil {
 			return nil, errors.Annotate(err, "new task set").Err()
 		}
-		testTaskSets[i] = t
+		testTaskSets[k] = t
 	}
 	return &RequestTaskSet{
+		testKeys:         testKeys,
 		testTaskSets:     testTaskSets,
 		globalMaxRetries: inferGlobalMaxRetries(params),
 	}, nil
+}
+
+// testName returns a unique string used as a key for a test.
+func testKey(i int, test *steps.EnumerationResponse_AutotestInvocation) string {
+	return fmt.Sprintf("%d_%s", i, test.GetTest().GetName())
 }
 
 // Completed returns true if all tasks for this request have completed.
@@ -66,42 +80,51 @@ func inferGlobalMaxRetries(params *test_platform.Request_Params) int32 {
 
 // LaunchTasks launches initial tasks for all the tests in this request.
 func (r *RequestTaskSet) LaunchTasks(ctx context.Context, c skylab.Client) error {
-	for _, testTaskSet := range r.testTaskSets {
-		if err := testTaskSet.LaunchTask(ctx, c); err != nil {
+	for _, key := range r.testKeys {
+		if err := r.getTestTaskSet(key).LaunchTask(ctx, c); err != nil {
 			return err
 		}
 	}
 	return nil
 }
 
+func (r *RequestTaskSet) getTestTaskSet(key string) *testTaskSet {
+	ts, ok := r.testTaskSets[key]
+	if !ok {
+		panic(fmt.Sprintf("No test task set for key %s", key))
+	}
+	return ts
+}
+
 // CheckTasksAndRetry checks the status of currently running tasks for this
 // request and retries failed tasks when allowed.
 func (r *RequestTaskSet) CheckTasksAndRetry(ctx context.Context, c skylab.Client) error {
-	for _, testTaskSet := range r.testTaskSets {
-		if testTaskSet.Completed() {
+	for _, key := range r.testKeys {
+		ts := r.getTestTaskSet(key)
+		if ts.Completed() {
 			continue
 		}
 
-		latestTask := testTaskSet.GetLatestTask()
+		latestTask := ts.GetLatestTask()
 		rerr := latestTask.Refresh(ctx, c)
 		tr := latestTask.Result()
 		if rerr != nil {
 			return errors.Annotate(rerr, "tick for task %s", tr.LogUrl).Err()
 		}
 
-		if !testTaskSet.Completed() {
+		if !ts.Completed() {
 			continue
 		}
 
-		logging.Infof(ctx, "Task %s (%s) completed with verdict %s", tr.LogUrl, testTaskSet.Name, tr.GetState().GetVerdict())
+		logging.Infof(ctx, "Task %s (%s) completed with verdict %s", tr.LogUrl, ts.Name, tr.GetState().GetVerdict())
 
-		shouldRetry, err := r.shouldRetry(ctx, testTaskSet)
+		shouldRetry, err := r.shouldRetry(ctx, ts)
 		if err != nil {
 			return errors.Annotate(err, "tick for task %s", tr.LogUrl).Err()
 		}
 		if shouldRetry {
-			logging.Infof(ctx, "Retrying %s", testTaskSet.Name)
-			if err := testTaskSet.LaunchTask(ctx, c); err != nil {
+			logging.Infof(ctx, "Retrying %s", ts.Name)
+			if err := ts.LaunchTask(ctx, c); err != nil {
 				return errors.Annotate(err, "tick for task %s: retry test", tr.LogUrl).Err()
 			}
 			r.retries++
@@ -197,10 +220,10 @@ func successfulVerdict(v test_platform.TaskState_Verdict) bool {
 }
 
 func (r *RequestTaskSet) results() []*steps.ExecuteResponse_ConsolidatedResult {
-	rs := make([]*steps.ExecuteResponse_ConsolidatedResult, len(r.testTaskSets))
-	for i, test := range r.testTaskSets {
+	rs := make([]*steps.ExecuteResponse_ConsolidatedResult, len(r.testKeys))
+	for i, key := range r.testKeys {
 		rs[i] = &steps.ExecuteResponse_ConsolidatedResult{
-			Attempts: test.TaskResult(),
+			Attempts: r.getTestTaskSet(key).TaskResult(),
 		}
 	}
 	return rs
