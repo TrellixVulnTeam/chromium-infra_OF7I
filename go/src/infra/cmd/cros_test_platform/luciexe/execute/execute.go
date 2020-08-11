fdb6f05d850744a412ea4243a9a60cb7d91bcb83
Prathmesh Prabhu <pprabhu@chromium.org>
2020-08-11 03:07:51 +0000

    cros_test_platform: Add a luciexe binary target
    Bug: 1112514
    Change-Id: Ia0d92f28f7ffdd631c06fabe51b442124e02d8d6
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/2344069
    Reviewed-by: Sean Abraham <seanabraham@chromium.org>
    Commit-Queue: Prathmesh Prabhu <pprabhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#34096}

---
+++ go/src/infra/cmd/cros_test_platform/luciexe/execute/execute.go
@@ -0,0 +1,270 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package execute houses the top-level logic for the execute step.
+package execute
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	"github.com/golang/protobuf/proto"
+	"github.com/golang/protobuf/ptypes"
+
+	"go.chromium.org/chromiumos/infra/proto/go/test_platform"
+	"go.chromium.org/chromiumos/infra/proto/go/test_platform/config"
+	"go.chromium.org/chromiumos/infra/proto/go/test_platform/steps"
+	bbpb "go.chromium.org/luci/buildbucket/proto"
+	"go.chromium.org/luci/common/errors"
+	"go.chromium.org/luci/common/logging"
+	"go.chromium.org/luci/common/proto/google"
+
+	"infra/cmd/cros_test_platform/internal/execution"
+	"infra/cmd/cros_test_platform/internal/execution/bb"
+	"infra/cmd/cros_test_platform/internal/execution/skylab"
+	"infra/cmd/cros_test_platform/internal/execution/swarming"
+	"infra/cmd/cros_test_platform/luciexe/common"
+)
+
+// Args contains all the arguments necessary to Run() an execute step.
+type Args struct {
+	InputPath      string
+	OutputPath     string
+	SwarmingTaskID string
+}
+
+// Run is the entry point for an execute step.
+func Run(ctx context.Context, args Args) error {
+	var request steps.ExecuteRequests
+	if err := common.ReadRequest(args.InputPath, &request); err != nil {
+		return err
+	}
+
+	if err := validateRequests(request.TaggedRequests); err != nil {
+		return err
+	}
+
+	cfg := extractOneConfig(request.TaggedRequests)
+	skylab, err := newSkylabClient(ctx, cfg, request.TaggedRequests)
+	if err != nil {
+		return err
+	}
+
+	deadline, err := inferDeadline(&request)
+	if err != nil {
+		return err
+	}
+
+	ea := execution.Args{
+		// TODO: Plumb through actual build input and send function.
+		Build: &bbpb.Build{},
+		Send:  func() {},
+
+		Request:      request,
+		WorkerConfig: cfg.SkylabWorker,
+		ParentTaskID: args.SwarmingTaskID,
+		Deadline:     deadline,
+	}
+	var resps map[string]*steps.ExecuteResponse
+	tErr, err := runWithDeadline(
+		ctx,
+		func(ctx context.Context) error {
+			var err error
+			// Captured: resps
+			resps, err = execution.Run(ctx, skylab, ea)
+			return err
+		},
+		deadline,
+	)
+	if err != nil {
+		return err
+	}
+	if tErr != nil {
+		// Timeout while waiting for tasks is not considered an Test Platform
+		// infrastructure error because root cause is mostly related to fleet
+		// capacity or long test runtimes.
+		logging.Warningf(ctx, "Exited wait dut to timeout: %s", tErr)
+		logging.Warningf(ctx, "Execution responses will contain test failures as a consequence of the timeout.")
+	}
+
+	updateWithEnumerationErrors(ctx, resps, request.TaggedRequests)
+	return common.WriteResponse(
+		args.OutputPath,
+		&steps.ExecuteResponses{
+			TaggedResponses: resps,
+		},
+	)
+}
+
+func extractOneConfig(trs map[string]*steps.ExecuteRequest) *config.Config {
+	for _, r := range trs {
+		return r.Config
+	}
+	return nil
+}
+
+func newSkylabClient(ctx context.Context, cfg *config.Config, trs map[string]*steps.ExecuteRequest) (skylab.Client, error) {
+	if useTestRunner(trs) {
+		return bb.NewSkylabClient(ctx, cfg)
+	}
+	return swarming.NewSkylabClient(ctx, cfg)
+}
+
+func useTestRunner(trs map[string]*steps.ExecuteRequest) bool {
+	for _, r := range trs {
+		return r.GetRequestParams().GetMigrations().GetUseTestRunner()
+	}
+	return false
+}
+
+func inferDeadline(r *steps.ExecuteRequests) (time.Time, error) {
+	c := google.TimeFromProto(r.GetBuild().GetCreateTime())
+	if c.IsZero() {
+		return c, errors.Reason("infer deadline: build creation time not known").Err()
+	}
+	return c.Add(inferTimeout(r.TaggedRequests)), nil
+}
+
+const defaultTaskTimout = 12 * time.Hour
+
+func inferTimeout(trs map[string]*steps.ExecuteRequest) time.Duration {
+	for _, r := range trs {
+		if maxDuration, err := ptypes.Duration(r.RequestParams.Time.MaximumDuration); err == nil {
+			return maxDuration
+		}
+		return defaultTaskTimout
+	}
+	return defaultTaskTimout
+}
+
+func validateRequests(trs map[string]*steps.ExecuteRequest) error {
+	if len(trs) == 0 {
+		return errors.Reason("zero requests").Err()
+	}
+
+	for t, r := range trs {
+		if err := validateRequest(r); err != nil {
+			return errors.Annotate(err, "validate request %s", t).Err()
+		}
+	}
+
+	var sTag string
+	var sReq *steps.ExecuteRequest
+	for t, r := range trs {
+		sTag = t
+		sReq = r
+		break
+	}
+
+	sCfg := sReq.Config
+	if err := validateRequestConfig(sCfg); err != nil {
+		return errors.Annotate(err, "validate request %s", sTag).Err()
+	}
+	for t, r := range trs {
+		o := r.Config
+		if !proto.Equal(sCfg, o) {
+			return errors.Reason("validate request: mistmatched config: %s[%#v] vs %s[%#v]", sTag, sCfg, t, o).Err()
+		}
+	}
+
+	sTimeout := sReq.RequestParams.Time.MaximumDuration
+	for t, r := range trs {
+		o := r.RequestParams.Time.MaximumDuration
+		if !proto.Equal(sTimeout, o) {
+			return errors.Reason("validate request: per-request timeout support unimplemented: %s[%s] vs %s[%s]", sTag, sTimeout, t, o).Err()
+		}
+	}
+
+	sUseTestRunner := sReq.RequestParams.GetMigrations().GetUseTestRunner()
+	for t, r := range trs {
+		o := r.RequestParams.GetMigrations().GetUseTestRunner()
+		if sUseTestRunner != o {
+			return errors.Reason("validate request: mismatched use_test_runner: %s[%v] vs %s[%v]", sTag, sUseTestRunner, t, o).Err()
+		}
+	}
+	return nil
+}
+
+func validateRequest(request *steps.ExecuteRequest) error {
+	if request == nil {
+		return fmt.Errorf("nil request")
+	}
+	if request.Config == nil {
+		return fmt.Errorf("nil request.config")
+	}
+	return nil
+}
+
+func validateRequestConfig(cfg *config.Config) error {
+	if cfg.SkylabSwarming == nil {
+		return fmt.Errorf("nil request.config.skylab_swarming")
+	}
+	if cfg.SkylabIsolate == nil {
+		return fmt.Errorf("nil request.config.skylab_isolate")
+	}
+	if cfg.SkylabWorker == nil {
+		return fmt.Errorf("nil request.config.skylab_worker")
+	}
+	return nil
+}
+
+func updateWithEnumerationErrors(ctx context.Context, resps map[string]*steps.ExecuteResponse, reqs map[string]*steps.ExecuteRequest) {
+	for t, resp := range resps {
+		req, ok := reqs[t]
+		if !ok {
+			panic(fmt.Sprintf("request for non-existent request for %s", t))
+		}
+		if es := req.GetEnumeration().GetErrorSummary(); es != "" {
+			if resp.State == nil {
+				resp.State = &test_platform.TaskState{}
+			}
+			resp.State.Verdict = test_platform.TaskState_VERDICT_FAILED
+			logging.Infof(ctx, "Set request %s to VERDICT_FAILED because of enumeration error: %s", t, es)
+		}
+	}
+}
+
+// runWithDeadline runs f() with the given deadline.
+//
+// In case of a highest level timeout, the error is returned as timeoutError.
+// All other errors are returned as fErr.
+func runWithDeadline(ctx context.Context, f func(context.Context) error, deadline time.Time) (timeoutError error, fErr error) {
+	ctx, cancel := context.WithDeadline(ctx, deadline)
+	defer cancel()
+
+	err := f(ctx)
+	if isGlobalTimeoutError(ctx, err) {
+		return errors.Annotate(err, "hit cros_test_platform request deadline (%s)", deadline).Err(), nil
+	}
+	return nil, err
+}
+
+// isGlobalTimeoutError returns true iff the error is consistent with being due
+// to hitting the high level deadline.
+//
+// If the error contains no DeadlineExceeded then the cause is not a timeout.
+// If the error contains DeadlineExceeded and the deadline has not been
+// reached then it is due to a timeout lower in the stack.
+func isGlobalTimeoutError(ctx context.Context, err error) bool {
+	d, ok := ctx.Deadline()
+	return ok && time.Now().After(d) && isDeadlineExceededError(err)
+}
+
+func isDeadlineExceededError(err error) bool {
+	// The original error raised is context.DeadlineExceeded but the prpc client
+	// library may transmute that into its own error type.
+	return errors.Any(err, func(err error) bool {
+		if err == context.DeadlineExceeded {
+			return true
+		}
+		if s, ok := status.FromError(err); ok {
+			return s.Code() == codes.DeadlineExceeded
+		}
+		return false
+	})
+}
---
+++ go/src/infra/cmd/cros_test_platform/luciexe/main.go
@@ -0,0 +1,106 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Command luciexe implements the cros_test_platform build logic.
+//
+// This is a luciexe binary as defined at
+// https://godoc.org/go.chromium.org/luci/luciexe
+//
+// This binary is intended to slowly replace all the sub-commands of the
+// cros_test_platform binary, eventually replacing the cros_test_platform
+// recipe and Go binary.
+package main
+
+import (
+	"compress/zlib"
+	"context"
+	"flag"
+	"fmt"
+	"infra/cmd/cros_test_platform/luciexe/execute"
+	"os"
+
+	"go.chromium.org/luci/common/errors"
+	"go.chromium.org/luci/common/logging"
+	"go.chromium.org/luci/luciexe/exe"
+
+	bbpb "go.chromium.org/luci/buildbucket/proto"
+)
+
+func main() {
+	exe.Run(luciEXEMain, exe.WithZlibCompression(zlib.BestCompression))
+}
+
+// The exe.MainFn entry point for this luciexe binary.
+//
+// At the moment, this binary replaces only the skylab-execute step
+// of cros_test_platform.
+//
+// All errors in skylab-execute step are interpreted as infrastructure
+// failures by the recipe.
+// Test failures and user errors are reported in other recipe steps.
+//
+// Thus,
+// [1] All errors from this binary are tagged as INFRA_FAILURE
+// [2] Non-INFRA_FAILUREs are suppressed (before they get to this top-level
+//     function)
+func luciEXEMain(ctx context.Context, input *bbpb.Build, userArgs []string, send exe.BuildSender) error {
+	ca, err := parseArgs(userArgs)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		flag.PrintDefaults()
+		return err
+	}
+
+	ctx = setupLogging(ctx)
+	if err := execute.Run(ctx, execute.Args{
+		InputPath:      ca.InputPath,
+		OutputPath:     ca.OutputPath,
+		SwarmingTaskID: os.Getenv("SWARMING_TASK_ID"),
+	}); err != nil {
+		logApplicationError(ctx, err)
+	}
+	return err
+}
+
+func setupLogging(ctx context.Context) context.Context {
+	return logging.SetLevel(ctx, logging.Info)
+}
+
+// logApplicationError logs the error returned to the entry function of an
+// application.
+func logApplicationError(ctx context.Context, err error) {
+	errors.Log(ctx, err)
+	// Also log to error stream, since logs are directed at the main output
+	// stream.
+	fmt.Fprintf(os.Stderr, "%s\n", err)
+}
+
+type cliArgs struct {
+	InputPath  string
+	OutputPath string
+}
+
+func parseArgs(args []string) (cliArgs, error) {
+	ca := cliArgs{}
+
+	fs := flag.NewFlagSet("cros_test_platform", flag.ContinueOnError)
+	fs.StringVar(&ca.InputPath, "input_json", "", "Path to JSON ExecuteRequests to read.")
+	fs.StringVar(&ca.OutputPath, "output_json", "", "Path to JSON ExecuteResponses to write.")
+	if err := fs.Parse(args); err != nil {
+		return ca, err
+	}
+
+	errs := errors.NewMultiError()
+	if ca.InputPath == "" {
+		errs = append(errs, errors.Reason("-input_json not specified").Err())
+	}
+	if ca.OutputPath == "" {
+		errs = append(errs, errors.Reason("-output_json not specified").Err())
+	}
+
+	if errs.First() != nil {
+		return ca, errs
+	}
+	return ca, nil
+}
