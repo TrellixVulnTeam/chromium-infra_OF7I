dd05d90d67f55887c69a484a71837eb31f24ff2d
Chan Li <chanli@chromium.org>
2020-08-21 18:41:00 +0000

    [result_adapter] Add result_adapter skeleton
    Bug: 1108016
    Change-Id: Id2fc0fc2501cedb6aa0a3ad055477247cadd0267
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/2363926
    Commit-Queue: Chan Li <chanli@chromium.org>
    Reviewed-by: Scott Lee <ddoman@chromium.org>
    Reviewed-by: Nodir Turakulov <nodir@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#34462}

---
+++ go/src/infra/tools/result_adapter/main.go
@@ -0,0 +1,42 @@
+// Copyright 2020 The LUCI Authors. All rights reserved.
+// Use of this source code is governed under the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
+package main
+
+import (
+	"context"
+	"os"
+
+	"github.com/maruel/subcommands"
+
+	"go.chromium.org/luci/common/cli"
+	"go.chromium.org/luci/common/data/rand/mathrand"
+	"go.chromium.org/luci/common/flag/fixflagpos"
+	"go.chromium.org/luci/common/logging/gologger"
+)
+
+func main() {
+	mathrand.SeedRandomly()
+
+	application := cli.Application{
+		Name:  "result_adapter",
+		Title: "A CLI tool to convert test results to ResultDB native format then upload them.",
+		Context: func(ctx context.Context) context.Context {
+			logCfg := gologger.LoggerConfig{
+				Format: `%{message}`,
+				Out:    os.Stderr,
+			}
+			return logCfg.Use(ctx)
+		},
+		Commands: []*subcommands.Command{
+			// TODO(crbug.com/1108016): add subcommands.
+			// cmdGtest(),
+			// cmdJson(),
+
+			{}, // a separator
+			subcommands.CmdHelp,
+		},
+	}
+	os.Exit(subcommands.Run(&application, fixflagpos.FixSubcommands(os.Args[1:])))
+}
