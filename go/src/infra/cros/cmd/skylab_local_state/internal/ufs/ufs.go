fdda21aaac54dd32c9b80a23aea84f091ab89398
Otabek Kasimov <otabek@google.com>
2020-08-25 22:10:02 +0000

    sls: Save DUT state to UFS.
    BUG=chromium:1064077
    TEST=run local, build, presubmit
    go run main.go save -input_json test.json
    
    Change-Id: I802c22f34786e1fa03f114441f8c9f498719d3be
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/2373371
    Reviewed-by: Jared Loucks <jaredloucks@google.com>
    Reviewed-by: Alex Zamorzaev <zamorzaev@chromium.org>
    Commit-Queue: Otabek Kasimov <otabek@google.com>
    Cr-Commit-Position: refs/heads/master@{#34525}

---
+++ go/src/infra/cros/cmd/skylab_local_state/internal/ufs/ufs.go
@@ -0,0 +1,52 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package ufs provides option t build connection to UFS service.
+package ufs
+
+import (
+	"context"
+
+	"go.chromium.org/luci/auth"
+	"go.chromium.org/luci/auth/client/authcli"
+	"go.chromium.org/luci/common/errors"
+	"go.chromium.org/luci/grpc/prpc"
+	"go.chromium.org/luci/lucictx"
+
+	ufsAPI "infra/unifiedfleet/api/v1/rpc"
+)
+
+// prpcOptions is used for UFS PRPC clients.
+var prpcOptions = &prpc.Options{
+	UserAgent: "skylab_local_state/3.0.0",
+}
+
+// NewClient initialize and return new client to work with UFS service.
+func NewClient(ctx context.Context, ufsService string, authFlags *authcli.Flags) (ufsAPI.FleetClient, error) {
+	if ufsService == "" {
+		return nil, errors.Reason("UFS service path not provided.").Err()
+	}
+	authOpts, err := authFlags.Options()
+	if err != nil {
+		return nil, errors.Annotate(err, "create UFS client").Err()
+	}
+	authCtx, err := lucictx.SwitchLocalAccount(ctx, "system")
+	if err == nil {
+		// If there's a system account use it (the case of running on Swarming).
+		// Otherwise default to user credentials (the local development case).
+		ctx = authCtx
+		authOpts.Method = auth.LUCIContextMethod
+	}
+	a := auth.NewAuthenticator(ctx, auth.SilentLogin, authOpts)
+	httpClient, err := a.Client()
+	if err != nil {
+		return nil, errors.Annotate(err, "create UFS client").Err()
+	}
+	ufsClient := ufsAPI.NewFleetPRPCClient(&prpc.Client{
+		C:       httpClient,
+		Host:    ufsService,
+		Options: prpcOptions,
+	})
+	return ufsClient, nil
+}
