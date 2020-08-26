d06bc0a164917a49603f060a68abd9bfc66605f7
Quang Minh Tuan Nguyen <nqmtuan@google.com>
2020-08-26 23:16:53 +0000

    Fix feedback URL in other places
    Bug: 1095694
    Change-Id: Idaa43ce38e28b54263d09ec346073c2708ea3c17
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/2376149
    Reviewed-by: Tiffany Zhang <zhangtiff@chromium.org>
    Commit-Queue: Quang Minh Tuan Nguyen <nqmtuan@google.com>
    Cr-Commit-Position: refs/heads/master@{#34577}

--- appengine/ui_catalogue/src/cuic-main.js
+++ appengine/ui_catalogue/src/cuic-main.js
@@ -82,6 +82,6 @@ window.customElements.define(Main.is, Main);
 (function(i,s,o,g,r,a,m){i['CrDXObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},a=s.createElement(o),
     m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://storage.googleapis.com/crdx-feedback.appspot.com/feedback.js','crdx');
+})(window,document,'script','https://storage.googleapis.com/chops-feedback/feedback.js','crdx');
 
 crdx('setFeedbackButtonLink', 'https://bugs.chromium.org/p/chromium/issues/entry?labels=Infra-DX&components=Infra>UICatalogue');
