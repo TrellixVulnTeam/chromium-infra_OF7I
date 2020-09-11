796b7eec531d7d619cfda52969556b619906a386
Sean Abraham <seanabraham@chromium.org>
2020-09-11 23:02:27 +0000

    prototype-rts: create the skeleton of a Remote Test Server
    I basically lifted prototype-tlw, subcommandized it, and made it use the
    ProgressSinkService. More to come :D.
    
    Bug: 1127071
    Change-Id: I0b67b44c22ef111862e31b60a80bc87514e43596
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/2406387
    Commit-Queue: Sean Abraham <seanabraham@chromium.org>
    Reviewed-by: Prathmesh Prabhu <pprabhu@google.com>
    Cr-Commit-Position: refs/heads/master@{#35003}

---
+++ go/src/infra/cros/cmd/prototype-rts/main.go
@@ -0,0 +1,34 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Application prototype-rts is a local demo Remote Test Server.
+package main
+
+import (
+	"context"
+	"os"
+
+	"github.com/maruel/subcommands"
+	"go.chromium.org/luci/common/cli"
+	"go.chromium.org/luci/common/logging/gologger"
+	"infra/cros/cmd/prototype-rts/internal/cmd"
+)
+
+func getApplication() *cli.Application {
+	return &cli.Application{
+		Name:  "prototype-rts",
+		Title: "A prototype Remote Test Server.",
+		Context: func(ctx context.Context) context.Context {
+			return gologger.StdConfig.Use(ctx)
+		},
+		Commands: []*subcommands.Command{
+			subcommands.CmdHelp,
+			cmd.StartProgressSink(),
+		},
+	}
+}
+
+func main() {
+	os.Exit(subcommands.Run(getApplication(), nil))
+}
