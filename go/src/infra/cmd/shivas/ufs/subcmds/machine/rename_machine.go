b38216e6b95a2b0f098fb7580168d83ecdf49228
Eshwar Narayan <eshwarn@google.com>
2020-10-08 23:47:09 +0000

    shivas: add namespace parameter for rename command
    BUG=1123991
    Test=make dev;make test;run shivas commands;
    
    Change-Id: I46a4a25c4dd63fbd720040c061cb3efb8ab7fe05
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/2461278
    Reviewed-by: Xixuan Wu <xixuan@chromium.org>
    Commit-Queue: Eshwar Narayan <eshwarn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#35647}

--- go/src/infra/cmd/shivas/ufs/subcmds/machine/rename_machine.go
+++ go/src/infra/cmd/shivas/ufs/subcmds/machine/rename_machine.go
@@ -67,6 +67,11 @@ func (c *renameMachine) innerRun(a subcommands.Application, args []string, env s
 		return err
 	}
 	ctx := cli.GetContext(a, c, env)
+	ns, err := c.envFlags.Namespace()
+	if err != nil {
+		return err
+	}
+	ctx = utils.SetupContext(ctx, ns)
 	hc, err := cmdlib.NewHTTPClient(ctx, &c.authFlags)
 	if err != nil {
 		return err
