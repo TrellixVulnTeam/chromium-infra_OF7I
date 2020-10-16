e1bf04f78bbdc5fdf32f3e27adb3f04c488478d5
Andrii Shyshkalov <tandrii@google.com>
2020-10-16 19:24:21 +0000

    cv: update migration API Python protos.
    R=qyearsley, yiwzhang
    
    Bug: 1139324
    Change-Id: Ic9511b46634f72c009b49182061a13a800313027
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/2481422
    Commit-Queue: Andrii Shyshkalov <tandrii@google.com>
    Commit-Queue: Yiwei Zhang <yiwzhang@google.com>
    Auto-Submit: Andrii Shyshkalov <tandrii@google.com>
    Reviewed-by: Yiwei Zhang <yiwzhang@google.com>
    Cr-Commit-Position: refs/heads/master@{#35805}

--- python_pb2/go/chromium/org/luci/cv/api/migration/migration_pb2.py
+++ python_pb2/go/chromium/org/luci/cv/api/migration/migration_pb2.py
@@ -21,7 +21,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   syntax='proto3',
   serialized_options=b'Z1go.chromium.org/luci/cv/api/migration;migrationpb',
   create_key=_descriptor._internal_create_key,
-  serialized_pb=b'\n5go.chromium.org/luci/cv/api/migration/migration.proto\x12\tmigration\x1a\x1bgoogle/protobuf/empty.proto\x1a\x35go.chromium.org/luci/cv/api/bigquery/v1/attempt.proto\"9\n\x11ReportRunsRequest\x12$\n\x04runs\x18\x01 \x03(\x0b\x32\x16.migration.ReportedRun\"1\n\x0bReportedRun\x12\"\n\x07\x61ttempt\x18\x01 \x01(\x0b\x32\x11.bigquery.Attempt2O\n\tMigration\x12\x42\n\nReportRuns\x12\x1c.migration.ReportRunsRequest\x1a\x16.google.protobuf.EmptyB3Z1go.chromium.org/luci/cv/api/migration;migrationpbb\x06proto3'
+  serialized_pb=b'\n5go.chromium.org/luci/cv/api/migration/migration.proto\x12\tmigration\x1a\x1bgoogle/protobuf/empty.proto\x1a\x35go.chromium.org/luci/cv/api/bigquery/v1/attempt.proto\"9\n\x11ReportRunsRequest\x12$\n\x04runs\x18\x01 \x03(\x0b\x32\x16.migration.ReportedRun\"1\n\x0bReportedRun\x12\"\n\x07\x61ttempt\x18\x01 \x01(\x0b\x32\x11.bigquery.Attempt\"?\n\x18ReportFinishedRunRequest\x12#\n\x03run\x18\x01 \x01(\x0b\x32\x16.migration.ReportedRun2\xa1\x01\n\tMigration\x12\x42\n\nReportRuns\x12\x1c.migration.ReportRunsRequest\x1a\x16.google.protobuf.Empty\x12P\n\x11ReportFinishedRun\x12#.migration.ReportFinishedRunRequest\x1a\x16.google.protobuf.EmptyB3Z1go.chromium.org/luci/cv/api/migration;migrationpbb\x06proto3'
   ,
   dependencies=[google_dot_protobuf_dot_empty__pb2.DESCRIPTOR,go_dot_chromium_dot_org_dot_luci_dot_cv_dot_api_dot_bigquery_dot_v1_dot_attempt__pb2.DESCRIPTOR,])
 
@@ -91,10 +91,44 @@ _REPORTEDRUN = _descriptor.Descriptor(
   serialized_end=260,
 )
 
+
+_REPORTFINISHEDRUNREQUEST = _descriptor.Descriptor(
+  name='ReportFinishedRunRequest',
+  full_name='migration.ReportFinishedRunRequest',
+  filename=None,
+  file=DESCRIPTOR,
+  containing_type=None,
+  create_key=_descriptor._internal_create_key,
+  fields=[
+    _descriptor.FieldDescriptor(
+      name='run', full_name='migration.ReportFinishedRunRequest.run', index=0,
+      number=1, type=11, cpp_type=10, label=1,
+      has_default_value=False, default_value=None,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+  ],
+  extensions=[
+  ],
+  nested_types=[],
+  enum_types=[
+  ],
+  serialized_options=None,
+  is_extendable=False,
+  syntax='proto3',
+  extension_ranges=[],
+  oneofs=[
+  ],
+  serialized_start=262,
+  serialized_end=325,
+)
+
 _REPORTRUNSREQUEST.fields_by_name['runs'].message_type = _REPORTEDRUN
 _REPORTEDRUN.fields_by_name['attempt'].message_type = go_dot_chromium_dot_org_dot_luci_dot_cv_dot_api_dot_bigquery_dot_v1_dot_attempt__pb2._ATTEMPT
+_REPORTFINISHEDRUNREQUEST.fields_by_name['run'].message_type = _REPORTEDRUN
 DESCRIPTOR.message_types_by_name['ReportRunsRequest'] = _REPORTRUNSREQUEST
 DESCRIPTOR.message_types_by_name['ReportedRun'] = _REPORTEDRUN
+DESCRIPTOR.message_types_by_name['ReportFinishedRunRequest'] = _REPORTFINISHEDRUNREQUEST
 _sym_db.RegisterFileDescriptor(DESCRIPTOR)
 
 ReportRunsRequest = _reflection.GeneratedProtocolMessageType('ReportRunsRequest', (_message.Message,), {
@@ -111,6 +145,13 @@ ReportedRun = _reflection.GeneratedProtocolMessageType('ReportedRun', (_message.
   })
 _sym_db.RegisterMessage(ReportedRun)
 
+ReportFinishedRunRequest = _reflection.GeneratedProtocolMessageType('ReportFinishedRunRequest', (_message.Message,), {
+  'DESCRIPTOR' : _REPORTFINISHEDRUNREQUEST,
+  '__module__' : 'go.chromium.org.luci.cv.api.migration.migration_pb2'
+  # @@protoc_insertion_point(class_scope:migration.ReportFinishedRunRequest)
+  })
+_sym_db.RegisterMessage(ReportFinishedRunRequest)
+
 
 DESCRIPTOR._options = None
 
@@ -121,8 +162,8 @@ _MIGRATION = _descriptor.ServiceDescriptor(
   index=0,
   serialized_options=None,
   create_key=_descriptor._internal_create_key,
-  serialized_start=262,
-  serialized_end=341,
+  serialized_start=328,
+  serialized_end=489,
   methods=[
   _descriptor.MethodDescriptor(
     name='ReportRuns',
@@ -130,6 +171,16 @@ _MIGRATION = _descriptor.ServiceDescriptor(
     index=0,
     containing_service=None,
     input_type=_REPORTRUNSREQUEST,
+    output_type=google_dot_protobuf_dot_empty__pb2._EMPTY,
+    serialized_options=None,
+    create_key=_descriptor._internal_create_key,
+  ),
+  _descriptor.MethodDescriptor(
+    name='ReportFinishedRun',
+    full_name='migration.Migration.ReportFinishedRun',
+    index=1,
+    containing_service=None,
+    input_type=_REPORTFINISHEDRUNREQUEST,
     output_type=google_dot_protobuf_dot_empty__pb2._EMPTY,
     serialized_options=None,
     create_key=_descriptor._internal_create_key,
