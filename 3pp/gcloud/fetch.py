9041d2cb05f2fe8adf86f90579b5269e094e921e
Akash Mukherjee <akashmukherjee@google.com>
2020-11-06 03:18:45 +0000

    [3pp] First candidate for script migration.
    With the newer checkout workflow where we don't allow custom fetch
    scripts to download artifacts into bots, instead spit download url to be
    used from within recipe.
    
    This CL introduces a new get_url args to the fetch script. The reason
    behind not changing do_checkout in-place is to avoid errors when
    traditional checkout workflow runs in prod.
    
    Added todos to remove the traditional, once new workflow is enabled by
    default.
    
    Bug: 1131685
    Change-Id: Ib0fe19c44fc9ed29ce0fd4b70040aa384da27897
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/2518148
    Reviewed-by: Brian Ryner <bryner@google.com>
    Reviewed-by: Yiwei Zhang <yiwzhang@google.com>
    Commit-Queue: Brian Ryner <bryner@google.com>
    Auto-Submit: Akash Mukherjee <akashmukherjee@google.com>
    Cr-Commit-Position: refs/heads/master@{#36227}

--- 3pp/gcloud/fetch.py
+++ 3pp/gcloud/fetch.py
@@ -17,6 +17,7 @@ def do_latest():
   print json.load(urllib.urlopen(BASE_URL + '/components-2.json'))['version']
 
 
+# TODO(akashmukherjee): Remove checkout once new workflow is enabled.
 def do_checkout(version, platform, checkout_path):
   targ_os, targ_arch = platform.split('-')
   ext = 'zip' if targ_os == 'windows' else 'tar.gz'
@@ -36,6 +37,27 @@ def do_checkout(version, platform, checkout_path):
                                                 'archive.' + ext))
 
 
+def get_download_url(version, platform):
+  targ_os, targ_arch = platform.split('-')
+  ext = '.zip' if targ_os == 'windows' else '.tar.gz'
+  download_url = (
+    BASE_URL + '/downloads/google-cloud-sdk-%(version)s-%(os)s-%(arch)s%(ext)s'
+    % {
+      'version': version,
+      'os': {'mac': 'darwin'}.get(targ_os, targ_os),
+      'arch': {
+        '386':   'x86',
+        'amd64': 'x86_64',
+      }[targ_arch],
+      'ext': ext
+    })
+  partial_manifest = {
+    'url': download_url,
+    'ext': ext,
+  }
+  print(json.dumps(partial_manifest))
+
+
 def main():
   ap = argparse.ArgumentParser()
   sub = ap.add_subparsers()
@@ -43,12 +65,20 @@ def main():
   latest = sub.add_parser("latest")
   latest.set_defaults(func=lambda _opts: do_latest())
 
+  # TODO(akashmukherjee): Remove checkout once new workflow is enabled.
   checkout = sub.add_parser("checkout")
   checkout.add_argument("checkout_path")
   checkout.set_defaults(
     func=lambda opts: do_checkout(
       os.environ['_3PP_VERSION'], os.environ['_3PP_PLATFORM'],
       opts.checkout_path))
+
+  download = sub.add_parser("get_url")
+  download.set_defaults(
+    func=lambda opts: get_download_url(
+      os.environ['_3PP_VERSION'], os.environ['_3PP_PLATFORM']
+    )
+  )
 
   opts = ap.parse_args()
   return opts.func(opts)
