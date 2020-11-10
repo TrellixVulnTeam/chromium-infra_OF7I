c34c7c20838154e9b3afc556833205c79ba032c7
Akash Mukherjee <akashmukherjee@google.com>
2020-11-10 16:04:10 +0000

    [3pp] Update git for script migration.
    With the newer checkout workflow where we don't allow custom fetch
    scripts to download artifacts into bots, instead spit download url to be
    used from within recipe.
    
    This CL introduces a new get_url args to the fetch script. The reason
    behind not changing do_checkout in-place is to avoid errors when
    traditional checkout workflow runs in prod.
    
    Added todos to remove the traditional, once new workflow is enabled by
    default.
    
    Bug: 1131685
    Change-Id: Ic328b01dbe14401af3812c5908c3c34a22d3ec6c
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/2520328
    Commit-Queue: Akash Mukherjee <akashmukherjee@google.com>
    Reviewed-by: Brian Ryner <bryner@google.com>
    Cr-Commit-Position: refs/heads/master@{#36283}

--- 3pp/git/fetch_win.py
+++ 3pp/git/fetch_win.py
@@ -26,6 +26,7 @@ def do_latest():
   )['tag_name'].lstrip('v')
 
 
+# TODO(akashmukherjee): Remove.
 def do_checkout(version, platform, checkout_path):
   arch = platform.split('-')[-1]
   download_url = None
@@ -49,6 +50,31 @@ def do_checkout(version, platform, checkout_path):
   urllib.urlretrieve(download_url, os.path.join(checkout_path, asset_name))
 
 
+def get_download_url(version, platform):
+  arch = platform.split('-')[-1]
+  download_url = None
+
+  target_tag = 'v%s' % (version,)
+  for release in json.load(
+      urllib.urlopen(
+          'https://api.github.com/repos/git-for-windows/git/releases')):
+    if str(release['tag_name']) == target_tag:
+      for asset in release['assets']:
+        asset_name = str(asset['name'])
+        if WINDOWS_ASSET_RE[arch].match(asset_name):
+          download_url = asset['browser_download_url']
+          break
+      break
+  if not download_url:
+    raise Exception('could not find download_url')
+
+  partial_manifest = {
+    'url': download_url,
+    'ext': '.exe',
+  }
+  print(json.dumps(partial_manifest))
+
+
 def main():
   ap = argparse.ArgumentParser()
   sub = ap.add_subparsers()
@@ -56,12 +82,18 @@ def main():
   latest = sub.add_parser("latest")
   latest.set_defaults(func=lambda _opts: do_latest())
 
+  # TODO(akashmukherjee): Remove.
   checkout = sub.add_parser("checkout")
   checkout.add_argument("checkout_path")
   checkout.set_defaults(
     func=lambda opts: do_checkout(
       os.environ['_3PP_VERSION'], os.environ['_3PP_PLATFORM'],
       opts.checkout_path))
+
+  download = sub.add_parser("get_url")
+  download.set_defaults(
+    func=lambda opts: get_download_url(
+      os.environ['_3PP_VERSION'], os.environ['_3PP_PLATFORM']))
 
   opts = ap.parse_args()
   return opts.func(opts)
