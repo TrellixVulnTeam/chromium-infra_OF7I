19b1f511ca3a19b014a25e4af8e7d53bca32cb1a
Akash Mukherjee <akashmukherjee@google.com>
2020-11-13 10:56:47 +0000

    [3pp] Fix pip_bootstrap install script.
    Pip is the only package so far in our 3pp land, where name of the remote
    artifact is partially important. Current configuration does not allow us
    to reflect the remote name into our source cache, where we sequentially
    name cloned artifacts with raw_source_{i} prefix.
    
    When pip_bootstrap is used as a dep/tool for any other package, a python
    script pip_bootstrap is run. This script looks for pip python wheel by
    looking for any file with `pip-*` wildcard.
    
    Two options for us to,
      * bake artifact name into source spec, which seemed like an overkill
        since this is not commonly used.
      * Modify the install/build script for pip_bootstrap to manipulate the
        name of the produced files there. This will eventually be propagated
        to all package that uses and relies on artifact name.
    
    Bug: 1131685
    Change-Id: I1d6bc9fdb8b321a3744523cebe58872f00074982
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/2537611
    Auto-Submit: Akash Mukherjee <akashmukherjee@google.com>
    Reviewed-by: Brian Ryner <bryner@google.com>
    Commit-Queue: Brian Ryner <bryner@google.com>
    Cr-Commit-Position: refs/heads/master@{#36408}

--- 3pp/pip_bootstrap/install.sh
+++ 3pp/pip_bootstrap/install.sh
@@ -11,5 +11,13 @@ PREFIX="$1"
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
+# Rename downloaded wheels to original filenames. `pip_bootstrap.py` installer
+# script looks for python wheel with `pip-*`, whenever pip_bootstrap is used
+# for another package (e.g. cpython3), cipd should return these wheel files
+# with formatted names.
+mv raw_source_0.whl pip-source.whl
+mv raw_source_1.whl setuptools-source.whl
+mv raw_source_2.whl wheel-source.whl
+
 # Copy all wheel files and bootstrap script
-cp -a * "$SCRIPT_DIR/pip_bootstrap.py" "$PREFIX"
+cp -a ./* "$SCRIPT_DIR/pip_bootstrap.py" "$PREFIX"
