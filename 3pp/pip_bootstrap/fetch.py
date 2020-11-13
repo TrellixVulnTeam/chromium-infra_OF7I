08fa6cb0e51dc75e25c60fbc2c6dbd96ed3d0419
Akash Mukherjee <akashmukherjee@google.com>
2020-11-13 03:30:09 +0000

    [3pp] Update pip for script migration.
    With the newer checkout workflow where we don't allow custom fetch
    scripts to download artifacts into bots, instead spit download url to be
    used from within recipe.
    
    This CL introduces a new get_url args to the fetch script. The reason
    behind not changing do_checkout in-place is to avoid errors when
    traditional checkout workflow runs in prod.
    
    Added todos to remove the traditional, once new workflow is enabled by
    default.
    
    R=bryner@google.com
    
    Bug: 1131685
    Change-Id: I792e3402734955b54847ea73658a7143144c97a6
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/2520700
    Auto-Submit: Akash Mukherjee <akashmukherjee@google.com>
    Reviewed-by: Brian Ryner <bryner@google.com>
    Commit-Queue: Brian Ryner <bryner@google.com>
    Cr-Commit-Position: refs/heads/master@{#36400}

--- 3pp/pip_bootstrap/fetch.py
+++ 3pp/pip_bootstrap/fetch.py
@@ -47,6 +47,7 @@ def do_latest():
                                         _get_version('wheel'))
 
 
+# TODO(akashmukherjee): Remove.
 def do_checkout(version, checkout_path):
   # split version pip<vers>.setuptools<vers>.wheel<vers>
   m = re.match(
@@ -63,6 +64,27 @@ def do_checkout(version, checkout_path):
     urllib.urlretrieve(url, os.path.join(checkout_path, name))
 
 
+def get_download_url(version):
+  # split version pip<vers>.setuptools<vers>.wheel<vers>
+  m = re.match(
+    r'^pip(.*)\.setuptools(.*)\.wheel(.*)$',
+    version)
+  versions = {
+    'pip': m.group(1),
+    'setuptools': m.group(2),
+    'wheel': m.group(3),
+  }
+  download_urls = []
+  for pkgname, vers in versions.iteritems():
+    url, _ = _get_wheel_url(pkgname, vers)
+    download_urls.append(url)
+  partial_manifest = {
+    'url': download_urls,
+    'ext': '.whl',
+  }
+  print(json.dumps(partial_manifest))
+
+
 def main():
   ap = argparse.ArgumentParser()
   sub = ap.add_subparsers()
@@ -70,11 +92,16 @@ def main():
   latest = sub.add_parser("latest")
   latest.set_defaults(func=lambda _opts: do_latest())
 
+  # TODO(akashmukherjee): Remove.
   checkout = sub.add_parser("checkout")
   checkout.add_argument("checkout_path")
   checkout.set_defaults(
     func=lambda opts: do_checkout(
       os.environ['_3PP_VERSION'], opts.checkout_path))
+
+  download = sub.add_parser("get_url")
+  download.set_defaults(
+    func=lambda opts: get_download_url(os.environ['_3PP_VERSION']))
 
   opts = ap.parse_args()
   return opts.func(opts)
