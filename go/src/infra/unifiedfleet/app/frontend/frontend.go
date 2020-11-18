d2973e34ad982976e9da7875707c14808aa5323d
Eshwar Narayan <eshwarn@google.com>
2020-11-18 20:32:32 +0000

    UFS: Add rpc permission for atl and acs labs mdb groups
    Bug: None
    Test: make test;make dev
    Change-Id: I10e896df3d39f1f079c49116047670e924dcd3f0
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/2519920
    Reviewed-by: Xixuan Wu <xixuan@chromium.org>
    Commit-Queue: Eshwar Narayan <eshwarn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#36543}

--- go/src/infra/unifiedfleet/app/frontend/frontend.go
+++ go/src/infra/unifiedfleet/app/frontend/frontend.go
@@ -49,7 +49,7 @@ func checkAccess(ctx context.Context, rpcName string, _ proto.Message) (context.
 		group = []string{"mdb/chrome-fleet-software-team"}
 	}
 	if strings.HasPrefix(rpcName, "List") || strings.HasPrefix(rpcName, "Get") || strings.HasPrefix(rpcName, "BatchGet") {
-		group = []string{"mdb/chrome-labs", "mdb/chrome-fleet-software-team", "machine-db-readers"}
+		group = []string{"mdb/chrome-labs", "mdb/chrome-fleet-software-team", "machine-db-readers", "mdb/chromeos-labs", "mdb/chromeos-labs-tvcs", "mdb/acs-labs"}
 	}
 	switch rpcName {
 	case "CreateMachineLSE", "UpdateMachineLSE", "CreateVM", "UpdateVM":
