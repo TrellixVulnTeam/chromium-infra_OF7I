9f3d9ea0b89c7f28c642f73ea81f0270468f7331
Jessan Hutchison-Quillian <jessan@google.com>
2020-12-02 22:48:58 +0000

    Fix google.protobuf import issue for chromium_bugs.
    Not 100% clear why this fix is necessary, as it seemed that the workaround in gae_ts_mon should be sufficient
    
    Bug: monorail:8731
    Change-Id: Ic9a3fa0b05d9961f1575d1060ea1fe9cc9a483c1
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/2568450
    Auto-Submit: Jessan Hutchison-Quillian <jessan@google.com>
    Reviewed-by: Kevin Weng <kweng@google.com>
    Commit-Queue: Kevin Weng <kweng@google.com>
    Cr-Commit-Position: refs/heads/master@{#36840}

--- appengine/chromium_bugs/appengine_config.py
+++ appengine/chromium_bugs/appengine_config.py
@@ -10,3 +10,13 @@ import sys
 
 # Enable third-party imports
 sys.path.append(os.path.join(os.path.dirname(__file__), 'third_party'))
+
+# Add the gae_ts_mon/protobuf directory into the path for the google package, so
+# "import google.protobuf" works.
+# For some reason the fix in gae_ts_mon.__init__ does not work for this app.
+# The differences here are the updated path below, and reload().
+protobuf_dir = os.path.join(os.path.dirname(__file__), 'gae_ts_mon', 'protobuf')
+import google
+google.__path__.append(os.path.join(protobuf_dir, 'google'))
+sys.path.insert(0, protobuf_dir)
+reload(google)
