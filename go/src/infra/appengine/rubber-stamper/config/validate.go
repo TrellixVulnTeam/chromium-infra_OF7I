334f974d46374e9639cd3544bc9f1332034f791a
Xinyu Yang <xinyuoffline@google.com>
2020-12-04 20:41:50 +0000

    [rubber-stamper] Validate configs.
    Bug: 1092603
    Change-Id: If770dfa2b3ce22580694ad917f3b56e234b81104
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/2570532
    Commit-Queue: Xinyu Yang <xinyuoffline@google.com>
    Reviewed-by: Yulan Lin <yulanlin@google.com>
    Reviewed-by: Jason Clinton <jclinton@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#36906}

--- go/src/infra/appengine/rubber-stamper/config/validate.go
+++ go/src/infra/appengine/rubber-stamper/config/validate.go
@@ -5,9 +5,45 @@
 package config
 
 import (
+	"path"
+
 	"go.chromium.org/luci/config/validation"
 )
 
 func validateConfig(c *validation.Context, cfg *Config) {
-	// TODO: validate configs
+	for key, hostConfig := range cfg.HostConfigs {
+		c.Enter("host_config %s", key)
+		validateHostConfig(c, hostConfig)
+		c.Exit()
+	}
+}
+
+func validateHostConfig(c *validation.Context, hostConfig *HostConfig) {
+	c.Enter("benign_file_pattern")
+	validateBenignFilePattern(c, hostConfig.BenignFilePattern)
+	c.Exit()
+}
+
+func validateBenignFilePattern(c *validation.Context, benignFilePattern *BenignFilePattern) {
+	m := benignFilePattern.FileExtensionMap
+	for ext, paths := range m {
+		if ext != path.Ext(ext) {
+			c.Errorf("invalid file extension %s", ext)
+		}
+
+		for _, p := range paths.Paths {
+			// This two match statements validate that it's a valid-enough
+			// path. They should not error when trying to match on it.
+			if _, err := path.Match(p, "test"); err != nil {
+				c.Errorf("invalid path %s: %s", p, err)
+			}
+			if _, err := path.Match(p, "src/"); err != nil {
+				c.Errorf("invalid path %s: %s", p, err)
+			}
+
+			if pExt := path.Ext(p); ext != pExt && p[len(p)-1] != '/' {
+				c.Errorf("the extension of path %s does not match the extension %s", p, ext)
+			}
+		}
+	}
 }
---
+++ go/src/infra/appengine/rubber-stamper/config/validate_test.go
@@ -0,0 +1,77 @@
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package config
+
+import (
+	"context"
+	"testing"
+
+	"github.com/golang/protobuf/proto"
+	. "github.com/smartystreets/goconvey/convey"
+	. "go.chromium.org/luci/common/testing/assertions"
+	"go.chromium.org/luci/config/validation"
+)
+
+var sampleConfigStr = `
+	host_configs {
+		key: "test-host"
+		value: {
+			benign_file_pattern {
+				file_extension_map {
+					key: ".txt"
+					value: {
+						paths: "a/b.txt",
+						paths: "a/*/c.txt",
+						paths: "d/"
+					}
+				}
+				file_extension_map {
+					key: ""
+					value: {
+						paths: "a/b"
+					}
+				}
+			}
+		}
+  	}
+`
+
+func createConfig() *Config {
+	var cfg Config
+	So(proto.UnmarshalText(sampleConfigStr, &cfg), ShouldBeNil)
+	return &cfg
+}
+
+func TestConfigValidator(t *testing.T) {
+	validate := func(cfg *Config) error {
+		c := validation.Context{Context: context.Background()}
+		validateConfig(&c, cfg)
+		return c.Finalize()
+	}
+
+	Convey("sampleConfigStr is valid", t, func() {
+		cfg := createConfig()
+		So(validate(cfg), ShouldBeNil)
+	})
+
+	Convey("validateConfig catches errors", t, func() {
+		cfg := createConfig()
+		Convey("validateBenignFilePattern catches errors", func() {
+			m := cfg.HostConfigs["test-host"].BenignFilePattern.FileExtensionMap
+			Convey("invalid file extension", func() {
+				m["a.txt"] = &Paths{}
+				So(validate(cfg), ShouldErrLike, "invalid file extension a.txt")
+			})
+			Convey("invalid path", func() {
+				m[".txt"].Paths[0] = "\\"
+				So(validate(cfg), ShouldErrLike, "invalid path \\")
+			})
+			Convey("invalid path with extension", func() {
+				m[".txt"].Paths[0] = "a/b.md"
+				err := validate(cfg)
+				So(err, ShouldErrLike, "the extension of path a/b.md does not match the extension .txt")
+			})
+		})
+	})
+}
