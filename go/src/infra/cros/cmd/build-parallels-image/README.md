4ca1361545d504566ac3220a8a271938110b0cb7
Patrick Meiring <meiring@google.com>
2020-12-07 00:19:48 +0000

    build-parallels-image: initial checkin of CIPD tool
    Adds skeleton code for defining a new CIPD tool, build-parallels-image.
    This tool will be used in the build_parallels_image CI recipe proposed
    in crrev.com/c/2563154.
    
    BUG=chromium:1105967
    TEST=build/build.py
    
    Change-Id: Ibcdfe8ffc1aeaaa997dfe8b62f6d262e2399ac76
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/2569075
    Commit-Queue: Patrick Meiring <meiring@google.com>
    Reviewed-by: Matthew Warton <mwarton@google.com>
    Reviewed-by: Allen Li <ayatane@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#36921}

---
+++ go/src/infra/cros/cmd/build-parallels-image/README.md
@@ -0,0 +1,3 @@
+# Build Parallels Image utility
+
+This binary's subcommands implement steps of the build_parallels_image recipe.
---
+++ go/src/infra/cros/cmd/build-parallels-image/main.go
@@ -0,0 +1,37 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Command result_flow implements the result flow recipe's steps.
+package main
+
+import (
+	"context"
+	"os"
+
+	"github.com/maruel/subcommands"
+
+	"go.chromium.org/luci/common/cli"
+	"go.chromium.org/luci/common/data/rand/mathrand"
+	"go.chromium.org/luci/common/logging/gologger"
+)
+
+func getApplication() *cli.Application {
+	return &cli.Application{
+		Name:  "build-parallels-image",
+		Title: "Binary steps implementations for build_parallels_image recipe.",
+		Context: func(ctx context.Context) context.Context {
+			return gologger.StdConfig.Use(ctx)
+		},
+		Commands: []*subcommands.Command{
+			subcommands.CmdHelp,
+
+			subcommands.Section("Steps"),
+		},
+	}
+}
+
+func main() {
+	mathrand.SeedRandomly()
+	os.Exit(subcommands.Run(getApplication(), nil))
+}
