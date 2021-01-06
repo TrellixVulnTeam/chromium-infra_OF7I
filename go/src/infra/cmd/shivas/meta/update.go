a2f5599defea9b9219edbf2ed05de7766b3590f2
Eshwar Narayan <eshwarn@google.com>
2021-01-06 21:32:02 +0000

    shivas: support upgrade command with reference tag
    Bug: None
    Test: run command
    
    ./shivas upgrade -dev
    [P3263071 17:23:19.904 action_plan.go:140 I] In root:
    [P3263071 17:23:19.904 action_plan.go:152 I]   to update:
    [P3263071 17:23:19.904 action_plan.go:154 I]     infra/shivas/linux-amd64 (TZrfejA5lf3m9QZf7pm63YTrnh7xWRZOCZkJ8ZhtP4MC -> A1DGBKh6cVELuc-aNh-KZsxh1LSEBrb4KLVjoASiq0oC)
    [P3263071 17:23:19.905 client.go:1506 I] cipd: resolving fetch URL for infra/shivas/linux-amd64:A1DGBKh6cVELuc-aNh-KZsxh1LSEBrb4KLVjoASiq0oC
    [P3263071 17:23:20.000 storage.go:308 I] cipd: initiating the fetch
    [P3263071 17:23:20.229 storage.go:275 I] cipd: about to fetch 14.0 MB
    [P3263071 17:23:20.230 storage.go:256 I] cipd: fetching - 0% (?? KB/s)
    [P3263071 17:23:20.390 storage.go:256 I] cipd: fetching - 100% (?? KB/s)
    [P3263071 17:23:20.391 storage.go:256 I] cipd: fetching - 100% (?? KB/s)
    [P3263071 17:23:20.391 client.go:1500 I] cipd: successfully fetched infra/shivas/linux-amd64:A1DGBKh6cVELuc-aNh-KZsxh1LSEBrb4KLVjoASiq0oC in 0.5s
    [P3263071 17:23:20.391 deployer.go:301 I] Deploying infra/shivas/linux-amd64:A1DGBKh6cVELuc-aNh-KZsxh1LSEBrb4KLVjoASiq0oC into /usr/local/google/home/eshwarn/cr(/)
    [P3263071 17:23:20.391 reader.go:386 I] cipd: about to extract 30.9 MB (3 files)
    [P3263071 17:23:20.392 reader.go:425 I] cipd: extracting - 0%
    [P3263071 17:23:20.680 reader.go:425 I] cipd: extracting - 100%
    [P3263071 17:23:20.680 deployer.go:385 I] Moving files to their final destination...
    [P3263071 17:23:20.681 deployer.go:400 I] Cleaning up...
    [P3263071 17:23:20.689 deployer.go:406 I] Deployed infra/shivas/linux-amd64:A1DGBKh6cVELuc-aNh-KZsxh1LSEBrb4KLVjoASiq0oC
    [P3263071 17:23:20.693 client.go:1726 I] All changes applied.
    
    shivas: You may need to run shivas login again after the upgrade
    shivas: Run shivas whoami to check login status
    
    Change-Id: Ida5c2cf3179698ce890d893477883ed21a7e5cf1
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/2607845
    Reviewed-by: Otabek Kasimov <otabek@google.com>
    Reviewed-by: Xixuan Wu <xixuan@chromium.org>
    Commit-Queue: Eshwar Narayan <eshwarn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#37345}

--- go/src/infra/cmd/shivas/meta/update.go
+++ go/src/infra/cmd/shivas/meta/update.go
@@ -14,6 +14,9 @@ import (
 	"infra/libs/cipd"
 )
 
+// cipd reference tag for the dev version. Reference tag for prod version is prod.
+var devVersionRefTag string = "latest"
+
 // Update subcommand: Update shivas tool.
 var Update = &subcommands.Command{
 	UsageLine: "upgrade", // we have a separate update cmd under UFS, so changing this to upgrade.
@@ -24,6 +27,8 @@ This is a thin wrapper around CIPD.`,
 	CommandRun: func() subcommands.CommandRun {
 		c := &updateRun{}
 		c.authFlags.Register(&c.Flags, site.DefaultAuthOptions)
+
+		c.Flags.BoolVar(&c.dev, "dev", false, "Upgrade to dev version")
 		return c
 	},
 }
@@ -31,6 +36,8 @@ This is a thin wrapper around CIPD.`,
 type updateRun struct {
 	subcommands.CommandRunBase
 	authFlags authcli.Flags
+
+	dev bool
 }
 
 func (c *updateRun) Run(a subcommands.Application, args []string, env subcommands.Env) int {
@@ -42,10 +49,16 @@ func (c *updateRun) Run(a subcommands.Application, args []string, env subcommand
 }
 
 func (c *updateRun) innerRun(a subcommands.Application, args []string, env subcommands.Env) error {
-	if err := cipd.UpdatePackageToProd(site.CipdInstalledPath, a.GetOut(), a.GetErr()); err != nil {
-		return err
+	if c.dev {
+		if err := cipd.UpdatePackage(site.CipdInstalledPath, devVersionRefTag, a.GetOut(), a.GetErr()); err != nil {
+			return err
+		}
+	} else {
+		if err := cipd.UpdatePackageToProd(site.CipdInstalledPath, a.GetOut(), a.GetErr()); err != nil {
+			return err
+		}
 	}
-	fmt.Fprintf(a.GetErr(), "%s: You may need to run shivas login again after the update\n", a.GetName())
+	fmt.Fprintf(a.GetErr(), "%s: You may need to run shivas login again after the upgrade\n", a.GetName())
 	fmt.Fprintf(a.GetErr(), "%s: Run shivas whoami to check login status\n", a.GetName())
 	return nil
 }
