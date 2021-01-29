c6c04529642e7ad280cc84c672e845d4a763aef7
Jason D. Clinton <jclinton@chromium.org>
2021-01-29 01:30:27 +0000

    Update the TBR text for Audit App filed bugs
    Change-Id: I1818b48f53bfe1a7cbe825d4cbe71aac925743a8
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/2658603
    Commit-Queue: Jason Clinton <jclinton@chromium.org>
    Reviewed-by: Yulan Lin <yulanlin@google.com>
    Cr-Commit-Position: refs/heads/master@{#38039}

--- go/src/infra/appengine/cr-audit-commits/app/rules/tbr_rules.go
+++ go/src/infra/appengine/cr-audit-commits/app/rules/tbr_rules.go
@@ -26,7 +26,9 @@ const (
 
 Beginning in Q1 2021, Chrome is disallowing TBRs. Learn more at go/chrome-cr-owners-site. Getting code review on all CLs will avoid having these bugs filed.
 
-This bug is a warning; you can close it to signal that you've gotten review for the CL that this bug was filed against.`
+CHERRY-PICKS and REVERTS: Rubber Stamper is now available to approve clean cherry-picks and reverts if they were created via the Gerrit UI. Adding the bot (rubber-stamper@appspot.gserviceaccount.com) as a reviewer to your CL will cause it to scan and approve it. Just typing "Rubber St" will autocomplete the full address for you.
+
+This bug is a warning that what you have done will stop working when Gerrit blocks such submissions; you can close it to signal that you've gotten review for the CL that this bug was filed against.`
 )
 
 // getMaxLabelValue determines the highest possible value of a vote for a given
