4c7c69fb88fb1dbdedaa2878ea84f0a63142e488
Sean Abraham <seanabraham@chromium.org>
2021-02-08 18:41:21 +0000

    cros_test_runner: add some more functionality from test_runner
    BUG=chromium:1165962
    TEST=led runs
    
    Change-Id: I8daa9f5df8cc17301e3ac64f500718f0d561607b
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/2679710
    Commit-Queue: Sean Abraham <seanabraham@chromium.org>
    Reviewed-by: Prathmesh Prabhu <pprabhu@google.com>
    Cr-Commit-Position: refs/heads/master@{#38322}

--- go/src/infra/cros/cmd/cros_test_runner/execute/execute.go
+++ go/src/infra/cros/cmd/cros_test_runner/execute/execute.go
@@ -11,9 +11,14 @@ import (
 	"strings"
 
 	"github.com/golang/protobuf/jsonpb"
+	"go.chromium.org/chromiumos/infra/proto/go/test_platform/skylab_test_runner"
 	"go.chromium.org/chromiumos/infra/proto/go/test_platform/skylab_test_runner/steps"
 	"go.chromium.org/luci/common/errors"
 	"go.chromium.org/luci/luciexe/build"
+)
+
+const (
+	dummyTestID = "original test"
 )
 
 func logInputs(ctx context.Context, input *steps.RunTestsRequest) (err error) {
@@ -28,17 +33,63 @@ func logInputs(ctx context.Context, input *steps.RunTestsRequest) (err error) {
 	return nil
 }
 
+func marshalInput(ctx context.Context, input *steps.RunTestsRequest) (tests map[string]*skylab_test_runner.Request_Test, err error) {
+	step, ctx := build.StartStep(ctx, "marshal input")
+	defer func() { step.End(err) }()
+	tests = make(map[string]*skylab_test_runner.Request_Test)
+	for k, v := range input.GetRequest().GetTests() {
+		if k == dummyTestID {
+			return tests, errors.Reason("cannot use reserved key %s as test key in input", dummyTestID).Err()
+		}
+		tests[k] = v
+	}
+	if input.GetRequest().GetTest() != nil {
+		tests[dummyTestID] = input.GetRequest().GetTest()
+	}
+	return tests, nil
+}
+
+func parseEnvironment(ctx context.Context) (hostName string, err error) {
+	step, ctx := build.StartStep(ctx, "parse environment information")
+	defer func() { step.End(err) }()
+	botID := os.Getenv("SWARMING_BOT_ID")
+	prefix := "crossk-"
+	if !strings.HasPrefix(botID, "crossk-") {
+		return "", errors.Reason("expected SWARMING_BOT_ID to start with %v, instead %v", prefix, botID).Err()
+	}
+	hn := strings.TrimPrefix(botID, prefix)
+	step.SetSummaryMarkdown("hostname: " + hn)
+	return hn, nil
+}
+
+func executionSteps(ctx context.Context, tests map[string]*skylab_test_runner.Request_Test) (err error) {
+	step, ctx := build.StartStep(ctx, "execution steps")
+	defer func() { step.End(err) }()
+
+	// TODO(https://crbug.com/1165962) publish to result flow here
+
+	_, err = parseEnvironment(ctx)
+	if err != nil {
+		return errors.Annotate(err, "failed to parse environment information").Err()
+	}
+
+	return nil
+}
+
 // Run executes the core logic for cros_test_runner.
 func Run(ctx context.Context, input *steps.RunTestsRequest) (err error) {
 	if err = logInputs(ctx, input); err != nil {
 		return err
 	}
 
-	botID := os.Getenv("SWARMING_BOT_ID")
-	prefix := "crossk-"
-	if !strings.HasPrefix(botID, "crossk-") {
-		return fmt.Errorf("expected SWARMING_BOT_ID to start with %v, instead %v", prefix, botID)
+	tests, err := marshalInput(ctx, input)
+	if err != nil {
+		return errors.Annotate(err, "failed to marshal input").Err()
 	}
-	_ = strings.TrimPrefix(botID, prefix)
+
+	if err = executionSteps(ctx, tests); err != nil {
+		err = errors.Annotate(err, "execution steps failed").Err()
+	}
+
 	return nil
 }
