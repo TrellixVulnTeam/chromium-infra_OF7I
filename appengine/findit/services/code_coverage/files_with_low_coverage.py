7cbd2cf96a431ad8a3c734f6d675a71942b814f3
Your Name <pasthana@google.com>
2021-02-09 03:35:32 +0000

    Turn off in-context caching for low coverage file job
    Bug: 1141021
    Change-Id: I623cd88dd6ae62e05368d71b8a983cb95670f6fb
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/2681402
    Commit-Queue: Prakhar Asthana <pasthana@google.com>
    Auto-Submit: Prakhar Asthana <pasthana@google.com>
    Reviewed-by: Zhaoyang Li <zhaoyangli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#38349}

--- appengine/findit/services/code_coverage/files_with_low_coverage.py
+++ appengine/findit/services/code_coverage/files_with_low_coverage.py
@@ -56,53 +56,62 @@ def ExportFilesWithLowCoverage():
     more = True
     cursor = None
     while more:
-      results, cursor, more = query.fetch_page(_PAGE_SIZE, start_cursor=cursor)
-      bq_rows = []
-      for result in results:
-        bq_row = _CreateBigqueryRow(result, commit_timestamp)
-        if bq_row:
-          bq_rows.append(bq_row)
-          total_rows += 1
-      bigquery_helper.ReportRowsToBigquery(bq_rows, 'findit-for-me',
-                                           'code_coverage_summaries',
-                                           'files_with_low_coverage')
-
+      # NDB caches each result in the in-context cache while accessing.
+      # This is problematic as due to the size of the result set,
+      # cache grows beyond the memory quota. `use_cache = False` turns this off.
+      #
+      # Read more at:
+      # https://cloud.google.com/appengine/docs/standard/python/ndb/cache#incontext
+      # https://github.com/googlecloudplatform/datastore-ndb-python/issues/156#issuecomment-110869490
+      results, cursor, more = query.fetch_page(
+          _PAGE_SIZE,
+          start_cursor=cursor,
+          config=ndb.ContextOptions(use_cache=False))
+      bq_rows = _CreateBigqueryRows(results, commit_timestamp)
+      if bq_rows:
+        bigquery_helper.ReportRowsToBigquery(bq_rows, 'findit-for-me',
+                                             'code_coverage_summaries',
+                                             'files_with_low_coverage')
+        total_rows += len(bq_rows)
       logging.info('Total rows added so far = %d', total_rows)
 
+    logging.info('Total rows added for builder %s = %d', builder, total_rows)
 
-def _CreateBigqueryRow(file_coverage_data, commit_timestamp):
-  """Create a bigquery row for a file with low coverage.
 
-  Returns a dict whose keys are column names and values are column values
-  corresponding to the schema of the bigquery table. Returns None if
-  file's coverage is above the bar.
+def _CreateBigqueryRows(file_coverage_results, commit_timestamp):
+  """Create bigquery rows for files with low coverage.
+
+  Returns a list of dict objects whose keys are column names and
+  values are column values corresponding to the schema of the bigquery table.
+  Each dict object corresponds to exactly one file below the coverage bar.
 
   Args:
-    file_coverage_data (FileCoverageData): Coverage report for the
-      corresponding file.
+    file_coverage_results (list): List of FileCoverageData for the latest
+      full codebase report
     commit_timestamp (ndb.DateTimeProperty): Commit timestamp of the
-      revision for which coverage report was generated.
+      revision for which full codebase report was generated.
   """
-  try:
-    data = file_coverage_data.data
-    for metric in data['summaries']:
-      if metric['name'] == 'line':
-        total_lines = metric['total']
-        covered_lines = metric['covered']
-        break
-
-    if covered_lines / float(total_lines) < _MIN_ABS_COVERAGE_RATIO:
-      return {
-          'project': file_coverage_data.gitiles_commit.project,
-          'revision': file_coverage_data.gitiles_commit.revision,
-          'path': data['path'][2:],
-          'total_lines': total_lines,
-          'covered_lines': covered_lines,
-          'commit_timestamp': commit_timestamp.isoformat(),
-          'insert_timestamp': time_util.GetUTCNow().isoformat(),
-          'builder': file_coverage_data.builder
-      }
-  except Exception as e:
-    logging.warning("Encounted exception for file coverage %s",
-                    file_coverage_data)
-    logging.warning(e.message)
+  bq_rows = []
+  for file_coverage_result in file_coverage_results:
+    try:
+      data = file_coverage_result.data
+      for metric in data['summaries']:
+        if metric['name'] == 'line':
+          total_lines = metric['total']
+          covered_lines = metric['covered']
+          break
+      if covered_lines / float(total_lines) < _MIN_ABS_COVERAGE_RATIO:
+        bq_rows.append({
+            'project': file_coverage_result.gitiles_commit.project,
+            'revision': file_coverage_result.gitiles_commit.revision,
+            'path': data['path'][2:],
+            'total_lines': total_lines,
+            'covered_lines': covered_lines,
+            'commit_timestamp': commit_timestamp.isoformat(),
+            'insert_timestamp': time_util.GetUTCNow().isoformat(),
+            'builder': file_coverage_result.builder
+        })
+    except ZeroDivisionError:
+      logging.warning("Encounted total_lines = 0 for file coverage %s",
+                      file_coverage_result)
+  return bq_rows
