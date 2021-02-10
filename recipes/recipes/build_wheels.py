aafb7b6b597d60e73e13638265b859a874e4dbd8
Owen Rodley <orodley@chromium.org>
2021-02-10 00:46:35 +0000

    Set up MSVC etc. on Windows wheel builders.
    Bug: 1144965
    Change-Id: I74babe9ac961f3e09bdc60e2a923bebeb4be92e6
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/2682289
    Commit-Queue: Owen Rodley <orodley@chromium.org>
    Auto-Submit: Owen Rodley <orodley@chromium.org>
    Reviewed-by: Brian Ryner <bryner@google.com>
    Cr-Commit-Position: refs/heads/master@{#38397}

--- recipes/recipes/build_wheels.py
+++ recipes/recipes/build_wheels.py
@@ -4,12 +4,16 @@
 
 DEPS = [
     'depot_tools/gclient',
+    'depot_tools/windows_sdk',
     'depot_tools/osx_sdk',
     'recipe_engine/context',
     'recipe_engine/file',
     'recipe_engine/path',
+    'recipe_engine/platform',
     'recipe_engine/python',
 ]
+
+from contextlib import contextmanager
 
 
 def RunSteps(api):
@@ -26,10 +30,16 @@ def RunSteps(api):
   # Set MACOSX_DEPLOYMENT_TARGET to 10.13 to ensure compatibility regardless of
   # the OS version the bot is running. This environment variable is unnecessary
   # but harmless on other OSes, so we just set it unconditionally.
-  # Note: osx_sdk is a no-op on non-Mac platforms.
-  with api.osx_sdk('mac'), api.context(
+  # Same story with DISTUTILS_USE_SDK and MSSdk - these are necessary for
+  # distutils to correctly locate MSVC on Windows, and do nothing on other
+  # platforms.
+  with PlatformSdk(api), api.context(
       cwd=solution_path.join('infra'),
-      env={'MACOSX_DEPLOYMENT_TARGET': '10.13'}):
+      env={
+          'MACOSX_DEPLOYMENT_TARGET': '10.13',
+          'DISTUTILS_USE_SDK': '1',
+          'MSSdk': '1',
+      }):
     api.python('dockerbuild', solution_path.join('infra', 'run.py'), [
         'infra.tools.dockerbuild',
         '--root',
@@ -40,5 +50,22 @@ def RunSteps(api):
     ])
 
 
+@contextmanager
+def PlatformSdk(api):
+  sdk = None
+  if api.platform.is_win:
+    sdk = api.windows_sdk()
+  elif api.platform.is_mac:
+    sdk = api.osx_sdk('mac')
+
+  if sdk is None:
+    yield
+  else:
+    with sdk:
+      yield
+
+
 def GenTests(api):
   yield api.test('success')
+  yield api.test('win', api.platform('win', 64))
+  yield api.test('mac', api.platform('mac', 64))
