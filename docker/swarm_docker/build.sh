7e9b474d88166ddca8625ca670792c76e6b6df39
Stephen Roe <steveroe@google.com>
2021-02-12 02:04:10 +0000

    Use Ubuntu 20.04 (focal) for arm64 and x64 docker images.
    Add packages required to run femu to arm64 docker images.
    
    Bug: 1058247
    Change-Id: Ibc1632923317e29f3bb05852259dd97df59466d5
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/2688627
    Reviewed-by: Ben Pastene <bpastene@chromium.org>
    Commit-Queue: Stephen Roe <steveroe@google.com>
    Cr-Commit-Position: refs/heads/master@{#38474}

--- docker/swarm_docker/build.sh
+++ docker/swarm_docker/build.sh
@@ -5,12 +5,17 @@
 
 arch=$(uname -m)
 if [[ $arch == *"aarch64"* ]]; then
-  # Add packages required to run qemu-kvm in linux-arm64 docker image.
-  arch_packages="libatomic1 libvirt-bin llvm virtinst"
+  # Add packages required to run femu in linux-arm64 docker image.
+  # TODO(1162314): Remove llvm package after migration to new SDK symbolizer.
+  arch_packages="google-perftools libatomic1 libgl1-mesa-glx libpcre2-16-0 llvm"
+  kvm_packages="libvirt-daemon-system libvirt-clients virtinst"
   arch_suffix="_arm64"
+  arch_build_options="--memory=8g"
 else
   arch_packages=""
+  kvm_packages=""
   arch_suffix=""
+  arch_build_options=""
 fi
 
 date=$(/bin/date +"%Y-%m-%d_%H-%M")
@@ -21,8 +26,10 @@ echo "Building for swarm_docker..."
   --no-cache=true \
   --pull \
   --build-arg "ARCH_PACKAGES=${arch_packages}" \
+  --build-arg "KVM_PACKAGES=${kvm_packages}" \
   -t "swarm_docker${arch_suffix}:${date}" \
   -t "swarm_docker${arch_suffix}:latest" \
+  ${arch_build_options} \
   "${par_dir}"
 
 exit 0
