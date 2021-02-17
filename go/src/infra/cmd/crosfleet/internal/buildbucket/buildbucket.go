f48f6b85e3bb49665e6019bcaea3c78130a1fd3a
Jared Loucks <jaredloucks@google.com>
2021-02-17 20:49:15 +0000

    Add Buildbucket Builds client to crosfleet CLI.
    Client functionality is thin for now, with only the commands needed for
    running test and creating leases. Also added some simple unit
    tests--maybe even so simple that we don't need them. But the old skylab
    CLI has had some regressions introduced due to careless typos in the
    past, so hopefully these super basic tests catch some of those in the
    future.
    
    Bug: 1176810
    Change-Id: I1abb58ab2baac427cd520b71043d8a810ec3a458
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/2699415
    Commit-Queue: Jared Loucks <jaredloucks@google.com>
    Reviewed-by: Taylor Clark <taylormade@google.com>
    Cr-Commit-Position: refs/heads/master@{#38588}

---
+++ go/src/infra/cmd/crosfleet/internal/buildbucket/buildbucket.go
@@ -0,0 +1,155 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package buildbucket provides a Buildbucket client with helper methods for
+// interacting with builds.
+package buildbucket
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"google.golang.org/genproto/protobuf/field_mask"
+	"strings"
+
+	"github.com/golang/protobuf/jsonpb"
+	"go.chromium.org/chromiumos/infra/proto/go/test_platform"
+	"go.chromium.org/luci/auth/client/authcli"
+	buildbucketpb "go.chromium.org/luci/buildbucket/proto"
+	"go.chromium.org/luci/common/errors"
+	"go.chromium.org/luci/grpc/prpc"
+	structbuilder "google.golang.org/protobuf/types/known/structpb"
+
+	"infra/cmd/crosfleet/internal/site"
+	"infra/cmdsupport/cmdlib"
+)
+
+var maxServiceVersion = &test_platform.ServiceVersion{CrosfleetTool: 1}
+
+// addServiceVersion marshals the max service version proto to a JSON-encoded
+// string, adds it to the given Buildbucket property map, and returns the
+// property map.
+func addServiceVersion(props map[string]interface{}) map[string]interface{} {
+	pbMarshaler := jsonpb.Marshaler{}
+	versionJSON, err := pbMarshaler.MarshalToString(maxServiceVersion)
+	if err != nil {
+		panic(err)
+	}
+	versionPropertyJSON, err := json.Marshal(
+		map[string]string{
+			"version": versionJSON,
+		})
+	if err != nil {
+		panic(err)
+	}
+	props["$chromeos/service_version"] = string(versionPropertyJSON)
+	return props
+}
+
+// Client provides helper methods to interact with Buildbucket builds.
+type Client struct {
+	client    buildbucketpb.BuildsClient
+	builderID *buildbucketpb.BuilderID
+}
+
+// NewClient returns a new client to interact with Buildbucket builds from the
+// given builder.
+func NewClient(ctx context.Context, builderInfo site.BuildbucketBuilderInfo, authFlags authcli.Flags) (*Client, error) {
+	httpClient, err := cmdlib.NewHTTPClient(ctx, &authFlags)
+	if err != nil {
+		return nil, err
+	}
+
+	prpcClient := &prpc.Client{
+		C:       httpClient,
+		Host:    builderInfo.Host,
+		Options: site.DefaultPRPCOptions,
+	}
+
+	return &Client{
+		client:    buildbucketpb.NewBuildsPRPCClient(prpcClient),
+		builderID: builderInfo.BuilderID,
+	}, nil
+}
+
+// ScheduleBuild schedules a new build (of the client's builder) with the given
+// properties, tags, bot dimensions, and Buildbucket priority, and returns the
+// ID of the scheduled build.
+//
+// Buildbucket requests take properties of type *structpb.Struct. To simplify
+// the conversion from other data structures to Structs, ScheduleBuild accepts
+// properties of type map[string]interface{}, where interface{} can be any of
+// Go's basic types: bool, string, number type, byte, or rune.
+//
+// NOTE: Buildbucket priority is separate from internal swarming priority.
+func (c *Client) ScheduleBuild(ctx context.Context, props map[string]interface{}, dims map[string]string, tags map[string]string, priority int32) (int64, error) {
+	props = addServiceVersion(props)
+	propStruct, err := structbuilder.NewStruct(props)
+	if err != nil {
+		return 0, err
+	}
+	request := &buildbucketpb.ScheduleBuildRequest{
+		Builder:    c.builderID,
+		Properties: propStruct,
+		Dimensions: bbDims(dims),
+		Tags:       bbTags(tags),
+		Priority:   priority,
+	}
+	build, err := c.client.ScheduleBuild(ctx, request)
+	if err != nil {
+		return 0, errors.Annotate(err, "schedule build").Err()
+	}
+	return build.GetId(), nil
+}
+
+// GetBuild gets a Buildbucket build by ID, with the given build fields
+// populated. If no fields are given, all fields will be populated.
+func (c *Client) GetBuild(ctx context.Context, ID int64, fields ...string) (*buildbucketpb.Build, error) {
+	if len(fields) == 0 {
+		fields = []string{"*"}
+	}
+	request := &buildbucketpb.GetBuildRequest{
+		Id:     ID,
+		Fields: &field_mask.FieldMask{Paths: fields},
+	}
+	build, err := c.client.GetBuild(ctx, request)
+	if err != nil {
+		return nil, errors.Annotate(err, "get build").Err()
+	}
+	return build, nil
+}
+
+// BuildURL constructs the URL for the LUCI page of the build (of the client's
+// builder) with the given ID.
+func (c *Client) BuildURL(ID int64) string {
+	return fmt.Sprintf(
+		"https://ci.chromium.org/ui/p/%s/builders/%s/%s/b%d",
+		c.builderID.Project, c.builderID.Bucket, c.builderID.Builder, ID)
+}
+
+// bbDims converts the given map[string]string of bot dimensions to the
+// required []*buildbucketpb.RequestedDimension type for Buildbucket requests.
+func bbDims(dims map[string]string) []*buildbucketpb.RequestedDimension {
+	var bbDimList []*buildbucketpb.RequestedDimension
+	for key, val := range dims {
+		bbDimList = append(bbDimList, &buildbucketpb.RequestedDimension{
+			Key:   strings.Trim(key, " "),
+			Value: strings.Trim(val, " "),
+		})
+	}
+	return bbDimList
+}
+
+// bbTags converts the given map[string]string of Buildbucket tags to the
+// required []*buildbucketpb.StringPair type for Buildbucket requests.
+func bbTags(tags map[string]string) []*buildbucketpb.StringPair {
+	var bbTagList []*buildbucketpb.StringPair
+	for key, val := range tags {
+		bbTagList = append(bbTagList, &buildbucketpb.StringPair{
+			Key:   strings.Trim(key, " "),
+			Value: strings.Trim(val, " "),
+		})
+	}
+	return bbTagList
+}
---
+++ go/src/infra/cmd/crosfleet/internal/buildbucket/buildbucket_test.go
@@ -0,0 +1,70 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package buildbucket
+
+import (
+	"github.com/google/go-cmp/cmp/cmpopts"
+	buildbucketpb "go.chromium.org/luci/buildbucket/proto"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestAddServiceVersion(t *testing.T) {
+	t.Parallel()
+	startingProps := map[string]interface{}{"foo": "bar"}
+	wantProps := map[string]interface{}{
+		"foo":                       "bar",
+		"$chromeos/service_version": `{"version":"{\"crosfleetTool\":\"1\"}"}`,
+	}
+	gotProps := addServiceVersion(startingProps)
+	diff := cmp.Diff(wantProps, gotProps)
+	if diff != "" {
+		t.Errorf("unexpected diff (%s)", diff)
+	}
+}
+
+func TestBuildURL(t *testing.T) {
+	client := Client{
+		builderID: &buildbucketpb.BuilderID{
+			Project: "chromeos",
+			Bucket:  "test_runner",
+			Builder: "dut_leaser",
+		},
+	}
+	wantURL := "https://ci.chromium.org/ui/p/chromeos/builders/test_runner/dut_leaser/b8855075479708816960"
+	gotURL := client.BuildURL(8855075479708816960)
+	if gotURL != wantURL {
+		t.Errorf("unexpected build URL: wanted %s, got %s", wantURL, gotURL)
+	}
+}
+
+func TestBBDims(t *testing.T) {
+	dims := map[string]string{
+		"foo ": " bar",
+	}
+	wantBBDims := []*buildbucketpb.RequestedDimension{
+		{Key: "foo", Value: "bar"},
+	}
+	gotBBDims := bbDims(dims)
+	diff := cmp.Diff(wantBBDims, gotBBDims, cmpopts.IgnoreUnexported(buildbucketpb.RequestedDimension{}))
+	if diff != "" {
+		t.Errorf("unexpected diff (%s)", diff)
+	}
+}
+
+func TestBBTags(t *testing.T) {
+	tags := map[string]string{
+		" foo": "bar ",
+	}
+	wantBBTags := []*buildbucketpb.StringPair{
+		{Key: "foo", Value: "bar"},
+	}
+	gotBBTags := bbTags(tags)
+	diff := cmp.Diff(wantBBTags, gotBBTags, cmpopts.IgnoreUnexported(buildbucketpb.StringPair{}))
+	if diff != "" {
+		t.Errorf("unexpected diff (%s)", diff)
+	}
+}
