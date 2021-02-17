a964f6e651222c77acbb364d99c316710351ae18
Xixuan Wu <xixuan@google.com>
2021-02-17 23:49:47 +0000

    UFS: add machine_lse_deployment table for tracking deployment info
    BUG=b/179941001
    TEST=make gen;make test
    
    Change-Id: I55c69dc803c7192ba1dd8b83beec2299fc4639ca
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/2699457
    Commit-Queue: Xixuan Wu <xixuan@chromium.org>
    Reviewed-by: Jonathan Pittman <jmpittman@google.com>
    Reviewed-by: Eshwar Narayan <eshwarn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#38600}

---
+++ go/src/infra/unifiedfleet/app/model/inventory/machine_lse_deployment.go
@@ -0,0 +1,19 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package inventory
+
+// MachineLSEDeploymentKind is the datastore entity kind for host deployment info.
+const MachineLSEDeploymentKind string = "MachineLSEDeployment"
+
+// MachineLSEDeploymentEntity is a datastore entity that tracks the deployment info for a host.
+type MachineLSEDeploymentEntity struct {
+	_kind                string `gae:"$kind,MachineLSEDeployment"`
+	ID                   string `gae:"$id"`
+	SerialNumber         string `gae:"serial_number"`
+	DeploymentIdentifier string `gae:"deployment_identifier"`
+	SwarmingHost         string `gae:"swarming_host"`
+	// Follow others entities, store ufspb.MachineLSEDeployment bytes.
+	DeploymentInfo []byte `gae:",noindex"`
+}
