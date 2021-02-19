3a7d39b55ca8400e77bb2bb2ce5bb1042949df8a
Nodir Turakulov <nodir@chromium.org>
2021-02-19 22:57:26 +0000

    [rts] Simplify rejection query
    Reduce the number of subqueries and JOINs.
    This CL is a preparation to further cleaning of the data.
    
    This also slightly improves performance.
    Before: https://screenshot.googleplex.com/Akf7pZzULH5h9Q9
    After:  https://screenshot.googleplex.com/4WX82NfGytxXhue
    
    This is a pure refactoring. The new query retrieves the same data.
    
    R=guterman@google.com, tandrii@chromium.org
    
    Bug: 1090668
    Change-Id: I6fc2a5638f3de8a7edb0435975b2280c48929582
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/2679322
    Commit-Queue: Nodir Turakulov <nodir@chromium.org>
    Reviewed-by: Gregory Guterman <guterman@google.com>
    Cr-Commit-Position: refs/heads/master@{#38681}

--- go/src/infra/rts/cmd/rts-chromium/rejections.go
+++ go/src/infra/rts/cmd/rts-chromium/rejections.go
@@ -64,65 +64,51 @@ func (r *fetchRejectionsRun) Run(a subcommands.Application, args []string, env s
 // rejectedPatchSetsSQL is a BigQuery query that returns patchsets with test
 // failures. Excludes flaky tests.
 const rejectedPatchSetsSQL = commonSubqueries + `
-	-- Test results annotated with patchset.
-	ps_test_results AS (
-		SELECT change, patchset, ps_approx_timestamp, tr.*
-		FROM tryjobs t
-		JOIN test_results_base tr ON t.id = tr.build_id
-		WHERE not exonerated  AND status != 'SKIP' -- not needed for RTS purposes
-	),
-
-	-- The following two sub-queries detect flaky tests.
-	-- It is important to exclude them from analysis because they represent
-	-- noise.
-	--
-	-- A test variant is considered flaky if it has mixed results in >=N
-	-- separate CLs. N=1 is too small because otherwise the query is vulnerable
-	-- to a single bad patchset that introduces flakiness and never lands.
-	--
-	-- The first sub-query finds test variants with mixed results per CL.
-	-- Note that GROUP BY includes patchset, but SELECT doesn't and uses
-	-- DISTINCT.
-	-- The second sub-query filters out test variant candidates that have mixed
-	-- results win fewer than @minCLFlakes CLs.
-	flaky_test_variants_per_cl AS (
-		SELECT DISTINCT change, testVariant.id as test_id, variant_hash
-		FROM ps_test_results
-		GROUP BY change, patchset, test_id, variant_hash
-		HAVING LOGICAL_OR(expected) AND LOGICAL_OR(NOT expected)
-	),
-	flaky_test_variants AS (
-		SELECT test_id, variant_hash
-		FROM flaky_test_variants_per_cl
-		GROUP BY test_id, variant_hash
-		HAVING COUNT(change) >= @minCLFlakes
-	),
-
-	-- Select test variants with unexpected results for each patchset.
-	-- Does not filter out flaky test variants just yet.
-	failed_test_variants_per_ps AS (
+	-- Group all test results by patchset, test_id and variant_hash
+	-- in order to analyze individual test variants in each patchset,
+	-- and in particular exclude flaky tests.
+	test_variants_per_ps AS (
 		SELECT
 			change,
 			patchset,
+			testVariant.id AS test_id,
 			variant_hash,
-			MIN(ps_approx_timestamp) as ps_approx_timestamp,
-			ANY_VALUE(testVariant) as testVariant
-		FROM ps_test_results tr
-		GROUP BY change, patchset, tr.testVariant.id, variant_hash
-		HAVING LOGICAL_AND(NOT expected)
+			ANY_VALUE(testVariant) as testVariant,
+			LOGICAL_OR(expected) AND LOGICAL_OR(NOT expected) AS flake,
+			LOGICAL_AND(NOT expected) all_unexpected,
+			ANY_VALUE(ps_approx_timestamp) AS ps_approx_timestamp,
+		FROM tryjobs t
+		JOIN test_results_base tr ON t.id = tr.build_id
+		WHERE not exonerated  AND status != 'SKIP'  -- not needed for RTS purposes
+		GROUP BY change, patchset, test_id, variant_hash
+
+		# Exclude all-expected results early on.
+		HAVING NOT LOGICAL_AND(expected)
+	),
+
+	-- Find all true test failures, without flakes.
+	failed_test_variants AS (
+		SELECT
+			test_id,
+			variant_hash,
+			ANY_VALUE(testVariant) AS testVariant,
+			ARRAY_AGG(
+				IF(all_unexpected, STRUCT(change, patchset, ps_approx_timestamp), NULL)
+				IGNORE NULLS
+			) AS patchsets_with_failures,
+		FROM test_variants_per_ps
+		GROUP BY test_id, variant_hash
+		# Exclude test variants where flakiness was observed in @minCLFlakes CLs
+		# (not patchsets) or more.
+		HAVING COUNT(DISTINCT IF(flake, change, NULL)) < @minCLFlakes
 	)
 
--- Exclude flaky tests from failed_test_variants_per_ps.
 -- Join all tables and produce rows in the Rejection protojson format.
 SELECT
-	patchsetArray(change, patchset, ANY_VALUE(af.files)) AS patchsets,
+	patchsetArray(change, patchset, ANY_VALUE(files)) AS patchsets,
 	RFC3339(MIN(ps_approx_timestamp)) as timestamp,
 	ARRAY_AGG(testVariant) as failedTestVariants
-FROM failed_test_variants_per_ps f
-LEFT JOIN flaky_test_variants flaky ON f.testVariant.id = flaky.test_id AND f.variant_hash = flaky.variant_hash
--- flaky.test_id is NULL if LEFT JOIN did not find a flaky_test_variants row
--- for this test variant, i.e. the test is not flaky
-JOIN affected_files af USING (change, patchset)
-WHERE flaky.test_id IS NULL
+FROM failed_test_variants tv, tv.patchsets_with_failures
+JOIN affected_files USING (change, patchset)
 GROUP BY change, patchset
 `
