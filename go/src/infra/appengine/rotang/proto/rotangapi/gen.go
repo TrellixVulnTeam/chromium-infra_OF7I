8e25fca941d277e2bfc12f5055271eea20f53817
Joey Scarr <jsca@google.com>
2021-02-24 22:57:02 +0000

    Delete rota-ng.
    Bug: 1095538
    Change-Id: I6d9b608e42ad3fdd07c9d88e1340c975969f1eb7
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/2705899
    Reviewed-by: Quang Minh Tuan Nguyen <nqmtuan@google.com>
    Reviewed-by: Owen Rodley <orodley@chromium.org>
    Commit-Queue: Joey Scarr <jsca@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#38795}

--- go/src/infra/appengine/rotang/proto/rotangapi/gen.go
+++
@@ -1,7 +0,0 @@
-//go:generate cproto
-//go:generate svcdec -type OncallInfoServer
-//go:generate svcdec -type RotationAdminServer
-//go:generate svcdec -type MemberAdminServer
-//go:generate goimports -w .
-
-package rotangapi
