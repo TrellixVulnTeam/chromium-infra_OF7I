19cbf0b78443cc976db9000f8e0a8e99ea3cf2a1
Brian Ryner <bryner@google.com>
2021-03-11 04:23:34 +0000

    Revert "Temporarily pin go to 1.15.x."
    This reverts commit f1d40dae9035aa9961f5a09976fe6eb8117f4e4a.
    
    Reason for revert: 1.15.9 packages are created.
    
    Original change's description:
    > Temporarily pin go to 1.15.x.
    >
    > Since we are continuing to use go 1.15 on mac-amd64, we want to
    > build the newly released 1.15.9. This will be reverted once the
    > builders have created packages for this release.
    >
    > BUG=1102967
    >
    > Change-Id: I301254f584e591fd314b4025d1521fb522c64eb7
    > Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/2748213
    > Reviewed-by: Vadim Shtayura <vadimsh@chromium.org>
    > Commit-Queue: Brian Ryner <bryner@google.com>
    > Cr-Commit-Position: refs/heads/master@{#39238}
    
    Bug: 1102967
    Change-Id: I8e8ce5886813e4515cc027e3a480f09ed6de3a3e
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/2751766
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Brian Ryner <bryner@google.com>
    Cr-Commit-Position: refs/heads/master@{#39246}

--- 3pp/go/fetch.py
+++ 3pp/go/fetch.py
@@ -15,9 +15,7 @@ from pkg_resources import parse_version
 def do_latest():
   versions = []
   for release in json.load(urllib.urlopen('https://golang.org/dl/?mode=json')):
-    ver = parse_version(release['version'].replace('go', ''))
-    if ver < parse_version('1.16'):
-      versions.append(ver)
+    versions.append(parse_version(release['version'].replace('go', '')))
   versions.sort()
   print versions[-1]
 
@@ -26,20 +24,21 @@ def get_download_url(version, platform, kind):
   if kind == 'prebuilt':
     platform = platform.replace('mac', 'darwin')
     ext = '.zip' if platform.startswith('windows') else '.tar.gz'
-    download_url = (('https://storage.googleapis.com/golang/go%(version)s.' +
-                     '%(platform)s%(ext)s') % {
-                         'version': version,
-                         'platform': platform,
-                         'ext': ext
-                     })
+    download_url = (
+      'https://storage.googleapis.com/golang/go%(version)s.%(platform)s%(ext)s'
+      % {
+        'version': version,
+        'platform': platform,
+        'ext': ext
+      })
   else:
     ext = '.tar.gz'
-    download_url = ('https://storage.googleapis.com/golang/go%s.src.tar.gz' %
-                    (version,))
+    download_url = (
+      'https://storage.googleapis.com/golang/go%s.src.tar.gz' % (version,))
 
   partial_manifest = {
-      'url': [download_url],
-      'ext': ext,
+    'url': [download_url],
+    'ext': ext,
   }
   print(json.dumps(partial_manifest))
 
@@ -54,12 +53,13 @@ def main():
   latest.set_defaults(func=lambda _opts: do_latest())
 
   download = sub.add_parser('get_url')
-  download.set_defaults(func=lambda opts: get_download_url(
-      os.environ['_3PP_VERSION'], os.environ['_3PP_PLATFORM'], opts.kind))
+  download.set_defaults(
+    func=lambda opts: get_download_url(
+      os.environ['_3PP_VERSION'], os.environ['_3PP_PLATFORM'],
+      opts.kind))
 
   opts = ap.parse_args()
   return opts.func(opts)
-
 
 if __name__ == '__main__':
   sys.exit(main())
