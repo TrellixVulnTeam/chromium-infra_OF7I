0300afe1eacf665138407ee2f701067c8ce5dca7
Vadim Shtayura <vadimsh@chromium.org>
2021-03-11 19:14:12 +0000

    [go] Update go to pick latest security fixes.
    R=iannucci@chromium.org, bryner@google.com
    
    Change-Id: I1ccf18634ba9ed5c486ccecdd6fcdad8fa3e0f85
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/2752800
    Commit-Queue: Vadim Shtayura <vadimsh@chromium.org>
    Commit-Queue: Robbie Iannucci <iannucci@chromium.org>
    Auto-Submit: Vadim Shtayura <vadimsh@chromium.org>
    Reviewed-by: Robbie Iannucci <iannucci@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#39263}

--- go/bootstrap.py
+++ go/bootstrap.py
@@ -55,9 +55,9 @@ GIT_EXE = 'git.bat' if sys.platform == 'win32' else 'git'
 #
 # Some builders use "legacy" and "bleeding_edge" variants.
 TOOLSET_VERSIONS = {
-  'default': '1.15.8',      # used on dev workstations and most try builders
-  'legacy': '1.15.8',       # used on OSX amd64 CI and prod builders
-  'bleeding_edge': '1.16',  # used on most CI and prod and some try builders
+  'default': '1.15.9',        # used on dev workstations and most try builders
+  'legacy': '1.15.9',         # used on OSX amd64 CI and prod builders
+  'bleeding_edge': '1.16.1',  # used on most CI and prod and some try builders
 }
 
 # Describes how to fetch 'glide'.
