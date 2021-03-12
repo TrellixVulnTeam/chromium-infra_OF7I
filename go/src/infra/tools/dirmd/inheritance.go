5995c1cdb3ca5ea61c7bbce6c6cacc99d7b52150
Nodir Turakulov <nodir@chromium.org>
2021-03-12 18:59:28 +0000

    [dirmd] Move code around
    Rename reduce.go to inheritance.go and move computation-related
    functions from mapping.go to inheritance.go.
    
    Pure refactoring, just moving code around, in preparation for actual
    functional changes.
    
    Bug: 1179786
    Change-Id: I6314a913c95b63f31a7e17b34cea7c621fe51009
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/2757289
    Commit-Queue: Nodir Turakulov <nodir@chromium.org>
    Reviewed-by: Edward Lesmes <ehmaldonado@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#39316}

--- go/src/infra/tools/dirmd/inheritance.go
+++ go/src/infra/tools/dirmd/inheritance.go
@@ -5,8 +5,87 @@
 package dirmd
 
 import (
+	"path"
+	"sort"
+
+	"google.golang.org/protobuf/proto"
 	"google.golang.org/protobuf/reflect/protoreflect"
+
+	dirmdpb "infra/tools/dirmd/proto"
 )
+
+// Compute computes metadata for the given directory key.
+func (m *Mapping) Compute(key string) *dirmdpb.Metadata {
+	parent := path.Dir(key)
+	if parent == key {
+		return cloneMD(m.Dirs[key])
+	}
+
+	ret := m.Compute(parent)
+	Merge(ret, m.Dirs[key])
+	return ret
+}
+
+// ComputeAll computes full metadata for each dir.
+func (m *Mapping) ComputeAll() {
+	// Process directories in the shorest-path to longest-path order,
+	// such that, when computing the expanded metadata for a given directory,
+	// we only need to check the nearest ancestor.
+	for _, dir := range m.keysByLength() {
+		meta := cloneMD(m.nearestAncestor(dir))
+		Merge(meta, m.Dirs[dir])
+		m.Dirs[dir] = meta
+	}
+}
+
+// nearestAncestor returns metadata of the nearest ancestor.
+func (m *Mapping) nearestAncestor(dir string) *dirmdpb.Metadata {
+	for {
+		parent := path.Dir(dir)
+		if parent == dir {
+			// We have reached the root.
+			return nil
+		}
+		dir = parent
+
+		if meta, ok := m.Dirs[dir]; ok {
+			return meta
+		}
+	}
+}
+
+// keysByLength returns keys sorted by length.
+// Key "." is treated as shortest of all.
+func (m *Mapping) keysByLength() []string {
+	ret := make([]string, 0, len(m.Dirs))
+	for k := range m.Dirs {
+		ret = append(ret, k)
+	}
+
+	sortKey := func(dirKey string) int {
+		// "." is considered shortest of all.
+		if dirKey == "." {
+			return -1
+		}
+		return len(dirKey)
+	}
+	sort.Slice(ret, func(i, j int) bool {
+		return sortKey(ret[i]) < sortKey(ret[j])
+	})
+	return ret
+}
+
+// Merge merges metadata from src to dst, where dst is metadata inherited from
+// ancestors and src contains directory-specific metadata.
+// Does nothing is src is nil.
+//
+// The current implementation is just proto.Merge, but it may change in the
+// future.
+func Merge(dst, src *dirmdpb.Metadata) {
+	if src != nil {
+		proto.Merge(dst, src)
+	}
+}
 
 // Reduce removes all redundant information.
 func (m *Mapping) Reduce() {
--- go/src/infra/tools/dirmd/mapping.go
+++ go/src/infra/tools/dirmd/mapping.go
@@ -5,9 +5,6 @@
 package dirmd
 
 import (
-	"path"
-	"sort"
-
 	"google.golang.org/protobuf/proto"
 
 	dirmdpb "infra/tools/dirmd/proto"
@@ -25,18 +22,6 @@ func NewMapping(size int) *Mapping {
 	}
 }
 
-// Compute computes metadata for the given directory key.
-func (m *Mapping) Compute(key string) *dirmdpb.Metadata {
-	parent := path.Dir(key)
-	if parent == key {
-		return cloneMD(m.Dirs[key])
-	}
-
-	ret := m.Compute(parent)
-	Merge(ret, m.Dirs[key])
-	return ret
-}
-
 // Proto converts m back to the protobuf message.
 func (m *Mapping) Proto() *dirmdpb.Mapping {
 	return (*dirmdpb.Mapping)(m)
@@ -45,67 +30,6 @@ func (m *Mapping) Proto() *dirmdpb.Mapping {
 // Clone returns a deep copy of m.
 func (m *Mapping) Clone() *Mapping {
 	return (*Mapping)(proto.Clone(m.Proto()).(*dirmdpb.Mapping))
-}
-
-// ComputeAll computes full metadata for each dir.
-func (m *Mapping) ComputeAll() {
-	// Process directories in the shorest-path to longest-path order,
-	// such that, when computing the expanded metadata for a given directory,
-	// we only need to check the nearest ancestor.
-	for _, dir := range m.keysByLength() {
-		meta := cloneMD(m.nearestAncestor(dir))
-		Merge(meta, m.Dirs[dir])
-		m.Dirs[dir] = meta
-	}
-}
-
-// nearestAncestor returns metadata of the nearest ancestor.
-func (m *Mapping) nearestAncestor(dir string) *dirmdpb.Metadata {
-	for {
-		parent := path.Dir(dir)
-		if parent == dir {
-			// We have reached the root.
-			return nil
-		}
-		dir = parent
-
-		if meta, ok := m.Dirs[dir]; ok {
-			return meta
-		}
-	}
-}
-
-// keysByLength returns keys sorted by length.
-// Key "." is treated as shortest of all.
-func (m *Mapping) keysByLength() []string {
-	ret := make([]string, 0, len(m.Dirs))
-	for k := range m.Dirs {
-		ret = append(ret, k)
-	}
-
-	sortKey := func(dirKey string) int {
-		// "." is considered shortest of all.
-		if dirKey == "." {
-			return -1
-		}
-		return len(dirKey)
-	}
-	sort.Slice(ret, func(i, j int) bool {
-		return sortKey(ret[i]) < sortKey(ret[j])
-	})
-	return ret
-}
-
-// Merge merges metadata from src to dst, where dst is metadata inherited from
-// ancestors and src contains directory-specific metadata.
-// Does nothing is src is nil.
-//
-// The current implementation is just proto.Merge, but it may change in the
-// future.
-func Merge(dst, src *dirmdpb.Metadata) {
-	if src != nil {
-		proto.Merge(dst, src)
-	}
 }
 
 // cloneMD returns a deep copy of meta.
