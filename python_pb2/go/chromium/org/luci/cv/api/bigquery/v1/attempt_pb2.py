3a1e8868bb3c637e1685a053a799d44966d09626
Andrii Shyshkalov <tandrii@google.com>
2021-03-22 19:01:49 +0000

    Update Python proto bindings.
    R=qyearsley, yiwzhang
    
    Bug: 1189817
    Change-Id: Ifa0cf82f92394b07b2bd4576586d831b498dde02
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/2778259
    Auto-Submit: Andrii Shyshkalov <tandrii@google.com>
    Commit-Queue: Yiwei Zhang <yiwzhang@google.com>
    Reviewed-by: Yiwei Zhang <yiwzhang@google.com>
    Cr-Commit-Position: refs/heads/master@{#39594}

--- python_pb2/go/chromium/org/luci/cv/api/bigquery/v1/attempt_pb2.py
+++ python_pb2/go/chromium/org/luci/cv/api/bigquery/v1/attempt_pb2.py
@@ -21,7 +21,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   syntax='proto3',
   serialized_options=b'Z0go.chromium.org/luci/cv/api/bigquery/v1;bigquery',
   create_key=_descriptor._internal_create_key,
-  serialized_pb=b'\n5go.chromium.org/luci/cv/api/bigquery/v1/attempt.proto\x12\x08\x62igquery\x1a\x1fgoogle/protobuf/timestamp.proto\"\xa0\x03\n\x07\x41ttempt\x12\x0b\n\x03key\x18\x01 \x01(\t\x12\x14\n\x0cluci_project\x18\x02 \x01(\t\x12\x14\n\x0c\x63onfig_group\x18\x0b \x01(\t\x12\x14\n\x0c\x63l_group_key\x18\x03 \x01(\t\x12\x1f\n\x17\x65quivalent_cl_group_key\x18\x04 \x01(\t\x12.\n\nstart_time\x18\x05 \x01(\x0b\x32\x1a.google.protobuf.Timestamp\x12,\n\x08\x65nd_time\x18\x06 \x01(\x0b\x32\x1a.google.protobuf.Timestamp\x12.\n\x0egerrit_changes\x18\x07 \x03(\x0b\x32\x16.bigquery.GerritChange\x12\x1f\n\x06\x62uilds\x18\x08 \x03(\x0b\x32\x0f.bigquery.Build\x12\'\n\x06status\x18\t \x01(\x0e\x32\x17.bigquery.AttemptStatus\x12-\n\tsubstatus\x18\n \x01(\x0e\x32\x1a.bigquery.AttemptSubstatus\x12\x1e\n\x16has_custom_requirement\x18\x0c \x01(\x08\"\xe4\x02\n\x0cGerritChange\x12\x0c\n\x04host\x18\x01 \x01(\t\x12\x0f\n\x07project\x18\x02 \x01(\t\x12\x0e\n\x06\x63hange\x18\x03 \x01(\x03\x12\x10\n\x08patchset\x18\x04 \x01(\x03\x12$\n\x1c\x65\x61rliest_equivalent_patchset\x18\x05 \x01(\x03\x12\x30\n\x0ctrigger_time\x18\x06 \x01(\x0b\x32\x1a.google.protobuf.Timestamp\x12\x1c\n\x04mode\x18\x07 \x01(\x0e\x32\x0e.bigquery.Mode\x12:\n\rsubmit_status\x18\x08 \x01(\x0e\x32#.bigquery.GerritChange.SubmitStatus\"a\n\x0cSubmitStatus\x12\x1d\n\x19SUBMIT_STATUS_UNSPECIFIED\x10\x00\x12\x0b\n\x07PENDING\x10\x01\x12\x0b\n\x07UNKNOWN\x10\x02\x12\x0b\n\x07\x46\x41ILURE\x10\x03\x12\x0b\n\x07SUCCESS\x10\x04\"\xab\x01\n\x05\x42uild\x12\n\n\x02id\x18\x01 \x01(\x03\x12\x0c\n\x04host\x18\x02 \x01(\t\x12&\n\x06origin\x18\x03 \x01(\x0e\x32\x16.bigquery.Build.Origin\x12\x10\n\x08\x63ritical\x18\x04 \x01(\x08\"N\n\x06Origin\x12\x16\n\x12ORIGIN_UNSPECIFIED\x10\x00\x12\x10\n\x0cNOT_REUSABLE\x10\x01\x12\x0e\n\nNOT_REUSED\x10\x02\x12\n\n\x06REUSED\x10\x03*7\n\x04Mode\x12\x14\n\x10MODE_UNSPECIFIED\x10\x00\x12\x0b\n\x07\x44RY_RUN\x10\x01\x12\x0c\n\x08\x46ULL_RUN\x10\x02*v\n\rAttemptStatus\x12\x1e\n\x1a\x41TTEMPT_STATUS_UNSPECIFIED\x10\x00\x12\x0b\n\x07STARTED\x10\x01\x12\x0b\n\x07SUCCESS\x10\x02\x12\x0b\n\x07\x41\x42ORTED\x10\x03\x12\x0b\n\x07\x46\x41ILURE\x10\x04\x12\x11\n\rINFRA_FAILURE\x10\x05*\xe4\x01\n\x10\x41ttemptSubstatus\x12!\n\x1d\x41TTEMPT_SUBSTATUS_UNSPECIFIED\x10\x00\x12\x10\n\x0cNO_SUBSTATUS\x10\x01\x12\x12\n\x0e\x46\x41ILED_TRYJOBS\x10\x02\x12\x0f\n\x0b\x46\x41ILED_LINT\x10\x03\x12\x0e\n\nUNAPPROVED\x10\x04\x12\x15\n\x11PERMISSION_DENIED\x10\x05\x12\x1a\n\x16UNSATISFIED_DEPENDENCY\x10\x06\x12\x11\n\rMANUAL_CANCEL\x10\x07\x12 \n\x1c\x42UILDBUCKET_MISCONFIGURATION\x10\x08\x42\x32Z0go.chromium.org/luci/cv/api/bigquery/v1;bigqueryb\x06proto3'
+  serialized_pb=b'\n5go.chromium.org/luci/cv/api/bigquery/v1/attempt.proto\x12\x08\x62igquery\x1a\x1fgoogle/protobuf/timestamp.proto\"\xa0\x03\n\x07\x41ttempt\x12\x0b\n\x03key\x18\x01 \x01(\t\x12\x14\n\x0cluci_project\x18\x02 \x01(\t\x12\x14\n\x0c\x63onfig_group\x18\x0b \x01(\t\x12\x14\n\x0c\x63l_group_key\x18\x03 \x01(\t\x12\x1f\n\x17\x65quivalent_cl_group_key\x18\x04 \x01(\t\x12.\n\nstart_time\x18\x05 \x01(\x0b\x32\x1a.google.protobuf.Timestamp\x12,\n\x08\x65nd_time\x18\x06 \x01(\x0b\x32\x1a.google.protobuf.Timestamp\x12.\n\x0egerrit_changes\x18\x07 \x03(\x0b\x32\x16.bigquery.GerritChange\x12\x1f\n\x06\x62uilds\x18\x08 \x03(\x0b\x32\x0f.bigquery.Build\x12\'\n\x06status\x18\t \x01(\x0e\x32\x17.bigquery.AttemptStatus\x12-\n\tsubstatus\x18\n \x01(\x0e\x32\x1a.bigquery.AttemptSubstatus\x12\x1e\n\x16has_custom_requirement\x18\x0c \x01(\x08\"\xe4\x02\n\x0cGerritChange\x12\x0c\n\x04host\x18\x01 \x01(\t\x12\x0f\n\x07project\x18\x02 \x01(\t\x12\x0e\n\x06\x63hange\x18\x03 \x01(\x03\x12\x10\n\x08patchset\x18\x04 \x01(\x03\x12$\n\x1c\x65\x61rliest_equivalent_patchset\x18\x05 \x01(\x03\x12\x30\n\x0ctrigger_time\x18\x06 \x01(\x0b\x32\x1a.google.protobuf.Timestamp\x12\x1c\n\x04mode\x18\x07 \x01(\x0e\x32\x0e.bigquery.Mode\x12:\n\rsubmit_status\x18\x08 \x01(\x0e\x32#.bigquery.GerritChange.SubmitStatus\"a\n\x0cSubmitStatus\x12\x1d\n\x19SUBMIT_STATUS_UNSPECIFIED\x10\x00\x12\x0b\n\x07PENDING\x10\x01\x12\x0b\n\x07UNKNOWN\x10\x02\x12\x0b\n\x07\x46\x41ILURE\x10\x03\x12\x0b\n\x07SUCCESS\x10\x04\"\xab\x01\n\x05\x42uild\x12\n\n\x02id\x18\x01 \x01(\x03\x12\x0c\n\x04host\x18\x02 \x01(\t\x12&\n\x06origin\x18\x03 \x01(\x0e\x32\x16.bigquery.Build.Origin\x12\x10\n\x08\x63ritical\x18\x04 \x01(\x08\"N\n\x06Origin\x12\x16\n\x12ORIGIN_UNSPECIFIED\x10\x00\x12\x10\n\x0cNOT_REUSABLE\x10\x01\x12\x0e\n\nNOT_REUSED\x10\x02\x12\n\n\x06REUSED\x10\x03*J\n\x04Mode\x12\x14\n\x10MODE_UNSPECIFIED\x10\x00\x12\x0b\n\x07\x44RY_RUN\x10\x01\x12\x0c\n\x08\x46ULL_RUN\x10\x02\x12\x11\n\rQUICK_DRY_RUN\x10\x03*v\n\rAttemptStatus\x12\x1e\n\x1a\x41TTEMPT_STATUS_UNSPECIFIED\x10\x00\x12\x0b\n\x07STARTED\x10\x01\x12\x0b\n\x07SUCCESS\x10\x02\x12\x0b\n\x07\x41\x42ORTED\x10\x03\x12\x0b\n\x07\x46\x41ILURE\x10\x04\x12\x11\n\rINFRA_FAILURE\x10\x05*\xe4\x01\n\x10\x41ttemptSubstatus\x12!\n\x1d\x41TTEMPT_SUBSTATUS_UNSPECIFIED\x10\x00\x12\x10\n\x0cNO_SUBSTATUS\x10\x01\x12\x12\n\x0e\x46\x41ILED_TRYJOBS\x10\x02\x12\x0f\n\x0b\x46\x41ILED_LINT\x10\x03\x12\x0e\n\nUNAPPROVED\x10\x04\x12\x15\n\x11PERMISSION_DENIED\x10\x05\x12\x1a\n\x16UNSATISFIED_DEPENDENCY\x10\x06\x12\x11\n\rMANUAL_CANCEL\x10\x07\x12 \n\x1c\x42UILDBUCKET_MISCONFIGURATION\x10\x08\x42\x32Z0go.chromium.org/luci/cv/api/bigquery/v1;bigqueryb\x06proto3'
   ,
   dependencies=[google_dot_protobuf_dot_timestamp__pb2.DESCRIPTOR,])
 
@@ -47,11 +47,16 @@ _MODE = _descriptor.EnumDescriptor(
       serialized_options=None,
       type=None,
       create_key=_descriptor._internal_create_key),
+    _descriptor.EnumValueDescriptor(
+      name='QUICK_DRY_RUN', index=3, number=3,
+      serialized_options=None,
+      type=None,
+      create_key=_descriptor._internal_create_key),
   ],
   containing_type=None,
   serialized_options=None,
   serialized_start=1052,
-  serialized_end=1107,
+  serialized_end=1126,
 )
 _sym_db.RegisterEnumDescriptor(_MODE)
 
@@ -96,8 +101,8 @@ _ATTEMPTSTATUS = _descriptor.EnumDescriptor(
   ],
   containing_type=None,
   serialized_options=None,
-  serialized_start=1109,
-  serialized_end=1227,
+  serialized_start=1128,
+  serialized_end=1246,
 )
 _sym_db.RegisterEnumDescriptor(_ATTEMPTSTATUS)
 
@@ -157,8 +162,8 @@ _ATTEMPTSUBSTATUS = _descriptor.EnumDescriptor(
   ],
   containing_type=None,
   serialized_options=None,
-  serialized_start=1230,
-  serialized_end=1458,
+  serialized_start=1249,
+  serialized_end=1477,
 )
 _sym_db.RegisterEnumDescriptor(_ATTEMPTSUBSTATUS)
 
@@ -166,6 +171,7 @@ AttemptSubstatus = enum_type_wrapper.EnumTypeWrapper(_ATTEMPTSUBSTATUS)
 MODE_UNSPECIFIED = 0
 DRY_RUN = 1
 FULL_RUN = 2
+QUICK_DRY_RUN = 3
 ATTEMPT_STATUS_UNSPECIFIED = 0
 STARTED = 1
 SUCCESS = 2
