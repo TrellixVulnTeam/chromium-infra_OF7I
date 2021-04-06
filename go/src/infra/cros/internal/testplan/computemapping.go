53aff9acc9cffb287c046cea3656cdcd8f2d03bb
Andrew Lamb <andrewlamb@chromium.org>
2021-04-06 15:21:40 +0000

    testplans: Checkout patchsets and compute Mapping.
    - Since test_plan may run in a context without a full ChromeOS
    checkout (e.g. orchestrator), it needs to checkout the input
    CLs in order to compute the relevant testing config.
    
    BUG=b:182898188
    TEST=CQ
    
    Change-Id: I242f2a21ee01c19450da6272be9416ca16956eb9
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/2806397
    Commit-Queue: Andrew Lamb <andrewlamb@chromium.org>
    Reviewed-by: Sean McAllister <smcallis@google.com>
    Cr-Commit-Position: refs/heads/master@{#39933}

---
+++ go/src/infra/cros/internal/testplan/computemapping.go
@@ -0,0 +1,136 @@
+package testplan
+
+import (
+	"fmt"
+	"infra/cros/internal/gerrit"
+	"infra/cros/internal/git"
+	"infra/tools/dirmd"
+	dirmdpb "infra/tools/dirmd/proto"
+	"io/ioutil"
+	"os"
+	"sort"
+	"strings"
+
+	"go.chromium.org/luci/common/data/stringset"
+)
+
+// projectMappingInfo groups a computed Mapping and affected files for a set
+// of ChangeRevs in a project.
+type projectMappingInfo struct {
+	Mapping       *dirmd.Mapping
+	AffectedFiles []string
+}
+
+var (
+	// Allow overriding workdir creation and cleanup behavior for testing.
+	workdirFn        = ioutil.TempDir
+	workdirCleanupFn = os.RemoveAll
+)
+
+// checkoutChangeRevs checkouts changeRevs to dir.
+//
+// changeRevs must all have the same project.
+func checkoutChangeRevs(dir string, changeRevs []*gerrit.ChangeRev) error {
+	for i, changeRev := range changeRevs {
+		if i > 0 && changeRev.Project != changeRevs[0].Project {
+			// Change revs are sorted by project in the callers.
+			panic(
+				"all changeRevs passed to checkoutChangeRevs must have the same Project",
+			)
+		}
+	}
+
+	// Checkout the newest change for the project, which should have all other
+	// changes below it.
+	//
+	// TODO(b/182898188): Checking out the newest change might cause problems
+	// in edge cases such as rebases that reorder CLs. Investigate whether
+	// cherry-picking all changes is necessary.
+	sort.Slice(changeRevs, func(i, j int) bool {
+		return changeRevs[i].ChangeNum < changeRevs[j].ChangeNum
+	})
+
+	changeRev := changeRevs[len(changeRevs)-1]
+
+	googlesourceHost := strings.Replace(changeRev.Host, "-review", "", 1)
+	remote := fmt.Sprintf("https://%s/%s", googlesourceHost, changeRev.Project)
+
+	if err := git.Clone(remote, dir); err != nil {
+		return err
+	}
+
+	if err := git.Fetch(dir, remote, changeRev.Ref); err != nil {
+		return err
+	}
+
+	return git.Checkout(dir, "FETCH_HEAD")
+}
+
+// computeMappingForChangeRevs checks out a project with changeRevs applied and
+// computes the Mapping.
+//
+// changeRevs must all have the same project.
+func computeMappingForChangeRevs(changeRevs []*gerrit.ChangeRev) (*dirmd.Mapping, error) {
+	workdir, err := workdirFn("", "")
+	if err != nil {
+		return nil, err
+	}
+
+	defer workdirCleanupFn(workdir)
+
+	if err := checkoutChangeRevs(workdir, changeRevs); err != nil {
+		return nil, err
+	}
+
+	return dirmd.ReadMapping(workdir, dirmdpb.MappingForm_COMPUTED)
+}
+
+// computeProjectMappingInfos calculates a projectMappingInfo for each project
+// in changeRevs.
+func computeProjectMappingInfos(changeRevs []*gerrit.ChangeRev) ([]*projectMappingInfo, error) {
+	projectToChangeRevs := make(map[string][]*gerrit.ChangeRev)
+	projectToAffectedFiles := make(map[string]stringset.Set)
+
+	for _, changeRev := range changeRevs {
+		project := changeRev.Project
+
+		if _, found := projectToChangeRevs[project]; !found {
+			projectToChangeRevs[project] = make([]*gerrit.ChangeRev, 0)
+		}
+
+		projectToChangeRevs[project] = append(projectToChangeRevs[project], changeRev)
+
+		if _, found := projectToAffectedFiles[project]; !found {
+			projectToAffectedFiles[project] = stringset.New(0)
+		}
+
+		projectToAffectedFiles[project].AddAll(changeRev.Files)
+	}
+
+	projectMappingInfos := make([]*projectMappingInfo, 0, len(projectToChangeRevs))
+
+	// Use a sorted list of keys from projectToChangeRevs, so iteration order is
+	// deterministic.
+	keys := make([]string, 0, len(projectToChangeRevs))
+	for project := range projectToChangeRevs {
+		keys = append(keys, project)
+	}
+
+	sort.Strings(keys)
+
+	for _, project := range keys {
+		changeRevs := projectToChangeRevs[project]
+
+		mapping, err := computeMappingForChangeRevs(changeRevs)
+		if err != nil {
+			return nil, err
+		}
+
+		projectMappingInfos = append(projectMappingInfos, &projectMappingInfo{
+			AffectedFiles: projectToAffectedFiles[project].ToSlice(),
+			Mapping:       mapping,
+		})
+	}
+
+	return projectMappingInfos, nil
+}
---
+++ go/src/infra/cros/internal/testplan/computemapping_test.go
@@ -0,0 +1,130 @@
+package testplan
+
+import (
+	"infra/cros/internal/cmd"
+	"infra/cros/internal/gerrit"
+	"infra/cros/internal/git"
+	"infra/tools/dirmd"
+	dirmdpb "infra/tools/dirmd/proto"
+	"infra/tools/dirmd/proto/chromeos"
+	"reflect"
+	"sort"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"go.chromium.org/chromiumos/config/go/test/plan"
+	"google.golang.org/protobuf/testing/protocmp"
+)
+
+func TestComputeProjectMappingInfos(t *testing.T) {
+	// Two changes from testprojectA, one from testprojectB.
+	changeRevs := []*gerrit.ChangeRev{
+		{
+			ChangeRevKey: gerrit.ChangeRevKey{
+				Host:      "chromium-review.googlesource.com",
+				ChangeNum: 123,
+			},
+			Project: "chromium/testprojectA",
+			Ref:     "refs/changes/23/123/5",
+			Files:   []string{"a/b/test1.txt", "a/b/test2.txt"},
+		},
+		{
+			ChangeRevKey: gerrit.ChangeRevKey{
+				Host:      "chromium-review.googlesource.com",
+				ChangeNum: 456,
+			},
+			Project: "chromium/testprojectA",
+			Ref:     "refs/changes/45/456/2",
+			Files:   []string{"DIR_METADATA"},
+		},
+		{
+			ChangeRevKey: gerrit.ChangeRevKey{
+				Host:      "chromium-review.googlesource.com",
+				ChangeNum: 789,
+			},
+			Project: "chromium/testprojectB",
+			Ref:     "refs/changes/78/789/5",
+			Files:   []string{"test.c", "test.h"},
+		},
+	}
+
+	// The newest change for each project should be checked out.
+	git.CommandRunnerImpl = &cmd.FakeCommandRunnerMulti{
+		CommandRunners: []cmd.FakeCommandRunner{
+			{
+				ExpectedCmd: []string{"git", "clone", "https://chromium.googlesource.com/chromium/testprojectA", "testdata"},
+			},
+			{
+				ExpectedCmd: []string{"git", "fetch", "https://chromium.googlesource.com/chromium/testprojectA", "refs/changes/45/456/2"},
+			},
+			{
+				ExpectedCmd: []string{"git", "checkout", "FETCH_HEAD"},
+			},
+			{
+				ExpectedCmd: []string{"git", "clone", "https://chromium.googlesource.com/chromium/testprojectB", "testdata"},
+			},
+			{
+				ExpectedCmd: []string{"git", "fetch", "https://chromium.googlesource.com/chromium/testprojectB", "refs/changes/78/789/5"},
+			},
+			{
+				ExpectedCmd: []string{"git", "checkout", "FETCH_HEAD"},
+			},
+		},
+	}
+
+	// Set workdirFn so the CommandRunners can know where commands are run,
+	// and the DIR_METADATA in testdata is read. Don't cleanup the testdata.
+	workdirFn = func(_, _ string) (string, error) { return "./testdata", nil }
+	workdirCleanupFn = func(_ string) error { return nil }
+
+	projectMappingInfos, err := computeProjectMappingInfos(changeRevs)
+	if err != nil {
+		t.Fatalf("computeProjectMappingInfos(%v) failed: %s", changeRevs, err)
+	}
+
+	// Both projects read the same DIR_METADATA, so have the same expected
+	// Mapping.
+	expectedMapping := &dirmd.Mapping{
+		Dirs: map[string]*dirmdpb.Metadata{
+			".": {
+				Chromeos: &chromeos.ChromeOS{
+					Cq: &chromeos.ChromeOS_CQ{
+						SourceTestPlans: []*plan.SourceTestPlan{
+							{
+								EnabledTestEnvironments: []plan.SourceTestPlan_TestEnvironment{
+									plan.SourceTestPlan_HARDWARE,
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	expectedAffectedFiles := [][]string{
+		{"a/b/test1.txt", "a/b/test2.txt", "DIR_METADATA"},
+		{"test.c", "test.h"},
+	}
+
+	for i, pmi := range projectMappingInfos {
+		if diff := cmp.Diff(
+			expectedMapping.Proto(), pmi.Mapping.Proto(), protocmp.Transform(),
+		); diff != "" {
+			t.Errorf(
+				"computeProjectMappingInfos returned unexpected diff in mappings at index %d (-want +got):\n%s",
+				i, diff,
+			)
+		}
+
+		sort.Strings(expectedAffectedFiles[i])
+		sort.Strings(pmi.AffectedFiles)
+
+		if !reflect.DeepEqual(expectedAffectedFiles[i], pmi.AffectedFiles) {
+			t.Errorf(
+				"computeProjectMappingInfos returned affectedFiles %v, expected %v",
+				pmi.AffectedFiles,
+				expectedAffectedFiles[i],
+			)
+		}
+	}
+}
