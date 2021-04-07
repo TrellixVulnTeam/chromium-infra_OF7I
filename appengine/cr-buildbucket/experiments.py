ddf5c06d226b5f4ff9678b5f5787491d2ac3dbb1
Robbie Iannucci <iannucci@chromium.org>
2021-04-07 00:21:11 +0000

    Revert "[buildbucket/py] Add luci.swarming.use_rbe_cas temporarially."
    This reverts commit ee103cb455c55aef0cc37cf0d7db17e608bb00df.
    
    Reason for revert: Experiment was renamed to not be 'luci.'
    
    Original change's description:
    > [buildbucket/py] Add luci.swarming.use_rbe_cas temporarially.
    >
    > This will allow us to roll out the new experiment support CLs today; We
    > can rename the experiment in parallel.
    >
    > In particular, this does NOT ensure that "luci.swarming.use_rbe_cas"
    > is marked as negative on builds which don't have it, since we're
    > planning to rename it.
    >
    > R=​smut, tikuta, vadimsh, yiwzhang, yuanjunh
    >
    > Bug: 1093970,1143122
    > Change-Id: I84c3b9e3fb292ff88e084c6e258dc89600600abb
    > Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/2807534
    > Commit-Queue: Robbie Iannucci <iannucci@chromium.org>
    > Auto-Submit: Robbie Iannucci <iannucci@chromium.org>
    > Reviewed-by: Vadim Shtayura <vadimsh@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#39946}
    
    Bug: 1093970
    Bug: 1143122
    Change-Id: I1b8beb029fc7d3ee74d2398bb46f6ff9dcca2a31
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/2809833
    Auto-Submit: Robbie Iannucci <iannucci@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#39964}

--- appengine/cr-buildbucket/experiments.py
+++ appengine/cr-buildbucket/experiments.py
@@ -6,13 +6,9 @@ import re
 
 # Names of well-known experiments
 CANARY = 'luci.buildbucket.canary_software'
-USE_BBAGENT = 'luci.buildbucket.use_bbagent'
 NON_PROD = 'luci.non_production'
+USE_BBAGENT = 'luci.buildbucket.use_bbagent'
 USE_REALMS = 'luci.use_realms'
-
-# TODO(iannucci): remove this when uses of this experiment rename to a
-# different experiment.
-USE_RBE_CAS = 'luci.swarming.use_rbe_cas'
 
 WELL_KNOWN = frozenset([
     CANARY,
@@ -28,10 +24,6 @@ def check_invalid_name(exp_name):
   """Returns an error message string if this is an invalid expirement.
 
   Returns None if `exp_name` is a valid experiment name."""
-  # TODO(iannucci): Remove this when USE_RBE_CAS is removed.
-  if exp_name == USE_RBE_CAS:
-    return
-
   if not _VALID_NAME.match(exp_name):
     return 'does not match %r' % _VALID_NAME.pattern
 
