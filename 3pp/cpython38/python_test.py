f964a42034913612e9693f12ddf7ce00b66ebfc3
Brian Ryner <bryner@google.com>
2021-04-12 04:17:40 +0000

    Also stop using a symlink for python_test.py.
    Bug: 1187306
    Change-Id: I07be07c7aaa9a050736562dc01886e043050c2fc
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/2820341
    Auto-Submit: Brian Ryner <bryner@google.com>
    Reviewed-by: Owen Rodley <orodley@chromium.org>
    Commit-Queue: Owen Rodley <orodley@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#40072}

--- 3pp/cpython38/python_test.py
+++ 3pp/cpython38/python_test.py
@@ -1,1 +1,90 @@
-../cpython_common/python_test.py
+# Copyright 2018 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import os
+import sys
+import shutil
+import subprocess
+import tempfile
+import unittest
+
+PYTHON_TEST_CIPD_PACKAGE = None
+
+
+class TestPython(unittest.TestCase):
+
+  # Public repository that uses HTTPS.
+  HTTPS_REPO_URL = 'https://chromium.googlesource.com/infra/infra'
+
+  @classmethod
+  def setUpClass(cls):
+    cls._is_windows = os.name == 'nt'
+    cls._exe_suffix = '.exe' if cls._is_windows else ''
+
+    cls.tdir = tempfile.mkdtemp(dir=os.getcwd(), suffix='test_python')
+
+    cls.pkg_dir = os.path.join(cls.tdir, 'install')
+    subprocess.check_call(
+        ['cipd', 'pkg-deploy', PYTHON_TEST_CIPD_PACKAGE, '-root', cls.pkg_dir],
+        shell=cls._is_windows)
+    cls.python = os.path.join(cls.pkg_dir, 'bin', 'python3' + cls._exe_suffix)
+
+  @classmethod
+  def tearDownClass(cls):
+    shutil.rmtree(cls.tdir, ignore_errors=True)
+
+  def setUp(self):
+    self.test_tdir = tempfile.mkdtemp(dir=self.tdir)
+    self.env = os.environ.copy()
+
+  def _write_file(self, content):
+    fd = None
+    try:
+      fileno, path = tempfile.mkstemp(dir=self.test_tdir)
+      fd = os.fdopen(fileno, 'w')
+      fd.write(content)
+      return path
+    finally:
+      if fd:
+        fd.close()
+
+  def test_version(self):
+    output = subprocess.check_output([self.python, '--version'],
+                                     stderr=subprocess.STDOUT)
+    self.assertTrue(output.startswith('Python '))
+
+    expected_version = os.environ['_3PP_VERSION']
+    # On windows we don't append the patch version because we actually bundle
+    # the official python release, so don't have an opportunity to change the
+    # version string.
+    if '_3PP_PATCH_VERSION' in os.environ and not self._is_windows:
+      expected_version += '+' + os.environ['_3PP_PATCH_VERSION']
+
+    self.assertEqual(output.lstrip('Python ').strip(), expected_version)
+
+  def test_package_import(self):
+    for pkg in ('ctypes', 'ssl', 'io', 'binascii', 'hashlib', 'sqlite3'):
+      script = 'import %s; print(%s)' % (pkg, pkg)
+      rv = subprocess.call([self.python, '-c', script])
+      self.assertEqual(rv, 0, 'Could not import %r.' % (pkg,))
+
+  def test_use_https(self):
+    script = 'import urllib.request; print(urllib.request.urlopen("%s"))' % (
+        self.HTTPS_REPO_URL)
+    rv = subprocess.call([self.python, '-c', script])
+    self.assertEqual(rv, 0)
+
+
+if __name__ == '__main__':
+  platform = os.environ['_3PP_PLATFORM']
+  tool_platform = os.environ['_3PP_TOOL_PLATFORM']
+  if 'windows' not in platform and platform != tool_platform:
+    print 'SKIPPING TESTS'
+    print '  platform:', platform
+    print '  tool_platform:', tool_platform
+    sys.exit(0)
+
+  PYTHON_TEST_CIPD_PACKAGE = sys.argv[1]
+  sys.argv.pop(1)
+  unittest.main()
