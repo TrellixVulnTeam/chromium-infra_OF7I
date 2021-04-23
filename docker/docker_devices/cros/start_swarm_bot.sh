09b0778eeacbaa794e8c8cd308ec3d12ab143dd4
Junji Watanabe <jwata@google.com>
2021-04-23 17:03:02 +0000

    migrate all CrOS docker bots to use Python3
    The bot build32-a1--build10-a1 has been running in Python3.
    https://chromium-swarm.appspot.com/bot?id=build32-a1--build10-a1
    
    This CL migrates all CrOS docker bots to use Python3 to run Swarming process.
    https://chromium-swarm.appspot.com/botlist?c=id&c=task&c=bot_config&c=os&c=status&d=asc&f=os%3AChromeOS&f=inside_docker%3A1&k=inside_docker&s=id
    
    Bug: 1111688
    Change-Id: I71b68ecaee269133e2582128d4381d109b03453c
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/2847902
    Auto-Submit: Junji Watanabe <jwata@google.com>
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Ben Pastene <bpastene@chromium.org>
    Commit-Queue: Ben Pastene <bpastene@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#40361}

--- docker/docker_devices/cros/start_swarm_bot.sh
+++ docker/docker_devices/cros/start_swarm_bot.sh
@@ -35,14 +35,6 @@ su -c "/usr/bin/curl -sSLOJ $SWARM_URL" chrome-bot
 echo "Starting $SWARM_ZIP"
 # Run the swarming bot in the background, and immediately wait for it. This
 # allows the signal trapping to actually work.
-# Test out python3 on a single container.
-# TODO(crbug.com/1111688): rollout all containers.
-if [[ "$(hostname -s)" = "build32-a1--build10-a1" ]]; then
-  py_bin="/usr/bin/python3"
-else
-  py_bin="/usr/bin/python"
-fi
-
-su -c "${py_bin} $SWARM_ZIP start_bot" chrome-bot &
+su -c "/usr/bin/python3 $SWARM_ZIP start_bot" chrome-bot &
 wait %1
 exit $?
