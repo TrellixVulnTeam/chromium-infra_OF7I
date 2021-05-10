c79be62ed1e73ee3c71c7c4331603fea9da7d266
Andrey Logvin <landrey@google.com>
2021-05-10 10:12:22 +0000

    chromeperf: Add skeleton for alert_groups server definition
    Bug: chromium:1206607
    Change-Id: Ie1e37b53dd9ae7c58484c9af2a4d52a6d361cba7
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/2878034
    Commit-Queue: Andrey Logvin <landrey@google.com>
    Reviewed-by: Dean Berris <dberris@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#40774}

---
+++ go/src/infra/chromeperf/alert_groups/server/server.go
@@ -0,0 +1,56 @@
+// Copyright 2021 The Chromium Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package server
+
+import (
+	"flag"
+	"infra/chromeperf/alert_groups"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/health"
+	"google.golang.org/grpc/health/grpc_health_v1"
+)
+
+type alertGroupsServer struct {
+	alert_groups.UnimplementedAlertGroupsServer
+}
+
+// Main is the actual main body function. It registers flags, parses them, and
+// kicks off a gRPC service to host
+func Main() {
+	port := flag.String("port", ":60800", "Tcp port that the service should listen.")
+
+	// TODO(crbug/1059667): Wire up a cloud logging implementation (Stackdriver).
+	flag.Parse()
+
+	lis, err := net.Listen("tcp", *port)
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	h := health.NewServer()
+
+	server := &alertGroupsServer{}
+	alert_groups.RegisterAlertGroupsServer(s, server)
+	h.SetServingStatus("alert_groups", grpc_health_v1.HealthCheckResponse_SERVING)
+	h.SetServingStatus("", grpc_health_v1.HealthCheckResponse_SERVING)
+	grpc_health_v1.RegisterHealthServer(s, h)
+	log.Println("Listening on ", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
+}
