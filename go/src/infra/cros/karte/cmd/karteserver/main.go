758863f9c4437bfc7f4999873cb0a28c4e3524df
Gregory Nisbet <gregorynisbet@google.com>
2021-05-11 23:30:11 +0000

    [karte] Add hello world route.
    Add a hello world route to Karte and config
    middleware.
    
    BUG=183724382
    TEST=make integ
    
    Change-Id: I353adbd739ee44d38416f8c045c2a06f254b5e2d
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/2857517
    Commit-Queue: Gregory Nisbet <gregorynisbet@google.com>
    Reviewed-by: Allen Li <ayatane@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#40841}

--- go/src/infra/cros/karte/cmd/karteserver/main.go
+++ go/src/infra/cros/karte/cmd/karteserver/main.go
@@ -9,24 +9,27 @@ package main
 // application starts.
 
 import (
-	"fmt"
-
+	"go.chromium.org/luci/common/logging"
+	"go.chromium.org/luci/config/server/cfgmodule"
 	"go.chromium.org/luci/server"
 	"go.chromium.org/luci/server/gaeemulation"
 	"go.chromium.org/luci/server/module"
+	"go.chromium.org/luci/server/router"
 )
-
-// Entrypoint is the server entrypoint. It installs services
-// and sets up background processes.
-func entrypoint(srv *server.Server) error {
-	return fmt.Errorf("not implemented")
-}
 
 // Transfer control to the LUCI server
 func main() {
 	modules := []module.Module{
 		gaeemulation.NewModuleFromFlags(),
+		cfgmodule.NewModuleFromFlags(),
 	}
 
-	server.Main(nil, modules, entrypoint)
+	server.Main(nil, modules, func(srv *server.Server) error {
+		srv.Routes.GET("/hello-world", router.MiddlewareChain{}, func(ctx *router.Context) {
+			logging.Debugf(ctx.Context, "Hello world. a2c29304-30e1-41a2-b85e-e7f85eef4fd9.")
+			ctx.Writer.Write([]byte("Hello world. 4a9cd07f-6dd9-4d00-9f99-4086b58045cb."))
+		})
+
+		return nil
+	})
 }
