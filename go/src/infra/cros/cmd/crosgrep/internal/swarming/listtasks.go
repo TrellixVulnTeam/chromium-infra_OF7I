6941363e1544e9677bc54558d67dbd7a28ce0faf
Gregory Nisbet <gregorynisbet@google.com>
2021-05-26 16:54:19 +0000

    [crosgrep] Add query for unix time range
    Add a parameterized bigquery query that gathers results for
    the past hour by default.
    
    BUG=b/183971231
    TEST=make AND ./crosgrep AND observe results for past hour
    
    Change-Id: Ifac7e66ca88944ce8f7160c7b8c8738762238b70
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/2915122
    Reviewed-by: Otabek Kasimov <otabek@google.com>
    Commit-Queue: Gregory Nisbet <gregorynisbet@google.com>
    Cr-Commit-Position: refs/heads/master@{#41247}

--- go/src/infra/cros/cmd/crosgrep/internal/swarming/listtasks.go
+++ go/src/infra/cros/cmd/crosgrep/internal/swarming/listtasks.go
@@ -8,6 +8,7 @@ import (
 	"context"
 	"fmt"
 	"strings"
+	"time"
 
 	"cloud.google.com/go/bigquery"
 	"go.chromium.org/luci/common/errors"
@@ -19,27 +20,59 @@ import (
 type bqRow = []bigquery.Value
 
 // Limit on the number of swarming tasks that can be retrieved by a single query.
-const swarmingTasksLimit = 1000
+const swarmingTasksLimit = 10000
 
 // This is the pattern for a query that grabs a number of rows corresponding
 // to swarming tasks out of bigquery.
 //
-// Free vars in order: [%d limit]
+// Free vars in order: [%d epoch_time_start, %d epoch_time_end, %d limit]
 //
-// Sample query for [1000]:
+// Note that the interval is closed at the beginning and open at the end
 //
-//   SELECT task_id,
-//   FROM `chromeos-swarming`.swarming.task_results_summary
-//   WHERE REGEXP_CONTAINS(bot.bot_id, r'^(?i)crossk[-]')
-//   ORDER BY end_time DESC
-//   LIMIT 1000
+// Sample query for [1621881767, 1621881769, 1234]:
 //
-const listAllSwarmingTasksPattern = "" +
-	"SELECT task_id,\n" +
+// SELECT bot.bot_id, task_id, UNIX_SECONDS(end_time),
+// FROM `chromeos-swarming`.swarming.task_results_summary
+// WHERE
+//   REGEXP_CONTAINS(bot.bot_id, r'^(?i)crossk[-]')
+//     AND 1621881767 <= UNIX_SECONDS(end_time)
+//     AND 1621881769  > UNIX_SECONDS(end_time)
+//     LIMIT 1234
+//
+const listSwarmingTasksInRangePattern = "" +
+	"SELECT bot.bot_id, task_id, UNIX_SECONDS(end_time),\n" +
 	"FROM `chromeos-swarming`.swarming.task_results_summary\n" +
-	"WHERE REGEXP_CONTAINS(bot.bot_id, r'(?i)^crossk[-]')\n" +
-	"ORDER BY end_time DESC\n" +
+	"WHERE\n" +
+	"    REGEXP_CONTAINS(bot.bot_id, r'^(?i)crossk[-]')\n" +
+	"    AND %d <= UNIX_SECONDS(end_time)" +
+	"    AND %d  > UNIX_SECONDS(end_time)" +
 	"LIMIT %d\n"
+
+// MakeSwarmingTasksInRangeQuery makes a list of all swarming tasks in a particular time range
+// up to a specified limit.
+//
+// limit:      limit on the number of tasks to return
+// rangeStart: the time to begin the search
+// rangeStop:  the time to end the search
+//
+func MakeSwarmingTasksInRangeQuery(limit int, rangeStart int64, rangeStop int64) string {
+	if limit == 0 {
+		limit = swarmingTasksLimit
+	}
+	// Default start time is one hour before the present.
+	if rangeStart == 0 {
+		rangeStop = time.Now().Unix() - 3600
+	}
+	// The default stop time is one second after the present.
+	// It is this value rather than simply the present because
+	// in the query the intervals are all of the form [start, stop).
+	if rangeStop == 0 {
+		rangeStop = time.Now().Unix() + 1
+	}
+	// Make sure that the variables below are provided in the correct order for the pattern.
+	// If the query changes, the order of its "free variables" may change as well.
+	return fmt.Sprintf(listSwarmingTasksInRangePattern, rangeStart, rangeStop, limit)
+}
 
 // getRowIterator returns a row iterator from a sql query.
 func getRowIterator(ctx context.Context, client *bigquery.Client, sql string) (*bigquery.RowIterator, error) {
@@ -55,7 +88,7 @@ func getSwarmingTasks(ctx context.Context, client *bigquery.Client) (*bigquery.R
 	if client == nil {
 		panic("client cannot be nil")
 	}
-	it, err := getRowIterator(ctx, client, fmt.Sprintf(listAllSwarmingTasksPattern, swarmingTasksLimit))
+	it, err := getRowIterator(ctx, client, MakeSwarmingTasksInRangeQuery(0, 0, 0))
 	return it, errors.Annotate(err, "get iterator").Err()
 }
 
