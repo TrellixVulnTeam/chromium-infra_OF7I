3a8b9192a38d0dad228e5921f12a1a679fc08910
Jae Hoon Kim <kimjae@chromium.org>
2021-07-01 22:38:22 +0000

    prototype-tlw: chromeos*-devservers are gone now
    Use chromeos*-cache* instead.
    
    BUG=none
    TEST=local TLW
    
    Change-Id: Id3c8197f2a0ac224c037187a4811ddb46069bf8e
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/3002384
    Auto-Submit: Jae Hoon Kim <kimjae@chromium.org>
    Reviewed-by: Congbin Guo <guocb@chromium.org>
    Commit-Queue: Congbin Guo <guocb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#42032}

--- go/src/infra/cros/cmd/prototype-tlw/cache.go
+++ go/src/infra/cros/cmd/prototype-tlw/cache.go
@@ -15,9 +15,10 @@ import (
 // cache implements the logic for the CacheForDut method and runs as a goroutine.
 func (s *server) cache(ctx context.Context, parsedURL *url.URL, dutName, opName string) {
 	log.Printf("CacheForDut: Started Operation = %v", opName)
-	// Devserver URL to be used. In the "real" CacheForDut implementation,
-	// devservers should be resolved here.
-	const baseURL = "http://chromeos6-devserver2:8888/download/"
+	// GS cache URL to be used.
+	// In the "real" CacheForDut implementation, GS cache should be resolved here.
+	// TODO(guocb): Use chromeos6-cache2 instead of IP.
+	const baseURL = "http://100.115.220.100:8888/download/"
 	if err := s.lroMgr.SetResult(opName, &tls.CacheForDutResponse{
 		Url: baseURL + parsedURL.Host + parsedURL.Path,
 	}); err != nil {
