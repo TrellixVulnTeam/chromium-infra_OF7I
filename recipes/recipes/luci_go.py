bcc68b2b93220b2bc2077504822add5736a85623
Vadim Shtayura <vadimsh@chromium.org>
2021-07-22 18:33:00 +0000

    [recipes] Switch all infra builders into using Go Modules.
    To verify it works end-to-end (i.e. staging GAE and GKE services
    deployed from thus built code still work).
    
    R=tandrii@chromium.org, iannucci@chromium.org
    BUG=1057067
    
    Change-Id: Ic1deb5df5317d71ebe28969acb7334d1d17ddeea
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/3044974
    Reviewed-by: Andrii Shyshkalov <tandrii@google.com>
    Commit-Queue: Vadim Shtayura <vadimsh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#42547}

--- recipes/recipes/luci_go.py
+++ recipes/recipes/luci_go.py
@@ -32,9 +32,6 @@ PROPERTIES = {
             default=None,
             kind=str,
             help='A go version variant to bootstrap, see bootstrap.py'),
-    'go_modules':
-        Property(
-            default=False, kind=bool, help='Whether to run in Go Modules mode'),
     'run_integration_tests':
         Property(
             default=False, kind=bool, help='Whether to run integration tests'),
@@ -96,13 +93,11 @@ def apply_golangci_lint(api, co):
 
 
 def RunSteps(
-    api, GOARCH, go_version_variant, go_modules,
-    run_integration_tests, run_lint):
+    api, GOARCH, go_version_variant, run_integration_tests, run_lint):
   co = api.infra_checkout.checkout(
       'luci_go',
       patch_root=LUCI_GO_PATH_IN_INFRA,
-      go_version_variant=go_version_variant,
-      go_modules=go_modules)
+      go_version_variant=go_version_variant)
   is_presubmit = 'presubmit' in api.buildbucket.builder_name.lower()
   if is_presubmit or run_lint:
     co.commit_change()
@@ -198,15 +193,4 @@ def GenTests(api):
         change_number=607472,
         patch_set=2,
     ) + api.properties(go_version_variant='bleeding_edge')
-  )
-
-  yield (
-    api.test('go_modules') +
-    api.platform('linux', 64) +
-    api.buildbucket.try_build(
-        'infra', 'try', 'luci-go-trusty-64',
-        git_repo='https://chromium.googlesource.com/infra/luci/luci-go',
-        change_number=607472,
-        patch_set=2,
-    ) + api.properties(go_modules=True)
   )
