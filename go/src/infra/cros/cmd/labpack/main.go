182a2f6689087d6f54b1ef3bb29230c7f3fc85f3
Otabek Kasimov <otabek@google.com>
2021-08-02 23:20:20 +0000

    labpack: Integrate recovery engine
    BUG=b:194847368
    TEST=build
    
    Change-Id: I11c11ada7b218535e5ca565922799bbfe4f93132
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/3064343
    Auto-Submit: Otabek Kasimov <otabek@google.com>
    Reviewed-by: Gregory NISBET <gregorynisbet@google.com>
    Commit-Queue: Otabek Kasimov <otabek@google.com>
    Cr-Commit-Position: refs/heads/master@{#42787}

--- go/src/infra/cros/cmd/labpack/main.go
+++ go/src/infra/cros/cmd/labpack/main.go
@@ -4,7 +4,7 @@
 
 /*
 The labpack program allows to run repair tasks for ChromeOS devices in the lab.
-For more information please read go/AdminRepair.
+For more information please read go/paris-.
 Managed by Chrome Fleet Software (go/chrome-fleet-software).
 */
 package main
@@ -16,76 +16,77 @@ import (
 	"os"
 	"path/filepath"
 
-	steps "infra/cros/cmd/labpack/steps"
-
 	"github.com/golang/protobuf/jsonpb"
 	"go.chromium.org/luci/common/errors"
-	"go.chromium.org/luci/common/logging"
 	"go.chromium.org/luci/luciexe/build"
+
+	"infra/cros/cmd/labpack/site"
+	steps "infra/cros/cmd/labpack/steps"
+	"infra/cros/cmd/labpack/tlw"
+	"infra/cros/recovery"
 )
 
 func main() {
 	log.SetPrefix(fmt.Sprintf("%s: ", filepath.Base(os.Args[0])))
-	log.Printf("Starting with args: %s", os.Args)
+	log.Printf("Running version: %s", site.VersionNumber)
 
 	input := &steps.LabpackInput{}
-	if isLocalRun() {
-		log.Printf("Running in local mode")
-		ctx := context.Background()
-		state, ctx, err := build.Start(ctx, nil)
-		defer func() { state.End(err) }()
-		err = internalRun(ctx, input, state)
-		return
-	}
 	log.Printf("Running in build-bucket mode")
-
-	/*
-		Notes for the next step.
-		The input looks like
-		{
-			"$chromeos/service_version": {
-				"version": {
-				"skylabTool": "2"
-				}
-			},
-			"labpack": "length",
-			"your_name": "Supper user 1234"
-		}
-		need proto to read it like:
-		message ServiceVersion {
-			message VersionInfo {
-				string skylabTool = 1;
-			}
-			VersionInfo version = 1;
-		}
-		to read version information
-		var crosServiceVersionReader func(context.Context) *ServiceVersion
-		func init() {
-			MakePropertyReader("$chromeos/service_version", &crosServiceVersionReader)
-		}
-		and then we can read version input as
-		serviceVersion := crosServiceVersionReader(ctx)
-	*/
 	var writeOutputProps func(*steps.LabpackResponse)
 	var mergeOutputProps func(*steps.LabpackResponse)
-
 	build.Main(input, &writeOutputProps, &mergeOutputProps,
 		func(ctx context.Context, args []string, state *build.State) error {
-			// this log expected to go to stdout
-			logging.Infof(ctx, "have input %v", input)
+			log.Printf("Input args: %v", input)
 			err := internalRun(ctx, input, state)
-			// actual build code here, build is already Start'd
-			// input was parsed from build.Input.Properties
-			writeOutputProps(&steps.LabpackResponse{})
-			return err // if err is nil then will mark the Build as SUCCESS
+			writeOutputProps(&steps.LabpackResponse{
+				Success: err == nil,
+			})
+			// if err is nil then will mark the Build as SUCCESS
+			return err
 		},
 	)
-
 	log.Printf("Exited successfully")
 }
 
-func logInputs(ctx context.Context, input *steps.LabpackInput) (err error) {
-	step, ctx := build.StartStep(ctx, "inputs")
+// internalRun main entry point to execution received request.
+func internalRun(ctx context.Context, in *steps.LabpackInput, state *build.State) (err error) {
+	if err := printInputs(ctx, in); err != nil {
+		log.Printf("Internal run: failed to marshal proto. Error: %s", err)
+	}
+	access, err := tlw.NewAccess(ctx, in)
+	if err != nil {
+		return errors.Annotate(err, "internal run").Err()
+	}
+	defer access.Close()
+
+	task := recovery.TaskNameRecovery
+	if t, ok := supportedTasks[in.TaskName]; ok {
+		task = t
+	}
+	// TODO(otabek@): Add custom logger.
+	// TODO(otabek@): Add custom stepper.
+	runArgs := &recovery.RunArgs{
+		UnitName:              in.UnitName,
+		TaskName:              task,
+		Access:                access,
+		EnableRecovery:        in.EnableRecovery,
+		EnableUpdateInventory: in.UpdateInventory,
+	}
+	if err := recovery.Run(ctx, runArgs); err != nil {
+		return errors.Annotate(err, "internal run").Err()
+	}
+	return nil
+}
+
+// Mapping of all supported tasks.
+var supportedTasks = map[string]recovery.TaskName{
+	string(recovery.TaskNameDeploy):   recovery.TaskNameDeploy,
+	string(recovery.TaskNameRecovery): recovery.TaskNameRecovery,
+}
+
+// printInputs prints input params.
+func printInputs(ctx context.Context, input *steps.LabpackInput) (err error) {
+	step, ctx := build.StartStep(ctx, "Input params")
 	defer func() { step.End(err) }()
 	req := step.Log("input proto")
 	marsh := jsonpb.Marshaler{Indent: "  "}
@@ -93,48 +94,4 @@ func logInputs(ctx context.Context, input *steps.LabpackInput) (err error) {
 		return errors.Annotate(err, "failed to marshal proto").Err()
 	}
 	return nil
-}
-
-func runRepairStep(ctx context.Context, idx int, input *steps.LabpackInput, state *build.State) (err error) {
-	stepName := fmt.Sprintf("Repair of %d", idx)
-	step, ctx := build.StartStep(ctx, stepName)
-	defer func() { step.End(err) }()
-
-	if idx > 5 {
-		return errors.Reason("Repair step fail %d", idx).Err()
-	}
-	return nil
-}
-
-func runVerifierStep(ctx context.Context, idx int, input *steps.LabpackInput, state *build.State) (err error) {
-	stepName := fmt.Sprintf("Verify of %d", idx)
-	step, ctx := build.StartStep(ctx, stepName)
-	defer func() { step.End(err) }()
-	hasRepair := idx%2 == 0
-	if hasRepair {
-		repairErr := runRepairStep(ctx, idx, input, state)
-		if repairErr != nil {
-			logging.Infof(ctx, "repair step fail %v", repairErr)
-		}
-	}
-	return nil
-}
-
-func internalRun(ctx context.Context, input *steps.LabpackInput, state *build.State) (err error) {
-	if err = logInputs(ctx, input); err != nil {
-		return err
-	}
-	for i := 1; i < 10; i++ {
-		runVerifierStep(ctx, i, input, state)
-	}
-	return nil
-}
-
-func isLocalRun() bool {
-	for _, arg := range os.Args {
-		if arg == "-local" {
-			return true
-		}
-	}
-	return false
 }
