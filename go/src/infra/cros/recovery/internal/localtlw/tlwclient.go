5572103201610781aaa59bbefacf8a647917d6ed
Otabek Kasimov <otabek@google.com>
2021-08-04 17:27:36 +0000

    recovery: Update dut-state
    BUG=b:193475692
    TEST=local run, build
    
    Change-Id: Id8c58d6fe23369b5b56ca0d766a39859d0fefe00
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/3069945
    Auto-Submit: Otabek Kasimov <otabek@google.com>
    Reviewed-by: Gregory NISBET <gregorynisbet@google.com>
    Commit-Queue: Gregory NISBET <gregorynisbet@google.com>
    Commit-Queue: Otabek Kasimov <otabek@google.com>
    Cr-Commit-Position: refs/heads/master@{#42859}

--- go/src/infra/cros/recovery/internal/localtlw/tlwclient.go
+++ go/src/infra/cros/recovery/internal/localtlw/tlwclient.go
@@ -20,6 +20,7 @@ import (
 	fleet "infra/appengine/crosskylabadmin/api/fleet/v1"
 	"infra/cros/recovery/internal/localtlw/dutinfo"
 
+	"infra/cros/dutstate"
 	tlwio "infra/cros/recovery/internal/localtlw/io"
 	"infra/cros/recovery/internal/localtlw/servod"
 	"infra/cros/recovery/internal/localtlw/ssh"
@@ -353,5 +354,12 @@ func (c *tlwClient) UpdateDut(ctx context.Context, dut *tlw.Dut) error {
 		return errors.Annotate(err, "update DUT %q", dut.Name).Err()
 	}
 	delete(c.devices, dut.Name)
+	if ufs, ok := c.ufsClient.(dutstate.UFSClient); ok {
+		if err := dutstate.Update(ctx, ufs, dut.Name, dut.State); err != nil {
+			return errors.Annotate(err, "update DUT %q", dut.Name).Err()
+		}
+	} else {
+		return errors.Reason("update DUT %q: dutstate.UFSClient interface is not implemented by client", dut.Name).Err()
+	}
 	return nil
 }
