b8682d816cdee9c63e5c271b52ef606beb3ecac6
Gregory Nisbet <gregorynisbet@google.com>
2021-09-10 18:29:05 +0000

    [karte] Remove hello-world route.
    Remove the hello world route now that karte has been deployed to dev.
    
    BUG=b/183724382
    TEST=make test
    
    Change-Id: I6dbd25b8d69b480fbb424ef90f54cd47005d7503
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/3150360
    Reviewed-by: Xianuo Wang <xianuowang@chromium.org>
    Commit-Queue: Gregory NISBET <gregorynisbet@google.com>
    Cr-Commit-Position: refs/heads/main@{#43724}

--- go/src/infra/cros/karte/cmd/karteserver/main.go
+++ go/src/infra/cros/karte/cmd/karteserver/main.go
@@ -15,7 +15,6 @@ import (
 	"go.chromium.org/luci/server"
 	"go.chromium.org/luci/server/gaeemulation"
 	"go.chromium.org/luci/server/module"
-	"go.chromium.org/luci/server/router"
 
 	"infra/cros/karte/internal/frontend"
 )
@@ -33,12 +32,6 @@ func main() {
 
 	server.Main(nil, modules, func(srv *server.Server) error {
 		logging.Infof(srv.Context, "Starting server.")
-		// TODO(gregorynisbet): remove this route
-		srv.Routes.GET("/hello-world", router.MiddlewareChain{}, func(ctx *router.Context) {
-			logging.Debugf(ctx.Context, "Hello world. a2c29304-30e1-41a2-b85e-e7f85eef4fd9.")
-			ctx.Writer.Write([]byte("Hello world. 4a9cd07f-6dd9-4d00-9f99-4086b58045cb."))
-		})
-
 		logging.Infof(srv.Context, "Installing Services.")
 		frontend.InstallServices(srv.PRPC)
 
