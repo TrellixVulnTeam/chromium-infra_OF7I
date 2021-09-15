908714c506ad9352e59fa05f99b26c30d3122b46
Jack Neus <jackneus@google.com>
2021-09-15 15:57:22 +0000

    manifest-doctor: generalize public buildspec creation
    Laying the groundwork for a legacy external buildspec creator --
    need to decouple public buildspec creation logic a the specific
    buildspec source.
    
    BUG=b/194500359
    TEST=go test
    
    Change-Id: I1486501d4733ef8cf8579005f7cdfdd72fa45347
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/3160319
    Reviewed-by: Sean McAllister <smcallis@google.com>
    Commit-Queue: Jack Neus <jackneus@google.com>
    Cr-Commit-Position: refs/heads/main@{#43862}

--- go/src/infra/cros/cmd/manifest_doctor/public_buildspec.go
+++ go/src/infra/cros/cmd/manifest_doctor/public_buildspec.go
@@ -5,7 +5,6 @@ package main
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/maruel/subcommands"
 	"go.chromium.org/luci/auth"
@@ -16,7 +15,6 @@ import (
 
 	"infra/cros/internal/gerrit"
 	"infra/cros/internal/gs"
-	"infra/cros/internal/manifestutil"
 	"infra/cros/internal/repo"
 )
 
@@ -93,93 +91,6 @@ func (b *publicBuildspec) Run(a subcommands.Application, args []string, env subc
 	return 0
 }
 
-// CreateProjectBuildspec creates a public buildspec as outlined in go/per-project-buildspecs.
-func (b *publicBuildspec) createPublicBuildspec(buildspecPath string, gsClient gs.Client, gerritClient *gerrit.Client) error {
-	internalPath := lgs.MakePath(internalBuildspecsGSBucket, buildspecPath)
-	buildspecData, err := gsClient.Read(internalPath)
-	if err != nil {
-		return err
-	}
-	buildspec, err := repo.ParseManifest(buildspecData)
-	if err != nil {
-		return err
-	}
-
-	remoteReference := buildspec
-	anyAnnotations := false
-	for _, remote := range buildspec.Remotes {
-		if len(remote.Annotations) > 0 {
-			anyAnnotations = true
-			break
-		}
-	}
-
-	if !anyAnnotations {
-		// If annotations are missing, fall back to downloading the ToT
-		// manifest and using that as reference.
-		remoteReference, err = manifestutil.LoadManifestFromGitilesWithIncludes(
-			context.Background(), gerritClient, chromeInternalHost, manifestInternalProject,
-			"HEAD", "default.xml")
-		if err != nil {
-			return err
-		}
-	}
-
-	// Look at remotes, filter out non public projects.
-	publicRemote := make(map[string]bool, len(buildspec.Remotes))
-	var publicRemotes []repo.Remote
-	for _, remote := range buildspec.Remotes {
-		referenceRemote := remoteReference.GetRemoteByName(remote.Name)
-		if referenceRemote == nil {
-			return fmt.Errorf("could not get public status for remote %v from reference manifest", remote.Name)
-		}
-
-		public, ok := referenceRemote.GetAnnotation("public")
-		if !ok {
-			return fmt.Errorf("could not get public status for remote %v from reference manifest", remote.Name)
-		}
-		publicRemote[remote.Name] = ok && (public == "true")
-		if remoteReference != buildspec {
-			remote.Annotations = referenceRemote.Annotations
-		}
-		if publicRemote[remote.Name] {
-			publicRemotes = append(publicRemotes, remote)
-		}
-	}
-
-	// Verify that the default is not a private remote.
-	defaultRemote := buildspec.Default.RemoteName
-	if public, ok := publicRemote[defaultRemote]; !(ok && public) {
-		return fmt.Errorf("default remote is private")
-	}
-
-	var publicProjects []repo.Project
-	for _, project := range buildspec.Projects {
-		// Check for the (implicit) default remote or a known public remote.
-		if public, ok := publicRemote[project.RemoteName]; project.RemoteName == "" || (ok && public) {
-			publicProjects = append(publicProjects, project)
-		}
-	}
-	buildspec.Remotes = publicRemotes
-	buildspec.Projects = publicProjects
-	externalBuildspecData, err := buildspec.WriteToBytes()
-	if err != nil {
-		return err
-	}
-
-	// Upload to external buildspec dir.
-	uploadPath := lgs.MakePath(externalBuildspecsGSBucket, buildspecPath)
-	if !b.push {
-		LogOut("Dry run, not uploading buildspec to %s...", string(uploadPath))
-		return nil
-	}
-	if err := gsClient.WriteFileToGS(uploadPath, externalBuildspecData); err != nil {
-		return err
-	}
-	LogOut("Uploaded buildspec to %s", string(uploadPath))
-	return nil
-}
-
 // CreateProjectBuildspec creates a public buildspec as
 // outlined in go/per-project-buildspecs.
 func (b *publicBuildspec) CreatePublicBuildspecs(ctx context.Context, gsClient gs.Client, gerritClient *gerrit.Client) error {
@@ -209,9 +120,25 @@ func (b *publicBuildspec) CreatePublicBuildspecs(ctx context.Context, gsClient g
 				continue
 			}
 
+			// Load internal buildspec.
+			internalPath := lgs.MakePath(internalBuildspecsGSBucket, internalBuildspec)
+			buildspecData, err := gsClient.Read(internalPath)
+			if err != nil {
+				LogErr(errors.Annotate(err, "failed to read internal buildspec %s", internalBuildspec).Err().Error())
+				errs = append(errs, err)
+				continue
+			}
+			buildspec, err := repo.ParseManifest(buildspecData)
+			if err != nil {
+				LogErr(errors.Annotate(err, "failed to load internal buildspec %s", internalBuildspec).Err().Error())
+				errs = append(errs, err)
+				continue
+			}
+
 			// Create and upload external buildspec.
 			LogOut("Attempting to create external buildspec for %s...", internalBuildspec)
-			if err := b.createPublicBuildspec(internalBuildspec, gsClient, gerritClient); err != nil {
+			uploadPath := lgs.MakePath(externalBuildspecsGSBucket, internalBuildspec)
+			if err := createPublicBuildspec(gsClient, gerritClient, buildspec, uploadPath, b.push); err != nil {
 				LogErr(errors.Annotate(err, "failed to create external buildspec %s", internalBuildspec).Err().Error())
 				errs = append(errs, err)
 			}
---
+++ go/src/infra/cros/cmd/manifest_doctor/util.go
@@ -0,0 +1,92 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+package main
+
+import (
+	"context"
+	"fmt"
+	"infra/cros/internal/gerrit"
+	"infra/cros/internal/gs"
+	"infra/cros/internal/manifestutil"
+	"infra/cros/internal/repo"
+
+	lgs "go.chromium.org/luci/common/gcloud/gs"
+)
+
+// CreateProjectBuildspec creates a public buildspec as outlined in go/per-project-buildspecs.
+func createPublicBuildspec(gsClient gs.Client, gerritClient *gerrit.Client, buildspec *repo.Manifest, uploadPath lgs.Path, push bool) error {
+	remoteReference := buildspec
+	anyAnnotations := false
+	for _, remote := range buildspec.Remotes {
+		if len(remote.Annotations) > 0 {
+			anyAnnotations = true
+			break
+		}
+	}
+
+	if !anyAnnotations {
+		// If annotations are missing, fall back to downloading the ToT
+		// manifest and using that as reference.
+		var err error
+		remoteReference, err = manifestutil.LoadManifestFromGitilesWithIncludes(
+			context.Background(), gerritClient, chromeInternalHost, manifestInternalProject,
+			"HEAD", "default.xml")
+		if err != nil {
+			return err
+		}
+	}
+
+	// Look at remotes, filter out non public projects.
+	publicRemote := make(map[string]bool, len(buildspec.Remotes))
+	var publicRemotes []repo.Remote
+	for _, remote := range buildspec.Remotes {
+		referenceRemote := remoteReference.GetRemoteByName(remote.Name)
+		if referenceRemote == nil {
+			return fmt.Errorf("could not get public status for remote %v from reference manifest", remote.Name)
+		}
+
+		public, ok := referenceRemote.GetAnnotation("public")
+		if !ok {
+			return fmt.Errorf("could not get public status for remote %v from reference manifest", remote.Name)
+		}
+		publicRemote[remote.Name] = ok && (public == "true")
+		if remoteReference != buildspec {
+			remote.Annotations = referenceRemote.Annotations
+		}
+		if publicRemote[remote.Name] {
+			publicRemotes = append(publicRemotes, remote)
+		}
+	}
+
+	// Verify that the default is not a private remote.
+	defaultRemote := buildspec.Default.RemoteName
+	if public, ok := publicRemote[defaultRemote]; !(ok && public) {
+		return fmt.Errorf("default remote is private")
+	}
+
+	var publicProjects []repo.Project
+	for _, project := range buildspec.Projects {
+		// Check for the (implicit) default remote or a known public remote.
+		if public, ok := publicRemote[project.RemoteName]; project.RemoteName == "" || (ok && public) {
+			publicProjects = append(publicProjects, project)
+		}
+	}
+	buildspec.Remotes = publicRemotes
+	buildspec.Projects = publicProjects
+	externalBuildspecData, err := buildspec.WriteToBytes()
+	if err != nil {
+		return err
+	}
+
+	// Upload to external buildspec dir.
+	if !push {
+		LogOut("Dry run, not uploading buildspec to %s...", string(uploadPath))
+		return nil
+	}
+	if err := gsClient.WriteFileToGS(uploadPath, externalBuildspecData); err != nil {
+		return err
+	}
+	LogOut("Uploaded buildspec to %s", string(uploadPath))
+	return nil
+}
