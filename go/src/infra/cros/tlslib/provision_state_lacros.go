4520c18edde21abd0ed4051ae7378bae2a215ede
Hyung Tae Kim <hyungtaekim@chromium.org>
2021-09-21 16:32:47 +0000

    tls: ProvisionLacros impl for autoupdate tests
    This change allows to override the version and the component path
    for autoupdate tests.
    See go/lacros-autoupdate-mock for the test plan.
    
    Also minor fixes:
    - Join a URL for the path of Lacros source image in remote, instead
    of path.Join that is useful only for local file path.
    - Change file owner of the component directories to chronos.
    
    BUG=1247311
    TEST=# Start TLW + TLS locally
    TEST=# Run ProvisionLacros with file:// path to Rootfs Lacros
    TEST=#   overriding version and component path in the request
    TEST=# Check the generated component files in the given local path
    TEST=# $> stop ui & start ui
    TEST=# Open Lacros to see if a newer version of Lacros is selected
    
    Change-Id: I3424bde8d064f02e046e4574bafb8c3a57a32003
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/3149591
    Commit-Queue: Hyung Tae Kim <hyungtaekim@chromium.org>
    Reviewed-by: Jae Hoon Kim <kimjae@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#43985}

--- go/src/infra/cros/tlslib/provision_state_lacros.go
+++ go/src/infra/cros/tlslib/provision_state_lacros.go
@@ -9,7 +9,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"net/url"
 	"path"
+	"regexp"
 	"strconv"
 	"strings"
 
@@ -18,9 +20,11 @@ import (
 )
 
 const (
-	lacrosRootComponentPath = "/var/lib/imageloader/lacros"
-	pageSize                = 4096
+	imageloaderLacrosRootComponentPath = "/var/lib/imageloader/lacros"
+	pageSize                           = 4096
 )
+
+var versionRegex = regexp.MustCompile(`^(\d+\.)(\d+\.)(\d+\.)(\d+)$`)
 
 type lacrosMetadata struct {
 	Content struct {
@@ -28,15 +32,26 @@ type lacrosMetadata struct {
 	} `json:"content"`
 }
 
+// lacrosSourceType describes the source types of Lacros for provisioning (either GS or device file).
+type lacrosSourceType int
+
+const (
+	GsPath lacrosSourceType = iota
+	DeviceFilePath
+)
+
 type provisionLacrosState struct {
-	s                   *Server
-	c                   *ssh.Client
-	dutName             string
-	gsPathPrefix        string
-	metadata            lacrosMetadata
-	lacrosComponentPath string
-	lacrosImagePath     string
-	lacrosTablePath     string
+	s                       *Server
+	c                       *ssh.Client
+	dutName                 string
+	sourceURL               string
+	sourceType              lacrosSourceType
+	metadata                lacrosMetadata
+	lacrosComponentRootPath string // component path, eg. "/home/chronos/cros-components/lacros-dogfood-dev/"
+	lacrosComponentPath     string // versioned path, eg. "/home/chronos/cros-components/lacros-dogfood-dev/9999.0.0.0/"
+	lacrosImagePath         string
+	lacrosTablePath         string
+	overrideVersion         string
 }
 
 func newProvisionLacrosState(s *Server, req *tls.ProvisionLacrosRequest) (*provisionLacrosState, error) {
@@ -48,11 +63,30 @@ func newProvisionLacrosState(s *Server, req *tls.ProvisionLacrosRequest) (*provi
 	// Verify the incoming request path oneof is valid.
 	switch t := req.GetImage().GetPathOneof().(type) {
 	case *tls.ProvisionLacrosRequest_LacrosImage_GsPathPrefix:
-		p.gsPathPrefix = req.GetImage().GetGsPathPrefix()
+		p.sourceURL = req.GetImage().GetGsPathPrefix()
+		p.sourceType = GsPath
+	case *tls.ProvisionLacrosRequest_LacrosImage_DeviceFilePrefix:
+		p.sourceURL = req.GetImage().GetDeviceFilePrefix()
+		p.sourceType = DeviceFilePath
 	default:
 		return nil, fmt.Errorf("newProvisionStateLacros: unsupported ImagePathOneof in ProvisionLacrosRequest, %T", t)
 	}
 
+	// Verify the override version is valid if |OverrideVersion| is specified.
+	if req.OverrideVersion != "" {
+		if !versionRegex.MatchString(req.OverrideVersion) {
+			return nil, fmt.Errorf("newProvisionLacrosState: failed to parse version: %v", req.OverrideVersion)
+		}
+		p.overrideVersion = req.OverrideVersion
+	}
+
+	// Override the component root path if |OverrideInstallPath| is specified.
+	if req.OverrideInstallPath != "" {
+		p.lacrosComponentRootPath = req.OverrideInstallPath
+	} else {
+		// Use the ImageLoader component path by default.
+		p.lacrosComponentRootPath = imageloaderLacrosRootComponentPath
+	}
 	return p, nil
 }
 
@@ -101,32 +135,41 @@ func (p *provisionLacrosState) provisionLacros(ctx context.Context) error {
 	}
 
 	// Write the Lacros version to the latest-version file.
-	lacrosLastestVersionPath := path.Join(lacrosRootComponentPath, "latest-version")
+	lacrosLastestVersionPath := path.Join(p.lacrosComponentRootPath, "latest-version")
 	if err := runCmd(p.c, fmt.Sprintf("echo -n %s > %s", p.metadata.Content.Version, lacrosLastestVersionPath)); err != nil {
 		return fmt.Errorf("provisionLacros: failed to write Lacros version to latest-version file, %s", err)
 	}
 
+	// Change file mode and owner of provisioned files if the path is prefixed with the CrOS component path.
+	const crosComponentRootPath = "/home/chronos/cros-components"
+	if strings.HasPrefix(p.lacrosComponentRootPath, crosComponentRootPath) {
+		if err := runCmd(p.c, fmt.Sprintf("chown -R chronos:chronos %s && chmod -R 0755 %s", crosComponentRootPath, crosComponentRootPath)); err != nil {
+			return fmt.Errorf("provisionLacros: failed to chown/chmod provisioned files under %s, %s", crosComponentRootPath, err)
+		}
+	}
 	return nil
 }
 
 // extractLacrosMetadata will unmarshal the metadata.json in the GS path into the state.
 func (p *provisionLacrosState) extractLacrosMetadata(ctx context.Context) error {
-	lacrosGSMetadataPath := path.Join(p.gsPathPrefix, "metadata.json")
-	// TODO(kimjae): Use CacheForDrone TLS API once implemented and then download
-	// from TLS side instead of from the DUT.
-	url, err := p.s.cacheForDut(ctx, lacrosGSMetadataPath, p.dutName)
+	metadataURL, err := p.getSourceFullURL(ctx, "metadata.json")
 	if err != nil {
-		return fmt.Errorf("extractMetadata: failed to CacheForDut Lacros metadata.json, %s", err)
+		return fmt.Errorf("extractMetadata: failed to get a metadata URL, %s", err)
 	}
-	metadataJSONStr, err := runCmdOutput(p.c, fmt.Sprintf("curl -s %s", url))
+	metadataJSONStr, err := runCmdOutput(p.c, fmt.Sprintf("curl -s %s", metadataURL))
 	if err != nil {
-		return fmt.Errorf("extractMetadata: failed to read Lacros metadata.json, %s", err)
+		return fmt.Errorf("extractMetadata: failed to read Lacros metadata.json from %v, %s", metadataURL, err)
 	}
+
 	metadataJSON := lacrosMetadata{}
 	if err := json.Unmarshal([]byte(metadataJSONStr), &metadataJSON); err != nil {
 		return fmt.Errorf("extractMetadata: failed to unmarshal Lacros metadata.json, %s", err)
 	}
 	p.metadata = metadataJSON
+	if p.overrideVersion != "" {
+		log.Printf("extractLacrosMetadata: override version %v to %v\n", p.metadata.Content.Version, p.overrideVersion)
+		p.metadata.Content.Version = p.overrideVersion
+	}
 	return nil
 }
 
@@ -134,15 +177,25 @@ func (p *provisionLacrosState) extractLacrosMetadata(ctx context.Context) error 
 // component directory with the version from the metadata.json file.
 // The Lacros image will also be modified with the correct verity output.
 func (p *provisionLacrosState) installLacrosAsComponent(ctx context.Context) error {
-	lacrosGSImagePath := path.Join(p.gsPathPrefix, "lacros_compressed.squash")
-	url, err := p.s.cacheForDut(ctx, lacrosGSImagePath, p.dutName)
-	if err != nil {
-		return fmt.Errorf("installLacrosAsComponent: failed to CacheForDut, %s", err)
+	var imageFileName string
+	switch p.sourceType {
+	case GsPath:
+		imageFileName = "lacros_compressed.squash"
+	case DeviceFilePath:
+		imageFileName = "lacros.squash"
+	default:
+		return fmt.Errorf("installLacrosAsComponent: unknown source type: %v", p.sourceType)
 	}
-	p.lacrosComponentPath = path.Join(lacrosRootComponentPath, p.metadata.Content.Version)
+	imageURL, err := p.getSourceFullURL(ctx, imageFileName)
+	if err != nil {
+		return fmt.Errorf("installLacrosAsComponent: failed to get a image URL, %s", err)
+	}
+
+	p.lacrosComponentPath = path.Join(p.lacrosComponentRootPath, p.metadata.Content.Version)
 	p.lacrosImagePath = path.Join(p.lacrosComponentPath, "image.squash")
-	if err := runCmd(p.c, fmt.Sprintf("mkdir -p %s && curl %s --output %s", p.lacrosComponentPath, url, p.lacrosImagePath)); err != nil {
-		return fmt.Errorf("installLacrosAsComponent: failed to install Lacros image from %s, %s", lacrosGSImagePath, err)
+
+	if err := runCmd(p.c, fmt.Sprintf("mkdir -p %s && curl %s --output %s", p.lacrosComponentPath, imageURL, p.lacrosImagePath)); err != nil {
+		return fmt.Errorf("installLacrosAsComponent: failed to install Lacros image from %s, %s", imageURL, err)
 	}
 
 	lacrosBlocks, err := alignImageToPage(ctx, p.c, p.lacrosImagePath)
@@ -254,4 +307,37 @@ func (p *provisionLacrosState) writeComponentManifest(ctx context.Context) error
 		return fmt.Errorf("writeComponentManifest: failed to write Lacros manifest json to DUT, %s", err)
 	}
 	return nil
+}
+
+// getSourceFullURL returns a full URL of the Lacros binary files in a given base URL and path.
+func (p *provisionLacrosState) getSourceFullURL(ctx context.Context, part string) (string, error) {
+	url, err := func(base string, part string) (*url.URL, error) {
+		parsed, err := url.Parse(base)
+		if err != nil {
+			return nil, err
+		}
+		parsed.Path = path.Join(parsed.Path, part)
+		return parsed, nil
+	}(p.sourceURL, part)
+	if err != nil {
+		return "", fmt.Errorf("getSourceFullURL: failed to join a URL, %s", err)
+	}
+
+	fullURL := ""
+	switch p.sourceType {
+	case GsPath:
+		// TODO(kimjae): Use CacheForDrone TLS API once implemented and then download
+		// from TLS side instead of from the DUT.
+		fullURL, err = p.s.cacheForDut(ctx, url.String(), p.dutName)
+		if err != nil {
+			return "", fmt.Errorf("getSourceFullURL: failed to CacheForDut, %s", err)
+		}
+	case DeviceFilePath:
+		if url.Scheme != "" && url.Scheme != "file" {
+			return "", fmt.Errorf("getSourceFullURL: wrong URL scheme. expected: file, actual: %s, url:%s", url.Scheme, url.String())
+		}
+		url.Scheme = "file"
+		fullURL = url.String()
+	}
+	return fullURL, nil
 }
