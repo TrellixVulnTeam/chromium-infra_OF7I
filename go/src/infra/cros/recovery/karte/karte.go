d13b502a02537203b6293cf80dab992c8cfb59de
Gregory Nisbet <gregorynisbet@google.com>
2021-09-27 23:07:54 +0000

    [recovery] [action-lifecycle] Add initial implementation of Create and Update in karte client.
    Wire up the implementation of create and update in Karte to the karte client
    in recoverylib.
    
    BUG=b/183724382
    TEST=make test
    
    Change-Id: I0e8e70a32f3d437ea1828795bae67929edb5af94
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/3163737
    Commit-Queue: Gregory NISBET <gregorynisbet@google.com>
    Reviewed-by: Otabek Kasimov <otabek@google.com>
    Cr-Commit-Position: refs/heads/main@{#44125}

--- go/src/infra/cros/recovery/karte/karte.go
+++ go/src/infra/cros/recovery/karte/karte.go
@@ -32,12 +32,34 @@ func NewMetrics(ctx context.Context, c *kclient.Config, o ...kclient.Option) (lo
 // Note that an action contains zero or more observations in it and that observations are not
 // separate.
 func (c *client) Create(ctx context.Context, action *logger.Action) (*logger.Action, error) {
-	panic("not implemented")
+	karteResp, err := c.impl.CreateAction(
+		ctx,
+		&kartepb.CreateActionRequest{
+			Action: convertActionToKarteAction(action),
+		},
+	)
+	if err != nil {
+		return nil, errors.Annotate(err, "karte create").Err()
+	}
+	return convertKarteActionToAction(karteResp), nil
 }
 
-// Update is a stub that does nothing.
-func (c *client) Update(ctx context.Context, res *logger.Action) (*logger.Action, error) {
-	panic("not implemented")
+// Update takes an action and updates the entry in the Karte service, the source of truth.
+// TODO(gregorynisbet): This implementation is not complete. A logger action has observations attached to it.
+// Updating Karte will require inspecting those observations and potentially updating or replacing them.
+func (c *client) Update(ctx context.Context, action *logger.Action) (*logger.Action, error) {
+	a := convertActionToKarteAction(action)
+	karteResp, err := c.impl.UpdateAction(
+		ctx,
+		&kartepb.UpdateActionRequest{
+			Action:     a,
+			UpdateMask: nil,
+		},
+	)
+	if err != nil {
+		return nil, errors.Annotate(err, "karte update").Err()
+	}
+	return convertKarteActionToAction(karteResp), nil
 }
 
 // Search takes a query struct and produces a resultset.
