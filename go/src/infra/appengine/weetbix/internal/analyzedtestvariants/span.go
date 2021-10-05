22883a88438583828ec79b9409ee3cd584b5880e
Chan Li <chanli@chromium.org>
2021-10-05 20:58:34 +0000

    [Weetbix] Query test variants with unexpected results by builder
    The query results will be used in CollectFlakeResults task to get their results in a build and then save those results as Verdicts.
    
    Bug: 1255001
    Change-Id: I9083abd39aa111db76b08a05a82ce65aac888aa5
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/3199411
    Commit-Queue: Chan Li <chanli@chromium.org>
    Reviewed-by: Matthew Warton <mwarton@google.com>
    Cr-Commit-Position: refs/heads/main@{#44281}

--- go/src/infra/appengine/weetbix/internal/analyzedtestvariants/span.go
+++ go/src/infra/appengine/weetbix/internal/analyzedtestvariants/span.go
@@ -29,3 +29,32 @@ func Read(ctx context.Context, ks spanner.KeySet, f func(*pb.AnalyzedTestVariant
 		},
 	)
 }
+
+// QueryTestVariantsByBuilder queries AnalyzedTestVariants with unexpected
+// results on the given builder.
+func QueryTestVariantsByBuilder(ctx context.Context, realm, builder string, f func(*pb.AnalyzedTestVariant) error) error {
+	st := spanner.NewStatement(`
+		SELECT TestId, VariantHash
+		FROM AnalyzedTestVariants@{FORCE_INDEX=AnalyzedTestVariantsPerBuilderAndStatus, spanner_emulator.disable_query_null_filtered_index_check=true}
+		WHERE Realm = @realm
+		AND Builder = @builder
+		AND Status in UNNEST(@statuses)
+		ORDER BY TestId, VariantHash
+	`)
+	st.Params = map[string]interface{}{
+		"realm":    realm,
+		"builder":  builder,
+		"statuses": []int{int(pb.AnalyzedTestVariantStatus_FLAKY), int(pb.AnalyzedTestVariantStatus_CONSISTENTLY_UNEXPECTED), int(pb.AnalyzedTestVariantStatus_FLAKY_SUSPECT)},
+	}
+
+	var b spanutil.Buffer
+	return span.Query(ctx, st).Do(
+		func(row *spanner.Row) error {
+			tv := &pb.AnalyzedTestVariant{}
+			if err := b.FromSpanner(row, &tv.TestId, &tv.VariantHash); err != nil {
+				return err
+			}
+			return f(tv)
+		},
+	)
+}
--- go/src/infra/appengine/weetbix/internal/analyzedtestvariants/span_test.go
+++ go/src/infra/appengine/weetbix/internal/analyzedtestvariants/span_test.go
@@ -8,6 +8,7 @@ import (
 	"testing"
 
 	"cloud.google.com/go/spanner"
+
 	"go.chromium.org/luci/server/span"
 
 	"infra/appengine/weetbix/internal/testutil"
@@ -17,33 +18,66 @@ import (
 	. "github.com/smartystreets/goconvey/convey"
 )
 
-func TestRead(t *testing.T) {
-	Convey(`TestRead`, t, func() {
+func TestAnalyzedTestVariantSpan(t *testing.T) {
+	Convey(`TestAnalyzedTestVariantSpan`, t, func() {
 		ctx := testutil.SpannerTestContext(t)
 		realm := "chromium:ci"
 		status := pb.AnalyzedTestVariantStatus_FLAKY
+		builder := "builder"
 		ms := []*spanner.Mutation{
-			insert.AnalyzedTestVariant(realm, "ninja://test1", "variantHash1", status, nil),
-			insert.AnalyzedTestVariant(realm, "ninja://test1", "variantHash2", status, nil),
-			insert.AnalyzedTestVariant(realm, "ninja://test2", "variantHash1", status, nil),
+			insert.AnalyzedTestVariant(realm, "ninja://test1", "variantHash1", status,
+				map[string]interface{}{
+					"Builder": builder,
+				}),
+			insert.AnalyzedTestVariant(realm, "ninja://test1", "variantHash2", pb.AnalyzedTestVariantStatus_FLAKY_SUSPECT, map[string]interface{}{
+				"Builder": builder,
+			}),
+			insert.AnalyzedTestVariant(realm, "ninja://test2", "variantHash1", status,
+				map[string]interface{}{
+					"Builder": "anotherbuilder",
+				}),
 			insert.AnalyzedTestVariant(realm, "ninja://test3", "variantHash", status, nil),
+			insert.AnalyzedTestVariant(realm, "ninja://test4", "variantHash", pb.AnalyzedTestVariantStatus_CONSISTENTLY_EXPECTED,
+				map[string]interface{}{
+					"Builder": builder,
+				}),
+			insert.AnalyzedTestVariant("anotherrealm", "ninja://test1", "variantHash1", status,
+				map[string]interface{}{
+					"Builder": builder,
+				}),
 		}
 		testutil.MustApply(ctx, ms...)
-		ctx, cancel := span.ReadOnlyTransaction(ctx)
-		defer cancel()
 
-		ks := spanner.KeySets(
-			spanner.Key{realm, "ninja://test1", "variantHash1"},
-			spanner.Key{realm, "ninja://test1", "variantHash2"},
-			spanner.Key{realm, "ninja://test-not-exists", "variantHash1"},
-		)
-		atvs := make([]*pb.AnalyzedTestVariant, 0)
-		err := Read(ctx, ks, func(atv *pb.AnalyzedTestVariant) error {
-			So(atv.Realm, ShouldEqual, realm)
-			atvs = append(atvs, atv)
-			return nil
+		Convey(`TestRead`, func() {
+			ctx, cancel := span.ReadOnlyTransaction(ctx)
+			defer cancel()
+
+			ks := spanner.KeySets(
+				spanner.Key{realm, "ninja://test1", "variantHash1"},
+				spanner.Key{realm, "ninja://test1", "variantHash2"},
+				spanner.Key{realm, "ninja://test-not-exists", "variantHash1"},
+			)
+			atvs := make([]*pb.AnalyzedTestVariant, 0)
+			err := Read(ctx, ks, func(atv *pb.AnalyzedTestVariant) error {
+				So(atv.Realm, ShouldEqual, realm)
+				atvs = append(atvs, atv)
+				return nil
+			})
+			So(err, ShouldBeNil)
+			So(len(atvs), ShouldEqual, 2)
 		})
-		So(err, ShouldBeNil)
-		So(len(atvs), ShouldEqual, 2)
+
+		Convey(`TestQueryTestVariantsByBuilder`, func() {
+			ctx, cancel := span.ReadOnlyTransaction(ctx)
+			defer cancel()
+
+			atvs := make([]*pb.AnalyzedTestVariant, 0)
+			err := QueryTestVariantsByBuilder(ctx, realm, builder, func(atv *pb.AnalyzedTestVariant) error {
+				atvs = append(atvs, atv)
+				return nil
+			})
+			So(err, ShouldBeNil)
+			So(len(atvs), ShouldEqual, 2)
+		})
 	})
 }
