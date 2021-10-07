cd759886adc0d459181db5fe22a7134214df51d7
Andrii Shyshkalov <tandrii@google.com>
2021-10-07 22:16:21 +0000

    Delete CQ status app.
    It has been replaced by LUCI CV.
    
    R=yiwzhang
    
    Bug: 962707
    Change-Id: I27e6e80be14c5b662b2ea88596ae5993b82b0988
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/3212194
    Reviewed-by: Roberto Carrillo <robertocn@chromium.org>
    Reviewed-by: Yiwei Zhang <yiwzhang@google.com>
    Commit-Queue: Andrii Shyshkalov <tandrii@google.com>
    Cr-Commit-Position: refs/heads/main@{#44349}

--- appengine/chromium_cq_status/model/password.py
+++
@@ -1,8 +0,0 @@
-# Copyright 2014 The Chromium Authors. All rights reserved.
-# Use of this source code is governed by a BSD-style license that can be
-# found in the LICENSE file.
-
-from google.appengine.ext import ndb
-
-class Password(ndb.Model):
-  sha1 = ndb.StringProperty(required=True)
