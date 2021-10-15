f18d2ea4e2b59f4dc7084f92b7b4cf6fb0facc02
Vineet Joshi <vkjoshi@google.com>
2021-10-15 16:34:41 +0000

    [recovery]: servo config for PARIS.
    This commit is a part of the PARIS project.
    
    This change introduces the comprehensive servo configurations required
    for migrating the servo verifier functionality from LABPACK.
    
    The actions declared in this CL will be defined as subsequent
    verifiers are migrated from LABPACK.
    
    bug=b:198809511
    TEST=Manual Execution
    
    Change-Id: I5fc35710262953d1e03c45eb832e2a46080c5fcb
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/3222482
    Reviewed-by: Otabek Kasimov <otabek@google.com>
    Commit-Queue: Vineet Joshi <vkjoshi@google.com>
    Cr-Commit-Position: refs/heads/main@{#44535}

--- go/src/infra/cros/recovery/config.go
+++ go/src/infra/cros/recovery/config.go
@@ -188,20 +188,309 @@ const defaultConfig = `
 		},
 		"servo_repair":{
 			"critical_actions": [
+				"servo_host_info",
+				"servod_info",
+				"create_host",
 				"cros_ssh",
+				"servo_v3_uptime",
+				"lock_labstation",
+				"has_enough_disk_space",
+				"servo_components_check",
+				"servo_fw_updated",
 				"servo_host_servod_init",
-				"servo_root_present",
 				"servod_echo",
-				"servo_detect_usbkey"
+				"servod_get_serialname",
+				"servod_servo_pd",
+				"servod_read_servo_type",
+				"servo_cr50_checks",
+				"servo_connection_pins",
+				"servo_ppdut5",
+				"servod_set_main_device",
+				"servo_ec_check",
+				"servo_testlab_enabled",
+				"servo_detect_usbkey",
+				"servo_audit_usbkey"
 			],
 			"actions": {
-				"servo_host_servod_restart":{
+				"servo_host_servod_restart": {
 					"exec_timeout": {
 						"seconds": 120
 					}
 				},
+				"servo_host_info": {
+					"dependencies" : [
+						"has_dut_name"
+					],
+					"exec_name":"sample_pass"
+				},
+				"servod_info": {
+					"exec_name":"sample_pass"
+				},
+				"create_host": {
+					"docs": [
+						"Only to create docker"
+					],
+					"exec_name":"sample_pass"
+				},
+				"servo_v3_uptime": {
+					"exec_name":"cros_validate_uptime",
+					"exec_extra_args": [
+						"max_duration:96h"
+					],
+					"conditions": [
+						"is_servo_v3"
+					],
+					"recovery_actions": [
+						"reboot"
+					]
+				},
+				"is_labstation": {
+					"docs": [
+						"condition to check is name contains labstation word"
+					],
+					"exec_name":"sample_pass"
+				},
+				"is_container": {
+					"docs": [
+						"condition to check is name contains labstation word"
+					],
+					"exec_name":"sample_pass"
+				},
+				"is_servo_v3": {
+					"docs": [
+						"Revers from labstation and docker"
+					],
+					"exec_name":"sample_pass"
+				},
+				"lock_labstation": {
+					"docs": [
+						"create lock file is_in_use"
+					],
+					"conditions": [
+						"is_labstation"
+					],
+					"exec_name":"sample_pass"
+				},
+				"has_enough_disk_space": {
+					"docs": [
+						"host.check_diskspace('/mnt/stateful_partition', 0.5)"
+					],
+					"conditions": [
+						"is_not_container"
+					],
+					"exec_name":"sample_pass"
+				},
+				"is_not_container": {
+					"conditions": [
+						"is_servo_v3",
+						"is_labstation"
+					],
+					"exec_name":"sample_fail"
+				},
+				"servo_components_check": {
+					"dependencies": [
+						"cros_ssh",
+						"root_present_servo_v3",
+						"root_present_servo_v4",
+						"servo_topology"
+					],
+					"exec_name":"sample_pass"
+				},
+				"servo_topology": {
+					"docs": [
+						"host.check_diskspace('/mnt/stateful_partition', 0.5)"
+					],
+					"conditions": [
+						"is_not_servo_v3"
+					],
+					"dependencies": [
+						"cros_ssh",
+						"servo_topology_single_child",
+						"servo_topology_dual_setup"
+					],
+					"exec_name":"sample_pass"
+				},
+				"servo_topology_single_child": {
+					"conditions": [
+						"is_not_servo_v3"
+					],
+					"exec_name":"sample_pass",
+					"exec_extra_args": [
+						"min_child:1"
+					]
+				},
+				"servo_topology_dual_setup": {
+					"conditions": [
+						"is_not_servo_v3",
+						"is_dual_setup"
+					],
+					"exec_name":"sample_pass",
+					"exec_extra_args": [
+						"min_child:2"
+					],
+					"recovery_actions": [
+						"servod_restart_dut"
+					],
+					"allow_fail_after_recovery": true
+				},
+				"root_present_servo_v3": {
+					"dependencies": [
+						"cros_ssh"
+					],
+					"conditions": [
+						"is_servo_v3"
+					],
+					"exec_name":"sample_pass"
+				},
+				"root_present_servo_v4": {
+					"dependencies": [
+						"cros_ssh"
+					],
+					"conditions": [
+						"is_not_servo_v3"
+					],
+					"exec_name":"sample_pass"
+				},
+				"servo_fw_updated": {
+					"conditions": [
+						"is_not_servo_v3"
+					],
+					"recovery_actions": [
+						"servo_fw_update"
+					],
+					"exec_name":"sample_pass"
+				},
+				"servod_get_serialname": {
+					"docs":[
+						"run command from xmlrpc"
+					],
+					"exec_name":"sample_pass"
+				},
+				"servod_servo_pd": {
+					"docs":[
+						"run command from xmlrpc"
+					],
+					"conditions": [
+						"servo_type_c"
+					],
+					"allow_fail_after_recovery": true,
+					"exec_name":"sample_pass",
+					"exec_extra_args": [
+						"exclude_model:puff,hh"
+					]
+				},
+				"servo_cr50_checks": {
+					"conditions": [
+						"is_not_servo_v3"
+					],
+					"dependencies": [
+						"servo_cr50_low_sbu",
+						"servo_cr50_enumerated",
+						"servo_cr50_console"
+					],
+					"exec_name":"sample_pass"
+				},
+				"servo_cr50_low_sbu": {
+					"conditions": [
+						"is_not_servo_v3",
+						"servo_type_c"
+					],
+					"exec_name":"sample_pass"
+				},
+				"servo_cr50_enumerated": {
+					"docs": [
+						"prev name servo_cr50_off"
+					],
+					"conditions": [
+						"is_not_servo_v3",
+						"servo_type_c"
+					],
+					"exec_name":"sample_pass"
+				},
+				"servo_cr50_console": {
+					"conditions": [
+						"is_not_servo_v3"
+					],
+					"dependencies": [
+						"servod_dut_controller"
+					],
+					"exec_extra_args": [
+						"cr50_ccd_level",
+						"cr50_testlab",
+						"cr50_ccd_state_flags"
+					],
+					"exec_name":"sample_pass"
+				},
+				"servod_dut_controller": {
+					"conditions": [
+						"is_not_servo_v3"
+					],
+					"exec_name":"sample_pass"
+				},
+				"servo_connection_pins": {
+					"conditions": [
+						"is_not_servo_v3"
+					],
+					"dependencies": [
+						"servo_cold_reset_pin",
+						"servo_warm_reset_pin"
+					],
+					"exec_name":"sample_pass"
+				},
+				"servo_pwr_button_pin": {
+					"exec_extra_args": [
+						"except:board1,board2"
+					],
+					"exec_name":"sample_pass"
+				},
+				"servo_ppdut5": {
+					"conditions": [
+						"is_not_servo_v3",
+						"servo_v4_type_a"
+					],
+					"exec_name":"sample_pass"
+				},
+				"servo_ec_check": {
+					"conditions": [
+						"is_not_servo_v3",
+						"dut_has_cros_ec"
+					],
+					"dependencies": [
+						"servo_ec_console",
+						"servo_pwr_button_pin",
+						"servo_lid_open",
+						"servo_battery_charging"
+					],
+					"exec_name":"sample_pass"
+				},
+				"servo_ec_console": {
+					"conditions": [
+						"is_not_servo_v3",
+						"dut_has_cros_ec"
+					],
+					"exec_name":"sample_pass"
+				},
+				"servo_battery_charging": {
+					"conditions": [
+						"is_not_servo_v3",
+						"dut_has_cros_ec"
+					],
+					"allow_fail_after_recovery": true,
+					"exec_name":"sample_pass"
+				},
+				"servo_testlab_enabled": {
+					"conditions": [
+						"is_not_servo_v3",
+						"servo_v4_type_c"
+					],
+					"exec_name":"sample_pass"
+				},
 				"servo_detect_usbkey": {
-					"docs": ["Will detect the path to USB Drive on servo-host."]
+					"docs": [
+						"Will detect the path to USB Drive on servo-host.",
+						"Verify that usb-key is responsive"
+					],
+					"allow_fail_after_recovery": true
 				},
 				"servo_audit_usbkey": {
 					"docs": ["This action will detect whether or not the USB drive is in working condition."],
@@ -210,6 +499,56 @@ const defaultConfig = `
 					"exec_timeout": {
 						"seconds":7300
 					}
+				},
+				"servo_type_c": {
+					"exec_name":"sample_pass"
+				},
+				"servo_lid_open": {
+					"exec_name":"sample_pass"
+				},
+				"is_not_servo_v3": {
+					"exec_name":"sample_pass"
+				},
+				"servo_v4_type_a": {
+					"exec_name":"sample_pass"
+				},
+				"is_dual_setup": {
+					"exec_name":"sample_pass"
+				},
+				"servod_set_main_device": {
+					"exec_name":"sample_pass"
+				},
+				"servo_v4_type_c": {
+					"exec_name":"sample_pass"
+				},
+				"servo_fw_update": {
+					"docs":[
+						"Try to update in  normal ways 3 times",
+						"if fail allow run force update"
+					],
+					"exec_extra_args": [
+						"try:3",
+						"use_force"
+					],
+					"exec_name":"sample_pass"
+				},
+				"servo_warm_reset_pin": {
+					"exec_name":"sample_pass"
+				},
+				"dut_has_cros_ec": {
+					"exec_name":"sample_pass"
+				},
+				"reboot":{
+					"exec_name":"sample_pass"
+				},
+				"servo_cold_reset_pin": {
+					"exec_name":"sample_pass"
+				},
+				"servod_read_servo_type": {
+					"exec_name":"sample_pass"
+				},
+				"servod_restart_dut": {
+					"exec_name":"sample_pass"
 				}
 			},
 			"allow_fail": true
