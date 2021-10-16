7c8c375d3c4d451c170c217ec971d88e070ec418
Yunzhi Yu <yunzhiyu@google.com>
2021-10-16 22:08:25 +0000

    recovery: Add two more tpm verifiers
    1. "cros_is_tpm_present"
    2. "cros_is_tpm_in_good_status"
    
    BUG=b:197527180
    TEST=manual test
    
    Change-Id: Idef15026567452e3c20a27c0a9326e1c8a046aa7
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/3180365
    Commit-Queue: Yunzhi Yu <yunzhiyu@google.com>
    Reviewed-by: Otabek Kasimov <otabek@google.com>
    Cr-Commit-Position: refs/heads/main@{#44555}

--- go/src/infra/cros/recovery/internal/execs/cros/tpm_execs.go
+++ go/src/infra/cros/recovery/internal/execs/cros/tpm_execs.go
@@ -10,6 +10,7 @@ import (
 	"go.chromium.org/luci/common/errors"
 
 	"infra/cros/recovery/internal/execs"
+	"infra/cros/recovery/internal/log"
 )
 
 const (
@@ -47,7 +48,83 @@ func matchDevTPMFirmwareVersionExec(ctx context.Context, args *execs.RunArgs, ac
 	return nil
 }
 
+// isTPMPresentExec confirms that the given DUT's TPM is present.
+func isTPMPresentExec(ctx context.Context, args *execs.RunArgs, actionArgs []string) error {
+	tpmRunner := args.NewRunner(args.ResourceName)
+	rawOutput, err := tpmRunner(ctx, "cryptohome --action=status")
+	if err != nil {
+		return errors.Annotate(err, "tpm present").Err()
+	}
+	_, readErr := ReadCryptoHomeStatusInfo(ctx, rawOutput)
+	return errors.Annotate(readErr, "tpm present: cannot read crypto home status info").Err()
+}
+
+// isTPMInGoodStatusExec confirms that the given DUT's TPM is in good state.
+func isTPMInGoodStatusExec(ctx context.Context, args *execs.RunArgs, actionArgs []string) error {
+	tpmRunner := args.NewRunner(args.ResourceName)
+	rawOutput, err := tpmRunner(ctx, "cryptohome --action=status")
+	if err != nil {
+		return errors.Annotate(err, "tpm in good status").Err()
+	}
+	cryptoHS, err := ReadCryptoHomeStatusInfo(ctx, rawOutput)
+	if err != nil {
+		return errors.Annotate(err, "tpm in good status").Err()
+	}
+	enabled, enabledOk := cryptoHS.ReadTPMBool("enabled")
+	if !enabledOk {
+		log.Error(ctx, `Cannot read field "enabled"`)
+		log.Error(ctx, `Cannot determine cryptohome valid status, skipping check`)
+		return nil
+	}
+	if !enabled {
+		log.Error(ctx, "TPM status: hardware is not working.")
+		return errors.Reason("tpm in good status: tpm is not enabled").Err()
+	}
+	canConnect, canConnectOk := cryptoHS.ReadTPMBool("can_connect")
+	lastErrorValue, lastErrorValueOk := cryptoHS.ReadTPMFloat64("last_error")
+	if !canConnectOk {
+		log.Error(ctx, `Cannot read field "can_connect"`)
+		log.Error(ctx, `Cannot determine cryptohome valid status, skipping check`)
+		return nil
+	}
+	if !lastErrorValueOk {
+		log.Error(ctx, `Cannot read field "last_error"`)
+		log.Error(ctx, `Cannot determine cryptohome valid status, skipping check`)
+		return nil
+	}
+	if !canConnect {
+		return errors.Reason("tpm in good status: tpm connect failed -- last_error=%v", lastErrorValue).Err()
+	}
+	owned, ownedOk := cryptoHS.ReadTPMBool("owned")
+	canLoadSrk, canLoadSrkOk := cryptoHS.ReadTPMBool("can_load_srk")
+	if !ownedOk {
+		log.Error(ctx, `Cannot read field value:"owned"`)
+		log.Error(ctx, `Cannot determine cryptohome valid status, skipping check`)
+		return nil
+	}
+	if !canLoadSrkOk {
+		log.Error(ctx, `Cannot read field value:"can_load_srk"`)
+		log.Error(ctx, `Cannot determine cryptohome valid status, skipping check`)
+		return nil
+	}
+	if owned && !canLoadSrk {
+		return errors.Reason("tpm in good status: cannot load the tpm srk").Err()
+	}
+	canLoadSrkPk, canLoadSrkPkOk := cryptoHS.ReadTPMBool("can_load_srk_pubkey")
+	if !canLoadSrkPkOk {
+		log.Error(ctx, `Cannot read field value:"can_load_srk_pubkey"`)
+		log.Error(ctx, `Cannot determine cryptohome valid status, skipping check`)
+		return nil
+	}
+	if canLoadSrk && !canLoadSrkPk {
+		return errors.Reason("tpm in good status: cannot load the tpm srk public key").Err()
+	}
+	return nil
+}
+
 func init() {
 	execs.Register("cros_match_dev_tpm_firmware_version", matchDevTPMFirmwareVersionExec)
 	execs.Register("cros_match_dev_tpm_kernel_version", matchDevTPMKernelVersionExec)
+	execs.Register("cros_is_tpm_present", isTPMPresentExec)
+	execs.Register("cros_is_tpm_in_good_status", isTPMInGoodStatusExec)
 }
