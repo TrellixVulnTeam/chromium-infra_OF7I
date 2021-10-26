a6e681305ca804ebd75c202db274d80c6d56e588
Prakhar <pasthana@google.com>
2021-10-26 20:30:26 +0000

    Create PostSubmitReport and SummaryCoverage entities for referenced coverage:
    Bug: 1221646
    Change-Id: I63c0e099bb016ccd05caf3317ae3d1509de205e0
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/3244760
    Auto-Submit: Prakhar Asthana <pasthana@google.com>
    Commit-Queue: Prakhar Asthana <pasthana@google.com>
    Reviewed-by: Zhaoyang Li <zhaoyangli@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#44759}

--- appengine/findit/services/code_coverage/referenced_coverage.py
+++ appengine/findit/services/code_coverage/referenced_coverage.py
@@ -14,7 +14,9 @@ from libs.gitiles.gitiles_repository import GitilesRepository
 from model.code_coverage import CoverageReportModifier
 from model.code_coverage import FileCoverageData
 from model.code_coverage import PostsubmitReport
+from model.code_coverage import SummaryCoverageData
 from services import bigquery_helper
+from services.code_coverage import aggregation_util
 from services.code_coverage import code_coverage_util
 from services.code_coverage import diff_util
 
@@ -41,7 +43,10 @@ _CHROMIUM_SERVER_HOST = 'chromium.googlesource.com'
 _CHROMIUM_GERRIT_HOST = 'chromium-review.googlesource.com'
 _CHROMIUM_PROJECT = 'chromium/src'
 _CHROMIUM_REPO = GitilesRepository(
-    FinditHttpClient(),
+    FinditHttpClient(
+        # Don't log 404 as it is expected
+        # e.g. a file can missing at a parent commit.
+        no_error_logging_statuses=[404]),
     'https://%s/%s.git' % (_CHROMIUM_SERVER_HOST, _CHROMIUM_PROJECT))
 
 
@@ -61,18 +66,16 @@ def _GetModifiedLinesSinceCommit(latest_lines, commit_lines):
   return modified_lines
 
 
-def _GetReferencedFileCoverage(file_coverage, modified_lines, modifier_id):
+def _GetReferencedFileCoverage(file_coverage, modified_lines):
   """Returns line coverage metrics for interesting lines in a file.
 
   Args:
     file_coverage (FileCoverageData): File coverage report from latest full
                                       codebase run.
     modified_lines (set): Set of lines modified since the reference commit.
-    modified_id (string): Id of the CoverageReportModifier corresponding to the
-                          reference commit.
 
   Returns:
-    A FileCoverageData object with coverage info dropped for all lines except
+    A dict containing coverage info dropped for all lines except
     modified_lines. Returns None if there are no lines with coverage info.
   """
 
@@ -84,10 +87,14 @@ def _GetReferencedFileCoverage(file_coverage, modified_lines, modifier_id):
     for line_num in range(line_range['first'], line_range['last'] + 1):
       if line_num in modified_lines:
         total += 1
-        if line_num == modified_line_ranges[-1]['last'] + 1:
+        if line_num == modified_line_ranges[-1]['last'] + 1 and line_range[
+            'count'] == modified_line_ranges[-1]['count']:
+          # Append to the last interesting line range if line numbers are
+          # continuous and they share the same execution count
           modified_line_ranges[-1]['last'] += 1
         else:
           # Line range gets broken by an unmodified line
+          # or new line range with a different execution count is encountered
           modified_line_ranges.append({
               'first': line_num,
               'last': line_num,
@@ -106,12 +113,7 @@ def _GetReferencedFileCoverage(file_coverage, modified_lines, modifier_id):
         }],
         'revision': file_coverage.gitiles_commit.revision
     }
-    return FileCoverageData.Create(file_coverage.gitiles_commit.server_host,
-                                   file_coverage.gitiles_commit.project,
-                                   file_coverage.gitiles_commit.ref,
-                                   file_coverage.gitiles_commit.revision,
-                                   file_coverage.path, file_coverage.bucket,
-                                   file_coverage.builder, data, modifier_id)
+    return data
 
 
 def _GetActiveReferenceCommits():
@@ -148,6 +150,72 @@ def _GetFileContentAtCommit(file_path, revision):
   return content.split('\n') if content else []
 
 
+def _FlushEntities(entities, total, last=False):
+  """Creates datastore entities in a batched manner"""
+  if len(entities) < 100 and not (last and entities):
+    return entities, total
+
+  ndb.put_multi(entities)
+  total += len(entities)
+  logging.info('Dumped %d coverage data entities', total)
+
+  return [], total
+
+
+def _ExportFileCoverage(files_coverage, postsubmit_report, modifier_id):
+  """Exports file coverage entities to datastore.
+
+  Args:
+    files_coverage(dict): Mapping from file path to corresponding
+                                coverage data.
+    postsubmit_report(PostsubmitReport): Full codebase report for which
+                                referenced entities are being exported.
+    modifier_id(int): Id of the CoverageReportModifier corresponding to the
+                      reference commit.
+  """
+  entities = []
+  total = 0
+  logging.info("Dumping file coverage")
+  for i in range(0, len(files_coverage)):
+    data = files_coverage[i]
+    entity = FileCoverageData.Create(
+        postsubmit_report.gitiles_commit.server_host,
+        postsubmit_report.gitiles_commit.project,
+        postsubmit_report.gitiles_commit.ref,
+        postsubmit_report.gitiles_commit.revision, data['path'],
+        postsubmit_report.bucket, postsubmit_report.builder, data, modifier_id)
+    entities.append(entity)
+    entities, total = _FlushEntities(entities, total)
+  _FlushEntities(entities, total, last=True)
+
+
+def _ExportDirSummaryCoverage(directories_coverage, postsubmit_report,
+                              modifier_id):
+  """Exports directory summary coverage entities to datastore.
+
+  Args:
+    directories_coverage(dict): Mapping from directory path to corresponding
+                                coverage data.
+    postsubmit_report(PostsubmitReport): Full codebase report for which
+                                referenced entities are being exported.
+    modifier_id(int): Id of the CoverageReportModifier corresponding to the
+                      reference commit.
+  """
+  entities = []
+  total = 0
+  logging.info("Dumping directory coverage")
+  for path, data in directories_coverage.items():
+    entity = SummaryCoverageData.Create(
+        postsubmit_report.gitiles_commit.server_host,
+        postsubmit_report.gitiles_commit.project,
+        postsubmit_report.gitiles_commit.ref,
+        postsubmit_report.gitiles_commit.revision, 'dirs', path,
+        postsubmit_report.bucket, postsubmit_report.builder, data, modifier_id)
+    entities.append(entity)
+    entities, total = _FlushEntities(entities, total)
+  _FlushEntities(entities, total, last=True)
+
+
 def _CreateReferencedCoverage(postsubmit_report):
   """Creates coverage entities referenced against a past commit.
 
@@ -157,6 +225,7 @@ def _CreateReferencedCoverage(postsubmit_report):
   """
 
   for reference_commit, modifier_id in _GetActiveReferenceCommits().items():
+    # Fetch file coverage entities corresponding to the full codebase report
     query = FileCoverageData.query(
         FileCoverageData.gitiles_commit.server_host ==
         postsubmit_report.gitiles_commit.server_host,
@@ -171,24 +240,46 @@ def _CreateReferencedCoverage(postsubmit_report):
         FileCoverageData.modifier_id == 0)
     more = True
     cursor = None
+    referenced_file_coverage = []
     while more:
       results, cursor, more = query.fetch_page(_PAGE_SIZE, start_cursor=cursor)
-      referenced_entities = []
       for file_coverage in results:
-        latest_lines = _GetFileContentAtCommit(
+        content_at_latest = _GetFileContentAtCommit(
             file_coverage.path, file_coverage.gitiles_commit.revision)
-        assert latest_lines
+        assert content_at_latest
 
-        reference_commit_lines = _GetFileContentAtCommit(
+        content_at_reference_commit = _GetFileContentAtCommit(
             file_coverage.path, reference_commit)
-        modified_lines = _GetModifiedLinesSinceCommit(latest_lines,
-                                                      reference_commit_lines)
         referenced_coverage = _GetReferencedFileCoverage(
-            file_coverage, modified_lines, modifier_id)
+            file_coverage,
+            _GetModifiedLinesSinceCommit(content_at_latest,
+                                         content_at_reference_commit))
         if referenced_coverage:
-          referenced_entities.append(referenced_coverage)
+          referenced_file_coverage.append(referenced_coverage)
 
-      ndb.put_multi(referenced_entities)
+    referenced_directory_coverage, _ = (
+        aggregation_util.get_aggregated_coverage_data_from_files(
+            referenced_file_coverage))
+    _ExportFileCoverage(referenced_file_coverage, postsubmit_report,
+                        modifier_id)
+    _ExportDirSummaryCoverage(referenced_directory_coverage, postsubmit_report,
+                              modifier_id)
+    # Create a top level PostsubmitReport entity with visible = True
+    if referenced_directory_coverage:
+      referenced_report = PostsubmitReport.Create(
+          server_host=postsubmit_report.gitiles_commit.server_host,
+          project=postsubmit_report.gitiles_commit.project,
+          ref=postsubmit_report.gitiles_commit.ref,
+          revision=postsubmit_report.gitiles_commit.revision,
+          bucket=postsubmit_report.bucket,
+          builder=postsubmit_report.builder,
+          commit_timestamp=postsubmit_report.commit_timestamp,
+          manifest=postsubmit_report.manifest,
+          summary_metrics=referenced_directory_coverage['//']['summaries'],
+          build_id=postsubmit_report.build_id,
+          visible=True,
+          modifier_id=modifier_id)
+      referenced_report.put()
 
 
 def _GetAllowedBuilders():
--- appengine/findit/services/code_coverage/test/referenced_coverage_test.py
+++ appengine/findit/services/code_coverage/test/referenced_coverage_test.py
@@ -13,6 +13,7 @@ from waterfall.test.wf_testcase import WaterfallTestCase
 from model.code_coverage import CoverageReportModifier
 from model.code_coverage import FileCoverageData
 from model.code_coverage import PostsubmitReport
+from model.code_coverage import SummaryCoverageData
 from services.code_coverage import referenced_coverage
 from services import bigquery_helper
 
@@ -24,7 +25,8 @@ class ReferencedCoverageTest(WaterfallTestCase):
       '_GetAllowedBuilders',
       return_value=['linux-code-coverage'])
   @mock.patch.object(GitilesRepository, 'GetSource')
-  def testFileModifiedSinceReferenceCommit(self, mock_file_content, *_):
+  def testFileModifiedSinceReferenceCommit_FileCoverageGetsCreated(
+      self, mock_file_content, *_):
     CoverageReportModifier(reference_commit='past_commit', id=123).put()
     postsubmit_report = PostsubmitReport.Create(
         server_host='chromium.googlesource.com',
@@ -44,7 +46,7 @@ class ReferencedCoverageTest(WaterfallTestCase):
         project='chromium/src',
         ref='refs/heads/main',
         revision='rev',
-        path='//myfile.cc',
+        path='//a/myfile.cc',
         bucket='ci',
         builder='linux-code-coverage',
         data={'lines': [{
@@ -64,13 +66,13 @@ class ReferencedCoverageTest(WaterfallTestCase):
         project='chromium/src',
         ref='refs/heads/main',
         revision='rev',
-        path='//myfile.cc',
+        path='//a/myfile.cc',
         bucket='ci',
         builder='linux-code-coverage',
         modifier_id=123)
     self.assertEqual(
         entity.data, {
-            'path': '//myfile.cc',
+            'path': '//a/myfile.cc',
             'lines': [{
                 'first': 4,
                 'last': 5,
@@ -83,6 +85,162 @@ class ReferencedCoverageTest(WaterfallTestCase):
             }],
             'revision': 'rev'
         })
+
+  @mock.patch.object(
+      referenced_coverage,
+      '_GetAllowedBuilders',
+      return_value=['linux-code-coverage'])
+  @mock.patch.object(GitilesRepository, 'GetSource')
+  def testFileModifiedSinceReferenceCommit_DirSummaryGetsCreated(
+      self, mock_file_content, *_):
+    CoverageReportModifier(reference_commit='past_commit', id=123).put()
+    postsubmit_report = PostsubmitReport.Create(
+        server_host='chromium.googlesource.com',
+        project='chromium/src',
+        ref='refs/heads/main',
+        revision='rev',
+        bucket='ci',
+        builder='linux-code-coverage',
+        commit_timestamp=datetime(2020, 1, 7),
+        manifest=[],
+        summary_metrics={},
+        build_id=2000,
+        visible=True)
+    postsubmit_report.put()
+    file_coverage_data = FileCoverageData.Create(
+        server_host='chromium.googlesource.com',
+        project='chromium/src',
+        ref='refs/heads/main',
+        revision='rev',
+        path='//a/myfile.cc',
+        bucket='ci',
+        builder='linux-code-coverage',
+        data={'lines': [{
+            'count': 10,
+            'first': 1,
+            'last': 5
+        }]})
+    file_coverage_data.put()
+    content_at_feature_commit = 'line1\nline2\nline3'
+    latest_content = 'line1\nline2\nline3\nline4\nline5'
+    mock_file_content.side_effect = [latest_content, content_at_feature_commit]
+
+    referenced_coverage.CreateReferencedCoverage()
+
+    entity1 = SummaryCoverageData.Get(
+        server_host='chromium.googlesource.com',
+        project='chromium/src',
+        ref='refs/heads/main',
+        revision='rev',
+        data_type='dirs',
+        path='//a/',
+        bucket='ci',
+        builder='linux-code-coverage',
+        modifier_id=123)
+    entity2 = SummaryCoverageData.Get(
+        server_host='chromium.googlesource.com',
+        project='chromium/src',
+        ref='refs/heads/main',
+        revision='rev',
+        data_type='dirs',
+        path='//',
+        bucket='ci',
+        builder='linux-code-coverage',
+        modifier_id=123)
+    self.assertEqual(
+        entity1.data, {
+            'dirs': [],
+            'path':
+                '//a/',
+            'summaries': [{
+                'covered': 2,
+                'total': 2,
+                'name': 'line'
+            }],
+            'files': [{
+                'path': '//a/myfile.cc',
+                'name': 'myfile.cc',
+                'summaries': [{
+                    'covered': 2,
+                    'total': 2,
+                    'name': 'line'
+                }]
+            }]
+        })
+    self.assertEqual(
+        entity2.data, {
+            'dirs': [{
+                'path': '//a/',
+                'name': 'a/',
+                'summaries': [{
+                    'covered': 2,
+                    'total': 2,
+                    'name': 'line'
+                }]
+            }],
+            'path': '//',
+            'summaries': [{
+                'covered': 2,
+                'total': 2,
+                'name': 'line'
+            }],
+            'files': []
+        })
+
+  @mock.patch.object(
+      referenced_coverage,
+      '_GetAllowedBuilders',
+      return_value=['linux-code-coverage'])
+  @mock.patch.object(GitilesRepository, 'GetSource')
+  def testFileModifiedSinceReferenceCommit_PostsubmitReportGetsCreated(
+      self, mock_file_content, *_):
+    CoverageReportModifier(reference_commit='past_commit', id=123).put()
+    postsubmit_report = PostsubmitReport.Create(
+        server_host='chromium.googlesource.com',
+        project='chromium/src',
+        ref='refs/heads/main',
+        revision='rev',
+        bucket='ci',
+        builder='linux-code-coverage',
+        commit_timestamp=datetime(2020, 1, 7),
+        manifest=[],
+        summary_metrics={},
+        build_id=2000,
+        visible=True)
+    postsubmit_report.put()
+    file_coverage_data = FileCoverageData.Create(
+        server_host='chromium.googlesource.com',
+        project='chromium/src',
+        ref='refs/heads/main',
+        revision='rev',
+        path='//a/myfile.cc',
+        bucket='ci',
+        builder='linux-code-coverage',
+        data={'lines': [{
+            'count': 10,
+            'first': 1,
+            'last': 5
+        }]})
+    file_coverage_data.put()
+    content_at_feature_commit = 'line1\nline2\nline3'
+    latest_content = 'line1\nline2\nline3\nline4\nline5'
+    mock_file_content.side_effect = [latest_content, content_at_feature_commit]
+
+    referenced_coverage.CreateReferencedCoverage()
+
+    entity = PostsubmitReport.Get(
+        server_host='chromium.googlesource.com',
+        project='chromium/src',
+        ref='refs/heads/main',
+        revision='rev',
+        bucket='ci',
+        builder='linux-code-coverage',
+        modifier_id=123)
+    self.assertEqual(entity.summary_metrics, [{
+        'covered': 2,
+        'total': 2,
+        'name': 'line'
+    }])
 
   @mock.patch.object(
       referenced_coverage,
@@ -109,7 +267,7 @@ class ReferencedCoverageTest(WaterfallTestCase):
         project='chromium/src',
         ref='refs/heads/main',
         revision='rev',
-        path='//myfile.cc',
+        path='//a/myfile.cc',
         bucket='ci',
         builder='linux-code-coverage',
         data={'lines': [{
@@ -129,7 +287,7 @@ class ReferencedCoverageTest(WaterfallTestCase):
         project='chromium/src',
         ref='refs/heads/main',
         revision='rev',
-        path='//myfile.cc',
+        path='//a/myfile.cc',
         bucket='ci',
         builder='linux-code-coverage',
         modifier_id=123)
