a0fa5f5937b2802272d918e019be95b6727c63c7
Vineet Joshi <vkjoshi@google.com>
2021-11-05 18:03:16 +0000

    [recovery]: Correctly name the execs in servo_execs.
    This commit is a part of the PARIS project.
    
    The servo-execs follow a specific naming convention. A few of the
    execs do not adhere to this convention. The current CL corrects this
    aberration.
    
    bug=b:198809511
    TEST=build
    
    Change-Id: I7f7a77e09c00148e31e7e3435793de0adde891dc
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/3259501
    Reviewed-by: Otabek Kasimov <otabek@google.com>
    Commit-Queue: Vineet Joshi <vkjoshi@google.com>
    Cr-Commit-Position: refs/heads/main@{#45016}

--- go/src/infra/cros/recovery/internal/execs/servo/servo_execs.go
+++ go/src/infra/cros/recovery/internal/execs/servo/servo_execs.go
@@ -84,27 +84,27 @@ func servodRestartActionExec(ctx context.Context, args *execs.RunArgs, actionArg
 	return nil
 }
 
-func servoDetectUSBKey(ctx context.Context, args *execs.RunArgs, actionArgs []string) error {
+func servoDetectUSBKeyExec(ctx context.Context, args *execs.RunArgs, actionArgs []string) error {
 	res, err := ServodCallGet(ctx, args, "image_usbkey_dev")
 	if err != nil {
 		args.DUT.ServoHost.UsbkeyState = tlw.HardwareStateNotDetected
-		return errors.Annotate(err, "servo detect usb key: could not obtain usb path on servo: %q", err).Err()
+		return errors.Annotate(err, "servo detect usb key exec: could not obtain usb path on servo: %q", err).Err()
 	} else if res.Value.GetString_() == "" {
 		args.DUT.ServoHost.UsbkeyState = tlw.HardwareStateNotDetected
-		return errors.Reason("servo detect usb key: the path to usb drive is empty").Err()
+		return errors.Reason("servo detect usb key exec: the path to usb drive is empty").Err()
 	}
 	servoUsbPath := res.Value.GetString_()
-	log.Debug(ctx, "Servo Detect USB-Key: USB-key path: %s.", servoUsbPath)
+	log.Debug(ctx, "Servo Detect USB-Key Exec: USB-key path: %s.", servoUsbPath)
 	r := args.Access.Run(ctx, args.DUT.ServoHost.Name, fmt.Sprintf("fdisk -l %s", servoUsbPath))
 	if r.ExitCode != 0 {
 		args.DUT.ServoHost.UsbkeyState = tlw.HardwareStateNotDetected
-		log.Debug(ctx, "Servo Detect USB-Key: fdisk command did not succeed, exit code %q.", r.ExitCode)
-		return errors.Reason("servo detect usb key: could not determine whether %q is a valid usb path", servoUsbPath).Err()
+		log.Debug(ctx, "Servo Detect USB-Key Exec: fdisk command did not succeed, exit code %q.", r.ExitCode)
+		return errors.Reason("servo detect usb key exec: could not determine whether %q is a valid usb path", servoUsbPath).Err()
 	}
 	if args.DUT.ServoHost.UsbkeyState == tlw.HardwareStateNeedReplacement {
 		// This device has been marked for replacement. A further
 		// audit action is required to correct this.
-		log.Debug(ctx, "Servo Detect USB-Key: device marked for replacement.")
+		log.Debug(ctx, "Servo Detect USB-Key Exec: device marked for replacement.")
 	} else {
 		args.DUT.ServoHost.UsbkeyState = tlw.HardwareStateNormal
 	}
@@ -134,23 +134,23 @@ func runCheckOnHost(ctx context.Context, args *execs.RunArgs, resourceName strin
 	}
 }
 
-func servoAuditUSBKey(ctx context.Context, args *execs.RunArgs, actionArgs []string) error {
+func servoAuditUSBKeyExec(ctx context.Context, args *execs.RunArgs, actionArgs []string) error {
 	dutUsb := ""
 	if cros.IsSSHable(ctx, args, args.DUT.Name) == nil {
-		log.Debug(ctx, "Servo Audit USB Key: %q is reachable through SSH", args.DUT.Name)
+		log.Debug(ctx, "Servo Audit USB-Key Exec: %q is reachable through SSH", args.DUT.Name)
 		var err error = nil
 		dutUsb, err = GetUSBDrivePathOnDut(ctx, args)
 		if err != nil {
-			log.Debug(ctx, "Servo Audit USB Key: could not determine USB-drive path on DUT: %q, error: %q. This is not critical. We will continue the audit by setting the path to empty string.", args.DUT.Name, err)
+			log.Debug(ctx, "Servo Audit USB-Key Exec: could not determine USB-drive path on DUT: %q, error: %q. This is not critical. We will continue the audit by setting the path to empty string.", args.DUT.Name, err)
 		}
 	} else {
-		log.Debug(ctx, "Servo Audit USB Key: continue audit from servo-host because DUT %q is not reachable through SSH", args.DUT.Name)
+		log.Debug(ctx, "Servo Audit USB-Key Exec: continue audit from servo-host because DUT %q is not reachable through SSH", args.DUT.Name)
 	}
 	if dutUsb != "" {
 		// DUT is reachable, and we found a USB drive on it.
 		state, err := runCheckOnHost(ctx, args, args.DUT.Name, dutUsb)
 		if err != nil {
-			return errors.Reason("servo audit usb key: could not check DUT usb path %q", dutUsb).Err()
+			return errors.Reason("servo audit usb key exec: could not check DUT usb path %q", dutUsb).Err()
 		}
 		args.DUT.ServoHost.UsbkeyState = state
 	} else {
@@ -168,17 +168,17 @@ func servoAuditUSBKey(ctx context.Context, args *execs.RunArgs, actionArgs []str
 			// changed between execution of dependency, and this
 			// operation.
 			args.DUT.ServoHost.UsbkeyState = tlw.HardwareStateNotDetected
-			return errors.Annotate(err, "servo audit usb key: could not obtain usb path on servo: %q", err).Err()
+			return errors.Annotate(err, "servo audit usb key exec: could not obtain usb path on servo: %q", err).Err()
 		}
 		servoUsbPath := strings.TrimSpace(result.Value.GetString_())
 		if servoUsbPath == "" {
 			args.DUT.ServoHost.UsbkeyState = tlw.HardwareStateNotDetected
-			log.Debug(ctx, "Servo Audit USB Key: cannot continue audit because the path to USB-Drive is empty")
-			return errors.Reason("servo audit usb key: the path to usb drive is empty").Err()
+			log.Debug(ctx, "Servo Audit USB-Key Exec: cannot continue audit because the path to USB-Drive is empty")
+			return errors.Reason("servo audit usb key exec: the path to usb drive is empty").Err()
 		}
 		state, err := runCheckOnHost(ctx, args, args.DUT.ServoHost.Name, servoUsbPath)
 		if err != nil {
-			log.Debug(ctx, "Servo Audit USB Key: error %q during audit of USB-Drive", err)
+			log.Debug(ctx, "Servo Audit USB-Key Exec: error %q during audit of USB-Drive", err)
 			return errors.Annotate(err, "servo audit usb key: could not check usb path %q on servo-host %q", servoUsbPath, args.DUT.ServoHost.Name).Err()
 		}
 		args.DUT.ServoHost.UsbkeyState = state
@@ -191,31 +191,31 @@ func isRootServoPresentExec(ctx context.Context, args *execs.RunArgs, actionArgs
 	runner := args.NewRunner(args.DUT.ServoHost.Name)
 	rootServo, err := topology.GetRootServo(ctx, runner, args.DUT.ServoHost.Servo.SerialNumber)
 	if err != nil {
-		return errors.Annotate(err, "is root servo present").Err()
+		return errors.Annotate(err, "is root servo present exec").Err()
 	}
 	if !topology.IsItemGood(ctx, rootServo) {
-		log.Info(ctx, "is servo root present: no good root servo found")
-		return errors.Reason("is servo root present: no good root servo found").Err()
+		log.Info(ctx, "Is Servo Root Present Exec: no good root servo found")
+		return errors.Reason("is servo root present exec: no good root servo found").Err()
 	}
-	log.Info(ctx, "is servo root present: success")
+	log.Info(ctx, "is servo root present exec: success")
 	return nil
 }
 
 // Verify that the root servo is enumerated/present on the host.
-func servoTopologyUpdate(ctx context.Context, args *execs.RunArgs, actionArgs []string) error {
+func servoTopologyUpdateExec(ctx context.Context, args *execs.RunArgs, actionArgs []string) error {
 	runner := args.NewRunner(args.DUT.ServoHost.Name)
 	servoTopology, err := topology.RetrieveServoTopology(ctx, runner, args.DUT.ServoHost.Servo.SerialNumber)
 	if err != nil {
-		return errors.Annotate(err, "servo verify topology").Err()
+		return errors.Annotate(err, "servo topology update exec").Err()
 	}
 	if servoTopology.Root == nil {
-		return errors.Reason("servo verify topology: root servo not found").Err()
+		return errors.Reason("servo topology update exec: root servo not found").Err()
 	}
 	argsMap := execs.ParseActionArgs(ctx, actionArgs, execs.DefaultSplitter)
 	minChildCount := topologyMinChildCountDefaultValue
 	persistTopology := persistTopologyDefaultValue
 	for k, v := range argsMap {
-		log.Debug(ctx, "Servo Topology Update: k:%q, v:%q", k, v)
+		log.Debug(ctx, "Servo Topology Update Exec: k:%q, v:%q", k, v)
 		if v != "" {
 			// a non-empty value string implies that the corresponding
 			// action arg was parsed correctly.
@@ -227,18 +227,18 @@ func servoTopologyUpdate(ctx context.Context, args *execs.RunArgs, actionArgs []
 				// of number of child servo based min_child.
 				minChildCount, err = strconv.Atoi(v)
 				if err != nil {
-					return errors.Reason("servo verify topology: malformed min child config in action arg %q:%q", k, v).Err()
+					return errors.Reason("servo topology update exec: malformed min child config in action arg %q:%q", k, v).Err()
 				}
 			case persistTopologyArg:
 				persistTopology, err = strconv.ParseBool(v)
 				if err != nil {
-					return errors.Reason("servo verify topology: malformed update servo config in action arg %q:%q", k, v).Err()
+					return errors.Reason("servo topology update exec: malformed update servo config in action arg %q:%q", k, v).Err()
 				}
 			}
 		}
 	}
 	if len(servoTopology.Children) < minChildCount {
-		return errors.Reason("servo verify topology: expected a min of %d children, found %d", minChildCount, len(servoTopology.Children)).Err()
+		return errors.Reason("servo topology update exec: expected a min of %d children, found %d", minChildCount, len(servoTopology.Children)).Err()
 	}
 	if persistTopology {
 		// This verified topology will be used in all subsequent
@@ -250,13 +250,13 @@ func servoTopologyUpdate(ctx context.Context, args *execs.RunArgs, actionArgs []
 }
 
 // Verify that servod is responsive
-func servoServodEchoHost(ctx context.Context, args *execs.RunArgs, actionArgs []string) error {
+func servoServodEchoHostExec(ctx context.Context, args *execs.RunArgs, actionArgs []string) error {
 	runner := args.NewRunner(args.DUT.ServoHost.Name)
 	v, err := runner(ctx, fmt.Sprintf(servodHostCheckupCmd, args.DUT.ServoHost.ServodPort))
 	if err != nil {
-		return errors.Annotate(err, "servo remote echo: servod is not responsive for dut-control commands").Err()
+		return errors.Annotate(err, "servo servod echo host exec: servod is not responsive for dut-control commands").Err()
 	}
-	log.Debug(ctx, "Servo Remote Echo: Servod is responsive: %q", v)
+	log.Debug(ctx, "Servo Servod Echo Host Exec: Servod is responsive: %q", v)
 	return nil
 }
 
@@ -279,14 +279,14 @@ func servoFirmwareNeedsUpdateExec(ctx context.Context, args *execs.RunArgs, acti
 		// re-compute the topology.
 		devices, err = topology.ListOfDevices(ctx, runner, args.DUT.ServoHost.Servo.SerialNumber)
 		if err != nil {
-			errors.Annotate(err, "servo verify firmware update").Err()
+			errors.Annotate(err, "servo firmware needs update exec").Err()
 		}
-		log.Debug(ctx, "Servo Verify Firmware Update: topology re-computer because pre-existing servo topology not found.")
+		log.Debug(ctx, "Servo Firmware Needs Update Exec: topology re-computer because pre-existing servo topology not found.")
 	}
 	for _, d := range devices {
 		if topology.IsItemGood(ctx, d) {
 			if needsUpdate(ctx, runner, d, args.DUT.ServoHost.Servo.FirmwareChannel, args.DUT.Board) {
-				return errors.Reason("servo verify firmware update: servo needs update").Err()
+				return errors.Reason("servo firmware needs update exec: servo needs update").Err()
 			}
 		}
 	}
@@ -297,10 +297,10 @@ func init() {
 	execs.Register("servo_host_servod_init", servodInitActionExec)
 	execs.Register("servo_host_servod_stop", servodStopActionExec)
 	execs.Register("servo_host_servod_restart", servodRestartActionExec)
-	execs.Register("servo_detect_usbkey", servoDetectUSBKey)
-	execs.Register("servo_audit_usbkey", servoAuditUSBKey)
+	execs.Register("servo_detect_usbkey", servoDetectUSBKeyExec)
+	execs.Register("servo_audit_usbkey", servoAuditUSBKeyExec)
 	execs.Register("servo_v4_root_present", isRootServoPresentExec)
-	execs.Register("servo_topology_update", servoTopologyUpdate)
-	execs.Register("servo_servod_echo_host", servoServodEchoHost)
+	execs.Register("servo_topology_update", servoTopologyUpdateExec)
+	execs.Register("servo_servod_echo_host", servoServodEchoHostExec)
 	execs.Register("servo_fw_need_update", servoFirmwareNeedsUpdateExec)
 }
