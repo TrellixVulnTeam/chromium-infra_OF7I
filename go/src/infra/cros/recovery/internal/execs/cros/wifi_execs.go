a1cbbcf605aab074a90ea94e0c76b3c7bb57e03f
Yunzhi Yu <yunzhiyu@google.com>
2021-11-05 17:26:25 +0000

    recovery: Add cros_audit_wifi verifier.
    BUG=b:197527180
    TEST=manual test
    
    Please enter the commit message for your changes. Lines starting
    
    Change-Id: I5864543d0cc1b0109e7c44b26a4b6d6a5ba988a8
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/3243275
    Commit-Queue: Yunzhi Yu <yunzhiyu@google.com>
    Reviewed-by: Otabek Kasimov <otabek@google.com>
    Reviewed-by: Vineet Joshi <vkjoshi@google.com>
    Cr-Commit-Position: refs/heads/main@{#45015}

---
+++ go/src/infra/cros/recovery/internal/execs/cros/wifi_execs.go
@@ -0,0 +1,55 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package cros
+
+import (
+	"context"
+
+	"go.chromium.org/luci/common/errors"
+
+	"infra/cros/recovery/internal/execs"
+	"infra/cros/recovery/internal/log"
+	"infra/cros/recovery/tlw"
+)
+
+const (
+	// command to check whether the wifi device has been recogonized
+	// and its device driver been loaded by the kernel.
+	wifiDetectCmd = `lspci -vvn | grep iwlwifi`
+)
+
+// auditWiFiExec will validate wifi chip and update state.
+//
+// Detect if the DUT has wifi device listed in the output of 'lspci' command.
+func auditWiFiExec(ctx context.Context, args *execs.RunArgs, actionArgs []string) error {
+	r := args.NewRunner(args.ResourceName)
+	_, err := r(ctx, wifiDetectCmd)
+	if err == nil {
+		// successfully detected
+		args.DUT.Wifi.State = tlw.HardwareStateNormal
+		log.Info(ctx, "set wifi state to be: %s", tlw.HardwareStateNormal)
+		return nil
+	}
+	if execs.SSHErrorInternal.In(err) || execs.SSHErrorCLINotFound.In(err) {
+		args.DUT.Wifi.State = tlw.HardwareStateUnspecified
+		return errors.Annotate(err, "audit wifi").Err()
+	}
+	if args.DUT.Wifi.ChipName != "" {
+		// If wifi chip is not detected, but was expected by setup info then we
+		// set needs_replacement as it is probably a hardware issue.
+		args.DUT.Wifi.State = tlw.HardwareStateNeedReplacement
+		log.Info(ctx, "set wifi state to be: %s", tlw.HardwareStateNeedReplacement)
+		return errors.Annotate(err, "audit wifi").Err()
+	}
+	// the wifi state cannot be determined due to cmd failed
+	// therefore, set it to HardwareStateNotDetected
+	args.DUT.Wifi.State = tlw.HardwareStateNotDetected
+	log.Info(ctx, "set wifi state to be: %s", tlw.HardwareStateNotDetected)
+	return errors.Annotate(err, "audit wifi").Err()
+}
+
+func init() {
+	execs.Register("cros_audit_wifi", auditWiFiExec)
+}
