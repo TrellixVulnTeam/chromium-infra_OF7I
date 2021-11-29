d93bced37188afe922b111dd70a8f26ed9120b1e
Gregory Nisbet <gregorynisbet@google.com>
2021-11-29 22:21:49 +0000

    [crosskylabadmin] Install services in GAEv2 main.
    Install the tracker server in the GAEv2 entrypoint for crosskylabadmin.
    
    BUG=b:173543443
    TEST=run CrOSkylabAdmin locally
    
    Change-Id: I97f1c3bb69144afca0ae0fdd9f5659772592cd25
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/3307127
    Reviewed-by: Otabek Kasimov <otabek@google.com>
    Commit-Queue: Gregory Nisbet <gregorynisbet@google.com>
    Cr-Commit-Position: refs/heads/main@{#45594}

--- go/src/infra/appengine/crosskylabadmin/gaev2/crosadm/main.go
+++ go/src/infra/appengine/crosskylabadmin/gaev2/crosadm/main.go
@@ -9,9 +9,13 @@ package main
 import (
 	"go.chromium.org/luci/common/logging"
 	"go.chromium.org/luci/config/server/cfgmodule"
+	"go.chromium.org/luci/grpc/prpc"
 	"go.chromium.org/luci/server"
 	"go.chromium.org/luci/server/gaeemulation"
 	"go.chromium.org/luci/server/module"
+
+	"infra/appengine/crosskylabadmin/api/fleet/v1"
+	"infra/appengine/crosskylabadmin/internal/app/frontend"
 )
 
 // Main is the entrypoint for the GAEv2 version of CrOSSkylabAdmin.
@@ -22,7 +26,16 @@ func main() {
 	}
 
 	server.Main(nil, modules, func(srv *server.Server) error {
-		logging.Infof(srv.Context, "This GAEv2 app does nothing.")
+		logging.Infof(srv.Context, "Installing services.")
+		installServices(srv.PRPC)
+		logging.Infof(srv.Context, "Finished installing services.")
 		return nil
+	})
+}
+
+// Install the CrOSSkylabAdminServices into a prpc registrar.
+func installServices(r prpc.Registrar) {
+	fleet.RegisterTrackerServer(r, &fleet.DecoratedTracker{
+		Service: &frontend.TrackerServerImpl{},
 	})
 }
