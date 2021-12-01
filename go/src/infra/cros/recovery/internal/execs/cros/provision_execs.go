1fcc2dee0b0aadb772de64622b9ea3d141a1e09e
Otabek Kasimov <otabek@google.com>
2021-12-01 23:20:36 +0000

    recovery: Add custom args for provision exec
    BUG=b:206051851
    TEST=build, go test ./...
    
    Change-Id: If5a0d9343d681be9b3d92d30a144ae8d48333849
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/3309833
    Commit-Queue: Otabek Kasimov <otabek@google.com>
    Auto-Submit: Otabek Kasimov <otabek@google.com>
    Reviewed-by: Vineet Joshi <vkjoshi@google.com>
    Cr-Commit-Position: refs/heads/main@{#45711}

--- go/src/infra/cros/recovery/internal/execs/cros/provision_execs.go
+++ go/src/infra/cros/recovery/internal/execs/cros/provision_execs.go
@@ -18,19 +18,34 @@ import (
 // provisionExec performs provisioning of the device.
 //
 // To prevent reboot of device please provide action exec argument 'no_reboot'.
+// To provide custom image data please use 'os_name', 'os_bucket', 'os_image_path'.
 func provisionExec(ctx context.Context, args *execs.RunArgs, actionArgs []string) error {
 	req := &tlw.ProvisionRequest{
 		Resource:      args.ResourceName,
 		PreventReboot: false,
 	}
-	for _, arg := range actionArgs {
-		if arg == "no_reboot" {
-			req.PreventReboot = true
-			log.Debug(ctx, "Cros provision will be perform without reboot.")
-		}
+	argsMap := execs.ParseActionArgs(ctx, actionArgs, execs.DefaultSplitter)
+	if _, ok := argsMap["no_reboot"]; ok {
+		req.PreventReboot = true
+		log.Debug(ctx, "Cros provision will be perform without reboot.")
 	}
-	req.SystemImagePath = fmt.Sprintf("%s/%s", gsCrOSImageBucket, args.DUT.StableVersion.CrosImage)
-	log.Debug(ctx, "System image path: %s", req.SystemImagePath)
+	osImageName := args.DUT.StableVersion.CrosImage
+	if image, ok := argsMap["os_name"]; ok {
+		osImageName = image
+		log.Debug(ctx, "Cros provision received custom OS image name: %s", osImageName)
+	}
+	osImageBucket := gsCrOSImageBucket
+	if bucket, ok := argsMap["os_bucket"]; ok {
+		osImageBucket = bucket
+		log.Debug(ctx, "Cros provision received custom OS bucket name: %s", osImageBucket)
+	}
+	osImagePath := fmt.Sprintf("%s/%s", osImageBucket, osImageName)
+	if imagePath, ok := argsMap["os_image_path"]; ok {
+		osImagePath = imagePath
+		log.Debug(ctx, "Cros provision received custom OS image path: %s", osImagePath)
+	}
+	req.SystemImagePath = osImagePath
+	log.Debug(ctx, "Cros provision OS image path: %s", req.SystemImagePath)
 	err := args.Access.Provision(ctx, req)
 	return errors.Annotate(err, "cros provision").Err()
 }
