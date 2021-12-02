521fdcc93976541f57858f6eb066ebe9cf1808a1
Vadim Shtayura <vadimsh@chromium.org>
2021-12-02 23:09:02 +0000

    [cipd] Update cloudbuildhelper.
    To pick up https://chromium-review.googlesource.com/c/3313845.
    
    R=iannucci@chromium.org
    
    Change-Id: I32cc3d7cb03f72f8724ab621257f7ee0bd10520f
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/3313847
    Reviewed-by: Robbie Iannucci <iannucci@chromium.org>
    Commit-Queue: Vadim Shtayura <vadimsh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#45751}

--- recipes/recipe_modules/cloudbuildhelper/api.py
+++ recipes/recipe_modules/cloudbuildhelper/api.py
@@ -16,7 +16,7 @@ from recipe_engine import recipe_api
 
 
 # Version of `cloudbuildhelper` to use by default.
-CBH_VERSION = 'git_revision:2b72803a9cdd7aff59c9ba1df4941a1c36c25082'
+CBH_VERSION = 'git_revision:b41a9cdf220ff45b285d81bd4201a0cfe8ca7f98'
 
 
 class CloudBuildHelperApi(recipe_api.RecipeApi):
