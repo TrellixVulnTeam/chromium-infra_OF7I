e0d208111dbb0bedad5ba50b0453e89ca81bc65f
Ivan Brovkovich <ivanbrovkovich@chromium.org>
2021-12-10 21:32:23 +0000

    labservices: DutTopology mapping mapping
    This is a first part in a serise of CLs to implement the mapping
    
    Bug: 194817795
    Test: unittest
    Change-Id: I7ff6af626682750970f353deb2c0db3a8f8ffaad
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/3319705
    Auto-Submit: Ivan Brovkovich <ivanbrovkovich@chromium.org>
    Reviewed-by: Allen Li <ayatane@chromium.org>
    Commit-Queue: Ivan Brovkovich <ivanbrovkovich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#45981}

--- go/src/infra/cros/cmd/labservice/internal/ufs/ufs.go
+++ go/src/infra/cros/cmd/labservice/internal/ufs/ufs.go
@@ -14,6 +14,7 @@ import (
 	labapi "go.chromium.org/chromiumos/config/go/test/lab/api"
 
 	ufspb "infra/unifiedfleet/api/v1/models"
+	lab "infra/unifiedfleet/api/v1/models/chromeos/lab"
 	ufsapi "infra/unifiedfleet/api/v1/rpc"
 )
 
@@ -37,11 +38,15 @@ func GetDutTopology(ctx context.Context, c ufsapi.FleetClient, id string) (*laba
 	if err != nil {
 		return nil, status.Errorf(codes.FailedPrecondition, "ID %q: %s", id, err)
 	}
-	dt := &labapi.DutTopology{
-		Id:   &labapi.DutTopology_Id{Value: id},
-		Duts: []*labapi.Dut{d},
-	}
+	dt := makeTopology(id)
+	dt.Duts = append(dt.Duts, d)
 	return dt, nil
+}
+
+func makeTopology(id string) *labapi.DutTopology {
+	return &labapi.DutTopology{
+		Id: &labapi.DutTopology_Id{Value: id},
+	}
 }
 
 // getSchedulingUnitDutTopology returns a DutTopology constructed from
@@ -54,7 +59,7 @@ func getSchedulingUnitDutTopology(ctx context.Context, c ufsapi.FleetClient, id 
 		// Use the gRPC status from the UFS call.
 		return nil, err
 	}
-	dt := &labapi.DutTopology{}
+	dt := makeTopology(id)
 	for _, name := range resp.GetMachineLSEs() {
 		lse, err := c.GetMachineLSE(ctx, &ufsapi.GetMachineLSERequest{Name: name})
 		if err != nil {
@@ -75,6 +80,23 @@ func makeDutProto(lse *ufspb.MachineLSE) (*labapi.Dut, error) {
 	if hostname == "" {
 		return nil, errors.New("make dut proto: empty hostname")
 	}
+	croslse := lse.GetChromeosMachineLse()
+	if croslse == nil {
+		return nil, errors.New("make dut proto: empty chromeos_machine_lse")
+	}
+	dlse := croslse.GetDeviceLse()
+	if dlse == nil {
+		return nil, errors.New("make dut proto: empty device_lse")
+	}
+	d := dlse.GetDut()
+	if d == nil {
+		return nil, errors.New("make dut proto: empty dut")
+	}
+	p := d.GetPeripherals()
+	if p == nil {
+		return nil, errors.New("make dut proto: empty peripherals")
+	}
+
 	return &labapi.Dut{
 		Id: &labapi.Dut_Id{Value: hostname},
 		DutType: &labapi.Dut_Chromeos{
@@ -83,7 +105,164 @@ func makeDutProto(lse *ufspb.MachineLSE) (*labapi.Dut, error) {
 					Address: hostname,
 					Port:    22,
 				},
+				Servo:     getServo(p),
+				Chameleon: getChameleon(p),
+				Audio:     getAudio(p),
+				Wifi:      getWifi(p),
+				Touch:     getTouch(p),
+				Camerabox: getCamerabox(p),
+				Cables:    getCables(p),
 			},
 		},
 	}, nil
+}
+
+func getServo(p *lab.Peripherals) *labapi.Servo {
+	s := p.GetServo()
+	if s != nil && s.GetServoHostname() != "" {
+		return &labapi.Servo{
+			ServodAddress: &labapi.IpEndpoint{
+				Address: s.GetServoHostname(),
+				Port:    s.GetServoPort(),
+			},
+		}
+	}
+	return nil
+}
+
+func getChameleon(p *lab.Peripherals) *labapi.Chameleon {
+	c := p.GetChameleon()
+	if c == nil {
+		return nil
+	}
+	return &labapi.Chameleon{
+		AudioBoard:  c.GetAudioBoard(),
+		Peripherals: mapChameleonPeripherals(p, c),
+	}
+}
+
+func mapChameleonPeripherals(p *lab.Peripherals, c *lab.Chameleon) []labapi.Chameleon_Peripheral {
+	res := []labapi.Chameleon_Peripheral{}
+	for _, cp := range c.GetChameleonPeripherals() {
+		m := labapi.Chameleon_PREIPHERAL_UNSPECIFIED
+		switch cp {
+		case lab.ChameleonType_CHAMELEON_TYPE_INVALID:
+			m = labapi.Chameleon_PREIPHERAL_UNSPECIFIED
+		case lab.ChameleonType_CHAMELEON_TYPE_DP:
+			m = labapi.Chameleon_DP
+		case lab.ChameleonType_CHAMELEON_TYPE_DP_HDMI:
+			m = labapi.Chameleon_DP_HDMI
+		case lab.ChameleonType_CHAMELEON_TYPE_VGA:
+			m = labapi.Chameleon_VGA
+		case lab.ChameleonType_CHAMELEON_TYPE_HDMI:
+			m = labapi.Chameleon_HDMI
+			// TODO(ivanbrovkovich): are these not mapped to anything?
+			// BT_BLE_HID
+			// BT_A2DP_SINK
+			// BT_PEER
+		}
+		res = append(res, m)
+	}
+	return res
+}
+
+func getAudio(p *lab.Peripherals) *labapi.Audio {
+	a := p.GetAudio()
+	if a == nil {
+		return nil
+	}
+	return &labapi.Audio{
+		AudioBox: a.AudioBox,
+		Atrus:    a.Atrus,
+	}
+}
+
+func getWifi(p *lab.Peripherals) *labapi.Wifi {
+	res := &labapi.Wifi{}
+	w := p.GetWifi()
+	if p.GetChaos() {
+		res.Environment = labapi.Wifi_ROUTER_802_11AX
+	} else if w != nil && w.GetWificell() {
+		res.Environment = labapi.Wifi_WIFI_CELL
+	} else if w != nil && w.GetRouter() == lab.Wifi_ROUTER_802_11AX {
+		res.Environment = labapi.Wifi_ROUTER_802_11AX
+	} else if w != nil {
+		res.Environment = labapi.Wifi_STANDARD
+	}
+	// TODO(ivanbrovkovich): Do we still get antenna for Chaos and wificell?
+	if w != nil {
+		res.Antenna = &labapi.WifiAntenna{
+			Connection: mapWifiAntenna(w.GetAntennaConn()),
+		}
+	}
+	return res
+}
+
+func mapWifiAntenna(wa lab.Wifi_AntennaConnection) labapi.WifiAntenna_Connection {
+	switch wa {
+	case lab.Wifi_CONN_UNKNOWN:
+		return labapi.WifiAntenna_CONNECTION_UNSPECIFIED
+	case lab.Wifi_CONN_CONDUCTIVE:
+		return labapi.WifiAntenna_CONDUCTIVE
+	case lab.Wifi_CONN_OTA:
+		return labapi.WifiAntenna_OTA
+	}
+	return labapi.WifiAntenna_CONNECTION_UNSPECIFIED
+}
+
+func getTouch(p *lab.Peripherals) *labapi.Touch {
+	if t := p.GetTouch(); t != nil {
+		return &labapi.Touch{
+			Mimo: t.GetMimo(),
+		}
+	}
+	return nil
+}
+
+func getCamerabox(p *lab.Peripherals) *labapi.Camerabox {
+	if !p.GetCamerabox() {
+		return nil
+	}
+	cb := p.GetCameraboxInfo()
+	return &labapi.Camerabox{
+		Facing: mapCameraFacing(cb.Facing),
+	}
+}
+
+func mapCameraFacing(cf lab.Camerabox_Facing) labapi.Camerabox_Facing {
+	switch cf {
+	case lab.Camerabox_FACING_UNKNOWN:
+		return labapi.Camerabox_FACING_UNSPECIFIED
+	case lab.Camerabox_FACING_BACK:
+		return labapi.Camerabox_BACK
+	case lab.Camerabox_FACING_FRONT:
+		return labapi.Camerabox_FRONT
+	}
+	return labapi.Camerabox_FACING_UNSPECIFIED
+}
+
+func getCables(p *lab.Peripherals) []*labapi.Cable {
+	ret := []*labapi.Cable{}
+	for _, c := range p.GetCable() {
+		ret = append(ret, &labapi.Cable{
+			Type: mapCables(c.GetType()),
+		})
+	}
+	return ret
+}
+
+func mapCables(ct lab.CableType) labapi.Cable_Type {
+	switch ct {
+	case lab.CableType_CABLE_INVALID:
+		return labapi.Cable_TYPE_UNSPECIFIED
+	case lab.CableType_CABLE_AUDIOJACK:
+		return labapi.Cable_AUDIOJACK
+	case lab.CableType_CABLE_USBAUDIO:
+		return labapi.Cable_USBAUDIO
+	case lab.CableType_CABLE_USBPRINTING:
+		return labapi.Cable_USBPRINTING
+	case lab.CableType_CABLE_HDMIAUDIO:
+		return labapi.Cable_HDMIAUDIO
+	}
+	return labapi.Cable_TYPE_UNSPECIFIED
 }
--- go/src/infra/cros/cmd/labservice/internal/ufs/ufs_test.go
+++ go/src/infra/cros/cmd/labservice/internal/ufs/ufs_test.go
@@ -16,6 +16,7 @@ import (
 	"google.golang.org/grpc/test/bufconn"
 
 	ufspb "infra/unifiedfleet/api/v1/models"
+	lab "infra/unifiedfleet/api/v1/models/chromeos/lab"
 	ufsapi "infra/unifiedfleet/api/v1/rpc"
 )
 
@@ -27,6 +28,48 @@ func TestGetDutTopology_single(t *testing.T) {
 		ChromeOSDeviceData: &ufspb.ChromeOSDeviceData{
 			LabConfig: &ufspb.MachineLSE{
 				Hostname: "mary",
+				Lse: &ufspb.MachineLSE_ChromeosMachineLse{
+					ChromeosMachineLse: &ufspb.ChromeOSMachineLSE{
+						ChromeosLse: &ufspb.ChromeOSMachineLSE_DeviceLse{
+							DeviceLse: &ufspb.ChromeOSDeviceLSE{
+								Device: &ufspb.ChromeOSDeviceLSE_Dut{
+									Dut: &lab.DeviceUnderTest{
+										Peripherals: &lab.Peripherals{
+											Audio: &lab.Audio{
+												AudioBox: true,
+												Atrus:    true,
+											},
+											Chameleon: &lab.Chameleon{
+												AudioBoard:           true,
+												ChameleonPeripherals: []lab.ChameleonType{lab.ChameleonType_CHAMELEON_TYPE_DP},
+											},
+											Servo: &lab.Servo{
+												ServoHostname: "servo_host",
+												ServoPort:     33,
+											},
+											Wifi: &lab.Wifi{
+												Wificell:    true,
+												AntennaConn: lab.Wifi_CONN_CONDUCTIVE,
+											},
+											Touch: &lab.Touch{
+												Mimo: true,
+											},
+											Camerabox: true,
+											CameraboxInfo: &lab.Camerabox{
+												Facing: lab.Camerabox_FACING_FRONT,
+											},
+											Cable: []*lab.Cable{
+												{
+													Type: lab.CableType_CABLE_AUDIOJACK,
+												},
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
 			},
 		},
 	}
@@ -42,9 +85,40 @@ func TestGetDutTopology_single(t *testing.T) {
 				Id: &labapi.Dut_Id{Value: "mary"},
 				DutType: &labapi.Dut_Chromeos{
 					Chromeos: &labapi.Dut_ChromeOS{
+						Audio: &labapi.Audio{
+							AudioBox: true,
+							Atrus:    true,
+						},
+						Chameleon: &labapi.Chameleon{
+							AudioBoard:  true,
+							Peripherals: []labapi.Chameleon_Peripheral{labapi.Chameleon_DP},
+						},
+						Servo: &labapi.Servo{
+							ServodAddress: &labapi.IpEndpoint{
+								Address: "servo_host",
+								Port:    33,
+							},
+						},
 						Ssh: &labapi.IpEndpoint{
 							Address: "mary",
 							Port:    22,
+						},
+						Wifi: &labapi.Wifi{
+							Environment: labapi.Wifi_WIFI_CELL,
+							Antenna: &labapi.WifiAntenna{
+								Connection: labapi.WifiAntenna_CONDUCTIVE,
+							},
+						},
+						Touch: &labapi.Touch{
+							Mimo: true,
+						},
+						Camerabox: &labapi.Camerabox{
+							Facing: labapi.Camerabox_FRONT,
+						},
+						Cables: []*labapi.Cable{
+							{
+								Type: labapi.Cable_AUDIOJACK,
+							},
 						},
 					},
 				},
