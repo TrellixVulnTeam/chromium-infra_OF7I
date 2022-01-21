52ea92007419d7a0c9f8b3d96c17e8f90d77fc3f
Andrew Chang <andrewjc@google.com>
2022-01-21 16:07:43 +0000

    Bump infra_libs version to 2.3.0
    Adds Python 3 install_requires entries.
    
    Bug: chromium:1225767
    Change-Id: I40a089cabb6f51c8448599c3e729d944fa8c3586
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/3402356
    Auto-Submit: Andrew Chang <andrewjc@google.com>
    Reviewed-by: Dave Tu <dtu@chromium.org>
    Reviewed-by: Scott Lee <ddoman@chromium.org>
    Commit-Queue: Andrew Chang <andrewjc@google.com>
    Cr-Commit-Position: refs/heads/main@{#46508}

--- packages/infra_libs/setup.py
+++ packages/infra_libs/setup.py
@@ -7,18 +7,22 @@ from setuptools import setup, find_packages
 
 setup(
     name='infra_libs',
-    version='2.2.1',
+    version='2.3.0',
     description='Chrome Infra Libraries',
     long_description='Chrome Infra Libraries',
     classifiers=[
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.9',
     ],
     packages=find_packages(exclude=['*.test']),
     install_requires=[
         'google-api-python-client',
+        'google-auth; python_version >= "3.0.0"',
+        'google-auth-httplib2; python_version >= "3.0.0"',
         'oauth2client==3.0.0',
         'protobuf',
         'requests',
+        'six',
     ],
     package_data={
         '': ['*.md', '*.proto'],
