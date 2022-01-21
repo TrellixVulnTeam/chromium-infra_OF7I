ff460a131a132854d8e716eb422a9d35e9f382c4
Kathleen Qian <sykq@google.com>
2022-01-21 04:34:23 +0000

    Get enough number of test logs
    Change-Id: Iad4eeec379a1ef289c65803170b8044c09c4964e
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/3376685
    Reviewed-by: Patrick Meiring <meiring@google.com>
    Commit-Queue: Kathleen Qian <sykq@google.com>
    Cr-Commit-Position: refs/heads/main@{#46504}

--- go/src/infra/experimental/loganalysis/main.go
+++ go/src/infra/experimental/loganalysis/main.go
@@ -34,19 +34,23 @@ const (
 type LogsInfo struct {
 	// LogsURL is the path to the test's logs in the Google Cloud Storage test logs bucket.
 	LogsURL string
-	// Test is the name of the test that was run.
-	Test string
 	// FinishedTime is the finished time of the test in the standard time zone of the project.
 	FinishedTime string
+	// Board is the board name of the test results that was downloaded.
+	Board string
+	// Status is the status of test for the downloaded logs (usually FAIL/GOOD).
+	Status string
 }
 
 var (
-	dateFlag = flag.String("date", "", "test taken date (in the 'yyyymmdd' format)")
-	status   = flag.String("status", "FAIL", "status of test results to download (i.e. FAIL/GOOD)")
-	board    = flag.String("board", "eve", "reference board name to download test results for")
-	limit    = flag.Int("limit", 10, "maximum number of test results to download")
+	dateFlag    = flag.String("date", "", "test taken date (in the 'yyyymmdd' format)")
+	board       = flag.String("board", "eve", "reference board name to download test results for")
+	limit       = flag.Int("limit", 10, "maximum number of test results to download in each query")
+	test        = flag.String("test", "tast.lacros.Basic", "name of the test that was run")
+	requiredNum = flag.Int("number", 20, "total required number of test results to download for each test status")
 
 	boardRE = regexp.MustCompile(`[a-z0-9\-]+`)
+	testRE  = regexp.MustCompile(`tast.(\w+).(\w+)(.(\w+))?$`)
 )
 
 func main() {
@@ -58,31 +62,63 @@ func main() {
 	if err != nil {
 		log.Fatalf("Cannot parse --date: expected yyyymmdd format, got: %v", err)
 	}
-	if *status != "FAIL" && *status != "GOOD" {
-		log.Fatalf("Cannot parse --status: expected FAIL or GOOD, got: %v", *status)
-	}
 	if !boardRE.MatchString(*board) {
 		log.Fatalf("Cannot parse --board: unexpected characters, got: %v", *board)
 	}
 	if *limit <= 0 {
 		log.Fatalf("Cannot parse --limit: expected a positive integer, got: %v", *limit)
 	}
+	if !testRE.MatchString(*test) {
+		log.Fatalf("Cannot parse --test: expected tast.suite.TestCase format, got: %v", *test)
+	}
+	if *requiredNum <= 0 {
+		log.Fatalf("Cannot parse --number: expected a positive integer, got: %v", *requiredNum)
+	}
+
 	ctx := context.Background()
-	logs, err := FindLogsToDownload(ctx, projectID, date, *status, *board, *limit)
-	if err != nil {
-		log.Fatalf("Error finding logs to download: %v", err)
-	}
-	for _, info := range logs {
-		contents, err := gcs.ReadFileContents(ctx, bucketID, CreateObjectID(info))
-		if err != nil {
-			log.Fatalf("Error downloading log contents: %v", err)
+	remainingFailNum := *requiredNum
+	remainingPassNum := *requiredNum
+	currentDate := date
+	checkSameBoard := true
+	// Terminates log downloads if the number of files reaches expectations; otherwise, queries tests day by day.
+	for remainingFailNum > 0 || remainingPassNum > 0 {
+		// Checks whether the test searching date is more than 30 days old:
+		// if so, for the specified board, we will search the recent results for the same test in other boards;
+		// if we have already searched all boards, the log downloads will be terminated.
+		if currentDate.Before(time.Now().AddDate(0, 0, -30)) {
+			if checkSameBoard {
+				checkSameBoard = false
+				currentDate = date
+			} else {
+				break
+			}
 		}
-		path := filepath.Join(storagePathPrefix, *board, *status, info.FinishedTime+"_"+info.Test+".txt")
-		err = SaveContentsToFile(path, contents)
+		logs, err := FindLogsToDownload(ctx, projectID, currentDate, *board, *limit, *test, checkSameBoard, remainingFailNum > 0, remainingPassNum > 0)
 		if err != nil {
-			log.Fatalf("Error saving log contents to a file: %v", err)
+			log.Fatalf("Error finding logs to download: %v", err)
+		}
+		currentDate = currentDate.AddDate(0, 0, -1)
+		for _, info := range logs {
+			if info.Status != "FAIL" && info.Status != "GOOD" {
+				continue
+			}
+			contents, err := gcs.ReadFileContents(ctx, bucketID, CreateObjectID(info, *test))
+			if err != nil {
+				log.Fatalf("Error downloading log contents: %v", err)
+			}
+			path := filepath.Join(storagePathPrefix, *test, info.Board, info.Status, info.FinishedTime+".txt")
+			err = SaveContentsToFile(path, contents)
+			if err != nil {
+				log.Fatalf("Error saving log contents to a file: %v", err)
+			}
+			if info.Status == "FAIL" {
+				remainingFailNum -= 1
+			} else {
+				remainingPassNum -= 1
+			}
 		}
 	}
+
 }
 
 // SaveContentsToFile saves log contents to a local file with the storage path as the combination of attributes for a test.
@@ -100,32 +136,40 @@ func SaveContentsToFile(storagePath string, contents []byte) error {
 }
 
 // FindLogsToDownload finds logs satisfying specific requirements in the specified project using BigQuery.
-func FindLogsToDownload(ctx context.Context, projectID string, date time.Time, status string, board string, limit int) ([]LogsInfo, error) {
+func FindLogsToDownload(ctx context.Context, projectID string, date time.Time, board string, limit int, test string, specifiedBoard bool, requireFailTest bool, requirePassTest bool) ([]LogsInfo, error) {
 	client, err := bigquery.NewClient(ctx, projectID)
 	if err != nil {
 		return nil, errors.Annotate(err, "create client").Err()
 	}
-	// Gets logs url and test name (with the required attributes)
-	// considering only task tests in the "tast.suite.TestCase" format in the "chromeos-test-logs" project to have the same path to the "log.txt" file.
+	var boardClause string
+	if specifiedBoard {
+		boardClause = "board = @board"
+	} else {
+		boardClause = "board != @board"
+	}
 	q := client.Query(`
 			SELECT
 				logs_url AS LogsURL,
-				test AS Test,
-				STRING(finished_time) AS FinishedTime
+				STRING(finished_time) AS FinishedTime,
+				board AS Board,
+				status AS Status
 			FROM ` + "`google.com:stainless-prod.stainless.tests*`" + `
 			WHERE
 				_TABLE_SUFFIX = @date
-				AND status = @status
-				AND board = @board
+				AND ((@requireFailTest AND status = "FAIL")
+					OR (@requirePassTest AND status = "GOOD"))
+				AND (` + boardClause + `)
 				AND logs_url LIKE '/browse/chromeos-test-logs/%'
-				AND test LIKE 'tast.%.%'
+				AND test = @test
 			LIMIT
 				@limit
 	`)
 	q.Parameters = []bigquery.QueryParameter{
 		{Name: "date", Value: date.Format("20060102")},
-		{Name: "status", Value: status},
+		{Name: "requireFailTest", Value: requireFailTest},
+		{Name: "requirePassTest", Value: requirePassTest},
 		{Name: "board", Value: board},
+		{Name: "test", Value: test},
 		{Name: "limit", Value: limit},
 	}
 	logsIterator, err := q.Read(ctx)
@@ -149,6 +193,6 @@ func FindLogsToDownload(ctx context.Context, projectID string, date time.Time, s
 
 // CreateObjectID creates object ID for a log given its basic information.
 // The object ID is used to download log contents.
-func CreateObjectID(info LogsInfo) string {
-	return strings.TrimPrefix(info.LogsURL, "/browse/"+bucketID+"/") + "/autoserv_test/tast/results/tests/" + info.Test[5:] + "/log.txt"
+func CreateObjectID(info LogsInfo, test string) string {
+	return strings.TrimPrefix(info.LogsURL, "/browse/"+bucketID+"/") + "/autoserv_test/tast/results/tests/" + test[5:] + "/log.txt"
 }
