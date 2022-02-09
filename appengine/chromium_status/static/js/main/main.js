c87976815ac1e9b4ac9e544bab40fca012de9afc
Matthew Warton <mwarton@google.com>
2022-02-09 21:03:52 +0000

    tree-status: remove old dojo library
    This was flagged as a minor security issue.  Since it is only used for
    converting timezones to local time, we will just let timezones display
    in UTC.
    
    Bug: 1295153
    Change-Id: I292809a9ba4ff8b3d9b4a5680f9a07e1087f9bf3
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/3445566
    Auto-Submit: Matthew Warton <mwarton@google.com>
    Reviewed-by: Tuan Nguyen <nqmtuan@google.com>
    Commit-Queue: Matthew Warton <mwarton@google.com>
    Cr-Commit-Position: refs/heads/main@{#46948}

--- appengine/chromium_status/static/js/main/main.js
+++ appengine/chromium_status/static/js/main/main.js
@@ -9,69 +9,8 @@
 window.onload = function() {
   document.add_new_message.message.focus();
   help_init();
-  localize_times();
   change_init();
 };
-
-/*
- * Code for dealing with localization of timestamps.
- */
-
-function localize_times() {
-  // Localize all the UTC timestamps coming from the server to whatever
-  // the user has set in their browser.
-
-  require(["dojo/date/locale"], function(locale) {
-    function format(date, datePattern, timePattern) {
-      // The dojo guys like to add a sep between the date and the time
-      // fields for us (based on locale).  Since we want a standards
-      // format, that sep is pure noise, so kill it with {...}.
-      // https://bugs.dojotoolkit.org/ticket/17544
-      return locale.format(new Date(date), {
-          formatLength: 'short',
-          datePattern: datePattern + '{',
-          timePattern: '}' + timePattern
-        }).replace(/{.*}/, ' ');
-    }
-
-    function long_date(date) { // RFC2822
-      return format(date, 'EEE, dd MMM yyyy', 'HH:mm:ss z');
-    }
-
-    function short_date(date) {
-      return format(date, 'EEE, dd MMM', 'HH:mm');
-    }
-
-    var now = new Date();
-    var curr_year = now.getFullYear();
-
-    var tzname = locale.format(now, {
-        selector: 'time',
-        timePattern: 'z'
-      });
-
-    var i, elements;
-
-    // Convert all the fields that have a timezone already.
-    elements = document.getElementsByName('date.datetz');
-    for (i = 0; i < elements.length; ++i)
-      elements[i].innerText = long_date(elements[i].innerText);
-
-    // Convert all the fields that lack a timezone (which we know is UTC).
-    // We'll assume the timestamps represent the current year as it'll only
-    // really affect the short day-of-week name, and even then it'll only be
-    // slightly off during the ~1st week of January.
-    elements = document.getElementsByName('date.date');
-    for (i = 0; i < elements.length; ++i)
-      elements[i].innerText = short_date(elements[i].innerText + ' ' +
-        curr_year + ' UTC');
-
-    // Convert all the fields that are just a timezone.
-    elements = document.getElementsByName('date.tz');
-    for (i = 0; i < elements.length; ++i)
-      elements[i].innerText = tzname;
-  });
-}
 
 /*
  * Functions for managing the help text.
