6653444dfea008e447494544ced0de719ea3c079
Yunzhi Yu <yunzhiyu@google.com>
2022-02-11 01:26:04 +0000

    recovery: Add resetEcExec func in PARIS:
    This exec func resets EC from DUT side to wake CR50 up.
    
    BUG=b:198819047
    TEST=manual test
    
    Change-Id: Idc021bbfdca194f2417866e4537968e9352cce63
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/3453448
    Reviewed-by: Otabek Kasimov <otabek@google.com>
    Commit-Queue: Yunzhi Yu <yunzhiyu@google.com>
    Cr-Commit-Position: refs/heads/main@{#47000}

---
+++ go/src/infra/cros/recovery/internal/execs/cros/cros_ectool_execs.go
@@ -0,0 +1,46 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package cros
+
+import (
+	"context"
+	"time"
+
+	"go.chromium.org/luci/common/errors"
+
+	"infra/cros/recovery/internal/execs"
+	"infra/cros/recovery/internal/log"
+)
+
+// resetEcExec resets EC from DUT side to wake CR50 up.
+//
+// @params: actionArgs should be in the format of:
+// Ex: ["wait_timeout:x"]
+func resetEcExec(ctx context.Context, info *execs.ExecInfo) error {
+	argsMap := info.GetActionArgs(ctx)
+	// Delay to wait for the ec reset command to be efftive. Default to be 30s.
+	waitTimeout := argsMap.AsInt(ctx, "wait_timeout", 30)
+	// TODO: (@otabek & @yunzhiyu) Figure out why, in PARIS,
+	// we must add '&& exit' to make ssh proxy return immediately without
+	// waiting for the command to finish execution.
+	//
+	// Command to reset EC from DUT side.
+	const ecResetCmd = "ectool reboot_ec cold && exit"
+	run := info.NewRunner(info.RunArgs.DUT.Name)
+	if out, err := run(ctx, 30*time.Second, ecResetCmd); err != nil {
+		// Client closed connected as rebooting.
+		log.Debug(ctx, "Client exit as device rebooted: %s", err)
+		return errors.Annotate(err, "reset ec").Err()
+	} else {
+		log.Debug(ctx, "Stdout: %s", out)
+	}
+	log.Debug(ctx, "waiting for %d seconds to let ec reset be effective.", waitTimeout)
+	time.Sleep(time.Duration(waitTimeout) * time.Second)
+	return nil
+}
+
+func init() {
+	execs.Register("cros_reset_ec", resetEcExec)
+}
