af6ce0f154f5d81d0092862fbe3a8607416eefc9
Gregory Nisbet <gregorynisbet@google.com>
2022-02-16 20:52:16 +0000

    [satlab] Add but do not implement set-stable-version command
    BUG=b:219072576
    TEST=does it compile
    
    Change-Id: I7b42825dcfa74009842e8c7804ce8d59e4a5806f
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/3469378
    Reviewed-by: Otabek Kasimov <otabek@google.com>
    Commit-Queue: Gregory Nisbet <gregorynisbet@google.com>
    Cr-Commit-Position: refs/heads/main@{#47114}

---
+++ go/src/infra/cros/cmd/satlab/internal/stableversion/set_stable_version.go
@@ -0,0 +1,69 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package stableversion
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/maruel/subcommands"
+	"go.chromium.org/luci/auth/client/authcli"
+	"go.chromium.org/luci/common/cli"
+	"go.chromium.org/luci/common/errors"
+
+	"infra/cros/cmd/satlab/internal/site"
+)
+
+var SetStableVersionCmd = &subcommands.Command{
+	UsageLine: `set-stable-version`,
+	ShortDesc: `Set the stable version using {board, model} or {hostname}.`,
+	CommandRun: func() subcommands.CommandRun {
+		r := &setStableVersionRun{}
+
+		r.authFlags.Register(&r.Flags, site.DefaultAuthOptions)
+		r.envFlags.Register(&r.Flags)
+		r.commonFlags.Register(&r.Flags)
+
+		r.Flags.StringVar(&r.board, "board", "", `the board or build target (used with model)`)
+		r.Flags.StringVar(&r.model, "model", "", `the model (used with board)`)
+		r.Flags.StringVar(&r.hostname, "hostname", "", `the hostname (used by itself)`)
+		r.Flags.StringVar(&r.os, "os", "", `the OS version to set (no change if empty)`)
+		r.Flags.StringVar(&r.fw, "fw", "", `the firmware version to set (no change if empty)`)
+		r.Flags.StringVar(&r.fwImage, "fwImage", "", `the firmware image version to set (no change if empty)`)
+
+		return r
+	},
+}
+
+// SetStableVersionRun is the command for adminclient set-stable-version.
+type setStableVersionRun struct {
+	subcommands.CommandRunBase
+
+	authFlags   authcli.Flags
+	envFlags    site.EnvFlags
+	commonFlags site.CommonFlags
+
+	board    string
+	model    string
+	hostname string
+	os       string
+	fw       string
+	fwImage  string
+}
+
+// Run runs the command, prints the error if there is one, and returns an exit status.
+func (c *setStableVersionRun) Run(a subcommands.Application, args []string, env subcommands.Env) int {
+	ctx := cli.GetContext(a, c, env)
+	if err := c.innerRun(ctx, a, args, env); err != nil {
+		fmt.Fprintf(a.GetErr(), "%s: %s\n", a.GetName(), err)
+		return 1
+	}
+	return 0
+}
+
+// InnerRun creates a client, sends a GetStableVersion request, and prints the response.
+func (c *setStableVersionRun) innerRun(ctx context.Context, a subcommands.Application, args []string, env subcommands.Env) error {
+	return errors.Reason("not yet implemented").Err()
+}
