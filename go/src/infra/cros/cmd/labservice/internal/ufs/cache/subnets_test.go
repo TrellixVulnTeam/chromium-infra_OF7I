5cf7456331acbf948a95a9c5ad26298c19d2f789
Ivan Brovkovich <ivanbrovkovich@chromium.org>
2022-02-17 00:06:46 +0000

    Add cache server address to dut topology
    Bug: b/213944640
    Test: unit tests
    Change-Id: I848aa29f051a3d00c120f947e6e7e80e30aa9faa
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/3435054
    Reviewed-by: Congbin Guo <guocb@chromium.org>
    Auto-Submit: Ivan Brovkovich <ivanbrovkovich@chromium.org>
    Commit-Queue: Ivan Brovkovich <ivanbrovkovich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#47123}

---
+++ go/src/infra/cros/cmd/labservice/internal/ufs/cache/subnets_test.go
@@ -0,0 +1,62 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package cache
+
+import (
+	"net"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	ufspb "infra/unifiedfleet/api/v1/models"
+	ufsapi "infra/unifiedfleet/api/v1/rpc"
+)
+
+func TestGetSubnets_single(t *testing.T) {
+	t.Parallel()
+
+	c := &fakeClient{
+		CachingServices: &ufsapi.ListCachingServicesResponse{
+			CachingServices: []*ufspb.CachingService{
+				{
+					Name:           "cachingservice/200.200.200.208",
+					Port:           55,
+					ServingSubnets: []string{"200.200.200.200/24"},
+					State:          ufspb.State_STATE_SERVING,
+				},
+				{
+					Name:           "cachingservice/200.200.200.108",
+					Port:           155,
+					ServingSubnets: []string{"200.200.200.200/24"},
+					State:          ufspb.State_STATE_SERVING,
+				},
+				{
+					Name:           "cachingservice/200.200.100.208",
+					Port:           255,
+					ServingSubnets: []string{"200.200.100.200/24"},
+					State:          ufspb.State_STATE_SERVING,
+				},
+			},
+		},
+	}
+	subnets := newSubnetsFinder()
+	got, err := subnets.getSubnets(c)
+	if err != nil {
+		t.Fatal(err)
+	}
+	m := net.CIDRMask(24, 32)
+	want := []Subnet{
+		{
+			IPNet:    &net.IPNet{IP: net.IPv4(200, 200, 200, 0), Mask: m},
+			Backends: []address{{Ip: "200.200.200.108", Port: 155}, {Ip: "200.200.200.208", Port: 55}},
+		},
+		{
+			IPNet:    &net.IPNet{IP: net.IPv4(200, 200, 100, 0), Mask: m},
+			Backends: []address{{Ip: "200.200.100.208", Port: 255}},
+		},
+	}
+	if diff := cmp.Diff(want, got); diff != "" {
+		t.Errorf("getSubnets() mismatch (-want +got):\n%s", diff)
+	}
+}
