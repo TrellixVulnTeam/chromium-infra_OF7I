8e6f1672a1faf7597a15ff41caa9d268ff199eb4
Takuto Ikuta <tikuta@chromium.org>
2022-02-21 06:48:19 +0000

    chromium_build_stats: add ToAVRO function
    This is used to make map[string]interface{} passed to avro file writer.
    
    Bug: 1288639
    Change-Id: Ic591fcddbf174d15a81a562b9789ece7af51554c
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/3474744
    Reviewed-by: Fumitoshi Ukai <ukai@google.com>
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#47223}

--- go/src/infra/appengine/chromium_build_stats/ninjalog/avro.go
+++ go/src/infra/appengine/chromium_build_stats/ninjalog/avro.go
@@ -5,9 +5,14 @@
 package ninjalog
 
 import (
+	"crypto/rand"
 	_ "embed"
+	"encoding/binary"
 	"fmt"
+	"sort"
+	"strings"
 	"sync"
+	"time"
 
 	goavro "github.com/linkedin/goavro/v2"
 	"sigs.k8s.io/yaml"
@@ -37,4 +42,70 @@ func AVROCodec() (*goavro.Codec, error) {
 	})
 
 	return codec, codecErr
+}
+
+// This is overridden in test.
+var timeNow = time.Now
+
+// ToAVRO returns ninja log passed to AVRO codec.
+func ToAVRO(info *NinjaLog) map[string]interface{} {
+	weightedTime := WeightedTime(info.Steps)
+	steps := Dedup(info.Steps)
+
+	buildID := info.Metadata.BuildID
+	if buildID == 0 {
+		// Set random number if buildID is not set.
+		// This is mainly for ninjalog from chromium developer.
+		binary.Read(rand.Reader, binary.BigEndian, &buildID)
+	}
+
+	os := "UNKNOWN"
+	// Parse platform as it is returned from python's platform.system().
+	switch platform := info.Metadata.Platform; {
+	case platform == "Windows" || strings.Contains(platform, "CYGWIN"):
+		os = "WIN"
+	case platform == "Linux":
+		os = "LINUX"
+	case platform == "Darwin":
+		os = "MAC"
+	}
+
+	buildConfigs := make([]map[string]interface{}, 0, len(info.Metadata.BuildConfigs))
+	for k, v := range info.Metadata.BuildConfigs {
+		buildConfigs = append(buildConfigs, map[string]interface{}{
+			"key":   k,
+			"value": v,
+		})
+	}
+
+	// Configuring order is matter for same key.
+	sort.SliceStable(buildConfigs, func(i, j int) bool {
+		return buildConfigs[i]["key"].(string) < buildConfigs[j]["key"].(string)
+	})
+
+	logEntries := make([]map[string]interface{}, 0, len(steps))
+
+	for _, s := range steps {
+		outputs := append(s.Outs, s.Out)
+		sort.Strings(outputs)
+		logEntries = append(logEntries, map[string]interface{}{
+			"outputs":               outputs,
+			"start_duration_sec":    s.Start.Seconds(),
+			"end_duration_sec":      s.End.Seconds(),
+			"weighted_duration_sec": weightedTime[s.Out].Seconds(),
+		})
+	}
+
+	return map[string]interface{}{
+		"targets":       info.Metadata.getTargets(),
+		"build_id":      buildID,
+		"os":            os,
+		"step_name":     info.Metadata.StepName,
+		"jobs":          info.Metadata.Jobs,
+		"cpu_core":      int(info.Metadata.CPUCore),
+		"build_configs": buildConfigs,
+		"log_entries":   logEntries,
+		"created_at":    timeNow(),
+	}
+
 }
--- go/src/infra/appengine/chromium_build_stats/ninjalog/avro_test.go
+++ go/src/infra/appengine/chromium_build_stats/ninjalog/avro_test.go
@@ -4,10 +4,108 @@
 
 package ninjalog
 
-import "testing"
+import (
+	"testing"
+	"time"
+
+	"github.com/google/go-cmp/cmp"
+)
 
 func TestAVROCodec(t *testing.T) {
 	if _, err := AVROCodec(); err != nil {
 		t.Fatalf("failed to parse AVRO schema: %v", err)
+	}
+}
+
+func TestToAVRO(t *testing.T) {
+	outputTestCase := append([]Step{
+		{
+			Start:   76 * time.Millisecond,
+			End:     187 * time.Millisecond,
+			Out:     "resources/inspector/devtools_api.js",
+			CmdHash: "75430546595be7c2",
+		},
+		{
+			Start:   78 * time.Millisecond,
+			End:     286 * time.Millisecond,
+			Out:     "gen/angle/commit_id_2.py",
+			CmdHash: "4ede38e2c1617d8c",
+		},
+		{
+			Start:   78 * time.Millisecond,
+			End:     286 * time.Millisecond,
+			Out:     "gen/angle/commit_id_3.py",
+			CmdHash: "4ede38e2c1617d8c",
+		}}, stepsTestCase...)
+
+	info := NinjaLog{
+		Filename: ".ninja_log",
+		Start:    1,
+		Steps:    outputTestCase,
+		Metadata: metadataTestCase,
+	}
+
+	createdTime := time.Unix(1514768400, 12345678)
+	defer func() {
+		timeNow = time.Now
+	}()
+	timeNow = func() time.Time { return createdTime }
+
+	got := ToAVRO(&info)
+	if diff := cmp.Diff(map[string]interface{}{
+		"build_configs": []map[string]interface{}{},
+		"build_id":      int64(12345),
+		"cpu_core":      int(0),
+		"created_at":    createdTime,
+		"jobs":          int(50),
+		"os":            string("LINUX"),
+		"step_name":     string("compile"),
+		"targets":       []string{"all"},
+		"log_entries": []map[string]interface{}{
+			{
+				"end_duration_sec":      float64(0.187),
+				"outputs":               []string{"resources/inspector/devtools_api.js", "resources/inspector/devtools_extension_api.js"},
+				"start_duration_sec":    float64(0.076),
+				"weighted_duration_sec": float64(0.025783333),
+			},
+			{
+				"end_duration_sec":      float64(0.286),
+				"outputs":               []string{"gen/angle/commit_id.py", "gen/angle/commit_id_2.py", "gen/angle/commit_id_3.py"},
+				"start_duration_sec":    float64(0.078),
+				"weighted_duration_sec": float64(0.043683333),
+			},
+			{
+				"end_duration_sec":      float64(0.287),
+				"outputs":               []string{"gen/angle/copy_compiler_dll.bat"},
+				"start_duration_sec":    float64(0.079),
+				"weighted_duration_sec": float64(0.043516666),
+			},
+			{
+				"end_duration_sec":      float64(0.284),
+				"outputs":               []string{"gen/autofill_regex_constants.cc"},
+				"start_duration_sec":    float64(0.08),
+				"weighted_duration_sec": float64(0.04235),
+			},
+			{
+				"end_duration_sec":      float64(0.287),
+				"outputs":               []string{"PepperFlash/manifest.json"},
+				"start_duration_sec":    float64(0.141),
+				"weighted_duration_sec": float64(0.027933333),
+			},
+			{
+				"end_duration_sec":      float64(0.288),
+				"outputs":               []string{"PepperFlash/libpepflashplayer.so"},
+				"start_duration_sec":    float64(0.142),
+				"weighted_duration_sec": float64(0.028233333),
+			},
+			{
+				"end_duration_sec":      float64(0.29),
+				"outputs":               []string{"obj/third_party/angle/src/copy_scripts.actions_rules_copies.stamp"},
+				"start_duration_sec":    float64(0.287),
+				"weighted_duration_sec": float64(0.0025),
+			},
+		},
+	}, got); diff != "" {
+		t.Errorf("ToAVRO(%v) mismatch (-want +got):\n%s", info, diff)
 	}
 }
