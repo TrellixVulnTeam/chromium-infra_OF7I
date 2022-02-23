cef75800b78966f8d3daef1943f0726a807fcbd6
Gregory Nisbet <gregorynisbet@google.com>
2022-02-23 23:44:32 +0000

    [crosadm] [satlab] Add implementation of delete stable version in satlab CLI
    The command implemented here is very similar to the command in
    adminclient, which is located in this CL:
    https://chromium-review.googlesource.com/c/infra/infra/+/3485282.
    
    That command is a little bit safer because it writes to the staging
    instance of CrosAdm by default, so I tested this CL by testing the other
    one.
    
    BUG=b:219072576
    TEST=make test
    
    Change-Id: Ic48b0f73d388438f8b9b1a0be539a4192771a828
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/3485283
    Reviewed-by: Prasad Vuppalapu <prasadv@chromium.org>
    Commit-Queue: Gregory Nisbet <gregorynisbet@google.com>
    Cr-Commit-Position: refs/heads/main@{#47276}

--- go/src/infra/cros/cmd/satlab/internal/stableversion/delete_stable_version.go
+++ go/src/infra/cros/cmd/satlab/internal/stableversion/delete_stable_version.go
@@ -5,11 +5,19 @@
 package stableversion
 
 import (
+	"context"
 	"fmt"
-	"infra/cros/cmd/satlab/internal/site"
 
 	"github.com/maruel/subcommands"
 	"go.chromium.org/luci/auth/client/authcli"
+	"go.chromium.org/luci/common/cli"
+	"go.chromium.org/luci/common/errors"
+	"go.chromium.org/luci/grpc/prpc"
+	"google.golang.org/protobuf/encoding/protojson"
+
+	fleet "infra/appengine/crosskylabadmin/api/fleet/v1"
+	"infra/cmdsupport/cmdlib"
+	"infra/cros/cmd/satlab/internal/site"
 )
 
 // DeleteStableVersionCmd deletes a stable version entry.
@@ -46,6 +54,59 @@ type deleteStableVersionRun struct {
 
 // Run runs the command, prints the error if there is one, and returns an exit status.
 func (c *deleteStableVersionRun) Run(a subcommands.Application, args []string, env subcommands.Env) int {
-	fmt.Fprintf(a.GetErr(), "%s\n", "not yet implemented")
-	return 2
+	ctx := cli.GetContext(a, c, env)
+	if err := c.innerRun(ctx, a, args, env); err != nil {
+		fmt.Fprintf(a.GetErr(), "%s: %s\n", a.GetName(), err)
+		return 1
+	}
+	return 0
+}
+
+// InnerRun creates a client, sends a DeleteStableVersion request, and prints the response.
+func (c *deleteStableVersionRun) innerRun(ctx context.Context, a subcommands.Application, args []string, env subcommands.Env) error {
+	hc, err := cmdlib.NewHTTPClient(ctx, &c.authFlags)
+	if err != nil {
+		return errors.Annotate(err, "delete stable version").Err()
+	}
+
+	host := c.envFlags.GetCrosAdmService()
+
+	options := site.DefaultPRPCOptions
+
+	invWithSVClient := fleet.NewInventoryPRPCClient(
+		&prpc.Client{
+			C:       hc,
+			Host:    host,
+			Options: options,
+		},
+	)
+
+	req := &fleet.DeleteSatlabStableVersionRequest{}
+	if c.hostname == "" {
+		req.Strategy = &fleet.DeleteSatlabStableVersionRequest_SatlabBoardModelDeletionCriterion{
+			SatlabBoardModelDeletionCriterion: &fleet.SatlabBoardModelDeletionCriterion{
+				Board: c.board,
+				Model: c.model,
+			},
+		}
+	} else {
+		req.Strategy = &fleet.DeleteSatlabStableVersionRequest_SatlabHostnameDeletionCriterion{
+			SatlabHostnameDeletionCriterion: &fleet.SatlabHostnameDeletionCriterion{
+				Hostname: c.hostname,
+			},
+		}
+	}
+
+	resp, err := invWithSVClient.DeleteSatlabStableVersion(ctx, req)
+	if err != nil {
+		return errors.Annotate(err, "delete stable version").Err()
+	}
+	out, err := protojson.MarshalOptions{
+		Indent: "    ",
+	}.Marshal(resp)
+	if err != nil {
+		return errors.Annotate(err, "delete stable version").Err()
+	}
+	fmt.Fprintf(a.GetOut(), "%s\n", out)
+	return nil
 }
