ae28b7f0438fd3de040d866f517b40d69d2ab985
Sergey Fetisov <sfetisov@google.com>
2022-03-01 03:03:49 +0000

    Updating ufsdutinfo to use GetDeviceData API and fetch device data for Chrome OS and Android devices.
    Bug=b:219545342
    Test=unittest
    ok  	infra/cmd/skylab_swarming_worker	0.093s
    
    Change-Id: I6799de060858447c51ef0071c452eb4b49f882fc
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/3491768
    Reviewed-by: Xixuan Wu <xixuan@chromium.org>
    Reviewed-by: Allen Li <ayatane@chromium.org>
    Reviewed-by: Garry Wang <xianuowang@chromium.org>
    Commit-Queue: Sergey Fetisov <sfetisov@google.com>
    Cr-Commit-Position: refs/heads/main@{#47355}

--- go/src/infra/cmd/skylab_swarming_worker/internal/swmbot/harness/ufsdutinfo/ufsdutinfo.go
+++ go/src/infra/cmd/skylab_swarming_worker/internal/swmbot/harness/ufsdutinfo/ufsdutinfo.go
@@ -8,13 +8,13 @@ package ufsdutinfo
 
 import (
 	"context"
+	"fmt"
 	"log"
 
 	"github.com/golang/protobuf/proto"
 	"go.chromium.org/luci/common/errors"
 	"go.chromium.org/luci/common/retry"
-
-	fleet "infra/appengine/crosskylabadmin/api/fleet/v1"
+	"infra/appengine/crosskylabadmin/api/fleet/v1"
 	"infra/cmd/skylab_swarming_worker/internal/swmbot"
 	"infra/libs/skylab/inventory"
 	ufspb "infra/unifiedfleet/api/v1/models"
@@ -22,8 +22,16 @@ import (
 	ufsutil "infra/unifiedfleet/app/util"
 )
 
+type DeviceType int64
+
+const (
+	ChromeOSDevice DeviceType = iota
+	AndroidDevice
+)
+
 // Store holds a DUT's inventory info and adds a Close method.
 type Store struct {
+	DeviceType     DeviceType
 	DUT            *inventory.DeviceUnderTest
 	oldDUT         *inventory.DeviceUnderTest
 	StableVersions map[string]string
@@ -40,12 +48,12 @@ func (s *Store) Close(ctx context.Context) error {
 		return nil
 	}
 	c := s.DUT.GetCommon()
-	new := c.GetLabels()
-	inventory.SortLabels(new)
+	labels := c.GetLabels()
+	inventory.SortLabels(labels)
 	old := s.oldDUT.GetCommon().GetLabels()
 	inventory.SortLabels(old)
-	if new.GetUselessSwitch() {
-		*new.UselessSwitch = false
+	if labels.GetUselessSwitch() {
+		*labels.UselessSwitch = false
 	}
 
 	log.Printf("Calling label update function")
@@ -58,14 +66,14 @@ func (s *Store) Close(ctx context.Context) error {
 
 // UpdateFunc is used to implement inventory updating for any changes
 // to the loaded DUT info.
-type UpdateFunc func(ctx context.Context, dutID string, old *inventory.DeviceUnderTest, new *inventory.DeviceUnderTest) error
+type UpdateFunc func(ctx context.Context, dutID string, oldDut *inventory.DeviceUnderTest, newDut *inventory.DeviceUnderTest) error
 
 // LoadByID loads the DUT's info from the UFS by ID.
 // This function returns a Store that should be closed to update the inventory
 // with any changes to the info, using a supplied UpdateFunc. If UpdateFunc is
 // nil, the inventory is not updated.
 func LoadByID(ctx context.Context, b *swmbot.Info, dutID string, f UpdateFunc) (*Store, error) {
-	return load(ctx, b, &ufsAPI.GetChromeOSDeviceDataRequest{ChromeosDeviceId: dutID}, f, getDutInfoFromUFS)
+	return load(ctx, b, &ufsAPI.GetDeviceDataRequest{DeviceId: dutID}, f)
 }
 
 // LoadByHostname loads the DUT's info from the UFS by hostname.
@@ -73,10 +81,28 @@ func LoadByID(ctx context.Context, b *swmbot.Info, dutID string, f UpdateFunc) (
 // with any changes to the info, using a supplied UpdateFunc. If UpdateFunc is
 // nil, the inventory is not updated.
 func LoadByHostname(ctx context.Context, b *swmbot.Info, hostname string, f UpdateFunc) (*Store, error) {
-	return load(ctx, b, &ufsAPI.GetChromeOSDeviceDataRequest{Hostname: hostname}, f, getDutInfoFromUFS)
+	return load(ctx, b, &ufsAPI.GetDeviceDataRequest{Hostname: hostname}, f)
 }
 
-type getDutInfoFuncUFS func(context.Context, ufsAPI.FleetClient, *ufsAPI.GetChromeOSDeviceDataRequest) (*ufspb.ChromeOSDeviceData, error)
+// GetDeviceData fetches a device entry from UFS.
+// This function returns GetDeviceDataResponse which contains info about DUT,
+// attached device or scheduling unit.
+func GetDeviceData(ctx context.Context, b *swmbot.Info, req *ufsAPI.GetDeviceDataRequest) (*ufsAPI.GetDeviceDataResponse, error) {
+	client, err := swmbot.UFSClient(ctx, b)
+	if err != nil {
+		return nil, errors.Annotate(err, "get device data: initialize UFS client").Err()
+	}
+	var resp *ufsAPI.GetDeviceDataResponse
+	f := func() (err error) {
+		osCtx := swmbot.SetupContext(ctx, ufsutil.OSNamespace)
+		resp, err = client.GetDeviceData(osCtx, req)
+		return err
+	}
+	if err := retry.Retry(ctx, retry.Default, f, retry.LogCallback(ctx, "ufsdutinfo.GetDeviceData")); err != nil {
+		return nil, errors.Annotate(err, "get device data: retry GetDeviceData").Err()
+	}
+	return resp, nil
+}
 
 // getStableVersion fetches the current stable version from an inventory client
 func getStableVersion(ctx context.Context, client fleet.InventoryClient, hostname string) (map[string]string, error) {
@@ -104,62 +130,47 @@ func getStableVersion(ctx context.Context, client fleet.InventoryClient, hostnam
 	return s, nil
 }
 
-func loadFromUFS(ctx context.Context, b *swmbot.Info, req *ufsAPI.GetChromeOSDeviceDataRequest, gf getDutInfoFuncUFS) (*inventory.DeviceUnderTest, error) {
-	client, err := swmbot.UFSClient(ctx, b)
-	if err != nil {
-		return nil, errors.Annotate(err, "load from UFS: initialize UFS client").Err()
-	}
-	resp, err := gf(ctx, client, req)
-	if err != nil {
-		return nil, errors.Annotate(err, "load from UFS").Err()
-	}
-	return resp.GetDutV1(), nil
-}
-
-func load(ctx context.Context, b *swmbot.Info, req *ufsAPI.GetChromeOSDeviceDataRequest, uf UpdateFunc, gfUFS getDutInfoFuncUFS) (*Store, error) {
+func load(ctx context.Context, b *swmbot.Info, req *ufsAPI.GetDeviceDataRequest, uf UpdateFunc) (*Store, error) {
 	ctx, err := swmbot.WithSystemAccount(ctx)
 	if err != nil {
 		return nil, errors.Annotate(err, "setup system account").Err()
 	}
 	log.Printf("Loading DUT info from UFS")
-	dut, err := loadFromUFS(ctx, b, req, gfUFS)
+	deviceData, err := GetDeviceData(ctx, b, req)
 	if err != nil {
 		return nil, errors.Annotate(err, "load DUT info from UFS").Err()
 	}
+	switch deviceData.GetResourceType() {
+	case ufsAPI.GetDeviceDataResponse_RESOURCE_TYPE_CHROMEOS_DEVICE:
+		return createChromeOSDeviceStore(ctx, b, deviceData.GetChromeOsDeviceData(), uf)
+	case ufsAPI.GetDeviceDataResponse_RESOURCE_TYPE_ATTACHED_DEVICE:
+		// Attached devices are not supported.
+		return &Store{DeviceType: AndroidDevice}, nil
+	}
+	return nil, fmt.Errorf("load from UFS: invalid DUT type - %s", deviceData.GetResourceType())
+}
 
+func createChromeOSDeviceStore(ctx context.Context, b *swmbot.Info, deviceData *ufspb.ChromeOSDeviceData, uf UpdateFunc) (*Store, error) {
+	dut := deviceData.GetDutV1()
+	// TODO(gregorynisbet): should failure to get the stableversion information
+	// cause the entire request to error out?
 	c, err := swmbot.InventoryClient(ctx, b)
 	if err != nil {
 		return nil, errors.Annotate(err, "setup inventory client for stable_version").Err()
 	}
-	// TODO(gregorynisbet): should failure to get the stableversion information
-	// cause the entire request to error out?
-	hostname := dut.GetCommon().GetHostname()
-	sv, err := getStableVersion(ctx, c, hostname)
+	sv, err := getStableVersion(ctx, c, dut.GetCommon().GetHostname())
 	if err != nil {
 		sv = map[string]string{}
-		log.Printf("load: getting stable version: sv (%v) err (%v)", sv, err)
+		log.Printf("create ChromeOS device store: getting stable version: sv (%v) err (%v)", sv, err)
 	}
 	// once we reach this point, sv is guaranteed to be non-nil
-	store := &Store{
+	return &Store{
+		DeviceType:     ChromeOSDevice,
 		DUT:            dut,
 		oldDUT:         proto.Clone(dut).(*inventory.DeviceUnderTest),
 		updateFunc:     uf,
 		StableVersions: sv,
-	}
-	return store, nil
-}
-
-func getDutInfoFromUFS(ctx context.Context, c ufsAPI.FleetClient, req *ufsAPI.GetChromeOSDeviceDataRequest) (*ufspb.ChromeOSDeviceData, error) {
-	var resp *ufspb.ChromeOSDeviceData
-	f := func() (err error) {
-		osCtx := swmbot.SetupContext(ctx, ufsutil.OSNamespace)
-		resp, err = c.GetChromeOSDeviceData(osCtx, req)
-		return err
-	}
-	if err := retry.Retry(ctx, retry.Default, f, retry.LogCallback(ctx, "ufsdutinfo.getDutInfoFuncUFS")); err != nil {
-		return nil, errors.Annotate(err, "retry getDutInfoFuncUFS").Err()
-	}
-	return resp, nil
+	}, nil
 }
 
 func retryGetStableVersion(ctx context.Context, client fleet.InventoryClient, req *fleet.GetStableVersionRequest) (*fleet.GetStableVersionResponse, error) {
