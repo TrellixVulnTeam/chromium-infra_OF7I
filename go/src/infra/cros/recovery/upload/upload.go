0aec90377a66ca3d75bed7cacca0625f4cfc8b55
Gregory Nisbet <gregorynisbet@google.com>
2022-03-02 22:15:26 +0000

    [recovery] Add real implementation of upload.
    BUG=b:200059229
    TEST=make test
    
    Change-Id: I0ff6d5f91bf0447187eec1bec5400d1639f279e5
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/3496070
    Reviewed-by: Otabek Kasimov <otabek@google.com>
    Commit-Queue: Gregory Nisbet <gregorynisbet@google.com>
    Cr-Commit-Position: refs/heads/main@{#47404}

--- go/src/infra/cros/recovery/upload/upload.go
+++ go/src/infra/cros/recovery/upload/upload.go
@@ -8,6 +8,9 @@ import (
 	"context"
 
 	"go.chromium.org/luci/common/errors"
+	lucigs "go.chromium.org/luci/common/gcloud/gs"
+
+	"infra/libs/skylab/gs"
 )
 
 // Params is a collection of parameters for uploading a directory.
@@ -15,12 +18,32 @@ type Params struct {
 	// Dir is the path to the primary directory to offload.
 	//
 	// Example: /path/to/swarming/logs
-	Dir string
+	SourceDir string
 	// GSURL is the destination path.
 	GSURL string
+	// MaxConcurrentJobs is the maximum number of concurrent uploads that can happen at once
+	MaxConcurrentJobs int
+}
+
+// GSClient is a Google Storage client.
+//
+// This interface is a subset of the gs.Client interface.
+type gsClient interface {
+	// NewWriter creates a new google storage writer rooted at a gs path.
+	NewWriter(p lucigs.Path) (lucigs.Writer, error)
 }
 
 // Upload a list of directories to Google Storage.
-func Upload(ctx context.Context, params *Params) error {
-	return errors.Reason("upload: not yet implemented").Err()
+func Upload(ctx context.Context, gsClient gsClient, params *Params) error {
+	if params == nil {
+		return errors.Reason("upload: params cannot be nil").Err()
+	}
+	if params.MaxConcurrentJobs <= 0 {
+		return errors.Reason("upload: max jobs must be positive").Err()
+	}
+	dirWriter := gs.NewDirWriter(gsClient, params.MaxConcurrentJobs)
+	if err := dirWriter.WriteDir(ctx, params.SourceDir, lucigs.Path(params.GSURL)); err != nil {
+		return errors.Annotate(err, "upload: upload main directory").Err()
+	}
+	return nil
 }
---
+++ go/src/infra/cros/recovery/upload/upload_linux_test.go
@@ -0,0 +1,94 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package upload
+
+import (
+	"context"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"go.chromium.org/luci/common/errors"
+	"go.chromium.org/luci/common/gcloud/gs"
+	lucigs "go.chromium.org/luci/common/gcloud/gs"
+)
+
+// Implements InnerClient interface, writing to a temporary directory
+// instead of Google Storage.
+type fakeInnerClient struct {
+	initialized bool
+	baseDir     string
+}
+
+// FakeWriter implements gs.Writer interface.
+type fakeWriter struct {
+	os.File
+}
+
+func (w *fakeInnerClient) Init(tempdir func() string) error {
+	if w.initialized {
+		return nil
+	}
+	path := ""
+	if tempdir == nil {
+		var err error
+		path, err = ioutil.TempDir("", "upload_test-")
+		if err != nil {
+			return errors.Annotate(err, "initialize fake writer").Err()
+		}
+	} else {
+		path = tempdir()
+	}
+	w.baseDir = path
+	w.initialized = true
+	return nil
+}
+
+func (w *fakeWriter) Count() int64 {
+	// Not _really_ implemented.
+	return 0
+}
+
+func (c *fakeInnerClient) NewWriter(p lucigs.Path) (gs.Writer, error) {
+	path := filepath.Join(c.baseDir, string(p))
+	d := filepath.Dir(path)
+	if err := os.MkdirAll(d, 0b111_111_111); err != nil {
+		return nil, errors.Reason("failed to create directory %s: %s", d, err).Err()
+	}
+	f, err := os.Create(path)
+	if err != nil {
+		return nil, err
+	}
+	return &fakeWriter{File: *f}, nil
+}
+
+func TestUpload(t *testing.T) {
+	t.Parallel()
+	ctx := context.Background()
+
+	td := t.TempDir()
+
+	if err := ioutil.WriteFile(filepath.Join(td, "a.txt"), []byte("a"), 0o777); err != nil {
+		t.Errorf("unexpected error: %s", err)
+	}
+
+	client := &fakeInnerClient{}
+	if err := client.Init(t.TempDir); err != nil {
+		t.Errorf("unexpected error: %s", err)
+	}
+
+	if err := Upload(ctx, client, &Params{
+		SourceDir:         td,
+		GSURL:             "gs://1/2/3",
+		MaxConcurrentJobs: 1,
+	}); err != nil {
+		t.Errorf("unexpected error: %s", err)
+	}
+
+	if _, err := os.Stat(filepath.Join(client.baseDir, "gs://1/2/3/a.txt")); err != nil {
+		t.Errorf("failed to stat %q: %s", "a.txt", err)
+	}
+}
