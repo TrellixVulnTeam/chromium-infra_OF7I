67fb0936dfdf29a7dc9d0b6af972a059d4cce233
Otabek Kasimov <otabek@google.com>
2022-03-08 17:55:42 +0000

    recovery: Pass run timeout to execute OS install
    BUG=b:219630559
    TEST=go test ./...
    
    Change-Id: Ie8d3795786c8beff0adc179c090f53a01349620a
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/3508606
    Reviewed-by: Vineet Joshi <vkjoshi@google.com>
    Reviewed-by: Gregory Nisbet <gregorynisbet@google.com>
    Commit-Queue: Otabek Kasimov <otabek@google.com>
    Auto-Submit: Otabek Kasimov <otabek@google.com>
    Cr-Commit-Position: refs/heads/main@{#47537}

--- go/src/infra/cros/recovery/internal/components/cros/usb_drive.go
+++ go/src/infra/cros/recovery/internal/components/cros/usb_drive.go
@@ -86,7 +86,7 @@ func BootFromServoUSBDriveInDevMode(ctx context.Context, waitBootTimeout, waitBo
 
 // RunInstallOSCommand run chromeos-install command on the host.
 func RunInstallOSCommand(ctx context.Context, timeout time.Duration, run components.Runner, log logger.Logger) error {
-	out, err := run(ctx, time.Minute, "chromeos-install", "--yes")
+	out, err := run(ctx, timeout, "chromeos-install", "--yes")
 	log.Debug("Install OS:\n%s", out)
 	return errors.Annotate(err, "install OS").Err()
 }
