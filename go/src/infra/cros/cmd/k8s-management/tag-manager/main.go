4bc420734dbda5572531e5ce8eac4037047fd5f5
Congbin Guo <guocb@chromium.org>
2022-03-14 23:46:13 +0000

    Container cache: tag-manager: scan the container cache images
    BUG=b:223241265
    TEST=None
    
    Change-Id: I2c497fffd7965294711b4983d47293a92603fe95
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/3513739
    Auto-Submit: Congbin Guo <guocb@chromium.org>
    Reviewed-by: Allen Li <ayatane@chromium.org>
    Commit-Queue: Allen Li <ayatane@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#47637}

--- go/src/infra/cros/cmd/k8s-management/tag-manager/main.go
+++ go/src/infra/cros/cmd/k8s-management/tag-manager/main.go
@@ -73,6 +73,8 @@ func innerMain() error {
 
 	// Please ensure the official tag regex matches the whole tag, i.e. starting
 	// with '^' and ending with '$'.
+	const timestampPrefix = `^\d{8}T\d{6}`
+	const timestampOnly = `^\d{8}T\d{6}$`
 	// The order of tag policies matters! Because tag policies may depend on
 	// each other, e.g. policy of "prod" may depend on policy of "canary".
 	// Please add the dependent policy first.
@@ -84,35 +86,40 @@ func innerMain() error {
 		{
 			&gcrRepo{"gcr.io/chromeos-drone-images/drone", auth},
 			newAppConfig(
-				`^\d{8}T\d{6}-chromeos-test$`, latestOfficialPolicy, canaryMaxDistancePolicy, prodMaxDistancePolicy,
+				timestampPrefix+"-chromeos-test$", latestOfficialPolicy, canaryMaxDistancePolicy, prodMaxDistancePolicy,
 			),
 		},
 		// Images used by caching service.
 		{
 			&gcrRepo{"gcr.io/chromeos-cacheserver-images/nginx", auth},
-			newAppConfig(`^\d{8}T\d{6}-chromeos-test$`, latestOfficialPolicy),
+			newAppConfig(timestampPrefix+"-chromeos-test$", latestOfficialPolicy),
 		},
 		{
 			&gcrRepo{"gcr.io/chromeos-cacheserver-images/conf_creator", auth},
-			newAppConfig(`^\d{8}T\d{6}$`, latestOfficialPolicy),
+			newAppConfig(timestampOnly, latestOfficialPolicy),
 		},
 		{
 			&gcrRepo{"gcr.io/chromeos-cacheserver-images/nginx_access_log_metrics", auth},
-			newAppConfig(`^\d{8}T\d{6}$`, latestOfficialPolicy),
+			newAppConfig(timestampOnly, latestOfficialPolicy),
 		},
 		{
 			&gcrRepo{"gcr.io/chromeos-cacheserver-images/gsa_server", auth},
-			newAppConfig(`^\d{8}T\d{6}$`, latestOfficialPolicy),
+			newAppConfig(timestampOnly, latestOfficialPolicy),
 		},
 		// Image used by RPM service.
 		{
 			&gcrRepo{"gcr.io/chromeos-rpmserver-images/rpm", auth},
-			newAppConfig(`^\d{8}T\d{6}-cloudbuild$`, latestOfficialPolicy),
+			newAppConfig(timestampPrefix+"-cloudbuild$", latestOfficialPolicy),
 		},
 		// Image used by K8s Metrics service.
 		{
 			&gcrRepo{"gcr.io/cros-lab-servers/k8s-metrics", auth},
-			newAppConfig(`^\d{8}T\d{6}$`, latestOfficialPolicy),
+			newAppConfig(timestampOnly, latestOfficialPolicy),
+		},
+		// Image used by K8s container cache.
+		{
+			&gcrRepo{"gcr.io/cros-lab-servers/container-cache", auth},
+			newAppConfig(timestampOnly, latestOfficialPolicy),
 		},
 	}
 	ch := make(chan string, len(data))
