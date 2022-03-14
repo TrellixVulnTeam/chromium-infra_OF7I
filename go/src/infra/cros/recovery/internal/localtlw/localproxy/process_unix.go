29b7055d75dc9e4341621920d1ca5745aaa3ffe7
Otabek Kasimov <otabek@google.com>
2022-03-14 17:52:24 +0000

    recovery: Lookup Ip when build the proxy
    Using Ip address is critical as not all devices in the lab can look up all names and zones.
    
    BUG=None
    TEST=local run with proxy
    
    Change-Id: Id4416250f39455b1a0be9024409c0ce74758fe7c
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/3519244
    Reviewed-by: Vineet Joshi <vkjoshi@google.com>
    Auto-Submit: Otabek Kasimov <otabek@google.com>
    Commit-Queue: Otabek Kasimov <otabek@google.com>
    Cr-Commit-Position: refs/heads/main@{#47622}

--- go/src/infra/cros/recovery/internal/localtlw/localproxy/process_unix.go
+++ go/src/infra/cros/recovery/internal/localtlw/localproxy/process_unix.go
@@ -13,7 +13,9 @@ import (
 )
 
 func closeProxy(p *proxy) {
-	if err := syscall.Kill(-p.cmd.Process.Pid, syscall.SIGKILL); err != nil {
+	if p.cmd == nil {
+		log.Printf("Proxy %q is empty\n", p.host)
+	} else if err := syscall.Kill(-p.cmd.Process.Pid, syscall.SIGKILL); err != nil {
 		log.Printf("Closing proxy for %q finished with error: %s\n", p.address(), err)
 	} else {
 		log.Printf("Closing proxy for %q:\n", p.address())
--- go/src/infra/cros/recovery/internal/localtlw/localproxy/proxy.go
+++ go/src/infra/cros/recovery/internal/localtlw/localproxy/proxy.go
@@ -11,8 +11,11 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"net"
 	"os/exec"
 	"time"
+
+	"go.chromium.org/luci/common/errors"
 )
 
 var (
@@ -23,6 +26,7 @@ var (
 // Proxy holds info for active running ssh proxy for requested host.
 type proxy struct {
 	host         string
+	hostIp       string
 	hostPort     int
 	jumpHost     string
 	jumpHostPort int
@@ -33,17 +37,24 @@ type proxy struct {
 func newProxy(ctx context.Context, host string, hostPort int, jumpHost string, jumpHostPort int) *proxy {
 	p, ok := proxyPool[host]
 	if !ok {
+		ip, err := lookupHost(host)
 		p = &proxy{
 			host:         host,
+			hostIp:       ip,
 			hostPort:     hostPort,
 			jumpHost:     jumpHost,
 			jumpHostPort: jumpHostPort,
+		}
+		if err != nil {
+			fmt.Printf("Fail loopup ip for %s: %s\n", host, err)
+			proxyPool[p.host] = p
+			return p
 		}
 		// Ex.: the proxy create command will look something like this:
 		// "ssh -f -N -L jumpHostPort:127.0.0.1:22 -L hostPort:host:22 root@jumpHost"
 		p.cmd = exec.CommandContext(ctx, "ssh", "-f", "-N",
 			"-L", fmt.Sprintf("%d:127.0.0.1:22", p.jumpHostPort),
-			"-L", fmt.Sprintf("%d:%s:22", p.hostPort, p.host),
+			"-L", fmt.Sprintf("%d:%s:22", p.hostPort, p.hostIp),
 			fmt.Sprintf("root@%s", p.jumpHost))
 		initSystemProcAttr(p)
 		stderr, err := p.cmd.StderrPipe()
@@ -84,4 +95,17 @@ func (p *proxy) address() string {
 // Port provides proxy port information.
 func (p *proxy) Port() int {
 	return p.hostPort
+}
+
+// lookupHost is a helper function that looks up the IP address of the provided
+// host by using the local resolver.
+func lookupHost(hostname string) (string, error) {
+	addrs, err := net.LookupHost(hostname)
+	if err != nil {
+		return "", errors.Annotate(err, "lookup host %q", hostname).Err()
+	}
+	if len(addrs) == 0 {
+		return "", errors.Reason("lookup host %q: no ip addresses found", hostname).Err()
+	}
+	return addrs[0], nil
 }
