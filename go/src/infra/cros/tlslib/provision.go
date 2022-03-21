6ffcda00d626b41977189cff90fe2b23dcd4684a
Jae Hoon Kim <kimjae@chromium.org>
2022-03-21 22:04:15 +0000

    [TLS] Wait for sticky kernel after first reboot
    This builds up from chromium:3537030, but adds in a guard to protect
    against rollbacks/slot switches during the provisioning logic itself.
    
    BUG=b:223249027
    TEST=# TLS + TLW locally w/ provisioning
    
    Change-Id: I6e9fcfba71f25f8de4a559fb908eaed8565a6a94
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/3541106
    Reviewed-by: Garry Wang <xianuowang@chromium.org>
    Commit-Queue: Jae Hoon Kim <kimjae@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#47806}

--- go/src/infra/cros/tlslib/provision.go
+++ go/src/infra/cros/tlslib/provision.go
@@ -127,6 +127,20 @@ func (s *Server) provision(req *tls.ProvisionDutRequest, opName string) {
 		defer disconnect()
 		log.Printf("provision: time to provision OS took %v", time.Since(t))
 
+		// To be safe, wait for kernel to be "sticky" right after installing the new partitions and booting into it.
+		t = time.Now()
+		// Timeout is determined by 2x delay to mark new kernel successful + 10 seconds fuzz.
+		stickyKernelCtx, cancel := context.WithTimeout(ctx, 100*time.Second)
+		defer cancel()
+		if err := p.verifyKernelState(stickyKernelCtx); err != nil {
+			setError(newOperationError(
+				codes.Aborted,
+				fmt.Sprintf("provision: failed to wait for sticky kernel, %s", err),
+				tls.ProvisionDutResponse_REASON_PROVISIONING_FAILED.String()))
+			return
+		}
+		log.Printf("provision: time to wait for sticky kernel %v", time.Since(t))
+
 		t = time.Now()
 		if !req.GetPreserveStateful() && !req.PreventReboot {
 			if err := p.wipeStateful(ctx); err != nil {
