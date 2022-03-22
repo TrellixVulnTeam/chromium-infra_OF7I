6087819e9f17ecb823b847e820a660c2293bb068
Olivier Robin <olivierrobin@google.com>
2022-03-22 09:10:42 +0000

    Copy libparser dylib for swift-format
    Bug: 1263872
    Change-Id: I7552b88a0404fa7a53f9a01fffc929aee2abfb5d
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/3452625
    Reviewed-by: Brian Ryner <bryner@google.com>
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#47829}

--- 3pp/swift-format/3pp.pb
+++ 3pp/swift-format/3pp.pb
@@ -4,8 +4,10 @@ create {
     git {
       repo: "https://github.com/apple/swift-format.git"
       tag_pattern: "0.%s00.0"
+      tag_filter_re: "0.50500.0"
     }
     cpe_base_address: "cpe:/a:swiftformat_project:swiftformat"
+    patch_version: "chromium.1"
   }
   build {}
 }
--- 3pp/swift-format/install.sh
+++ 3pp/swift-format/install.sh
@@ -12,4 +12,9 @@ PREFIX="$1"
 swift build -c release
 cp $(swift build -c release --show-bin-path)/swift-format $PREFIX/swift-format
 
+# swift-format depends on lib_InternalSwiftSyntaxParser.dylib.
+# Apple github recommends to compile it or to ship the library with the application.
+# see https://github.com/apple/swift-syntax#embedding-swiftsyntax-in-an-application
+cp $(xcode-select -p)/Toolchains/XcodeDefault.xctoolchain/usr/lib/swift/macosx/lib_InternalSwiftSyntaxParser.dylib $PREFIX/lib_InternalSwiftSyntaxParser.dylib
+cp LICENSE.txt $PREFIX/LICENSE.txt
 
