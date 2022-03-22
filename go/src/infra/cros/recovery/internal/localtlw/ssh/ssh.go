fd6c48fd2141f35ccd88bccf3751055067643bd4
Otabek Kasimov <otabek@google.com>
2022-03-22 01:42:41 +0000

    recovery: Improve ssh connection and execution to react to context
    BUG=b:225602453
    TEST=manual testing, go test ./...
    
    Change-Id: Id361477590afd73075263f0c141a91a2d7cde332
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/3537388
    Reviewed-by: Ashish Gandhi <ashishgandhi@google.com>
    Commit-Queue: Otabek Kasimov <otabek@google.com>
    Auto-Submit: Otabek Kasimov <otabek@google.com>
    Cr-Commit-Position: refs/heads/main@{#47822}

--- go/src/infra/cros/recovery/internal/localtlw/ssh/ssh.go
+++ go/src/infra/cros/recovery/internal/localtlw/ssh/ssh.go
@@ -19,12 +19,7 @@ import (
 
 const (
 	defaultSSHUser = "root"
-
-	// Some tasks such as running badblocks for USB-Drive audit can
-	// take quite long (2-3 hours). We need to set timeout limit to
-	// accommodate such tasks.
-	defaultSSHTimeout = time.Hour * 3
-	DefaultPort       = 22
+	DefaultPort    = 22
 )
 
 // getSSHConfig provides default config for SSH.
@@ -32,8 +27,10 @@ func SSHConfig() *ssh.ClientConfig {
 	return &ssh.ClientConfig{
 		User:            defaultSSHUser,
 		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
-		Timeout:         defaultSSHTimeout,
 		Auth:            []ssh.AuthMethod{ssh.PublicKeys(SSHSigner)},
+		// The timeout specified to established connection only.
+		// That is not an execution timeout.
+		Timeout: 2 * time.Second,
 	}
 }
 
@@ -59,41 +56,61 @@ func Run(ctx context.Context, pool *sshpool.Pool, addr string, cmd string) (resu
 		return
 	}
 	defer func() { pool.Put(addr, sc) }()
-	result = internalRunSSH(cmd, sc)
+	result = createSessionAndExecute(ctx, cmd, sc)
 	log.Debug(ctx, "run SSH %q: Cmd: %q; ExitCode: %d; Stdout: %q;  Stderr: %q", addr, result.Command, result.ExitCode, result.Stdout, result.Stderr)
 	return
 }
 
-func internalRunSSH(cmd string, client *ssh.Client) (result *tlw.RunResult) {
+// createSessionAndExecute creates ssh session and perfrom execution by ssh.
+//
+// The function also aborted execution if context canceled.
+func createSessionAndExecute(ctx context.Context, cmd string, client *ssh.Client) (result *tlw.RunResult) {
 	result = &tlw.RunResult{
 		Command:  cmd,
 		ExitCode: -1,
 	}
 	session, err := client.NewSession()
 	if err != nil {
-		result.Stderr = fmt.Sprintf("internal run SSH: %s", err)
+		result.Stderr = fmt.Sprintf("internal run ssh: %v", err)
 		return
 	}
-	defer func() { session.Close() }()
+	defer func() {
+		session.Close()
+	}()
 	var stdOut, stdErr bytes.Buffer
 	session.Stdout = &stdOut
 	session.Stderr = &stdErr
-
-	err = session.Run(cmd)
-
-	result.Stdout = stdOut.String()
-	result.Stderr = stdErr.String()
-	if err == nil {
-		result.ExitCode = 0
-	} else if exitErr, ok := err.(*ssh.ExitError); ok {
-		result.ExitCode = exitErr.ExitStatus()
-	} else if _, ok := err.(*ssh.ExitMissingError); ok {
-		result.ExitCode = -2
-		result.Stderr = err.Error()
-	} else {
-		// Set error 1 as not expected exit.
-		result.ExitCode = -3
-		result.Stderr = err.Error()
+	exit := func(err error) *tlw.RunResult {
+		result.Stdout = stdOut.String()
+		result.Stderr = stdErr.String()
+		switch t := err.(type) {
+		case nil:
+			result.ExitCode = 0
+		case *ssh.ExitError:
+			result.ExitCode = t.ExitStatus()
+		case *ssh.ExitMissingError:
+			result.ExitCode = -2
+			result.Stderr = t.Error()
+		default:
+			// Set error 1 as not expected exit.
+			result.ExitCode = -3
+			result.Stderr = t.Error()
+		}
+		return result
 	}
-	return
+	// Chain to run ssh in separate thread and wait for single response from it.
+	// If context will be closed before it will abort the session.
+	sw := make(chan error, 1)
+	go func() {
+		sw <- session.Run(cmd)
+	}()
+	select {
+	case err := <-sw:
+		return exit(err)
+	case <-ctx.Done():
+		// At the end abort session.
+		// Session will be closed in defer.
+		session.Signal(ssh.SIGABRT)
+		return exit(ctx.Err())
+	}
 }
