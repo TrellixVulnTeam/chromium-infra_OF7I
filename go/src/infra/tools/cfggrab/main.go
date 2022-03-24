69b11a8fdb7d616e0fe1b65db79982758a4af65c
Robert Iannucci <iannucci@chromium.org>
2022-03-24 00:09:45 +0000

    [cfggrab] Add hacky support for decoding textpb.
    This is useful to pipe to `jq` or otherwise do semi-structured searching
    on the configs.
    
    Also adds a bit of output to -output-dir mode to show progress.
    
    R=vadimsh
    
    Change-Id: I9e6a243060a1e89de1a94f74b34dda2da1e43c42
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/3547566
    Reviewed-by: Vadim Shtayura <vadimsh@chromium.org>
    Auto-Submit: Robbie Iannucci <iannucci@chromium.org>
    Commit-Queue: Robbie Iannucci <iannucci@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#47894}

--- go/src/infra/tools/cfggrab/main.go
+++ go/src/infra/tools/cfggrab/main.go
@@ -26,19 +26,55 @@ import (
 	"path/filepath"
 	"sync"
 
+	"google.golang.org/protobuf/encoding/protojson"
+	"google.golang.org/protobuf/encoding/prototext"
+	"google.golang.org/protobuf/reflect/protoreflect"
+
 	"go.chromium.org/luci/auth"
+	bbpb "go.chromium.org/luci/buildbucket/proto"
 	"go.chromium.org/luci/common/errors"
 	"go.chromium.org/luci/common/logging"
 	"go.chromium.org/luci/common/logging/gologger"
+	projectpb "go.chromium.org/luci/common/proto/config"
+	realmspb "go.chromium.org/luci/common/proto/realms"
 	"go.chromium.org/luci/common/sync/parallel"
 	"go.chromium.org/luci/config"
 	"go.chromium.org/luci/config/cfgclient"
 	"go.chromium.org/luci/config/impl/remote"
+	triciumpb "go.chromium.org/luci/cv/api/config/legacy"
+	cvpb "go.chromium.org/luci/cv/api/config/v2"
 	"go.chromium.org/luci/hardcoded/chromeinfra"
+	logdogpb "go.chromium.org/luci/logdog/api/config/svcconfig"
+	notifypb "go.chromium.org/luci/luci_notify/api/config"
+	milopb "go.chromium.org/luci/milo/api/config"
+	schedulerpb "go.chromium.org/luci/scheduler/appengine/messages"
 )
 
 var outputDir = flag.String("output-dir", "-", "Where to store fetched files or - to print them to stdout for grepping")
 var configService = flag.String("config-service-host", chromeinfra.ConfigServiceHost, "Hostname of LUCI Config service to query")
+var convertJSON = flag.Bool("convert-json", false, ("Convert fetched files to JSONPB (best-effort)." +
+	" If output to -, wraps messages in {\"proj\": ..., \"data\": ...}."))
+
+// quick and dirty map for known config file names to the proto message they
+// contain.
+var messageMap map[string]protoreflect.Message = map[string]protoreflect.Message{
+	"commit-queue.cfg":       (*cvpb.Config)(nil).ProtoReflect(),
+	"cr-buildbucket-dev.cfg": (*bbpb.BuildbucketCfg)(nil).ProtoReflect(),
+	"cr-buildbucket.cfg":     (*bbpb.BuildbucketCfg)(nil).ProtoReflect(),
+	"luci-logdog-dev.cfg":    (*logdogpb.ProjectConfig)(nil).ProtoReflect(),
+	"luci-logdog.cfg":        (*logdogpb.ProjectConfig)(nil).ProtoReflect(),
+	"luci-milo-dev.cfg":      (*milopb.Project)(nil).ProtoReflect(),
+	"luci-milo.cfg":          (*milopb.Project)(nil).ProtoReflect(),
+	"luci-notify-dev.cfg":    (*notifypb.ProjectConfig)(nil).ProtoReflect(),
+	"luci-notify.cfg":        (*notifypb.ProjectConfig)(nil).ProtoReflect(),
+	"luci-scheduler-dev.cfg": (*schedulerpb.ProjectConfig)(nil).ProtoReflect(),
+	"luci-scheduler.cfg":     (*schedulerpb.ProjectConfig)(nil).ProtoReflect(),
+	"project.cfg":            (*projectpb.ProjectCfg)(nil).ProtoReflect(),
+	"realms-dev.cfg":         (*realmspb.RealmsCfg)(nil).ProtoReflect(),
+	"realms.cfg":             (*realmspb.RealmsCfg)(nil).ProtoReflect(),
+	"tricium-dev.cfg":        (*triciumpb.ProjectConfig)(nil).ProtoReflect(),
+	"tricium-prod.cfg":       (*triciumpb.ProjectConfig)(nil).ProtoReflect(),
+}
 
 var stdoutLock sync.Mutex
 
@@ -52,7 +88,18 @@ func main() {
 		os.Exit(2)
 	}
 
-	if err := run(ctx, flag.Arg(0), *outputDir); err != nil {
+	path := flag.Arg(0)
+
+	var msg protoreflect.Message
+	if *convertJSON {
+		var ok bool
+		if msg, ok = messageMap[flag.Arg(0)]; !ok {
+			fmt.Fprintf(os.Stderr, "Cannot convert %q to JSON: unknown file", path)
+			os.Exit(2)
+		}
+	}
+
+	if err := run(ctx, path, *outputDir, msg); err != nil {
 		errors.Log(ctx, err)
 		os.Exit(1)
 	}
@@ -69,7 +116,7 @@ func withConfigClient(ctx context.Context) (context.Context, error) {
 	})), nil
 }
 
-func run(ctx context.Context, path, output string) error {
+func run(ctx context.Context, path, output string, msg protoreflect.Message) error {
 	if output != "-" {
 		if err := os.MkdirAll(output, 0777); err != nil {
 			return err
@@ -90,7 +137,7 @@ func run(ctx context.Context, path, output string) error {
 		for _, proj := range projects {
 			proj := proj
 			work <- func() error {
-				if err := processProject(ctx, proj, path, output); err != nil {
+				if err := processProject(ctx, proj, path, output, msg); err != nil {
 					logging.Errorf(ctx, "Failed when processing %s: %s", proj, err)
 					return err
 				}
@@ -100,21 +147,45 @@ func run(ctx context.Context, path, output string) error {
 	})
 }
 
-func processProject(ctx context.Context, proj, path, output string) error {
+func processProject(ctx context.Context, proj, path, output string, msg protoreflect.Message) error {
 	var blob []byte
 	err := cfgclient.Get(ctx, config.Set("projects/"+proj), path, cfgclient.Bytes(&blob), nil)
 	if err != nil {
 		return err
 	}
 
+	if msg != nil {
+		decoded := msg.New().Interface()
+		if err = prototext.Unmarshal(blob, decoded); err != nil {
+			return errors.Annotate(err, "decoding textpb").Err()
+		}
+		blob, err = protojson.MarshalOptions{
+			Multiline:     true,
+			Indent:        "  ",
+			UseProtoNames: true,
+		}.Marshal(decoded)
+		if err != nil {
+			return errors.Annotate(err, "encoding jsonpb").Err()
+		}
+	}
+
 	if output != "-" {
+		stdoutLock.Lock()
+		defer stdoutLock.Unlock()
+		fmt.Printf("writing %s\n", proj)
 		return ioutil.WriteFile(filepath.Join(output, proj+".cfg"), blob, 0666)
 	}
 
 	stdoutLock.Lock()
 	defer stdoutLock.Unlock()
-	for _, line := range bytes.Split(blob, []byte{'\n'}) {
-		fmt.Printf("%s: %s\n", proj, line)
+	if msg == nil {
+		// raw
+		for _, line := range bytes.Split(blob, []byte{'\n'}) {
+			fmt.Printf("%s: %s\n", proj, line)
+		}
+	} else {
+		// json
+		fmt.Printf("{\"project\": \"%s\", \"data\": %s}\n", proj, blob)
 	}
 	return nil
 }
