d68aa01511e09e32fbc4d53ef8347a1b80dcd2f1
Otabek Kasimov <otabek@google.com>
2022-03-28 17:38:02 +0000

    recovery: Bring back old updater to verify update
    Teh update of device in the lab is failing to reduce complexity rollback
    new updater as part of testing as it used by SSW right now and doe not
    tells any issues.
    
    BUG=b:226964533
    TEST=run local
    
    Change-Id: I62ec8824c351e792d74237ff0f03b42c6201b864
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/3551008
    Auto-Submit: Otabek Kasimov <otabek@google.com>
    Reviewed-by: Bryan Ni <bniche@google.com>
    Commit-Queue: Otabek Kasimov <otabek@google.com>
    Cr-Commit-Position: refs/heads/main@{#47965}

--- go/src/infra/cros/recovery/internal/localtlw/dutinfo/dutinfo.go
+++ go/src/infra/cros/recovery/internal/localtlw/dutinfo/dutinfo.go
@@ -57,19 +57,16 @@ func ConvertAttachedDeviceToTlw(data *ufsAPI.AttachedDeviceData) (dut *tlw.Dut, 
 }
 
 // CreateUpdateDutRequest creates request instance to update UFS.
-func CreateUpdateDutRequest(dutID string, dut *tlw.Dut) (req *ufsAPI.UpdateDeviceRecoveryDataRequest, err error) {
+func CreateUpdateDutRequest(dutID string, dut *tlw.Dut) (req *ufsAPI.UpdateDutStateRequest, err error) {
 	defer func() {
 		if r := recover(); r != nil {
 			err = errors.Reason("update dut specs: %v\n%s", r, debug.Stack()).Err()
 		}
 	}()
-	return &ufsAPI.UpdateDeviceRecoveryDataRequest{
-		ChromeosDeviceId: dutID,
-		Hostname:         dut.Name,
-		DutState:         getUFSDutComponentStateFromSpecs(dutID, dut),
-		DutData:          getUFSDutDataFromSpecs(dutID, dut),
-		LabData:          getUFSLabDataFromSpecs(dutID, dut),
-		ResourceState:    dutstate.ConvertToUFSState(dut.State),
+	return &ufsAPI.UpdateDutStateRequest{
+		DutState: getUFSDutComponentStateFromSpecs(dutID, dut),
+		DutMeta:  getUFSDutMetaFromSpecs(dutID, dut),
+		LabMeta:  getUFSLabMetaFromSpecs(dutID, dut),
 	}, nil
 }
 
@@ -375,37 +372,33 @@ func configHasFeature(dc *ufsdevice.Config, hf ufsdevice.Config_HardwareFeature)
 	return false
 }
 
-func getUFSDutDataFromSpecs(dutID string, dut *tlw.Dut) *ufsAPI.UpdateDeviceRecoveryDataRequest_DutData {
-	dutData := &ufsAPI.UpdateDeviceRecoveryDataRequest_DutData{}
-	dutData.SerialNumber = dut.SerialNumber
-	dutData.HwID = dut.Hwid
+func getUFSDutMetaFromSpecs(dutID string, dut *tlw.Dut) *ufspb.DutMeta {
+	dutMeta := &ufspb.DutMeta{
+		ChromeosDeviceId: dutID,
+		Hostname:         dut.Name,
+	}
+	if dut.SerialNumber != "" {
+		dutMeta.SerialNumber = dut.SerialNumber
+	}
+	if dut.Hwid != "" {
+		dutMeta.HwID = dut.Hwid
+	}
 	// TODO: update logic if required by b/184391605
-	dutData.DeviceSku = dut.DeviceSku
-	return dutData
+	dutMeta.DeviceSku = dut.DeviceSku
+	return dutMeta
 }
 
-func getUFSLabDataFromSpecs(dutID string, dut *tlw.Dut) *ufsAPI.UpdateDeviceRecoveryDataRequest_LabData {
-	labData := &ufsAPI.UpdateDeviceRecoveryDataRequest_LabData{
-		WifiRouters: []*ufsAPI.UpdateDeviceRecoveryDataRequest_WifiRouter{},
+func getUFSLabMetaFromSpecs(dutID string, dut *tlw.Dut) (labconfig *ufspb.LabMeta) {
+	labMeta := &ufspb.LabMeta{
+		ChromeosDeviceId: dutID,
+		Hostname:         dut.Name,
 	}
 	if sh := dut.ServoHost; sh != nil {
-		labData.ServoType = sh.Servo.Type
-		labData.SmartUsbhub = sh.SmartUsbhubPresent
-		labData.ServoTopology = convertServoTopologyToUFS(sh.ServoTopology)
+		labMeta.ServoType = sh.Servo.Type
+		labMeta.SmartUsbhub = sh.SmartUsbhubPresent
+		labMeta.ServoTopology = convertServoTopologyToUFS(sh.ServoTopology)
 	}
-	for _, router := range dut.WifiRouterHosts {
-		labData.WifiRouters = append(labData.WifiRouters, &ufsAPI.UpdateDeviceRecoveryDataRequest_WifiRouter{
-			Hostname: router.GetName(),
-			State:    convertWifiRouterStateToUFS(router.GetState()),
-		})
-	}
-	for _, btp := range dut.BluetoothPeerHosts {
-		labData.BlueoothPeers = append(labData.BlueoothPeers, &ufsAPI.UpdateDeviceRecoveryDataRequest_BluetoothPeer{
-			Hostname: btp.Name,
-			State:    convertBluetoothPeerStateToUFS(btp.State),
-		})
-	}
-	return labData
+	return labMeta
 }
 
 // getUFSDutComponentStateFromSpecs collects all states for DUT and peripherals.
@@ -480,23 +473,6 @@ func getUFSDutComponentStateFromSpecs(dutID string, dut *tlw.Dut) *ufslab.DutSta
 		state.AudioLoopbackDongle = ufslab.PeripheralState_WORKING
 	} else {
 		state.AudioLoopbackDongle = ufslab.PeripheralState_UNKNOWN
-	}
-	state.WifiPeripheralState = getWifiPeripheralState(dut.WifiRouterHosts)
-	return state
-}
-
-// getWifiPeripheralState return the wifi peripheral state
-// it represent the state of all wifi routers. it is broken if any of the router state is not working
-func getWifiPeripheralState(wifiRouterHosts []*tlw.WifiRouterHost) ufslab.PeripheralState {
-	if len(wifiRouterHosts) == 0 {
-		return ufslab.PeripheralState_UNKNOWN
-	}
-	state := ufslab.PeripheralState_WORKING
-	for _, wifiRouter := range wifiRouterHosts {
-		if wifiRouter.State != tlw.WifiRouterHost_WORKING {
-			state = ufslab.PeripheralState_BROKEN
-			break
-		}
 	}
 	return state
 }
--- go/src/infra/cros/recovery/internal/localtlw/tlwclient.go
+++ go/src/infra/cros/recovery/internal/localtlw/tlwclient.go
@@ -19,6 +19,7 @@ import (
 	"google.golang.org/grpc/status"
 
 	fleet "infra/appengine/crosskylabadmin/api/fleet/v1"
+	"infra/cros/dutstate"
 	"infra/cros/recovery/docker"
 	"infra/cros/recovery/internal/localtlw/dutinfo"
 	tlwio "infra/cros/recovery/internal/localtlw/io"
@@ -31,6 +32,8 @@ import (
 	"infra/cros/recovery/internal/rpm"
 	"infra/cros/recovery/tlw"
 	"infra/libs/sshpool"
+	ufspb "infra/unifiedfleet/api/v1/models"
+	ufslab "infra/unifiedfleet/api/v1/models/chromeos/lab"
 	ufsAPI "infra/unifiedfleet/api/v1/rpc"
 )
 
@@ -50,6 +53,12 @@ type UFSClient interface {
 	GetDeviceData(ctx context.Context, req *ufsAPI.GetDeviceDataRequest, opts ...grpc.CallOption) (rsp *ufsAPI.GetDeviceDataResponse, err error)
 	// UpdateDeviceRecoveryData updates the labdata, dutdata, resource state, dut states for a DUT
 	UpdateDeviceRecoveryData(ctx context.Context, in *ufsAPI.UpdateDeviceRecoveryDataRequest, opts ...grpc.CallOption) (*ufsAPI.UpdateDeviceRecoveryDataResponse, error)
+	// GetSchedulingUnit retrieves the details of the SchedulingUnit.
+	GetSchedulingUnit(ctx context.Context, req *ufsAPI.GetSchedulingUnitRequest, opts ...grpc.CallOption) (rsp *ufspb.SchedulingUnit, err error)
+	// GetChromeOSDeviceData retrieves requested Chrome OS device data from the UFS and inventoryV2.
+	GetChromeOSDeviceData(ctx context.Context, req *ufsAPI.GetChromeOSDeviceDataRequest, opts ...grpc.CallOption) (rsp *ufspb.ChromeOSDeviceData, err error)
+	// UpdateDutState updates the state config for a DUT
+	UpdateDutState(ctx context.Context, in *ufsAPI.UpdateDutStateRequest, opts ...grpc.CallOption) (*ufslab.DutState, error)
 }
 
 // CSAClient is a client that knows how to respond to the GetStableVersion RPC call.
@@ -854,15 +863,21 @@ func (c *tlwClient) UpdateDut(ctx context.Context, dut *tlw.Dut) error {
 		return errors.Annotate(err, "update DUT %q", dut.Name).Err()
 	}
 	log.Debugf(ctx, "Update DUT: update request: %s", req)
-	rsp, err := c.ufsClient.UpdateDeviceRecoveryData(ctx, req)
-	if err != nil {
+	if _, err := c.ufsClient.UpdateDutState(ctx, req); err != nil {
 		return errors.Annotate(err, "update DUT %q", dut.Name).Err()
 	}
-	log.Debugf(ctx, "Update DUT: update response: %s", rsp)
 	c.unCacheDevice(dut)
-	// Update provisioning data on the execution env.
-	err = localinfo.UpdateProvisionInfo(ctx, dut)
-	return errors.Annotate(err, "udpate dut").Err()
+	if ufs, ok := c.ufsClient.(dutstate.UFSClient); ok {
+		if err := dutstate.Update(ctx, ufs, dut.Name, dut.State); err != nil {
+			return errors.Annotate(err, "update DUT %q", dut.Name).Err()
+		}
+	} else {
+		return errors.Reason("update DUT %q: dutstate.UFSClient interface is not implemented by client", dut.Name).Err()
+	}
+	if err := localinfo.UpdateProvisionInfo(ctx, dut); err != nil {
+		return errors.Annotate(err, "update dut").Err()
+	}
+	return nil
 }
 
 // Provision triggers provisioning of the device.
