8307fee0a15092a5e670cf2a6b143577a84ebc1b
Gregory Nisbet <gregorynisbet@google.com>
2022-03-29 21:44:46 +0000

    [recipe_bundler] Make recipe_bundler go vet clean.
    BUG=b:227367175
    TEST=go vet ./...
    
    Change-Id: If211c590e2ad4f7882eeb0c3768edb3a6ace66a9
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/3558518
    Reviewed-by: Xixuan Wu <xixuan@chromium.org>
    Reviewed-by: Robbie Iannucci <iannucci@chromium.org>
    Commit-Queue: Gregory Nisbet <gregorynisbet@google.com>
    Cr-Commit-Position: refs/heads/main@{#48018}

--- go/src/infra/tools/recipe_bundler/main.go
+++ go/src/infra/tools/recipe_bundler/main.go
@@ -20,7 +20,7 @@ import (
 
 func handleInterruption(ctx context.Context) context.Context {
 	ctx, cancel := context.WithCancel(ctx)
-	signalC := make(chan os.Signal)
+	signalC := make(chan os.Signal, 1)
 	signal.Notify(signalC, os.Interrupt)
 	go func() {
 		interrupted := false
