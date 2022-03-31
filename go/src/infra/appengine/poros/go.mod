08ec0b618304c6446cab1b6f855c138f2770dba9
Eswaran Krishnamoorthy <eswarank@google.com>
2022-03-31 17:42:26 +0000

    Initial GO setup with Lab entity
    Bug: 1311123
    Change-Id: I204a40f3e939c94f55811bab95c9da622d4f1b3c
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/3550813
    Reviewed-by: Xiang Ji <jxiang@google.com>
    Commit-Queue: Eswaran Krishnamoorthy <eswarank@google.com>
    Cr-Commit-Position: refs/heads/main@{#48075}

---
+++ go/src/infra/appengine/poros/go.mod
@@ -0,0 +1,8 @@
+module infra/appengine/poros
+
+go 1.16
+
+require (
+	go.chromium.org/luci v0.0.0-20220328223522-82b09e185f23
+	google.golang.org/protobuf v1.27.1
+)
---
+++ go/src/infra/appengine/poros/main.go
@@ -0,0 +1,28 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package main
+
+import (
+	"go.chromium.org/luci/config/server/cfgmodule"
+	"go.chromium.org/luci/gae/service/datastore"
+	"go.chromium.org/luci/server"
+	"go.chromium.org/luci/server/gaeemulation"
+	"go.chromium.org/luci/server/module"
+)
+
+func init() {
+	// TODO (crbug.com/1242998): Remove when this becomes the default (~Jan 2022).
+	datastore.EnableSafeGet()
+}
+
+func main() {
+	modules := []module.Module{
+		cfgmodule.NewModuleFromFlags(),
+		gaeemulation.NewModuleFromFlags(),
+	}
+	server.Main(nil, modules, func(srv *server.Server) error {
+		return nil
+	})
+}
